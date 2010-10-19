39e69367da45db5c8b3ec0b3a0a40a3f58850b82
Greg Schueler <greg.schueler@gmail.com>
2010-10-19 15:02:08 -0700

    Update tests for ResourceXMLGenerator: test use of outputstream, generating settings

--- core/src/test/com/dtolabs/shared/resources/TestResourceXMLGenerator.java
+++ core/src/test/com/dtolabs/shared/resources/TestResourceXMLGenerator.java
@@ -33,7 +33,9 @@ import org.dom4j.io.SAXReader;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.FileOutputStream;
 import java.util.HashSet;
+import java.util.HashMap;
 
 public class TestResourceXMLGenerator extends TestCase {
     ResourceXMLGenerator resourceXMLGenerator;
@@ -147,6 +149,80 @@ public class TestResourceXMLGenerator extends TestCase {
             assertEquals("test osname", root.selectSingleNode("node/@osName").getStringValue());
             assertEquals("test vers", root.selectSingleNode("node/@osVersion").getStringValue());
             assertEquals("test user", root.selectSingleNode("node/@username").getStringValue());
+        }
+        {  //test to outputstream
+            assertTrue(test1.delete());
+            final ResourceXMLGenerator gen = new ResourceXMLGenerator(new FileOutputStream(test1));
+            //add a node
+            final NodeEntryImpl node = new NodeEntryImpl("test1", "test1name");
+            node.setDescription("test desc");
+            node.setOsArch("test arch");
+            node.setOsFamily("test fam");
+            node.setOsName("test osname");
+            node.setOsVersion("test vers");
+            final HashSet<String> tags = new HashSet<String>();
+            tags.add("a");
+            tags.add("d");
+            node.setTags(tags);
+            node.setType("TestNode");
+            node.setUsername("test user");
+
+            gen.addNode(node);
+            gen.generate();
+            assertTrue(test1.exists());
+            assertTrue(test1.isFile());
+            //assert contents
+            final Document d = reader.read(test1);
+            assertNotNull(d);
+            final Element root = d.getRootElement();
+            assertEquals("project", root.getName());
+            assertEquals(1, root.selectNodes("/project/*").size());
+            assertEquals(1, root.selectNodes("node").size());
+            assertEquals("test1name", root.selectSingleNode("node/@name").getStringValue());
+            assertEquals("test1", root.selectSingleNode("node/@hostname").getStringValue());
+            assertEquals("a,d", root.selectSingleNode("node/@tags").getStringValue());
+            assertEquals("TestNode", root.selectSingleNode("node/@type").getStringValue());
+            assertEquals("test desc", root.selectSingleNode("node/@description").getStringValue());
+            assertEquals("test arch", root.selectSingleNode("node/@osArch").getStringValue());
+            assertEquals("test fam", root.selectSingleNode("node/@osFamily").getStringValue());
+            assertEquals("test osname", root.selectSingleNode("node/@osName").getStringValue());
+            assertEquals("test vers", root.selectSingleNode("node/@osVersion").getStringValue());
+            assertEquals("test user", root.selectSingleNode("node/@username").getStringValue());
+        }
+    }
+
+    public void testSettings() throws Exception{
+        {
+            final ResourceXMLGenerator gen = new ResourceXMLGenerator(test1);
+            //add a node
+            final NodeEntryImpl node = new NodeEntryImpl("test1", "test1name");
+            node.setDescription("test desc");
+            node.setOsArch("test arch");
+            node.setOsFamily("test fam");
+            node.setOsName("test osname");
+            node.setOsVersion("test vers");
+            node.setSettings(new HashMap<String, String>());
+            node.getSettings().put("testSetting", "testValue");
+
+            gen.addNode(node);
+            gen.generate();
+            assertTrue(test1.exists());
+            assertTrue(test1.isFile());
+            //assert contents
+            final Document d = reader.read(test1);
+            assertNotNull(d);
+            final Element root = d.getRootElement();
+            assertEquals("project", root.getName());
+            assertEquals(2, root.selectNodes("/project/*").size());
+            assertEquals(1, root.selectNodes("node").size());
+            assertEquals(1, root.selectNodes("setting").size());
+            assertEquals(1, root.selectNodes("node/resources/resource").size());
+            assertEquals("testSetting", root.selectSingleNode("node/resources/resource/@name").getStringValue());
+            assertEquals("Setting", root.selectSingleNode("node/resources/resource/@type").getStringValue());
+            
+            assertEquals("testSetting", root.selectSingleNode("setting/@name").getStringValue());
+            assertEquals("Setting", root.selectSingleNode("setting/@type").getStringValue());
+            assertEquals("testValue", root.selectSingleNode("setting/@settingValue").getStringValue());
         }
     }
 }
