97847208909696068d9d70efb1db51a931a85d2d
Greg Schueler <greg.schueler@gmail.com>
2010-11-18 15:35:06 -0800

    Fix calls to errorMsg when a test fails

--- test/testweb.sh
+++ test/testweb.sh
@@ -9,7 +9,7 @@ DIR=$(cd `dirname $0` && pwd)
 #source rcfile
 cd $RDECK_BASE 
 if [ 0 != $? ] ; then
-    errMsg "failed to cd to RDECK_BASE: $RDECK_BASE: $!"
+    errorMsg "failed to cd to RDECK_BASE: $RDECK_BASE: $!"
     exit 2
 fi
 
@@ -23,52 +23,52 @@ echo "WEB Starting tests."
 echo "WEB Trying login..."
 curl -s -S -L -c $DIR/cookies ${url}/menu/index > $DIR/curl.out 
 if [ 0 != $? ] ; then
-    errMsg "failed menu request to ${url}/menu/index"
+    errorMsg "failed menu request to ${url}/menu/index"
     exit 2
 fi
 
 grep 'j_security_check' -q $DIR/curl.out 
 if [ 0 != $? ] ; then
-    errMsg "login page not found"
+    errorMsg "login page not found"
     exit 2
 fi
 
 
 curl -s -S -L -c $DIR/cookies -b $DIR/cookies -d j_username=default -d j_password=default $loginurl > $DIR/curl.out 
 if [ 0 != $? ] ; then
-    errMsg "failed login request to ${loginurl}"
+    errorMsg "failed login request to ${loginurl}"
     exit 2
 fi
 
 
 grep 'id="indexMain"' -q $DIR/curl.out 
 if [ 0 != $? ] ; then
-    errMsg "login didnt seem to return the right page"
+    errorMsg "login didnt seem to return the right page"
     exit 2
 fi
 grep 'j_security_check' -q $DIR/curl.out 
 if [ 0 == $? ] ; then
-    errMsg "login was not successful"
+    errorMsg "login was not successful"
     exit 2
 fi
 
 echo "WEB Login OK"
 echo "WEB Testing Nodes..."
 # get nodes page
-curl -s -S -L -c $DIR/cookies -b $DIR/cookies ${url}/resources/nodes > $DIR/curl.out
+curl -s -S -L -c $DIR/cookies -b $DIR/cookies ${url}/resources/nodes?project=test > $DIR/curl.out
 if [ 0 != $? ] ; then
-    errMsg "failed nodes request"
+    errorMsg "failed nodes request"
     exit 2
 fi
 
 grep 'Nodes (1)' -q $DIR/curl.out 
 if [ 0 != $? ] ; then
-    errMsg "nodes output didnt have right nodes count"
+    errorMsg "nodes output didnt have right nodes count"
     exit 2
 fi
 grep 'j_security_check' -q $DIR/curl.out 
 if [ 0 == $? ] ; then
-    errMsg "login was not successful "
+    errorMsg "login was not successful "
     exit 2
 fi
 
