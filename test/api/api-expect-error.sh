759d78d33435501dfeaca6a0b0d4b0c8c2c99978
Greg Schueler <greg.schueler@gmail.com>
2011-02-04 12:48:26 -0800

    Update expect error, ad api-test-error and api-expect-code

--- test/api/api-expect-error.sh
+++ test/api/api-expect-error.sh
@@ -28,41 +28,24 @@ XMLSTARLET=xml
 params="$1"
 shift
 
-message="$*"
 
 # get listing
-$CURL --header "$VERSHEADER" ${requrl}?${params} > $DIR/curl.out
+$CURL -D $DIR/headers.out $CURL_REQ_OPTS --header "$VERSHEADER" ${requrl}?${params} > $DIR/curl.out
 if [ 0 != $? ] ; then
     errorMsg "FAIL: failed query request"
     exit 2
 fi
 
-#test curl.out for valid xml
-$XMLSTARLET val -w $DIR/curl.out > /dev/null 2>&1
-if [ 0 != $? ] ; then
-    errorMsg "FAIL: Response was not valid xml"
+
+grep "HTTP/1.1 200" -q $DIR/headers.out
+okheader=$?
+grep "HTTP/1.1 302" -q $DIR/headers.out
+ok2header=$?
+if [ 0 != $okheader -a 0 != $ok2header ] ; then
+    errorMsg "FAIL: Response was not 200 OK or 302:"
+    grep 'HTTP/1.1' $DIR/headers.out
     exit 2
 fi
+rm $DIR/headers.out
 
-#test for expected /joblist element
-$XMLSTARLET el $DIR/curl.out | grep -e '^result' -q
-if [ 0 != $? ] ; then
-    errorMsg "FAIL: Response did not contain expected result"
-    exit 2
-fi
-
-#If <result error="true"> then an error occured.
-waserror=$($XMLSTARLET sel -T -t -v "/result/@error" $DIR/curl.out)
-if [ "true" != "$waserror" ] ; then
-    errorMsg "FAIL: expected error result: ${message}"
-    exit 2
-fi
-if [ "" != "${message}" ] ; then 
-    errmsg=$($XMLSTARLET sel -T -t -v "/result/error/message" $DIR/curl.out)
-    if [ "${errmsg}" != "${message}" ] ; then
-        errorMsg "FAIL: wrong error message: ${errmsg}, expected ${message}"
-        exit 2
-    fi
-fi
-
-exit 0
+sh $DIR/api-test-error.sh $DIR/curl.out $*
---
+++ test/api/api-test-error.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# usage:
+#  api-test-error.sh <file> <message>
+# Tests xml file : expects result error="true", with result message if specified
+
+errorMsg() {
+   echo "$*" 1>&2
+}
+
+DIR=$(cd `dirname $0` && pwd)
+
+XMLSTARLET=xml
+
+file=$1
+shift
+
+message="$*"
+
+#test curl.out for valid xml
+$XMLSTARLET val -w ${file} > /dev/null 2>&1
+if [ 0 != $? ] ; then
+    errorMsg "FAIL: Response was not valid xml"
+    exit 2
+fi
+
+#test for expected /joblist element
+$XMLSTARLET el ${file} | grep -e '^result' -q
+if [ 0 != $? ] ; then
+    errorMsg "FAIL: Response did not contain expected result"
+    exit 2
+fi
+
+#If <result error="true"> then an error occured.
+waserror=$($XMLSTARLET sel -T -t -v "/result/@error" ${file})
+if [ "true" != "$waserror" ] ; then
+    errorMsg "FAIL: expected error result: ${message}"
+    exit 2
+fi
+if [ "" != "${message}" ] ; then 
+    errmsg=$($XMLSTARLET sel -T -t -v "/result/error/message" ${file})
+    if [ "${errmsg}" != "${message}" ] ; then
+        errorMsg "FAIL: wrong error message: ${errmsg}, expected ${message}"
+        exit 2
+    fi
+fi
+
+exit 0
