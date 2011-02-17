23db88a673b292b529ee09b1c0a965d78d484bcf
Greg Schueler <greg.schueler@gmail.com>
2011-02-16 18:18:54 -0800

    Add execution/abort utility script

---
+++ test/api/util-exec-abort.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Usage:
+#    util-exec-abort.sh <URL> <id>
+#   Abort a running execution by ID.
+
+DIR=$(cd `dirname $0` && pwd)
+source $DIR/include.sh
+
+execid=$1
+shift
+[ -z "$execid" ] && errorMsg "ID not specified" && exit 2
+
+# now submit req
+runurl="${APIURL}/execution/${execid}/abort"
+
+echo "# Abort execution: ${execid}"
+
+# get listing
+$CURL ${runurl}?${params} > $DIR/curl.out || fail "failed request: ${runurl}"
+
+sh $DIR/api-test-success.sh $DIR/curl.out || exit 2
+
+xmlsel "/result/success/message" -n $DIR/curl.out
+$XMLSTARLET sel -T -t -o "Abort status: " -v "/result/abort/@status" -n  $DIR/curl.out
+$XMLSTARLET sel -T -t -o "Execution status: " -v "/result/abort/execution/@status" -n  $DIR/curl.out
+
+rm $DIR/curl.out
+
