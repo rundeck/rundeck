9a25304b9a34f442cf8098c62fba05cecdd78600
Greg Schueler <greg.schueler@gmail.com>
2011-04-01 14:35:11 -0700

    Update

--- plugins/script-executor/src/main/java/com/dtolabs/rundeck/plugin/scriptexecutor/ExternalScriptExecutor.java
+++ plugins/script-executor/src/main/java/com/dtolabs/rundeck/plugin/scriptexecutor/ExternalScriptExecutor.java
@@ -28,8 +28,6 @@ import com.dtolabs.rundeck.core.common.INodeEntry;
 import com.dtolabs.rundeck.core.dispatcher.DataContextUtils;
 import com.dtolabs.rundeck.core.execution.ExecutionContext;
 import com.dtolabs.rundeck.core.execution.ExecutionException;
-import com.dtolabs.rundeck.core.execution.ExecutionListener;
-import com.dtolabs.rundeck.core.execution.impl.common.AntSupport;
 import com.dtolabs.rundeck.core.execution.service.NodeExecutor;
 import com.dtolabs.rundeck.core.execution.service.NodeExecutorResult;
 import com.dtolabs.rundeck.core.plugins.Plugin;
@@ -41,63 +39,72 @@ import java.io.IOException;
 import java.util.*;
 
 /**
- * ExternalScriptExecutor is a {@link NodeExecutor} that delegates execution to an external script. The external script
- * should be specified for a node via the {@value #SCRIPT_ATTRIBUTE} attribute on a node. This should be the local
- * filepath for a script to execute.  If using Windows, this should be a .bat file.
+ * <p>ExternalScriptExecutor is a {@link NodeExecutor} that delegates execution to an external script. The external
+ * script should be specified for a node via the {@value #SCRIPT_ATTRIBUTE} attribute on a node. This should be the full
+ * command to execute </p><p>   In addition to normal Data context references in this attribute, you can include these
+ * special data references: </p> <ul>
+ * <li><pre>${script-exec.command}</pre>: This is the user-entered command to execute</li>
+ * <li><pre>${script-exec.dir}</pre>: This is the value of the script-exec-dir attribute</li>
+ * </ul> <p> So for example, if you wanted to change the way the script is invoked, you could specify the
+ * script-exec-args like:
+ * <pre>
+ *         &lt;node name="mynode" ...
+ *         myscript-file="/some/script.sh"
+ *         script-exec="/bin/zsh ${node.myscript-file} ${script-exec.command} -- ${node.username}@${node.name}"/>
+ * </pre>
+ * This would execute /bin/zsh and pass the value of the script-exec-file attribute followed by the command, followed by
+ * -- and the node "username@hostname". </p>
  *
  * @author Greg Schueler <a href="mailto:greg@dtosolutions.com">greg@dtosolutions.com</a>
  */
 @Plugin (name = "script-exec", service = "NodeExecutor")
 public class ExternalScriptExecutor implements NodeExecutor {
-    public static String SCRIPT_ATTRIBUTE = "script-exec-file";
-    public static String ARGS_ATTRIBUTE = "script-exec-args";
+    public static String SCRIPT_ATTRIBUTE = "script-exec";
     public static String DIR_ATTRIBUTE = "script-exec-dir";
 
     public NodeExecutorResult executeCommand(final ExecutionContext executionContext, final String[] command,
                                              final INodeEntry node) throws ExecutionException {
-        File scriptfile;
         File workingdir = null;
         String scriptargs = null;
         if (null == node.getAttributes() || null == node.getAttributes().get(SCRIPT_ATTRIBUTE)) {
             throw new ExecutionException(
-                "[script-exec node executor] Node was missing the " + SCRIPT_ATTRIBUTE + " attribute: " + node.getNodename());
+                "[script-exec node executor] Node was missing the " + SCRIPT_ATTRIBUTE + " attribute: " + node
+                    .getNodename());
         }
-        scriptfile = new File(node.getAttributes().get(SCRIPT_ATTRIBUTE));
 
-        if (null != node.getAttributes().get(ARGS_ATTRIBUTE)) {
-            scriptargs = node.getAttributes().get(ARGS_ATTRIBUTE);
+        if (null != node.getAttributes().get(SCRIPT_ATTRIBUTE)) {
+            scriptargs = node.getAttributes().get(SCRIPT_ATTRIBUTE);
         }
         if (null != node.getAttributes().get(DIR_ATTRIBUTE)) {
             workingdir = new File(node.getAttributes().get(DIR_ATTRIBUTE));
         }
 
+        final Map<String, Map<String, String>> origDataContext = executionContext.getDataContext();
+
+        //add node context data
+        final Map<String, Map<String, String>> nodeContext =
+            DataContextUtils.addContext("node", DataContextUtils.nodeData(node), origDataContext);
+
         //expand data references within the command to execute
         final String[] expandCommand = DataContextUtils.replaceDataReferences(command,
-            executionContext.getDataContext());
+            nodeContext);
 
 
         //add some more data context values to allow templatized script-exec-args
         final HashMap<String, String> scptexec = new HashMap<String, String>();
         scptexec.put("command", StringArrayUtil.asString(expandCommand, " "));
-        scptexec.put("file", scriptfile.getAbsolutePath());
         if (null != workingdir) {
             scptexec.put("dir", workingdir.getAbsolutePath());
         }
         final Map<String, Map<String, String>> newDataContext = DataContextUtils.addContext("script-exec", scptexec,
-            executionContext.getDataContext());
+            nodeContext);
 
         //generate args that we are going to execute
         final ArrayList<String> argslist = new ArrayList<String>();
-        if (null != scriptargs) {
-            //use script-exec-args attribute and replace datareferences
-            final String[] split = scriptargs.split(" ");
-            argslist.addAll(Arrays.asList(DataContextUtils.replaceDataReferences(split, newDataContext)));
-        } else {
-            //execute the scriptfile with the command as the arguments
-            argslist.add(scriptfile.getAbsolutePath());
-            argslist.add(node.getNodename());
-            argslist.addAll(Arrays.asList(expandCommand));
-        }
+
+        //use script-exec attribute and replace datareferences
+        final String[] split = scriptargs.split(" ");
+        argslist.addAll(Arrays.asList(DataContextUtils.replaceDataReferences(split, newDataContext)));
 
         /**
          * final args array
@@ -105,8 +112,7 @@ public class ExternalScriptExecutor implements NodeExecutor {
         final String[] args = argslist.toArray(new String[argslist.size()]);
 
         //create system environment variables from the data context
-        final Map<String, String> envMap = DataContextUtils.generateEnvVarsFromContext(
-            executionContext.getDataContext());
+        final Map<String, String> envMap = DataContextUtils.generateEnvVarsFromContext(nodeContext);
         final ArrayList<String> envlist = new ArrayList<String>();
         for (final String key : envMap.keySet()) {
             final String envval = envMap.get(key);
@@ -117,25 +123,35 @@ public class ExternalScriptExecutor implements NodeExecutor {
 
         int result = -1;
         boolean success = false;
+        Thread errthread = null;
+        Thread outthread = null;
+        String errmsg;
+        executionContext.getExecutionListener().log(3, "[script-exec] executing: " + StringArrayUtil.asString(args,
+            " "));
+        final Runtime runtime = Runtime.getRuntime();
+        Process exec = null;
         try {
-            final Runtime runtime = Runtime.getRuntime();
-            final Process exec = runtime.exec(args, envarr, workingdir);
-            System.err.println("exec started: " + StringArrayUtil.asString(args, " "));
-            //TODO: bind process output/error to System.out/System.err
-
-            Thread errthread = Streams.copyStreamThread(exec.getErrorStream(), System.err);
-            Thread outthread = Streams.copyStreamThread(exec.getInputStream(), System.out);
+            exec = runtime.exec(args, envarr, workingdir);
+        } catch (IOException e) {
+            throw new ExecutionException(e);
+        }
+        try {
+            errthread = Streams.copyStreamThread(exec.getErrorStream(), System.err);
+            outthread = Streams.copyStreamThread(exec.getInputStream(), System.out);
             errthread.start();
             outthread.start();
             exec.getOutputStream().close();
             result = exec.waitFor();
-            System.err.println("exec result: " + result);
+            errthread.join();
+            outthread.join();
             success = 0 == result;
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            e.printStackTrace(System.err);
         } catch (IOException e) {
-            e.printStackTrace();
+            e.printStackTrace(System.err);
         }
+        executionContext.getExecutionListener().log(3,
+            "[script-exec]: result code: " + result + ", success: " + success);
         final int returnresult = result;
         final boolean returnsuccess = success;
         return new NodeExecutorResult() {
@@ -145,6 +161,11 @@ public class ExternalScriptExecutor implements NodeExecutor {
 
             public boolean isSuccess() {
                 return returnsuccess;
+            }
+
+            @Override
+            public String toString() {
+                return "[script-exec] success: " + isSuccess() + ", result code: " + getResultCode();
             }
         };
     }
---
+++ plugins/script-executor/src/main/resources/test-exec.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo This is the test-exec.sh script
+echo Arguments: $*
+echo environment is:
+
+env
