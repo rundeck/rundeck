6d93a9b5e82cfe9f166596b6188838a409f26f93
Greg Schueler <greg.schueler@gmail.com>
2011-04-27 14:50:56 -0700

    Fix use of commons-cli getOptionValue [#256 state:resolved]

--- rundeckapp/src/java/com/dtolabs/rundeck/ExpandRunServer.java
+++ rundeckapp/src/java/com/dtolabs/rundeck/ExpandRunServer.java
@@ -236,12 +236,11 @@ public class ExpandRunServer {
         }
         debug = debug || cl.hasOption('d');
         DEBUG("Debugging is turned on.");
-        
-        this.basedir = cl.getOptionValue("basedir", new File(thisJar.getAbsolutePath()).getParentFile().getAbsolutePath());  // TODO: is the first getAbsolutePath required?
+        this.basedir = cl.getOptionValue('b', new File(thisJar.getAbsolutePath()).getParentFile().getAbsolutePath());  // TODO: is the first getAbsolutePath required?
         this.serverdir = new File(cl.getOptionValue("serverdir", basedir+"/server"));
         this.configDir = new File(cl.getOptionValue("c", serverdir + "/config"));
         DEBUG("configDir is " + configDir.getAbsolutePath());
-        bindir = new File(cl.getOptionValue("bindir"), toolsdir+"/bin");
+        bindir = new File(cl.getOptionValue('x', toolsdir+"/bin"));
         
         
 
