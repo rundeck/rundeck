7f882fa97c886befb157f78d721bbee91ee30c6b
Greg Schueler <greg.schueler@gmail.com>
2011-05-19 17:02:47 -0700

    Add /api/1/system/info endpoint with some stats [#261]

--- rundeckapp/grails-app/controllers/ApiController.groovy
+++ rundeckapp/grails-app/controllers/ApiController.groovy
@@ -1,8 +1,11 @@
+import java.lang.management.ManagementFactory
+
 /**
  * Contains utility actions for API access and responses
  */
 class ApiController {
     def defaultAction = "invalid"
+    def quartzScheduler
     
     def invalid = {
         response.setStatus(404)
@@ -75,4 +78,77 @@ class ApiController {
         }
     }
 
+    /**
+     * API endpoints
+     */
+
+    /**
+     * /api/1/system/info: display stats and info about the server
+     */
+    def apiSystemInfo={
+        Date nowDate=new Date();
+        String nodeName= servletContext.getAttribute("FRAMEWORK_NODE")
+        String appVersion= grailsApplication.metadata['app.version']
+        double load= ManagementFactory.getOperatingSystemMXBean().systemLoadAverage
+        int processorsCount= ManagementFactory.getOperatingSystemMXBean().availableProcessors
+        String osName= ManagementFactory.getOperatingSystemMXBean().name
+        String osVersion= ManagementFactory.getOperatingSystemMXBean().version
+        String osArch= ManagementFactory.getOperatingSystemMXBean().arch
+        String vmName=ManagementFactory.getRuntimeMXBean().vmName
+        String vmVendor=ManagementFactory.getRuntimeMXBean().vmVendor
+        String vmVersion=ManagementFactory.getRuntimeMXBean().vmVersion
+        long durationTime=ManagementFactory.getRuntimeMXBean().uptime
+        Date startupDate = new Date(nowDate.getTime()-durationTime)
+        int threadActiveCount=Thread.activeCount()
+        return success { delegate ->
+            delegate.'success' {
+                message("System Stats for RunDeck ${appVersion} on node ${nodeName}")
+            }
+            delegate.'system'{
+                timestamp(epoch:nowDate.getTime(),unit:'ms'){
+                    datetime(g.w3cDateValue(date:nowDate))
+                }
+                rundeck{
+                    version(appVersion)
+                    build(grailsApplication.metadata['build.ident'])
+                    node(nodeName)
+                    base(servletContext.getAttribute("RDECK_BASE"))
+                }
+                os {
+                    arch(osArch)
+                    name(osName)
+                    version(osVersion)
+                }
+                jvm {
+                    name(vmName)
+                    vendor(vmVendor)
+                    version(vmVersion)
+                }
+                stats{
+                    uptime(duration:durationTime,unit: 'ms'){
+                        since(epoch: startupDate.getTime(),unit:'ms'){
+                            datetime(g.w3cDateValue(date: startupDate))
+                        }
+                    }
+    //                errorCount('12')
+//                    requestCount('12')
+                    cpu{
+                        loadAverage(unit:'percent',load)
+                        processors(processorsCount)
+                    }
+                    memory(unit:'byte'){
+                        max(Runtime.getRuntime().maxMemory())
+                        free(Runtime.getRuntime().freeMemory())
+                        total(Runtime.getRuntime().totalMemory())
+                    }
+                    scheduler{
+                        running(quartzScheduler.getCurrentlyExecutingJobs().size())
+                    }
+                    threads{
+                        active(threadActiveCount)
+                    }
+                }
+            }
+        }
+    }
 }
