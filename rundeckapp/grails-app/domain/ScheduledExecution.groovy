8e4ba5e601b6f10fbacc653c793cb22daab86666
Greg Schueler <greg.schueler@gmail.com>
2011-06-24 15:25:28 -0700

    Re-enable 'id' tag output in job xml/yaml for better backwards compatibility

--- rundeckapp/grails-app/domain/ScheduledExecution.groovy
+++ rundeckapp/grails-app/domain/ScheduledExecution.groovy
@@ -82,6 +82,7 @@ class ScheduledExecution extends ExecutionContext {
         }
         if(uuid){
             map.uuid=uuid
+            map.id=uuid
         }else if (id) {
             map.id = id
         }
