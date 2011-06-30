8dd6e5db829e1d0db09edb6e14caed32d82b4a57
Greg Schueler <greg.schueler@gmail.com>
2011-06-29 17:12:41 -0700

    Add unauthorized access api test

--- test/api/include.sh
+++ test/api/include.sh
@@ -33,10 +33,10 @@ API_CURRENT_VERSION=2
 
 API_VERSION=${API_VERSION:-$API_CURRENT_VERSION}
 
-AUTHHEADER="X-RunDeck-Auth-Token: $RDAUTH"
 
 # curl opts to use a cookie jar, and follow redirects, showing only errors
 if [ -n "$RDAUTH" ] ; then 
+    AUTHHEADER="X-RunDeck-Auth-Token: $RDAUTH"
     CURLOPTS="-s -S -L"
 else
     CURLOPTS="-s -S -L -c $DIR/cookies -b $DIR/cookies"
