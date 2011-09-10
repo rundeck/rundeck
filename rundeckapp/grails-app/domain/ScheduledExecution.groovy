ab6f2366f32f44e6a4abfa31521e58b351dcf5a6
Greg Schueler <greg.schueler@gmail.com>
2011-09-10 08:42:08 -0700

    Remove unnecessary mapping in ScheduledExecution

--- rundeckapp/grails-app/domain/ScheduledExecution.groovy
+++ rundeckapp/grails-app/domain/ScheduledExecution.groovy
@@ -71,12 +71,6 @@ class ScheduledExecution extends ExecutionContext {
         adhocFilepath(nullable:true, blank:true)
         uuid(unique: true, nullable:true, blank:false)
     }
-    static mapping = {
-        def config = ConfigurationHolder.config
-        if (config.rundeck.v14.rdbsupport == 'true') {
-            user column: "rduser"
-        }
-    }
     public static final daysofweeklist = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
     public static final monthsofyearlist = ['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC'];
 
