2f2e53b4a6709b268a1d4744d7d5905dbee0f6ec
Greg Schueler <greg.schueler@gmail.com>
2011-09-20 10:05:59 -0700

    Fix oracle test failures [#443]
    Resolve oracle test error with 'possible nonthreadsafe access to
    session' by flushing session
    
    Fix NPE exceptions in tests by flushing save

--- rundeckapp/test/integration/ScheduledExecValidationTests.groovy
+++ rundeckapp/test/integration/ScheduledExecValidationTests.groovy
@@ -27,7 +27,7 @@ import org.springframework.mock.web.MockMultipartFile
  */
 
 public class ScheduledExecValidationTests extends GrailsUnitTestCase{
-
+    def sessionFactory
     protected void setUp() {
         super.setUp();
     }
@@ -3689,6 +3689,8 @@ public class ScheduledExecValidationTests extends GrailsUnitTestCase{
             assertTrue exec.errors.hasFieldErrors('adhocExecution')
 
         }
+        sessionFactory.currentSession.clear()
+        sessionFactory.currentSession.flush()
         if(true){//test update from one adhoc type to another; remote -> local
             def se = new ScheduledExecution(jobName:'monkey1',project:'testProject',description:'blah',
                 adhocExecution:true,adhocRemoteString:'test remote',
@@ -4639,7 +4641,7 @@ public class ScheduledExecValidationTests extends GrailsUnitTestCase{
             def na2 = new Notification(eventTrigger:'onfailure',type:'email',content:'monkey@example.com')
             se.addToNotifications(na1)
             se.addToNotifications(na2)
-            se.save()
+            se.save(flush:true)
 
             assertNotNull se.id
 
@@ -4803,7 +4805,7 @@ public class ScheduledExecValidationTests extends GrailsUnitTestCase{
             def na2 = new Notification(eventTrigger:'onfailure',type:'email',content:'monkey@example.com')
             se.addToNotifications(na1)
             se.addToNotifications(na2)
-            se.save()
+            se.save(flush:true)
 
             assertNotNull se.id
 
