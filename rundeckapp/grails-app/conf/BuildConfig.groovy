0bd026c47b6c03535122de8d409754eceeca3985
Greg Schueler <greg.schueler@gmail.com>
2011-11-03 16:53:54 -0700

    Include ant-launcher dependency

--- rundeckapp/grails-app/conf/BuildConfig.groovy
+++ rundeckapp/grails-app/conf/BuildConfig.groovy
@@ -11,7 +11,7 @@ grails.project.dependency.resolution = {
         build 'org.yaml:snakeyaml:1.9','org.apache.ant:ant:1.7.1','org.apache.ant:ant-jsch:1.7.1','com.jcraft:jsch:0.1.42'
         test 'org.yaml:snakeyaml:1.9','org.apache.ant:ant:1.7.1','org.apache.ant:ant-jsch:1.7.1','com.jcraft:jsch:0.1.42'
         compile 'org.yaml:snakeyaml:1.9','org.apache.ant:ant:1.7.1','org.apache.ant:ant-jsch:1.7.1','com.jcraft:jsch:0.1.42'
-        runtime 'org.yaml:snakeyaml:1.9','org.apache.ant:ant:1.7.1','org.apache.ant:ant-jsch:1.7.1','com.jcraft:jsch:0.1.42'
+        runtime 'org.yaml:snakeyaml:1.9', 'org.apache.ant:ant:1.7.1', 'org.apache.ant:ant-launcher:1.7.1','org.apache.ant:ant-jsch:1.7.1','com.jcraft:jsch:0.1.42'
     }
     grails.plugin.location.'webrealms' = "webrealms"
 }
