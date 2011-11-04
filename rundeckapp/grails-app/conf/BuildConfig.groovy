62f9a33fe1ff4ad518dc31f6170d3217c3da3445
Greg Schueler <greg.schueler@gmail.com>
2011-11-03 18:02:53 -0700

    Add missing spring-test dependency needed for webhook response

--- rundeckapp/grails-app/conf/BuildConfig.groovy
+++ rundeckapp/grails-app/conf/BuildConfig.groovy
@@ -11,7 +11,7 @@ grails.project.dependency.resolution = {
         build 'org.yaml:snakeyaml:1.9','org.apache.ant:ant:1.7.1','org.apache.ant:ant-jsch:1.7.1','com.jcraft:jsch:0.1.42'
         test 'org.yaml:snakeyaml:1.9','org.apache.ant:ant:1.7.1','org.apache.ant:ant-jsch:1.7.1','com.jcraft:jsch:0.1.42'
         compile 'org.yaml:snakeyaml:1.9','org.apache.ant:ant:1.7.1','org.apache.ant:ant-jsch:1.7.1','com.jcraft:jsch:0.1.42'
-        runtime 'org.yaml:snakeyaml:1.9', 'org.apache.ant:ant:1.7.1', 'org.apache.ant:ant-launcher:1.7.1','org.apache.ant:ant-jsch:1.7.1','com.jcraft:jsch:0.1.42'
+        runtime 'org.yaml:snakeyaml:1.9', 'org.apache.ant:ant:1.7.1', 'org.apache.ant:ant-launcher:1.7.1','org.apache.ant:ant-jsch:1.7.1','com.jcraft:jsch:0.1.42', 'org.springframework:org.springframework.test:3.0.5.RELEASE'
     }
     grails.plugin.location.'webrealms' = "webrealms"
 }
