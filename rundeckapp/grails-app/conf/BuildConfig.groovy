f4e6e2b15c9a45f1a939f2b57fccd332b536a74d
Greg Schueler <greg.schueler@gmail.com>
2011-11-11 17:26:05 -0800

    Add log4j dependency, fix repos to resolve plugins

--- rundeckapp/grails-app/conf/BuildConfig.groovy
+++ rundeckapp/grails-app/conf/BuildConfig.groovy
@@ -2,17 +2,20 @@ grails.project.dependency.resolution = {
     inherits "global" // inherit Grails' default dependencies
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
+    	mavenCentral()
+        grailsPlugins()
+        grailsHome()
+        grailsCentral()
+        mavenLocal()
+
         //include snakeyaml from deps dir
         flatDir name:'sourceDeps', dirs:'../dependencies/snakeyaml/jars'
-        grailsHome()
-        mavenCentral()
-        mavenLocal()
     }
     dependencies {
-        build 'org.yaml:snakeyaml:1.9','org.apache.ant:ant:1.7.1','org.apache.ant:ant-jsch:1.7.1','com.jcraft:jsch:0.1.42'
-        test 'org.yaml:snakeyaml:1.9','org.apache.ant:ant:1.7.1','org.apache.ant:ant-jsch:1.7.1','com.jcraft:jsch:0.1.42'
-        compile 'org.yaml:snakeyaml:1.9','org.apache.ant:ant:1.7.1','org.apache.ant:ant-jsch:1.7.1','com.jcraft:jsch:0.1.42'
-        runtime 'org.yaml:snakeyaml:1.9', 'org.apache.ant:ant:1.7.1', 'org.apache.ant:ant-launcher:1.7.1','org.apache.ant:ant-jsch:1.7.1','com.jcraft:jsch:0.1.42', 'org.springframework:org.springframework.test:3.0.5.RELEASE'
+        build 'org.yaml:snakeyaml:1.9','org.apache.ant:ant:1.7.1','org.apache.ant:ant-jsch:1.7.1','com.jcraft:jsch:0.1.42','log4j:log4j:1.2.16'
+        test 'org.yaml:snakeyaml:1.9','org.apache.ant:ant:1.7.1','org.apache.ant:ant-jsch:1.7.1','com.jcraft:jsch:0.1.42','log4j:log4j:1.2.16'
+        compile 'org.yaml:snakeyaml:1.9','org.apache.ant:ant:1.7.1','org.apache.ant:ant-jsch:1.7.1','com.jcraft:jsch:0.1.42','log4j:log4j:1.2.16'
+        runtime 'org.yaml:snakeyaml:1.9', 'org.apache.ant:ant:1.7.1', 'org.apache.ant:ant-launcher:1.7.1','org.apache.ant:ant-jsch:1.7.1','com.jcraft:jsch:0.1.42', 'org.springframework:spring-test:3.0.5.RELEASE','log4j:log4j:1.2.16'
     }
     grails.plugin.location.'webrealms' = "webrealms"
 }
