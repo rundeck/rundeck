702e94e9ed8b8fe30d5182b531568caef9943e62
Greg Schueler <greg.schueler@gmail.com>
2011-11-21 18:06:04 -0800

    Add Partial line buffer util class

---
+++ core/src/main/java/com/dtolabs/rundeck/core/utils/PartialLineBuffer.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright 2011 DTO Solutions, Inc. (http://dtosolutions.com)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/*
+* PartialLineBuffer.java
+* 
+* User: Greg Schueler <a href="mailto:greg@dtosolutions.com">greg@dtosolutions.com</a>
+* Created: 11/21/11 4:27 PM
+* 
+*/
+package com.dtolabs.rundeck.core.utils;
+
+import java.util.*;
+
+/**
+ * A Line oriented String buffer which can also return the last incomplete line read
+ */
+public class PartialLineBuffer {
+    private ArrayList<String> lines = new ArrayList<String>();
+    String partialLine = null;
+    boolean newdata = false;
+
+    private void appendString(final String str, final boolean linending) {
+        if (linending) {
+            if (null != partialLine) {
+                if (partialLine.endsWith("\r") && str.equals("")) {
+                    lines.add(partialLine.substring(0, partialLine.length() - 1));
+                } else if (partialLine.endsWith("\r")) {
+                    lines.add(partialLine.substring(0, partialLine.length() - 1));
+                    lines.add(str);
+                } else {
+                    lines.add(partialLine + str);
+                }
+                partialLine = null;
+            } else {
+                lines.add(str);
+            }
+        } else {
+            if (null != partialLine) {
+                if (partialLine.endsWith("\r")) {
+                    lines.add(partialLine.substring(0, partialLine.length() - 1));
+                    if ("".equals(str)) {
+                        partialLine = null;
+                    } else {
+                        partialLine = str;
+                        newdata = true;
+                    }
+                } else {
+                    partialLine += str;
+                    newdata = true;
+                }
+            } else {
+                partialLine = str;
+                newdata = true;
+            }
+
+        }
+    }
+
+    /**
+     * Add character data to the buffer
+     */
+    public void addData(final char[] data, final int off, final int size) {
+        final String str = new String(data, off, size);
+        if (str.contains("\n")) {
+            final String[] lines = str.split("\\r?\\n", -1);
+            for (int i = 0 ; i < lines.length - 1 ; i++) {
+                appendString(lines[i], true);
+            }
+            if (lines.length > 0) {
+                final String last = lines[lines.length - 1];
+                if ("".equals(last)) {
+                    //end of previous line
+                } else {
+                    appendString(last, false);
+                }
+            }
+        } else if (str.contains("\r")) {
+            final String[] lines = str.split("\\r", -1);
+            for (int i = 0 ; i < lines.length - 2 ; i++) {
+                appendString(lines[i], true);
+            }
+            //last two split strings
+            //a: 1,1: yes,no
+            //b: 0,1: yes,no
+            //c: 0,0: yes(pass),no
+            //d: 1,0: yes(pass),
+            if (lines.length >= 2) {
+                final String last2 = lines[lines.length - 2];
+
+                final String last = lines[lines.length - 1];
+                if (!"".equals(last)) {
+                    appendString(last2, true);
+                    appendString(last, false);
+                } else {
+                    //pass \r for later \r\n resolution
+                    appendString(last2 + "\r", false);
+                }
+            }
+        } else {
+            appendString(str, false);
+        }
+    }
+
+    /**
+     * Return the last partial line read and mark it
+     */
+    public String getPartialLine() {
+        return getPartialLine(true);
+    }
+
+    /**
+     * Return the last partial line read, optionally marking it as already read. Subsequent calls with mark set to true
+     * will return null if there as been no new data.
+     */
+    public String getPartialLine(final boolean mark) {
+        if (mark && newdata) {
+            newdata = false;
+            return partialLine;
+        } else if (mark && !newdata) {
+            return null;
+        } else {
+            return partialLine;
+        }
+    }
+
+    /**
+     * Read the next line if any, and remove it from the buffer.
+     */
+    public String readLine() {
+        if (lines.size() > 0) {
+            return lines.remove(0);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Return the line buffer
+     */
+    public List<String> getLines() {
+        return lines;
+    }
+
+}
---
+++ core/src/test/java/com/dtolabs/rundeck/core/utils/TestPartialLineBuffer.java
@@ -0,0 +1,329 @@
+/*
+ * Copyright 2011 DTO Solutions, Inc. (http://dtosolutions.com)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/*
+* TestPartialLineBuffer.java
+* 
+* User: Greg Schueler <a href="mailto:greg@dtosolutions.com">greg@dtosolutions.com</a>
+* Created: 11/21/11 4:27 PM
+* 
+*/
+package com.dtolabs.rundeck.core.utils;
+
+import com.dtolabs.rundeck.core.utils.PartialLineBuffer;
+import junit.framework.TestCase;
+
+/**
+ * TestPartialLineBuffer is ...
+ *
+ * @author Greg Schueler <a href="mailto:greg@dtosolutions.com">greg@dtosolutions.com</a>
+ */
+public class TestPartialLineBuffer extends TestCase {
+
+    public void testPartialLineBuffer() {
+        final PartialLineBuffer partialLineBuffer = new PartialLineBuffer();
+        assertNull(partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.getPartialLine());
+
+        char[] data = "Test1".toCharArray();
+        partialLineBuffer.addData(data, 0, data.length);
+        assertNull(partialLineBuffer.readLine());
+        assertEquals("Test1", partialLineBuffer.getPartialLine());
+        assertNull(partialLineBuffer.getPartialLine());
+
+        data = " Test2".toCharArray();
+        partialLineBuffer.addData(data, 0, data.length);
+
+        assertNull(partialLineBuffer.readLine());
+        assertEquals("Test1 Test2", partialLineBuffer.getPartialLine());
+        assertNull(partialLineBuffer.getPartialLine());
+
+        data = " Test3\n".toCharArray();
+        partialLineBuffer.addData(data, 0, data.length);
+
+        assertEquals("Test1 Test2 Test3", partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.readLine());
+        assertNull("not null: '" + partialLineBuffer.getPartialLine(false) + "'", partialLineBuffer.getPartialLine(
+            false));
+    }
+
+    public void testSplit() {
+        String arr[] = "abc:def::ghi".split(":", -1);
+        assertEquals(4, arr.length);
+        String arr2[] = "ghi:".split(":", -1);
+        assertEquals(2, arr2.length);
+        String arr3[] = ":ghi::".split(":", -1);
+        assertEquals(4, arr3.length);
+        String arr4[] = "::ghi::".split(":", -1);
+        assertEquals(5, arr4.length);
+    }
+
+    public void testSplitLine() {
+        //a: 1,1: yes,no
+        //b: 0,1: yes,no
+        //c: 0,0: yes,no (pass)
+        //d: 1,0: yes,(skip)
+        String arr[] = "abc\rdef\r\rghi".split("\\r", -1);
+        assertEquals(4, arr.length);
+        String arr2[] = "ghi\r".split("\\r", -1);
+        assertEquals(2, arr2.length);
+        String arr3[] = "\rghi\r\r".split("\\r", -1);
+        assertEquals(4, arr3.length);
+        String arr4[] = "\r\rghi\r\r".split("\\r", -1);
+        assertEquals(5, arr4.length);
+        String arr5[] = "\r".split("\\r", -1);
+        assertEquals(2, arr5.length);
+    }
+
+    public void testPartialLineBufferMultiSplit() {
+        PartialLineBuffer partialLineBuffer = new PartialLineBuffer();
+        assertNull(partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.getPartialLine());
+
+        //multiline
+
+        char[] data = "Test4\nTest5".toCharArray();
+        partialLineBuffer.addData(data, 0, data.length);
+
+        assertEquals("Test4", partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.readLine());
+        assertEquals("Test5", partialLineBuffer.getPartialLine());
+        assertNull(partialLineBuffer.getPartialLine());
+
+        data = " Test6\r\nTest7".toCharArray();
+        partialLineBuffer.addData(data, 0, data.length);
+
+        assertEquals("Test5 Test6", partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.readLine());
+        assertEquals("Test7", partialLineBuffer.getPartialLine());
+        assertNull(partialLineBuffer.getPartialLine());
+
+        data = " Test8\rTest9".toCharArray();
+        partialLineBuffer.addData(data, 0, data.length);
+
+        assertEquals("Test7 Test8", partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.readLine());
+        assertEquals("Test9", partialLineBuffer.getPartialLine());
+        assertNull(partialLineBuffer.getPartialLine());
+    }
+
+    public void testPartialLineBufferMulti() {
+
+        PartialLineBuffer partialLineBuffer = new PartialLineBuffer();
+        char[] data = "Test10\nTest11\n".toCharArray();
+        partialLineBuffer.addData(data, 0, data.length);
+
+        assertEquals("Test10", partialLineBuffer.readLine());
+        assertEquals("Test11", partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.getPartialLine());
+
+        data = "Test10\r\nTest11\r\n".toCharArray();
+        partialLineBuffer.addData(data, 0, data.length);
+
+        assertEquals("Test10", partialLineBuffer.readLine());
+        assertEquals("Test11", partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.getPartialLine());
+
+        data = "Test10\rTest11\r".toCharArray();
+        partialLineBuffer.addData(data, 0, data.length);
+
+        assertEquals("Test10", partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.readLine());
+        assertEquals("Test11\r", partialLineBuffer.getPartialLine());
+        assertNull(partialLineBuffer.getPartialLine());
+    }
+
+    public void testPartialLineBufferLeftoverCR() {
+
+        PartialLineBuffer partialLineBuffer = new PartialLineBuffer();
+        char[] data = "Test10\rTest11\r".toCharArray();
+        partialLineBuffer.addData(data, 0, data.length);
+
+        assertEquals("Test10", partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.readLine());
+        assertEquals("Test11\r", partialLineBuffer.getPartialLine());
+        assertNull(partialLineBuffer.getPartialLine());
+
+        //append to line
+        data = " Test12\rTest13".toCharArray();
+        partialLineBuffer.addData(data, 0, data.length);
+
+        assertEquals("Test11", partialLineBuffer.readLine());
+        assertEquals(" Test12", partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.readLine());
+        assertEquals("Test13", partialLineBuffer.getPartialLine());
+        assertNull(partialLineBuffer.getPartialLine());
+    }
+
+    public void testPartialLineBufferLeftoverCRPartial() {
+
+        PartialLineBuffer partialLineBuffer = new PartialLineBuffer();
+        char[] data = "Test10\rTest11\r".toCharArray();
+        partialLineBuffer.addData(data, 0, data.length);
+
+        assertEquals("Test10", partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.readLine());
+        assertEquals("Test11\r", partialLineBuffer.getPartialLine());
+        assertNull(partialLineBuffer.getPartialLine());
+
+        //append to line
+        data = " Test12".toCharArray();
+        partialLineBuffer.addData(data, 0, data.length);
+
+        assertEquals("Test11", partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.readLine());
+        assertEquals(" Test12", partialLineBuffer.getPartialLine());
+        assertNull(partialLineBuffer.getPartialLine());
+    }
+
+    public void testPartialLineBufferLeftoverCRNL() {
+
+        PartialLineBuffer partialLineBuffer = new PartialLineBuffer();
+        char[] data = "Test10\r\nTest11\r".toCharArray();
+        partialLineBuffer.addData(data, 0, data.length);
+
+        assertEquals("Test10", partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.readLine());
+        assertEquals("Test11\r", partialLineBuffer.getPartialLine());
+        assertNull(partialLineBuffer.getPartialLine());
+
+        //append to line
+        data = " Test12\r\n".toCharArray();
+        partialLineBuffer.addData(data, 0, data.length);
+
+        assertEquals("Test11", partialLineBuffer.readLine());
+        assertEquals(" Test12", partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.getPartialLine());
+    }
+
+    public void testPartialLineBufferLeftoverCRNLPartialNL() {
+
+        PartialLineBuffer partialLineBuffer = new PartialLineBuffer();
+        char[] data = "Test10\r\nTest11\r".toCharArray();
+        partialLineBuffer.addData(data, 0, data.length);
+
+        assertEquals("Test10", partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.readLine());
+        assertEquals("Test11\r", partialLineBuffer.getPartialLine());
+        assertNull(partialLineBuffer.getPartialLine());
+
+        //append to line
+        data = "\n Test12\r\n".toCharArray();
+        partialLineBuffer.addData(data, 0, data.length);
+
+        assertEquals("Test11", partialLineBuffer.readLine());
+        assertEquals(" Test12", partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.getPartialLine());
+    }
+
+    public void testPartialLineBufferLeftoverCRNLPartial() {
+
+        PartialLineBuffer partialLineBuffer = new PartialLineBuffer();
+        char[] data = "Test10\r\nTest11\r".toCharArray();
+        partialLineBuffer.addData(data, 0, data.length);
+
+        assertEquals("Test10", partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.readLine());
+        assertEquals("Test11\r", partialLineBuffer.getPartialLine());
+        assertNull(partialLineBuffer.getPartialLine());
+
+        //append to line
+        data = "\n Test12".toCharArray();
+        partialLineBuffer.addData(data, 0, data.length);
+
+        assertEquals("Test11", partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.readLine());
+        assertEquals(" Test12", partialLineBuffer.getPartialLine());
+        assertNull(partialLineBuffer.getPartialLine());
+    }
+
+    public void testPartialLineBufferFull() {
+        PartialLineBuffer partialLineBuffer = new PartialLineBuffer();
+        char[] data = "Test1\rTest2\rTest3: ".toCharArray();
+        partialLineBuffer.addData(data, 0, data.length);
+
+        assertEquals(2, partialLineBuffer.getLines().size());
+        assertEquals("Test3: ", partialLineBuffer.getPartialLine(false));
+
+        assertEquals("Test1", partialLineBuffer.readLine());
+        assertEquals("Test2", partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.readLine());
+
+        assertEquals("Test3: ", partialLineBuffer.getPartialLine());
+        assertNull(partialLineBuffer.getPartialLine());
+
+    }
+
+    public void testPartialLineBufferFull2() {
+        PartialLineBuffer partialLineBuffer = new PartialLineBuffer();
+        char[] data = "Test1\r\nTest2\r\nTest3: ".toCharArray();
+        partialLineBuffer.addData(data, 0, data.length);
+
+        assertEquals(2, partialLineBuffer.getLines().size());
+        assertEquals("Test3: ", partialLineBuffer.getPartialLine(false));
+
+        assertEquals("Test1", partialLineBuffer.readLine());
+        assertEquals("Test2", partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.readLine());
+
+        assertEquals("Test3: ", partialLineBuffer.getPartialLine());
+        assertNull(partialLineBuffer.getPartialLine());
+    }
+
+    public void testPartialLineBufferFull3() {
+        PartialLineBuffer partialLineBuffer = new PartialLineBuffer();
+        char[] data = "Test1\nTest2\nTest3: ".toCharArray();
+        partialLineBuffer.addData(data, 0, data.length);
+
+        assertEquals(2, partialLineBuffer.getLines().size());
+        assertEquals("Test3: ", partialLineBuffer.getPartialLine(false));
+
+        assertEquals("Test1", partialLineBuffer.readLine());
+        assertEquals("Test2", partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.readLine());
+
+        assertEquals("Test3: ", partialLineBuffer.getPartialLine());
+        assertNull(partialLineBuffer.getPartialLine());
+    }
+
+    public void testPartialLineBufferEmptyLine() {
+        PartialLineBuffer partialLineBuffer = new PartialLineBuffer();
+        char[] data = "Test1\nTest2\n".toCharArray();
+        partialLineBuffer.addData(data, 0, data.length);
+
+        assertEquals(2, partialLineBuffer.getLines().size());
+        assertNull(partialLineBuffer.getPartialLine());
+
+        assertEquals("Test1", partialLineBuffer.readLine());
+        assertEquals("Test2", partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.readLine());
+
+        data = "\r\n".toCharArray();
+        partialLineBuffer.addData(data, 0, data.length);
+        assertEquals("", partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.getPartialLine());
+
+        data = "\r\n\r\n".toCharArray();
+        partialLineBuffer.addData(data, 0, data.length);
+        assertEquals("", partialLineBuffer.readLine());
+        assertEquals("", partialLineBuffer.readLine());
+        assertNull(partialLineBuffer.getPartialLine());
+    }
+}
