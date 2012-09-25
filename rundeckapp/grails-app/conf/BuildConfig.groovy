508b6eb5ec67900599714dbfcd17a265b6b63504
John Burbridge <jburbridge@salesforce.com>
2012-09-25 13:51:46 -0700

    Adding files that were missed in the build refactor commit

--- rundeckapp/grails-app/conf/BuildConfig.groovy
+++ rundeckapp/grails-app/conf/BuildConfig.groovy
@@ -2,7 +2,7 @@ grails.project.dependency.resolution = {
     inherits "global" // inherit Grails' default dependencies
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
-    	mavenCentral()
+        mavenCentral()
         grailsPlugins()
         grailsHome()
         grailsCentral()
@@ -26,10 +26,24 @@ grails.project.dependency.resolution = {
     }
 
     dependencies {
-        build 'org.yaml:snakeyaml:1.9','org.apache.ant:ant:1.7.1','org.apache.ant:ant-jsch:1.7.1','com.jcraft:jsch:0.1.45','log4j:log4j:1.2.16','commons-collections:commons-collections:3.2.1','commons-codec:commons-codec:1.5'
-        test 'org.yaml:snakeyaml:1.9','org.apache.ant:ant:1.7.1','org.apache.ant:ant-jsch:1.7.1','com.jcraft:jsch:0.1.45','log4j:log4j:1.2.16','commons-collections:commons-collections:3.2.1','commons-codec:commons-codec:1.5'
-        compile 'org.yaml:snakeyaml:1.9','org.apache.ant:ant:1.7.1','org.apache.ant:ant-jsch:1.7.1','com.jcraft:jsch:0.1.45','log4j:log4j:1.2.16','commons-collections:commons-collections:3.2.1','commons-codec:commons-codec:1.5'
-        runtime 'org.yaml:snakeyaml:1.9', 'org.apache.ant:ant:1.7.1', 'org.apache.ant:ant-launcher:1.7.1','org.apache.ant:ant-jsch:1.7.1','com.jcraft:jsch:0.1.45', 'org.springframework:spring-test:3.0.5.RELEASE','log4j:log4j:1.2.16'    ,'commons-collections:commons-collections:3.2.1','commons-codec:commons-codec:1.5'
+        
+        build 'org.yaml:snakeyaml:1.9', 'org.apache.ant:ant:1.7.1', 'org.apache.ant:ant-jsch:1.7.1', 
+              'com.jcraft:jsch:0.1.45', 'log4j:log4j:1.2.16', 'commons-collections:commons-collections:3.2.1',
+              'commons-codec:commons-codec:1.5'
+              
+        test 'org.yaml:snakeyaml:1.9', 'org.apache.ant:ant:1.7.1', 'org.apache.ant:ant-jsch:1.7.1', 
+             'com.jcraft:jsch:0.1.45', 'log4j:log4j:1.2.16', 'commons-collections:commons-collections:3.2.1', 
+             'commons-codec:commons-codec:1.5'
+             
+        compile 'org.yaml:snakeyaml:1.9', 'org.apache.ant:ant:1.7.1', 'org.apache.ant:ant-jsch:1.7.1', 
+                'com.jcraft:jsch:0.1.45','log4j:log4j:1.2.16','commons-collections:commons-collections:3.2.1',
+                'commons-codec:commons-codec:1.5'
+                
+        runtime 'org.yaml:snakeyaml:1.9', 'org.apache.ant:ant:1.7.1', 'org.apache.ant:ant-launcher:1.7.1',
+                'org.apache.ant:ant-jsch:1.7.1','com.jcraft:jsch:0.1.45', 'org.springframework:spring-test:3.0.5.RELEASE',
+                'log4j:log4j:1.2.16' ,'commons-collections:commons-collections:3.2.1','commons-codec:commons-codec:1.5',
+                "com.dtolabs.rundeck:rundeck-core:${app.version}"
     }
+    
     grails.plugin.location.'webrealms' = "webrealms"
 }
