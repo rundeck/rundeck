aeef0f8583ee0d71a57d5109b9a8eedeacc98ab2
Greg Schueler <greg.schueler@gmail.com>
2013-01-21 19:44:44 -0800

    Add script for use by jenkins builds

---
+++ jenkins-build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+# script for use by jenkins to build rundeck
+# arguments: ['release' [tag]]
+# if the first argument is 'release', a release build will be made, with an optional release tag
+
+REL="${1:-no}"
+RTAG="${2:-GA}"
+
+export BUILD_ROOT=$WORKSPACE/build_root
+
+
+echo "RTAG: ${RTAG}"
+
+make clean
+make TAG=${RTAG} docs
+
+if [ "$REL" == "release" ] ; then
+    ./gradlew -Penvironment=release -PreleaseTag=${RTAG} build
+    groovy testbuild.groovy -gradle -Drelease -DreleaseTag=${RTAG}
+else
+    ./gradlew -Penvironment=build build
+    groovy testbuild.groovy -gradle
+fi
+
+make TAG=${RTAG} rpm deb
