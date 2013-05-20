32eb95a86323f422b2581a514d9fe452d094e097
Greg Schueler <greg.schueler@gmail.com>
2013-05-20 14:52:10 -0700

    Add note about vagrant issue workaround

--- test/vagrant/yum-aws-ci/run-vagrant-test.sh
+++ test/vagrant/yum-aws-ci/run-vagrant-test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+#Note: due to a bug/issue with vagrant: https://github.com/mitchellh/vagrant-aws/issues/72
+#   the vagrant up step might fail, when cloud-init doesn't finish before vagrant rsync starts.
+#   The sleep and subsequent vagrant provision should then succeed
+
 set -x
 
 vagrant up --provider aws --no-provision
