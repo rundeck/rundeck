1898a00fcc0b5e76071b52c86331c7f29e3d0a98
Greg Schueler <greg.schueler@gmail.com>
2013-05-20 16:52:41 -0700

    Add missing expect script for api tests

---
+++ test/api/api-expect-exec-success.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# usage:
+#  api-expect-exec-success.sh ID [message]
+# tests an execution status is succeeded
+
+execid="$1"
+shift
+expectstatus=${1:-succeeded}
+shift
+
+# arg to include.sh
+set -- -
+
+DIR=$(cd `dirname $0` && pwd)
+source $DIR/include.sh
+
+
+# now submit req
+runurl="${APIURL}/execution/${execid}"
+
+
+params=""
+
+# get listing
+docurl ${runurl}?${params} > $DIR/curl.out
+if [ 0 != $? ] ; then
+    errorMsg "ERROR: failed query request ${runurl}?${params}"
+    exit 2
+fi
+
+sh $SRC_DIR/api-test-success.sh $DIR/curl.out || exit 2
+
+#Check projects list
+itemcount=$($XMLSTARLET sel -T -t -v "/result/executions/@count" $DIR/curl.out)
+assert "1" "$itemcount" "execution count should be 1"
+status=$($XMLSTARLET sel -T -t -v "//execution[@id=$execid]/@status" $DIR/curl.out)
+assert "$expectstatus" "$status" "execution status should be succeeded"
+
+exit 0
