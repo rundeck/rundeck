47869f0245963649371a370eee05eccec50ed709
Greg Schueler <greg.schueler@gmail.com>
2013-08-12 16:39:28 -0700

    Update events script for webrealms

--- rundeckapp/webrealms/scripts/Events.groovy
+++ rundeckapp/webrealms/scripts/Events.groovy
@@ -1,27 +1,31 @@
-import org.mortbay.jetty.Server
-import org.mortbay.jetty.security.UserRealm
+import org.eclipse.jetty.plus.jaas.JAASLoginService
+import org.eclipse.jetty.server.Server;
 
-eventConfigureJetty = {jetty ->
+eventConfigureJetty = { Server server ->
+    println "Jetty server: ${server.class.name}"
     def config = getConfig()
     if (config && config.server) {
-        if(config.server.addrealm && config.server.addrealm.classname){
+        if (config.server.addrealm && config.server.addrealm.classname) {
             def Object o
-            try{
+            try {
                 Class clz = Class.forName(config.server.addrealm.classname)
                 o = clz.getConstructor(new Class[0]).newInstance(new Object[0])
 //                if(o instanceof org.mortbay.jetty.plus.jaas.JAASUserRealm){
 //                    def org.mortbay.jetty.plus.jaas.JAASUserRealm realm = (org.mortbay.jetty.plus.jaas.JAASUserRealm)o
 //                    realm.setCallbackHandlerClass("org.mortbay.jetty.plus.jaas.callback.DefaultCallbackHandler")
 //                }
-                if(o instanceof UserRealm){
+                if (o instanceof JAASLoginService) {
                     //configure properties
-                    def UserRealm realm = (UserRealm)o
+                    def JAASLoginService realm = (JAASLoginService) o
                     realm.setName(config.server.addrealm.name)
                     realm.setLoginModuleName(config.server.addrealm.LoginModuleName)
-                    jetty.addUserRealm realm
+                    server.addBean(realm)
+                    System.err.println("Added JAAS login service: ${o} (${config.server.addrealm.name} ${config.server.addrealm.LoginModuleName}")
+                }else{
+                    System.err.println "DID NOT ADD JAAS login service: ${o}"
                 }
-            }catch(Exception e){
-                println "Failed to add user realm: ${e}"
+            } catch (Exception e) {
+                System.err.println "Failed to add login service: ${e}"
                 throw e
             }
 
@@ -30,31 +34,31 @@ eventConfigureJetty = {jetty ->
 }
 
 getConfig = {
-        ClassLoader parent = getClass().getClassLoader()
-        GroovyClassLoader loader = new GroovyClassLoader(parent)
+    ClassLoader parent = getClass().getClassLoader()
+    GroovyClassLoader loader = new GroovyClassLoader(parent)
 
-        def config
+    def config
+
+    try {
+        def defaultConfigFile = loader.loadClass("DefaultWebrealmsConfig")
+        //log.info("Loading default config file: "+defaultConfigFile)
+        config = new ConfigSlurper().parse(defaultConfigFile)
 
         try {
-            def defaultConfigFile = loader.loadClass("DefaultWebrealmsConfig")
-            //log.info("Loading default config file: "+defaultConfigFile)
-            config = new ConfigSlurper().parse(defaultConfigFile)
-
-            try {
-                def appConfigFile = loader.loadClass("WebrealmsConfig")
-                //log.info("Found application config file: "+appConfigFile)
-                def appConfig = new ConfigSlurper().parse(appConfigFile)
-                if (appConfig) {
-                    //log.info("Merging application config file: "+appConfigFile)
-                    config = config.merge(appConfig)
-                }
-            } catch(ClassNotFoundException e) {
-                //log.warn("Did not find application config file: "+APP_CONFIG_FILE)
+            def appConfigFile = loader.loadClass("WebrealmsConfig")
+            //log.info("Found application config file: "+appConfigFile)
+            def appConfig = new ConfigSlurper().parse(appConfigFile)
+            if (appConfig) {
+                //log.info("Merging application config file: "+appConfigFile)
+                config = config.merge(appConfig)
             }
-        } catch(ClassNotFoundException e) {
-            //log.error("Did not find default config file: "+DEFAULT_CONFIG_FILE)
+        } catch (ClassNotFoundException e) {
+            //log.warn("Did not find application config file: "+APP_CONFIG_FILE)
         }
-
-        config?.webrealms
-
+    } catch (ClassNotFoundException e) {
+        //log.error("Did not find default config file: "+DEFAULT_CONFIG_FILE)
     }
+
+    config?.webrealms
+
+}
