83ccb6936e1a2873020d9c95b0f38dabe17d4e5c
Greg Schueler <greg.schueler@gmail.com>
2013-08-12 10:30:50 -0700

    Add missing api test script

---
+++ test/api/test-jobs-import-yaml-project.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+#test /api/jobs/import using yaml formatted content, project parameter
+
+DIR=$(cd `dirname $0` && pwd)
+source $DIR/include.sh
+
+args="echo hello there"
+
+project=$2
+if [ "" == "$2" ] ; then
+    project="test"
+fi
+
+#escape the string for xml
+xmlargs=$($XMLSTARLET esc "$args")
+xmlproj=$($XMLSTARLET esc "$project")
+
+#produce job.xml content corresponding to the dispatch request
+cat > $DIR/temp.out <<END
+- 
+  loglevel: INFO
+  sequence:
+    keepgoing: false
+    strategy: node-first
+    commands:
+    - exec: echo hello there
+  description: 'using project parameter'
+  name: import yaml job with project
+END
+
+# now submit req
+runurl="${APIURL}/jobs/import"
+
+echo "TEST: import RunDeck Jobs in yaml format (multipart file)"
+
+params="format=yaml&project=$project"
+
+# specify the file for upload with curl, named "xmlBatch"
+ulopts="-F xmlBatch=@$DIR/temp.out"
+
+# get listing
+docurl $ulopts  ${runurl}?${params} > $DIR/curl.out
+if [ 0 != $? ] ; then
+    errorMsg "ERROR: failed query request"
+    exit 2
+fi
+
+sh $SRC_DIR/api-test-success.sh $DIR/curl.out || exit 2
+
+#result will contain list of failed and succeeded jobs, in this
+#case there should only be 1 failed or 1 succeeded since we submit only 1
+
+failedcount=$($XMLSTARLET sel -T -t -v "/result/failed/@count" $DIR/curl.out)
+succount=$($XMLSTARLET sel -T -t -v "/result/succeeded/@count" $DIR/curl.out)
+skipcount=$($XMLSTARLET sel -T -t -v "/result/skipped/@count" $DIR/curl.out)
+
+if [ "1" != "$succount" ] ; then
+    errorMsg  "Upload was not successful."
+    echo $($XMLSTARLET sel -T -t -m "/result/failed" -v "job/error" $DIR/curl.out)
+    exit 2
+else
+    echo "OK"
+fi
+
+#test form data instead of multipart
+
+echo "TEST: import RunDeck Jobs in yaml format (urlencode)"
+
+params="format=yaml&project=$project"
+
+# specify the file for upload with curl, named "xmlBatch"
+ulopts="--data-urlencode xmlBatch@$DIR/temp.out"
+
+# get listing
+docurl $ulopts  ${runurl}?${params} > $DIR/curl.out
+if [ 0 != $? ] ; then
+    errorMsg "ERROR: failed query request"
+    exit 2
+fi
+
+sh $SRC_DIR/api-test-success.sh $DIR/curl.out || exit 2
+
+#result will contain list of failed and succeeded jobs, in this
+#case there should only be 1 failed or 1 succeeded since we submit only 1
+
+failedcount=$($XMLSTARLET sel -T -t -v "/result/failed/@count" $DIR/curl.out)
+succount=$($XMLSTARLET sel -T -t -v "/result/succeeded/@count" $DIR/curl.out)
+skipcount=$($XMLSTARLET sel -T -t -v "/result/skipped/@count" $DIR/curl.out)
+
+if [ "1" != "$succount" ] ; then
+    errorMsg  "Upload was not successful."
+    exit 2
+else
+    echo "OK"
+fi
+
+
+#rm $DIR/curl.out
+rm $DIR/temp.out
+
