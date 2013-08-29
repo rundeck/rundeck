6d8758e4e318abc279d8321b634a47b094fd3e88
Greg Schueler <greg.schueler@gmail.com>
2013-08-28 19:07:37 -0700

    Don't use mem for dev db

--- rundeckapp/grails-app/conf/DataSource.groovy
+++ rundeckapp/grails-app/conf/DataSource.groovy
@@ -14,7 +14,7 @@ environments {
 	development {
 		dataSource {
 			dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-            url = "jdbc:h2:mem:devDb"
+            url = "jdbc:h2:file:db/devDb"
         }
 	}
 	test {
