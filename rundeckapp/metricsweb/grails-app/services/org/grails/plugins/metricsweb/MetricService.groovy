744975e55f784020bec1463d510b0ed5ed63ca02
Greg Schueler <greg.schueler@gmail.com>
2013-10-21 17:24:27 -0700

    Remove methods using reflectionutils

--- rundeckapp/metricsweb/grails-app/services/org/grails/plugins/metricsweb/MetricService.groovy
+++ rundeckapp/metricsweb/grails-app/services/org/grails/plugins/metricsweb/MetricService.groovy
@@ -9,9 +9,6 @@ class MetricService {
     static transactional = false
     def metricRegistry
 
-    def Metric meter(String metricName) {
-        meter(ReflectionUtils.getCallingClass(0).name, metricName)
-    }
 
     def Metric meter(String classname, String metricName) {
         metricRegistry.meter(MetricRegistry.name(classname, metricName))
@@ -21,16 +18,8 @@ class MetricService {
         metricRegistry.timer(MetricRegistry.name(classname, metricName))
     }
 
-    def Timer timer(String metricName) {
-        timer(ReflectionUtils.getCallingClass(0).name, metricName)
-    }
-
     def Counter counter(String classname, String metricName) {
         metricRegistry.counter(MetricRegistry.name(classname, metricName))
-    }
-
-    def markMeter(String metricName) {
-        markMeter(ReflectionUtils.getCallingClass(0).name, metricName)
     }
 
     def markMeter(String classname, String metricName) {
@@ -41,28 +30,15 @@ class MetricService {
         counter(classname, metricName)?.inc()
     }
 
-    def incCounter(String metricName) {
-        incCounter(ReflectionUtils.getCallingClass(0).name, metricName)
-    }
 
     def decCounter(String classname, String metricName) {
         counter(classname, metricName)?.dec()
     }
 
-    def decCounter(String metricName) {
-        decCounter(ReflectionUtils.getCallingClass(0).name, metricName)
-    }
 
     def withTimer(String classname, String name, Closure clos) {
         timer(classname, name).time(clos)
     }
 
-    def withTimer(String name, Closure clos) {
-        withTimer(ReflectionUtils.getCallingClass(0).name, name, clos)
-    }
-
-    def withTimer(Closure clos) {
-        withTimer(ReflectionUtils.getCallingClass(0).name, 'Timer', clos)
-    }
 
 }
