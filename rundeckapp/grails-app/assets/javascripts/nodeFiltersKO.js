59d065231e4f54d7397426092479243bce690a0d
Greg Schueler <greg.schueler@gmail.com>
2014-01-07 17:19:25 -0800

    Add node filters knockout js

---
+++ rundeckapp/grails-app/assets/javascripts/nodeFiltersKO.js
@@ -0,0 +1,58 @@
+/*
+ Copyright 2014 SimplifyOps Inc, <http://simplifyops.com>
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+function NodeFilters(baseRunUrl, baseSaveJobUrl, data) {
+    var self = this;
+    self.baseRunUrl = baseRunUrl;
+    self.baseSaveJobUrl = baseSaveJobUrl;
+    self.filterName = ko.observable(data.filterName);
+    self.filter = ko.observable(data.filter);
+    self.total = ko.observable();
+    self.allcount = ko.observable();
+    self.nodesTitle = ko.computed(function () {
+        return self.allcount() == 1 ?
+            data.nodesTitleSingular || 'Node' :
+            data.nodesTitlePlural || 'Nodes';
+    });
+    self.filterAll = ko.observable(data.filterAll);
+    self.filterWithoutAll = ko.computed({
+        read: function () {
+            if (self.filterAll() && self.filter() == '.*') {
+                return '';
+            }
+            return self.filter();
+        },
+        write: function (value) {
+            self.filter(value);
+        },
+        owner: this
+    });
+    self.hasNodes = ko.computed(function () {
+        return 0 != self.allcount();
+    });
+    self.runCommand = function () {
+        document.location = _genUrl(self.baseRunUrl, {
+            filter: self.filter(),
+            filterName: self.filterName()
+        });
+    };
+    self.saveJob = function () {
+        document.location = _genUrl(baseSaveJobUrl, {
+            filter: self.filter(),
+            filterName: self.filterName()
+        });
+    };
+}
