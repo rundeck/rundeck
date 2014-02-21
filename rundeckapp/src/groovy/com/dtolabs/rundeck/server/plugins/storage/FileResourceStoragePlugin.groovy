d1c039540921e49efa850a396cf2d1f17c5e5dd9
Greg Schueler <greg.schueler@gmail.com>
2014-02-21 12:59:06 -0800

    Rename package

--- rundeckapp/src/groovy/com/dtolabs/rundeck/server/plugins/storage/FileResourceStoragePlugin.groovy
+++ rundeckapp/src/groovy/com/dtolabs/rundeck/server/plugins/storage/FileResourceStoragePlugin.groovy
@@ -1,25 +1,27 @@
-package com.dtolabs.rundeck.server.plugins.resourcetree
+package com.dtolabs.rundeck.server.plugins.storage
 
 import com.dtolabs.rundeck.core.plugins.Plugin
 import com.dtolabs.rundeck.core.storage.ResourceMeta
+import com.dtolabs.rundeck.core.storage.ResourceUtil
 import com.dtolabs.rundeck.plugins.ServiceNameConstants
 import com.dtolabs.rundeck.plugins.descriptions.PluginProperty
 import com.dtolabs.rundeck.plugins.storage.ResourceStoragePlugin
 import org.rundeck.storage.api.Tree
 import org.rundeck.storage.data.file.FileTreeUtil
+import org.rundeck.storage.impl.BaseDelegateTree
+import org.rundeck.storage.impl.DelegateTree
 
 /**
  * FileResourceStoragePlugin is ...
  * @author greg
  * @since 2014-02-19
  */
-@Plugin(name = "file", service = ServiceNameConstants.ResourceStorage)
-class FileResourceStoragePlugin implements ResourceStoragePlugin {
+@Plugin(name = FileResourceStoragePlugin.PROVIDER_NAME, service = ServiceNameConstants.ResourceStorage)
+class FileResourceStoragePlugin extends DelegateTree<ResourceMeta> implements ResourceStoragePlugin {
+    public static final String PROVIDER_NAME = "file"
     @PluginProperty(description = "Base directory", required = true)
     String baseDir
 
-
-    @Delegate
     Tree<ResourceMeta> delegate
 
     public Tree<ResourceMeta> getDelegate() {
@@ -28,7 +30,7 @@ class FileResourceStoragePlugin implements ResourceStoragePlugin {
                 throw new IllegalArgumentException("baseDir is not set")
             }
             def file = new File(baseDir)
-            delegate = FileTreeUtil.forRoot(file,)
+            delegate = FileTreeUtil.forRoot(file,ResourceUtil.factory())
         }
         return delegate;
     }
