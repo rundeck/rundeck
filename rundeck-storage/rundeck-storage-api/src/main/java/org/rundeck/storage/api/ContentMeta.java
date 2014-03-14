2021aba23a8a0329f89fd6df7f1f4d53ff685d19
Greg Schueler <greg.schueler@gmail.com>
2014-03-14 15:30:30 -0700

    Add javadoc

--- rundeck-storage/rundeck-storage-api/src/main/java/org/rundeck/storage/api/ContentMeta.java
+++ rundeck-storage/rundeck-storage-api/src/main/java/org/rundeck/storage/api/ContentMeta.java
@@ -5,10 +5,19 @@ import java.io.InputStream;
 import java.util.Map;
 
 /**
- * $INTERFACE is ... User: greg Date: 2/18/14 Time: 12:18 PM
+ * Represents a stream of data to store and metadata about it.
  */
 public interface ContentMeta {
+    /**
+     * Return the metadata about the content
+     * @return the metadata
+     */
     Map<String, String> getMeta();
 
+    /**
+     * Return the content stream
+     * @return the content stream
+     * @throws IOException
+     */
     InputStream readContent() throws IOException;
 }
