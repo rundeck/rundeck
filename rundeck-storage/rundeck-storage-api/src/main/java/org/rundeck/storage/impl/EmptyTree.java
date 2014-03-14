690844c4a58ce8e181fcc64a92ccada42d371249
Greg Schueler <greg.schueler@gmail.com>
2014-03-14 15:22:04 -0700

    Add unchecked exception type to storage api

--- rundeck-storage/rundeck-storage-api/src/main/java/org/rundeck/storage/impl/EmptyTree.java
+++ rundeck-storage/rundeck-storage-api/src/main/java/org/rundeck/storage/impl/EmptyTree.java
@@ -3,11 +3,12 @@ package org.rundeck.storage.impl;
 import org.rundeck.storage.api.ContentMeta;
 import org.rundeck.storage.api.Path;
 import org.rundeck.storage.api.Resource;
+import org.rundeck.storage.api.StorageException;
 
 import java.util.Set;
 
 /**
- * $INTERFACE is ... User: greg Date: 2/18/14 Time: 2:17 PM
+ * Empty tree which has no content and throws exceptions.
  */
 public class EmptyTree<T extends ContentMeta> extends StringToPathTree<T> {
     @Override
@@ -27,41 +28,41 @@ public class EmptyTree<T extends ContentMeta> extends StringToPathTree<T> {
 
     @Override
     public Resource<T> getPath(Path path) {
-        throw new IllegalArgumentException("Empty: no resource or directory for path: " + path);
+        throw StorageException.readException(path, "Empty: no resource or directory for path: " + path);
     }
 
     @Override
     public Resource<T> getResource(Path path) {
-        throw new IllegalArgumentException("Empty: no resource for path: " + path);
+        throw StorageException.readException(path, "Empty: no resource for path: " + path);
     }
 
     @Override
     public Set<Resource<T>> listDirectoryResources(Path path) {
-        throw new IllegalArgumentException("Empty: no resource for path: " + path);
+        throw StorageException.listException(path, "Empty: no resource for path: " + path);
     }
 
     @Override
     public Set<Resource<T>> listDirectory(Path path) {
-        throw new IllegalArgumentException("Empty: no resource for path: " + path);
+        throw StorageException.listException(path, "Empty: no resource for path: " + path);
     }
 
     @Override
     public Set<Resource<T>> listDirectorySubdirs(Path path) {
-        throw new IllegalArgumentException("Empty: no resource for path: " + path);
+        throw StorageException.listException(path, "Empty: no resource for path: " + path);
     }
 
     @Override
     public boolean deleteResource(Path path) {
-        throw new IllegalArgumentException("Empty: no resource for path: " + path);
+        throw StorageException.deleteException(path, "Empty: no resource for path: " + path);
     }
 
     @Override
     public Resource<T> createResource(Path path, T content) {
-        throw new IllegalArgumentException("Empty: cannot create resource");
+        throw StorageException.createException(path, "Empty: cannot create resource: " + path);
     }
 
     @Override
     public Resource<T> updateResource(Path path, T content) {
-        throw new IllegalArgumentException("Empty: no resource for path: " + path);
+        throw StorageException.updateException(path, "Empty: no resource for path: " + path);
     }
 }
