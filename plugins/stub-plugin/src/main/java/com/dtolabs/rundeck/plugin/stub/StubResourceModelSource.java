48034faadd65efb449a5a726f3280cc0d70cd5cb
Greg Schueler <greg.schueler@gmail.com>
2014-09-17 16:41:52 -0700

    Add a stub resource model generator plugin

---
+++ plugins/stub-plugin/src/main/java/com/dtolabs/rundeck/plugin/stub/StubResourceModelSource.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2014 SimplifyOps Inc, <http://simplifyops.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dtolabs.rundeck.plugin.stub;
+
+import com.dtolabs.rundeck.core.common.INodeSet;
+import com.dtolabs.rundeck.core.common.NodeEntryImpl;
+import com.dtolabs.rundeck.core.common.NodeSetImpl;
+import com.dtolabs.rundeck.core.resources.ResourceModelSource;
+import com.dtolabs.rundeck.core.resources.ResourceModelSourceException;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Properties;
+
+/**
+ * StubResourceModelSource is ...
+ *
+ * @author Greg Schueler <greg@simplifyops.com>
+ * @since 2014-09-03
+ */
+public class StubResourceModelSource implements ResourceModelSource {
+    Properties configuration;
+    int count;
+    String prefix;
+    String suffix;
+    String tags;
+
+    public StubResourceModelSource(Properties configuration) {
+        this.configuration = configuration;
+        count = Integer.parseInt(configuration.getProperty("count"));
+        prefix = configuration.getProperty("prefix");
+        suffix = configuration.getProperty("suffix");
+        tags = configuration.getProperty("tags");
+    }
+
+    @Override
+    public INodeSet getNodes() throws ResourceModelSourceException {
+        NodeSetImpl iNodeEntries = new NodeSetImpl();
+        for(int i=0;i<count;i++){
+            NodeEntryImpl nodeEntry = new NodeEntryImpl();
+            nodeEntry.setNodename((prefix != null ? prefix : "node") + "-" + i  + (suffix != null ? suffix : ""));
+            nodeEntry.setHostname((prefix != null ? prefix : "") + "host" + (suffix != null ? suffix : ""));
+            nodeEntry.setUsername((prefix != null ? prefix : "") + "user" + (suffix != null ? suffix : ""));
+            nodeEntry.setAttribute("node-executor", "stub");
+            nodeEntry.setAttribute("file-copier", "stub");
+            if(null!=tags){
+                nodeEntry.setTags(new HashSet(Arrays.asList(tags.split("\\s*,\\s*"))));
+            }
+
+            iNodeEntries.putNode(nodeEntry);
+        }
+        return iNodeEntries;
+    }
+}
---
+++ plugins/stub-plugin/src/main/java/com/dtolabs/rundeck/plugin/stub/StubResourceModelSourceFactory.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2014 SimplifyOps Inc, <http://simplifyops.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dtolabs.rundeck.plugin.stub;
+
+import com.dtolabs.rundeck.core.plugins.Plugin;
+import com.dtolabs.rundeck.core.plugins.configuration.*;
+import com.dtolabs.rundeck.core.resources.ResourceModelSource;
+import com.dtolabs.rundeck.core.resources.ResourceModelSourceFactory;
+import com.dtolabs.rundeck.plugins.ServiceNameConstants;
+import com.dtolabs.rundeck.plugins.util.DescriptionBuilder;
+import com.dtolabs.rundeck.plugins.util.PropertyBuilder;
+
+import java.util.Properties;
+
+/**
+ * StubResourceModelSourceFactory is ...
+ *
+ * @author Greg Schueler <greg@simplifyops.com>
+ * @since 2014-09-03
+ */
+@Plugin(name = StubResourceModelSourceFactory.SERVICE_PROVIDER_NAME, service = ServiceNameConstants.ResourceModelSource)
+public class StubResourceModelSourceFactory implements ResourceModelSourceFactory, Describable{
+    @Override
+    public ResourceModelSource createResourceModelSource(Properties configuration) throws ConfigurationException {
+        return new StubResourceModelSource(configuration);
+    }
+
+
+    static final Description DESC;
+
+    public static final String SERVICE_PROVIDER_NAME = "stub";
+
+    static {
+        DescriptionBuilder builder = DescriptionBuilder.builder();
+        builder.name(SERVICE_PROVIDER_NAME)
+                .title("Stub")
+                .description("Generates stub nodes with stub node executors, useful for testing")
+        ;
+
+        builder.property(PropertyUtil.string("prefix", "Prefix",
+                "Name prefix for all nodes",
+                true, "node"));
+        builder.property(PropertyUtil.string("suffix", "Suffix",
+                "Name suffix for all nodes",
+                false, null));
+        builder.property(PropertyUtil.integer("count", "Count",
+                "Number of nodes",
+                true, "1"));
+        builder.property(PropertyUtil.string("tags", "Tags",
+                "Comma separated tags to add to all nodes",
+                false, "stub"));
+
+        builder.mapping("prefix", "plugin.ResourceModelSource.stub.prefix");
+        builder.mapping("suffix", "plugin.ResourceModelSource.stub.suffix");
+        builder.mapping("count", "plugin.ResourceModelSource.stub.count");
+        builder.mapping("tags", "plugin.ResourceModelSource.stub.tags");
+
+        DESC = builder.build();
+    }
+
+
+    public Description getDescription() {
+        return DESC;
+    }
+}
