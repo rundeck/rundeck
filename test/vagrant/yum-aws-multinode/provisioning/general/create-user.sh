db956b437b87af6ce07e65551b4317f543ec1527
Greg Schueler <greg.schueler@gmail.com>
2014-11-05 12:10:52 -0800

    Fix ssh user home dir

--- test/vagrant/yum-aws-multinode/provisioning/general/create-user.sh
+++ test/vagrant/yum-aws-multinode/provisioning/general/create-user.sh
@@ -7,6 +7,8 @@ set -eu
 RUNDECK_USER=$1
 HOME_DIR=$2
 
-if ! id $RUNDECK_USER
-then useradd -d $HOME_DIR -m $RUNDECK_USER
+if ! id $RUNDECK_USER ; then
+    useradd -d $HOME_DIR -m $RUNDECK_USER
+    echo "Added user $RUNDECK_USER home dir $HOME_DIR"
+    id $RUNDECK_USER
 fi
