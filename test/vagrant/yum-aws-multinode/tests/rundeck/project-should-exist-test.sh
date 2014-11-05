917f9ab72203082fdbf67d3f723aeceb12f4b162
Greg Schueler <greg.schueler@gmail.com>
2014-11-05 11:41:58 -0800

    node list test

--- test/vagrant/yum-aws-multinode/tests/rundeck/project-should-exist-test.sh
+++ test/vagrant/yum-aws-multinode/tests/rundeck/project-should-exist-test.sh
@@ -36,14 +36,11 @@ it_should_contain_remote_node() {
     # Count the lines in the raw output to strip off the queue and follow link lines.
     size=${#rawout[@]}
 
-    # Create an array by slicing the lines with the command ouput.
-    eval cmdout=( ${rawout[@]:2:$size} )
-
-    # There should be two lines, one for each uname response.
-    if ! test 1 = ${#cmdout[*]}
+    # There should be 1 line for the node.
+    if ! test 1 = ${size}
     then
-        echo "FAIL: output did not contain 1 node. Contained ${#cmdout[*]}"
-        echo cmdout was: ${cmdout[@]}
+        echo "FAIL: output did not contain 1 node. Contained ${#rawout[*]}"
+        echo rawout was: ${rawout[@]}
         exit 1
     fi
     
