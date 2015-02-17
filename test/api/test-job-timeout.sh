3480a0236e78988cb1ae709491711353927719a8
Greg Schueler <greg.schueler@gmail.com>
2015-02-16 17:29:32 -0800

    functional tests: job timeout, retry, combo

---
+++ test/api/test-job-timeout.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+
+#test  /api/job/{id}/run
+
+DIR=$(cd `dirname $0` && pwd)
+source $DIR/include.sh
+
+###
+# setup: create a new job and acquire the ID
+###
+
+# job exec
+args="echo hello there ; sleep 30"
+
+project=$2
+if [ "" == "$2" ] ; then
+    project="test"
+fi
+
+#escape the string for xml
+xmlargs=$($XMLSTARLET esc "$args")
+xmlproj=$($XMLSTARLET esc "$project")
+
+#produce job.xml content corresponding to the dispatch request
+cat > $DIR/temp.out <<END
+<joblist>
+   <job>
+      <name>cli job</name>
+      <group>api-test/job-run-timeout</group>
+      <description></description>
+      <loglevel>INFO</loglevel>
+      <timeout>3s</timeout>
+      <dispatch>
+        <threadcount>1</threadcount>
+        <keepgoing>true</keepgoing>
+      </dispatch>
+      <sequence>
+        <command>
+        <exec>$xmlargs</exec>
+        </command>
+      </sequence>
+   </job>
+</joblist>
+
+END
+
+# now submit req
+runurl="${APIURL}/jobs/import"
+
+params="project=$xmlproj"
+
+# specify the file for upload with curl, named "xmlBatch"
+ulopts="-F xmlBatch=@$DIR/temp.out"
+
+# get listing
+docurl $ulopts  ${runurl}?${params} > $DIR/curl.out
+if [ 0 != $? ] ; then
+    errorMsg "ERROR: failed query request"
+    exit 2
+fi
+
+sh $SRC_DIR/api-test-success.sh $DIR/curl.out || exit 2
+
+#result will contain list of failed and succeeded jobs, in this
+#case there should only be 1 failed or 1 succeeded since we submit only 1
+
+succount=$($XMLSTARLET sel -T -t -v "/result/succeeded/@count" $DIR/curl.out)
+jobid=$($XMLSTARLET sel -T -t -v "/result/succeeded/job/id" $DIR/curl.out)
+
+if [ "1" != "$succount" -o "" == "$jobid" ] ; then
+    errorMsg  "Upload was not successful."
+    exit 
+fi
+
+
+###
+# Run the chosen id, expect success message
+###
+
+echo "TEST: job/id/run should succeed"
+
+
+# now submit req
+runurl="${APIURL}/job/${jobid}/run"
+params=""
+execargs="-opt2 a"
+
+# get listing
+$CURL -H "$AUTHHEADER" --data-urlencode "argString=${execargs}" ${runurl}?${params} > $DIR/curl.out || fail "failed request: ${runurl}"
+
+sh $SRC_DIR/api-test-success.sh $DIR/curl.out || exit 2
+
+#get execid
+
+execcount=$($XMLSTARLET sel -T -t -v "/result/executions/@count" $DIR/curl.out)
+execid=$($XMLSTARLET sel -T -t -v "/result/executions/execution/@id" $DIR/curl.out)
+
+if [ "1" == "${execcount}" -a "" != "${execid}" ] ; then
+    :
+else
+    errorMsg "FAIL: expected run success message for execution id. (count: ${execcount}, id: ${execid})"
+    exit 2
+fi
+
+#wait for execution to complete
+
+rd-queue follow -q -e $execid || fail "Failed waiting for execution $execid to complete"
+
+# test execution status
+# 
+runurl="${APIURL}/execution/${execid}"
+
+params=""
+
+# get listing
+docurl ${runurl}?${params} > $DIR/curl.out
+if [ 0 != $? ] ; then
+    errorMsg "ERROR: failed query request"
+    exit 2
+fi
+
+sh $SRC_DIR/api-test-success.sh $DIR/curl.out || exit 2
+
+#Check projects list
+itemcount=$($XMLSTARLET sel -T -t -v "/result/executions/@count" $DIR/curl.out)
+assert "1" "$itemcount" "execution count should be 1"
+status=$($XMLSTARLET sel -T -t -v "/result/executions/execution/@status" $DIR/curl.out)
+assert "timedout" "$status" "execution status should be succeeded"
+
+echo "OK"
+
+
+rm $DIR/curl.out
+
