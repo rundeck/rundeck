2badd66dd3cc60c3c41fff2fab30288a9acb09c8
Greg Schueler <greg.schueler@gmail.com>
2015-03-03 11:45:51 -0800

    Use config API to modify project config in test

--- test/api/test-project-resources-refresh.sh
+++ test/api/test-project-resources-refresh.sh
@@ -3,7 +3,7 @@
 #test update project resources file
 
 DIR=$(cd `dirname $0` && pwd)
-export API_VERSION=2 #/api/2/project/NAME/resources/refresh
+#export API_VERSION=2 #/api/2/project/NAME/resources/refresh
 source $DIR/include.sh
 file=$DIR/curl.out
 
@@ -38,16 +38,41 @@ if [ ! -f $TRES.testbackup -a -f $TRES ] ; then
     cp $TRES $TRES.testbackup
 fi
 
-echo "project.resources.url=http://invaliddomain:1235/resources.xml" >> $TPROPS
+proj_config_url="${APIURL}/project/${proj}/config"
+delete_url_config(){
+	prop=$1
 
+	docurl -X DELETE  -D $DIR/headers.out "${proj_config_url}/${prop}" > $DIR/curl.out
+	if [ 0 != $? ] ; then
+	    errorMsg "ERROR: failed DELETE request"
+	    exit 2
+	fi
+	assert_http_status 204 $DIR/headers.out
+	rm $DIR/headers.out
+}
+set_url_config(){
+	prop=$1
+	value=$2
 
+	docurl -X PUT --data-binary "${value}" -H 'Content-Type:text/plain' "${proj_config_url}/${prop}" > $DIR/curl.out
+	if [ 0 != $? ] ; then
+	    errorMsg "ERROR: failed PUT request"
+	    exit 2
+	fi
+	#echo "project.resources.url=http://invaliddomain:1235/resources.xml" >> $TPROPS
+}
+
+set_url_config "project.resources.url" "http://invaliddomain:1235/resources.xml"
 docurl -X POST  ${runurl}?${params} > ${file} || fail "ERROR: failed request"
 
 sh $SRC_DIR/api-test-error.sh ${file} "Error updating node resources file for project test: com.dtolabs.rundeck.core.common.FileUpdaterException: java.net.UnknownHostException: invaliddomain" || exit 2
 
 echo "OK"
 
-cp $TPROPS.testbackup $TPROPS
+## reset property
+#cp $TPROPS.testbackup $TPROPS
+delete_url_config "project.resources.url"
+
 sleep 1
 echo "TEST: /api/2/project/${proj}/resources/refresh (valid temp URL)"
 
@@ -62,7 +87,8 @@ END
 
 TEMPURL="file://$TETC/testUpdateResources.xml"
 
-echo "project.resources.url=$TEMPURL" >> $TPROPS
+set_url_config "project.resources.url" "${TEMPURL}"
+#echo "project.resources.url=$TEMPURL" >> $TPROPS
 
 
 docurl -X POST  ${runurl}?${params} > ${file} || fail "ERROR: failed request"
@@ -71,18 +97,16 @@ sh $SRC_DIR/api-test-success.sh ${file} "Resources were successfully updated for
 
 echo "OK"
 
-#restore backup props, no provider url
-if [ -f $TPROPS.testbackup ] ; then
-    cp $TPROPS.testbackup $TPROPS
-fi
+delete_url_config "project.resources.url"
 
-sleep 1
+#sleep 1
 # set allowed URL with port 1235
-echo "project.resources.allowedURL.0=http://invaliddomain:1235/resources.xml" >> $TPROPS
+#echo "project.resources.allowedURL.0=http://invaliddomain:1235/resources.xml" >> $TPROPS
+set_url_config "project.resources.allowedURL.0" "http://invaliddomain:1235/resources.xml"
 
 echo "TEST: /api/2/project/${proj}/resources/refresh (POST) (not allowed provider URL)"
 
-#backup project.properties and set invalid resources url
+
 
 data="providerURL=http://invaliddomain:1234/resources.xml"
 
@@ -95,7 +119,6 @@ echo "OK"
 
 echo "TEST: /api/2/project/${proj}/resources/refresh (POST) (allowed URL, invalid hostname)"
 
-#backup project.properties and set invalid resources url
 
 data="providerURL=http://invaliddomain:1235/resources.xml"
 
@@ -124,9 +147,10 @@ echo "TEST: /api/2/project/${proj}/resources/refresh (POST) (valid provider URL)
 
 
 TEMPURL="file://$TETC/testUpdateResources.xml"
-sleep 1
+
 # set allowed URL for test file
-echo "project.resources.allowedURL.1=file://$TETC/testUpdateResources.xml" >> $TPROPS
+#echo "project.resources.allowedURL.1=file://$TETC/testUpdateResources.xml" >> $TPROPS
+set_url_config "project.resources.allowedURL.1" "file://$TETC/testUpdateResources.xml"
 
 data="providerURL=$TEMPURL"
 
@@ -138,9 +162,7 @@ sh $SRC_DIR/api-test-success.sh ${file} "Resources were successfully updated for
 echo "OK"
 
 #restore backup props, no provider url
-if [ -f $TPROPS.testbackup ] ; then
-    mv $TPROPS.testbackup $TPROPS
-fi
+delete_url_config "project.resources.url"
 
 if [ -f $TRES.testbackup ] ; then
     mv $TRES.testbackup $TRES
