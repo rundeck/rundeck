4a6f847f4e0459cf7441d15d0b6332cf441cb8cd
Greg Schueler <greg.schueler@gmail.com>
2015-04-09 16:49:41 -0700

    Use queue to process nodes on all threads

--- core/src/main/java/com/dtolabs/rundeck/core/execution/dispatch/OrchestratorNodeProcessor.java
+++ core/src/main/java/com/dtolabs/rundeck/core/execution/dispatch/OrchestratorNodeProcessor.java
@@ -20,12 +20,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 import com.dtolabs.rundeck.core.common.INodeEntry;
 import com.dtolabs.rundeck.core.execution.workflow.steps.node.NodeStepResult;
@@ -45,52 +40,94 @@ public class OrchestratorNodeProcessor {
     private final ExecutorService threadPool;
 
     private Set<INodeEntry> processedNodes;
-    
+    private BlockingQueue<Boolean> resultqueue;
+    private BlockingQueue<Entry> taskqueue ;
+
     public OrchestratorNodeProcessor(int threadCount, boolean keepgoing,
             Orchestrator orchestrator,
             Map<INodeEntry, Callable<NodeStepResult>> executions) {
         stop = false;
         this.threadCount = threadCount;
+        this.resultqueue = new LinkedBlockingQueue<>();
+        this.taskqueue = new LinkedBlockingQueue<>(threadCount);
         this.keepgoing = keepgoing;
         this.orchestrator = orchestrator;
         this.executions = executions;
 
         this.threadPool = Executors.newFixedThreadPool(this.threadCount);
-        
+
         this.processedNodes = Collections.newSetFromMap(new ConcurrentHashMap<INodeEntry, Boolean>());
 
     }
 
-    public void execute() throws ExecutionException{
-        List<OrchestratorRunnable> tasks = new ArrayList<OrchestratorRunnable>();
-        for (int i = 0; i < this.threadCount; i++) {
-            tasks.add(new OrchestratorRunnable());
+    public boolean execute() throws ExecutionException{
+        for (int i=0;i<threadCount;i++) {
+            threadPool.submit(new OrchestratorRunnable());
         }
+        boolean success=true;
         try {
-            for(Future<Boolean> future: threadPool.invokeAll(tasks)){
-                future.get();
+            int completedNodes=0;
+            while (completedNodes < executions.size() && !stop) {
+                try {
+                    Entry callable = getCallable();
+                    if (null != callable) {
+                        //node from orchestrator available, add it to queue, may block until runnable calls take()
+                        taskqueue.put(callable);
+                    } else if (completedNodes < processedNodes.size()) {
+                        //no nodes available, wait for previous tasks to complete
+                        if(!resultqueue.take()){
+                            success=false;
+                        }
+                        completedNodes++;
+                    }else{
+                        //no node available, and no nodes being processed
+                        break;
+                    }
+                } catch (DispatcherException e) {
+                    e.printStackTrace();
+                }
             }
         } catch (InterruptedException e) {
-            
+
         }finally{
+            //attempt to fill the queue to tell waiting threads to stop
+            int x=threadCount;
+            while (x > 0 && taskqueue.offer(new Entry(true))) {
+                x--;
+            }
             threadPool.shutdown();
         }
+        try {
+            threadPool.awaitTermination(60, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            threadPool.shutdownNow();
+        }
+
+        //stop indicates a node failed, but success might not
+        return !stop && success;
     }
 
     public class OrchestratorRunnable implements Callable<Boolean> {
-
         @Override
         public Boolean call() throws Exception {
-            
+            String originalname = Thread.currentThread().getName();
             while (!stop) {
+                boolean success=false;
                 Entry task = null;
+                Thread.currentThread().setName("OrchestratorNodeProcessor[take]");
                 try {
                     //make sure the get callable happens within the try catch so the finally will always be called
-                    task = getCallable();
-                    if(task == null) {
+                    task = taskqueue.take();
+                    if(task == null || task.finish) {
                         break;
                     }
+                    Thread.currentThread().setName(
+                            "OrchestratorNodeProcessor[running](node=" +
+                            task.node.getNodename() +
+                            ")"
+                    );
                     NodeStepResult result = task.callable.call();
+                    success=result.isSuccess();
                     if (!result.isSuccess() && !keepgoing) {
                         stop = true;
                         break;
@@ -101,9 +138,11 @@ public class OrchestratorNodeProcessor {
                         throw e;
                     }
                 } finally {
-                    if(task != null) {
+                    if(task != null && task.node!=null) {
                         returnNode(task.node);
                     }
+                    resultqueue.put(success);
+                    Thread.currentThread().setName(originalname);
                 }
             }
             return true;
@@ -115,11 +154,11 @@ public class OrchestratorNodeProcessor {
         if(node == null){
             return null;
         }
-        
+
         if(!this.processedNodes.add(node)){
             throw new DispatcherException("Can not process the same node twice " + node);
         }
-        
+
         Callable<NodeStepResult> callable = this.executions.get(node);
         return new Entry(node, callable);
     }
@@ -129,13 +168,21 @@ public class OrchestratorNodeProcessor {
     }
 
     public static class Entry {
+        private boolean finish;
         private final INodeEntry node;
         private final Callable<NodeStepResult> callable;
+
+        public Entry(final boolean finish) {
+            this.finish = finish;
+            this.node=null;
+            this.callable=null;
+        }
 
         public Entry(INodeEntry node, Callable<NodeStepResult> callable) {
             super();
             this.node = node;
             this.callable = callable;
+            this.finish=false;
         }
 
     }
