82e6752faed539d78ce6b0f595e98dd7267bf893
Greg Schueler <greg.schueler@gmail.com>
2015-06-19 16:06:39 -0700

    test node list and empty workflow for execution import

--- rundeckapp/test/unit/rundeck/ExecReportSpec.groovy
+++ rundeckapp/test/unit/rundeck/ExecReportSpec.groovy
@@ -92,4 +92,72 @@ class ExecReportSpec extends Specification {
         report.status=='succeed'
     }
 
+    def "node list split"(String succeeded, String failed, String result){
+        given:
+        def wf = new Workflow(commands: [new CommandExec(adhocRemoteString: "test exec")])
+        def exec = new Execution(
+                dateStarted: new Date(),
+                dateCompleted: new Date(),
+                failedNodeList: failed,
+                succeededNodeList: succeeded,
+                workflow: wf,
+                project: "test",
+                user: "user",
+                status: 'true'
+        ).save(flush:true)
+
+
+        when:
+        def report = ExecReport.fromExec(exec)
+
+        then:
+        exec!=null
+        report.jcExecId==exec.id.toString()
+        report.author=='user'
+        report.node==result
+        report.adhocExecution
+        report.adhocScript=='test exec'
+        report.ctxProject=='test'
+        report.status=='succeed'
+
+        where:
+        succeeded | failed | result
+        null | null | "0/0/0"
+        "node1" | null | "0/1/1"
+        "node1,node2" | null | "0/2/2"
+        null | "node1" | "1/0/1"
+        null | "node1,node2" | "2/0/2"
+        "node1" | "node2" | "1/1/2"
+        "node1,node2" | "node3" | "1/2/3"
+        "node1" | "node2,node3" | "2/1/3"
+    }
+
+    def "no commands"(){
+        given:
+        def wf = new Workflow()
+        def exec = new Execution(
+                dateStarted: new Date(),
+                dateCompleted: new Date(),
+                failedNodeList: null,
+                succeededNodeList: null,
+                workflow: wf,
+                project: "test",
+                user: "user",
+                status: 'true'
+        ).save(flush:true)
+
+
+        when:
+        def report = ExecReport.fromExec(exec)
+
+        then:
+        exec!=null
+        report.jcExecId==exec.id.toString()
+        report.author=='user'
+        report.adhocExecution
+        report.adhocScript==null
+        report.ctxProject=='test'
+        report.status=='succeed'
+        report.title=='[0 steps]'
+    }
 }
