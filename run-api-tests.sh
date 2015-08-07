bbffbd134ea926755b89607755810f6dffcd69e7
Greg Schueler <greg.schueler@gmail.com>
2015-08-07 11:11:04 -0700

    cleanup, add more cli options

--- run-api-tests.sh
+++ run-api-tests.sh
@@ -8,6 +8,9 @@
 set -euo pipefail
 IFS=$'\n\t'
 readonly ARGS=("$@")
+TESTS=yes
+WAIT=no
+CLEAN=yes
 
 usage() {
       grep '^#/' <"$0" | cut -c4- # prints the #/ lines above as usage info
@@ -18,6 +21,23 @@ die(){
 
 check_args(){
    : example to check args length
+
+   if [ ${#ARGS[@]} -gt 0 ] ; then
+	   for arg in "${ARGS[@]}"
+	   do
+	   		case $arg in
+			-notest)
+				TESTS=no
+				;;
+			-wait)
+				WAIT=yes
+				;;
+			-noclean)
+				CLEAN=no
+				;;
+			esac
+	   done
+   fi
 }
 wait_for(){
 	local FARGS=("$@")
@@ -40,12 +60,9 @@ wait_for(){
 start_rundeck(){
 	local FARGS=("$@")
 	local launcherJar=${FARGS[0]}
-	local outfile=${FARGS[1]}
-	(java -Xmx1024m -XX:MaxPermSize=256m -jar $launcherJar > $outfile 2>&1 ) &
+	java -Xmx1024m -XX:MaxPermSize=256m -jar $launcherJar 
 	# ( bash -c 'sleep 30; echo done; sleep 600' > $outfile 2>&1 ) &
-	local RDPID=$!
-
-	echo $RDPID
+	
 }
 stop_rundeck(){
 	local FARGS=("$@")
@@ -54,37 +71,84 @@ stop_rundeck(){
 	kill -9 $PID || echo "PID already stopped $PID"
 	trap "" EXIT SIGINT SIGTERM ERR
 }
-run_ci_test(){
+clean_dir(){
 	local FARGS=("$@")
+	local DIR=${FARGS[0]}
+
+	if [ $CLEAN == "yes" -a -d $DIR ] ; then
+		echo "Clean out existing test dir $DIR."
+		rm -rf $DIR
+	else
+		echo "Not cleaning dir $DIR."
+	fi
+
+	mkdir -p $DIR
+}
+copy_jar(){
+	local FARGS=("$@")
+	local DIR=${FARGS[0]}
 	local -a VERS=( $( rd_get_version ) )
 	local JAR=rundeck-launcher-${VERS[0]}-${VERS[2]}.jar
 	local buildJar=$PWD/rundeck-launcher/launcher/build/libs/$JAR
 	test -f $buildJar || die "Jar file not found $buildJar"
-	local SRC=$PWD
-	local DIR=$PWD/build/citest
 	mkdir -p $DIR
 	cp $buildJar $DIR/
-	local launcherJar=$DIR/$JAR
-	cd $DIR
+	echo $DIR/$JAR
+}
+run_tests(){
+	local FARGS=("$@")
+	local DIR=${FARGS[0]}
+	local SRC=${FARGS[1]}
+
 	local HOST=$(hostname)
-
-	echo "Start Rundeck"
-	# start rundeck
-	local RDPID=$( start_rundeck $launcherJar $DIR/rundeck.out )
-	trap "{ kill -9 $RDPID ; exit 255; }" EXIT SIGINT SIGTERM ERR
-	echo "Rundeck process PID: $RDPID"
-	wait_for $DIR/rundeck.out 'Started SelectChannelConnector@'
-	# wait_for $DIR/rundeck.out 'done'
-	echo "Rundeck started."
-
-	echo "Start tests"
 	RDECK_BASE=$DIR PATH=$PATH:$DIR/tools/bin  \
 		 bash -c  $SRC/test/src/test.sh http://$HOST:4440 admin admin
+}
+run_ci_test(){
+	local FARGS=("$@")
 
-	echo "Tests complete."
-	echo "Stop Rundeck"
-	stop_rundeck $RDPID
-	echo "Rundeck stopped."
+	local SRC=$PWD
+	local DIR=$PWD/build/citest
+
+	clean_dir $DIR
+
+	local launcherJar=$( copy_jar $DIR )
+
+	cd $DIR
+
+	echo "Start Rundeck"
+
+	# start rundeck
+
+	(java -Xmx1024m -XX:MaxPermSize=256m -jar $launcherJar > $DIR/rundeck.out 2>&1 ) &
+	local RDPID=$!
+
+	trap "{ kill -9 $RDPID ; echo Rundeck Killed ; exit 255; }" EXIT SIGINT SIGTERM ERR
+
+	echo "Rundeck process PID: $RDPID"
+
+	wait_for $DIR/rundeck.out 'Started SelectChannelConnector@'
+
+	echo "Rundeck started."
+
+	if [ $TESTS == "yes" ] ; then
+		echo "Start tests"
+
+		run_tests $DIR $SRC
+
+		echo "Tests complete."
+	else
+		echo "Tests skipped."
+	fi
+
+	if [ $WAIT == "yes" ] ; then
+		echo "Rundeck is now running on http://$(hostname):4440 . Type ctrl-c to stop."
+		wait $RDPID
+	else
+		echo "Stop Rundeck"
+		stop_rundeck $RDPID
+		echo "Rundeck stopped."
+	fi
 }
 
 
