caaafe187c823113d8b83b60519ae5dcfa1653dc
Greg Schueler <greg.schueler@gmail.com>
2015-08-28 09:00:05 -0700

    expand diff result api

--- core/src/main/java/com/dtolabs/rundeck/plugins/scm/ScmDiffResult.java
+++ core/src/main/java/com/dtolabs/rundeck/plugins/scm/ScmDiffResult.java
@@ -4,6 +4,28 @@ package com.dtolabs.rundeck.plugins.scm;
  * Created by greg on 8/25/15.
  */
 public interface ScmDiffResult {
+    /**
+     * @return true if there are differences between the source and target
+     */
+    boolean getModified();
+
+    /**
+     * @return true if the source was not found
+     */
+    boolean getSourceNotFound();
+
+    /**
+     * @return true if the target was not found
+     */
+    boolean getTargetNotFound();
+
+    /**
+     * @return content type
+     */
     String getContentType();
+
+    /**
+     * @return diff contents
+     */
     String getContent();
 }
