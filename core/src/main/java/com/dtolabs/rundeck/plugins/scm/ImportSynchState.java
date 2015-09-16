f64f17a1132bf8be1cd06b7ae5954a9137315b97
Greg Schueler <greg.schueler@gmail.com>
2015-09-16 11:35:18 -0700

    Add DELETE_NEEDED state for import synch

--- core/src/main/java/com/dtolabs/rundeck/plugins/scm/ImportSynchState.java
+++ core/src/main/java/com/dtolabs/rundeck/plugins/scm/ImportSynchState.java
@@ -4,5 +4,5 @@ package com.dtolabs.rundeck.plugins.scm;
  * SCM Import synch state
  */
 public enum ImportSynchState {
-    CLEAN, UNKNOWN, IMPORT_NEEDED,
+    CLEAN, UNKNOWN, IMPORT_NEEDED, DELETE_NEEDED
 }
