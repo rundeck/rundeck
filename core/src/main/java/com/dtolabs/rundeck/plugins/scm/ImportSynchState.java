69d722b14bf868e1ccb20826bddb925623cac273
Greg Schueler <greg.schueler@gmail.com>
2015-09-28 16:30:28 -0700

    Add REFRESH_NEEDED state

--- core/src/main/java/com/dtolabs/rundeck/plugins/scm/ImportSynchState.java
+++ core/src/main/java/com/dtolabs/rundeck/plugins/scm/ImportSynchState.java
@@ -4,5 +4,5 @@ package com.dtolabs.rundeck.plugins.scm;
  * SCM Import synch state
  */
 public enum ImportSynchState {
-    CLEAN, UNKNOWN, IMPORT_NEEDED, DELETE_NEEDED
+    CLEAN, UNKNOWN, REFRESH_NEEDED, IMPORT_NEEDED, DELETE_NEEDED
 }
