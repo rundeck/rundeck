3ac87e1ce00a5e5c46aa8172df35351b10987439
Greg Schueler <greg.schueler@gmail.com>
2015-10-07 14:08:41 -0700

    remove unused class, and import reference

--- rundeckapp/src/java/com/dtolabs/rundeck/server/plugins/services/ScmExportPluginProviderService.java
+++ rundeckapp/src/java/com/dtolabs/rundeck/server/plugins/services/ScmExportPluginProviderService.java
@@ -1,19 +1,7 @@
 package com.dtolabs.rundeck.server.plugins.services;
 
-import com.dtolabs.rundeck.core.execution.service.ExecutionServiceException;
-import com.dtolabs.rundeck.core.execution.service.ProviderCreationException;
-import com.dtolabs.rundeck.core.plugins.*;
-import com.dtolabs.rundeck.core.plugins.configuration.DescribableServiceUtil;
-import com.dtolabs.rundeck.core.plugins.configuration.Description;
 import com.dtolabs.rundeck.plugins.ServiceNameConstants;
-import com.dtolabs.rundeck.plugins.scm.PluginState;
-import com.dtolabs.rundeck.plugins.scm.ScmExportPlugin;
 import com.dtolabs.rundeck.plugins.scm.ScmExportPluginFactory;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by greg on 8/24/15.
