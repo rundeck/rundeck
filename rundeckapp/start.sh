b79bb47a5a7bad2a24bd60d192df60e935c01f8f
Greg Schueler <greg.schueler@gmail.com>
2015-10-07 10:29:50 -0700

    cleanup: remove inadvertent commit

--- rundeckapp/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export GRAILS_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -XX:MaxPermSize=256m -Xmx512m -Xms256m -server -Djava.security.auth.login.config=web-app/WEB-INF/jaas.conf -Drundeck.config.location=/Users/daplay/rundeck/rundeck-config.properties -Drdeck.base=/Users/daplay/rundeck/ "
-grails -reloading run-app
