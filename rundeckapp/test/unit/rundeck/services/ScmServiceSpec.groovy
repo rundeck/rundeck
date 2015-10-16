ef3ae2ed35399cf29ea96bff2e708d574952b32b
Greg Schueler <greg.schueler@gmail.com>
2015-10-16 10:35:16 -0700

    test plugin input validation

--- rundeckapp/test/unit/rundeck/services/ScmServiceSpec.groovy
+++ rundeckapp/test/unit/rundeck/services/ScmServiceSpec.groovy
@@ -1,6 +1,17 @@
 package rundeck.services
 
-import com.dtolabs.rundeck.core.common.IRundeckProject
+import com.dtolabs.rundeck.core.plugins.configuration.PropertyResolver
+import com.dtolabs.rundeck.core.plugins.configuration.PropertyScope
+import com.dtolabs.rundeck.core.plugins.configuration.Validator
+import com.dtolabs.rundeck.plugins.scm.ScmExportPlugin
+import com.dtolabs.rundeck.plugins.scm.ScmExportPluginFactory
+import com.dtolabs.rundeck.plugins.scm.ScmImportPlugin
+import com.dtolabs.rundeck.plugins.scm.ScmImportPluginFactory
+import com.dtolabs.rundeck.plugins.scm.ScmOperationContext
+import com.dtolabs.rundeck.plugins.scm.ScmPluginInvalidInput
+import com.dtolabs.rundeck.server.plugins.ValidatedPlugin
+import com.dtolabs.rundeck.server.plugins.services.ScmExportPluginProviderService
+import com.dtolabs.rundeck.server.plugins.services.ScmImportPluginProviderService
 import grails.test.mixin.TestFor
 import rundeck.services.scm.ScmPluginConfigData
 import spock.lang.Specification
@@ -93,6 +104,42 @@ class ScmServiceSpec extends Specification {
         2 * service.jobEventsService.removeListener(_)
         1 * service.pluginConfigService.removePluginConfiguration('test1', "etc/scm-import.properties")
         1 * service.pluginConfigService.removePluginConfiguration('test1', "etc/scm-export.properties")
+
+    }
+
+    def "validatePluginSetup"() {
+        given:
+        def config = [:]
+        service.pluginService = Mock(PluginService)
+        service.frameworkService = Mock(FrameworkService)
+        def resolver = Mock(PropertyResolver)
+        service.scmExportPluginProviderService = Mock(ScmExportPluginProviderService)
+        service.scmImportPluginProviderService = Mock(ScmImportPluginProviderService)
+
+        def validated = new ValidatedPlugin(valid: false)
+
+        when:
+        def result = service.validatePluginSetup(integration, 'test', 'type', config)
+
+
+        then:
+
+        1 * service.frameworkService.getFrameworkPropertyResolver('test', config) >> resolver
+        1 * service.pluginService.validatePlugin(
+                'type',
+                integration == ScmService.EXPORT ? service.scmExportPluginProviderService :
+                        service.scmImportPluginProviderService,
+                resolver,
+                PropertyScope.Instance,
+                PropertyScope.Project
+        ) >>
+                validated
+        result == validated
+
+        where:
+        integration       | _
+        ScmService.EXPORT | _
+        ScmService.IMPORT | _
 
     }
 }
