facd0733809e74ecc813a5f4f9d5620191fd5943
Greg Schueler <greg.schueler@gmail.com>
2015-10-29 11:48:13 -0700

    test plugin enable

--- test/api/test-scm-git-plugin-setup.sh
+++ test/api/test-scm-git-plugin-setup.sh
@@ -204,7 +204,7 @@ do_setup_export_json_valid(){
 	local plugin=$2
 	local project=$3
 
-	ENDPOINT="${APIURL}/project/$proj/scm/$integration/plugin/$plugin/setup"
+	ENDPOINT="${APIURL}/project/$project/scm/$integration/plugin/$plugin/setup"
 
 	TMPDIR=`tmpdir`
 	dirname=$TMPDIR/testdir
@@ -244,6 +244,7 @@ test_setup_export_json_valid(){
 	local integration=$1
 	local plugin=$2
 	local project=$3
+	ENDPOINT="${APIURL}/project/$project/scm/$integration/plugin/$plugin/setup"
 	test_begin "Setup SCM Export: JSON"
 	do_setup_export_json_valid $integration $plugin $project
 	test_succeed
@@ -265,6 +266,23 @@ disable_plugin_xml(){
 	assert_xml_value "true" '/scmActionResult/success' $DIR/curl.out
 
 }
+
+enable_plugin_xml(){
+	local integration=$1
+	local plugin=$2
+	local project=$3
+
+	METHOD=POST
+	ACCEPT=application/xml
+	ENDPOINT="${APIURL}/project/$project/scm/$integration/plugin/$plugin/enable"
+
+	api_request $ENDPOINT $DIR/curl.out
+
+	$SHELL $SRC_DIR/api-test-success.sh $DIR/curl.out || exit 2
+
+	assert_xml_value "true" '/scmActionResult/success' $DIR/curl.out
+
+}
 disable_plugin_json(){
 	local integration=$1
 	local plugin=$2
@@ -278,15 +296,74 @@ disable_plugin_json(){
 
 	assert_json_value "true" '.success' $DIR/curl.out
 }
+enable_plugin_json(){
+	local integration=$1
+	local plugin=$2
+	local project=$3
+
+	METHOD=POST
+	ACCEPT=application/json
+	ENDPOINT="${APIURL}/project/$project/scm/$integration/plugin/$plugin/enable"
+
+	api_request $ENDPOINT $DIR/curl.out
+
+	assert_json_value "true" '.success' $DIR/curl.out
+}
+assert_plugin_enabled(){
+	local integration=$1
+	local plugin=$2
+	local value=$3
+	local project=$4
+
+	ENDPOINT="${APIURL}/project/$project/scm/$integration/plugins"
+	ACCEPT=application/xml
+
+	api_request $ENDPOINT $DIR/curl.out
+
+	$SHELL $SRC_DIR/api-test-success.sh $DIR/curl.out || exit 2
+
+	#Check projects list
+	assert_xml_value $integration '/scmPluginList/integration' $DIR/curl.out
+
+	assert_xml_value "$value" "/scmPluginList/plugins/scmPluginDescription[type=\"$plugin\"]/enabled" $DIR/curl.out
+}
 
 test_disable_export_json(){
 	local project=$1
 	
-	test_begin "Disable plugin"
 
 	do_setup_export_json_valid "export" "git-export" $project
+
+	assert_plugin_enabled "export" "git-export" "true" $project
+	
+	ENDPOINT="${APIURL}/project/$project/scm/export/plugin/git-export/disable"
+	test_begin "Disable plugin JSON"
 	
 	disable_plugin_json "export" "git-export" $project
+	
+	assert_plugin_enabled "export" "git-export" "false" $project
+	
+	test_succeed
+}
+test_disable_enable_export_json(){
+	local project=$1
+	
+
+	do_setup_export_json_valid "export" "git-export" $project
+
+	assert_plugin_enabled "export" "git-export" "true" $project
+	
+	disable_plugin_json "export" "git-export" $project
+	
+	assert_plugin_enabled "export" "git-export" "false" $project
+	
+
+	ENDPOINT="${APIURL}/project/$project/scm/export/plugin/git-export/disable"
+	test_begin "Enable plugin JSON"
+
+	enable_plugin_json "export" "git-export" $project
+	
+	assert_plugin_enabled "export" "git-export" "true" $project
 	
 	test_succeed
 }
@@ -294,11 +371,41 @@ test_disable_export_json(){
 test_disable_export_xml(){
 	local project=$1
 	
-	test_begin "Disable plugin"
 
 	do_setup_export_json_valid "export" "git-export" $project
+
+	assert_plugin_enabled "export" "git-export" "true" $project
+	
+
+	ENDPOINT="${APIURL}/project/$project/scm/export/plugin/git-export/disable"
+	test_begin "Disable plugin XML"
+
+	disable_plugin_xml "export" "git-export" $project
+
+	assert_plugin_enabled "export" "git-export" "false" $project
+	
+	test_succeed
+}
+
+test_disable_enable_export_xml(){
+	local project=$1
+	
+
+	do_setup_export_json_valid "export" "git-export" $project
+
+	assert_plugin_enabled "export" "git-export" "true" $project
 	
 	disable_plugin_xml "export" "git-export" $project
+
+	assert_plugin_enabled "export" "git-export" "false" $project
+	
+
+	ENDPOINT="${APIURL}/project/$project/scm/export/plugin/git-export/enable"
+	test_begin "Enable plugin XML"
+
+	enable_plugin_xml "export" "git-export" $project
+
+	assert_plugin_enabled "export" "git-export" "true" $project
 	
 	test_succeed
 }
@@ -331,6 +438,16 @@ main(){
 	create_project "testscm4"
 	test_disable_export_xml "testscm4"
 	remove_project "testscm4"
+
+	# disable using json
+	create_project "testscm5"
+	test_disable_enable_export_json "testscm5"
+	remove_project "testscm5"
+	
+	# disable using xml
+	create_project "testscm6"
+	test_disable_enable_export_xml "testscm6"
+	remove_project "testscm6"
 }
 
 main
