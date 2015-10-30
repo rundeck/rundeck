400179988b866ac1bf77c18163776e8d49f2fed5
Greg Schueler <greg.schueler@gmail.com>
2015-10-30 12:14:01 -0700

    fixup

---
+++ rundeckapp/src/groovy/com/dtolabs/rundeck/app/api/scm/JobReference.groovy
@@ -0,0 +1,10 @@
+package com.dtolabs.rundeck.app.api.scm
+
+/**
+ * Created by greg on 10/30/15.
+ */
+class JobReference {
+    String jobId
+    String jobName
+    String groupPath
+}
---
+++ rundeckapp/src/groovy/com/dtolabs/rundeck/app/api/scm/ScmProjectActionInput.groovy
@@ -0,0 +1,10 @@
+package com.dtolabs.rundeck.app.api.scm
+
+/**
+ * Created by greg on 10/30/15.
+ */
+class ScmProjectActionInput extends ScmActionInput {
+
+    List<ScmImportActionItem> importItems
+    List<ScmExportActionItem> exportItems
+}
