317cc786514520d628824772b6727f972406eb9a
Greg Schueler <greg.schueler@gmail.com>
2016-01-05 16:19:06 -0800

    test DELETE execution 404

---
+++ test/api/test-execution-delete-404.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#test output from /api/execution/{id}/abort
+# test 404 response
+
+DIR=$(cd `dirname $0` && pwd)
+source $DIR/include.sh
+
+execid="000"
+TITLE="DELETE execution/ID not found"
+
+test_execution_notfound_xml(){
+
+	####
+	# Test: request with 404 response
+	####
+
+
+	ENDPOINT="$APIURL/execution/$execid"
+	ACCEPT=application/xml
+	METHOD=DELETE
+	EXPECT_STATUS=404
+
+	test_begin "$TITLE (xml)"
+
+	api_request $ENDPOINT $DIR/curl.out
+
+	assert_xml_valid $DIR/curl.out
+
+	test_succeed
+
+}
+
+test_execution_notfound_json(){
+
+	ENDPOINT="$APIURL/execution/$execid"
+	ACCEPT=application/json
+	METHOD=DELETE
+	EXPECT_STATUS=404
+
+	test_begin "$TITLE (json)"
+
+	api_request $ENDPOINT $DIR/curl.out
+
+	assert_json_value "api.error.item.doesnotexist" ".errorCode" $DIR/curl.out
+
+	test_succeed
+}
+
+main(){
+	test_execution_notfound_xml
+	test_execution_notfound_json
+}
+main
