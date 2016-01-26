e3ce7261c17e2480844121e1c3af65e4bf28817e
Greg Schueler <greg.schueler@gmail.com>
2016-01-26 12:02:48 -0800

    cleanup

--- rundeckapp/grails-app/assets/javascripts/ko/binding-popover.js
+++ rundeckapp/grails-app/assets/javascripts/ko/binding-popover.js
@@ -4,7 +4,6 @@
  */
 ko.bindingHandlers.bootstrapPopover = {
     init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
-        console.log("bootstrapPopover init for elem:",jQuery(element),allBindings.get('bootstrapPopoverContentRef'));
         if(allBindings.get('bootstrapPopoverContentRef')){
             _initPopoverContentRef(null,{element:element,contentRef:ko.unwrap(allBindings.get('bootstrapPopoverContentRef'))});
         }else if (allBindings.get('bootstrapPopoverContentFor')) {
