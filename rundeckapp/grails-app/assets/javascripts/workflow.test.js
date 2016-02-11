c43e9f595112a53ffd45826b2241e9b4d020bf9a
Greg Schueler <greg.schueler@gmail.com>
2016-02-11 12:27:30 -0800

    fix #1673 handle plugin metadata missing for step info

--- rundeckapp/grails-app/assets/javascripts/workflow.test.js
+++ rundeckapp/grails-app/assets/javascripts/workflow.test.js
@@ -36,6 +36,41 @@ RDWorkflow.test = function () {
     console.assert(RDWorkflow.cleanContextId('1/2/3')==='1/2/3','wrong value');
     console.assert(RDWorkflow.cleanContextId('1e@abc/2/3')==='1/2/3','wrong value');
     console.assert(RDWorkflow.cleanContextId('1/2e@asdf=xyz/3')==='1/2/3','wrong value');
+
+    //render string, with descriptions
+    var wf1=new RDWorkflow([{"type":"example-node-step","nodeStep":true,"configuration":{"example":"whatever"}}],{
+        nodeSteppluginDescriptions:{
+            "example-node-step":{
+                "title":"blah"
+            }
+        },
+        wfSteppluginDescriptions:{}
+    });
+    console.assert(wf1.renderContextString("1") === "blah");
+
+    var wf2=new RDWorkflow([{"type":"example-node-step","nodeStep":false,"configuration":{"example":"whatever"}}],{
+        nodeSteppluginDescriptions:{},
+        wfSteppluginDescriptions:{
+            "example-node-step":{
+                "title":"blah"
+            }
+        }
+    });
+    console.assert(wf2.renderContextString("1") === "blah");
+
+    //render string, missing descriptions
+    var wf3=new RDWorkflow([{"type":"example-node-step","nodeStep":true,"configuration":{"example":"whatever"}}],{
+        nodeSteppluginDescriptions:{},
+        wfSteppluginDescriptions:{}
+    });
+    console.assert(wf3.renderContextString("1") === "Plugin example-node-step");
+
+    var wf4=new RDWorkflow([{"type":"example-node-step","nodeStep":false,"configuration":{"example":"whatever"}}],{
+        nodeSteppluginDescriptions:{},
+        wfSteppluginDescriptions:{}
+    });
+    console.assert(wf4.renderContextString("1") === "Plugin example-node-step");
+
 };
 
 jQuery(function () {
