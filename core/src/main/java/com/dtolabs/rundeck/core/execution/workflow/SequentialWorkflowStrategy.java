55aa87962f8f103150c02e1d771505d11927b073
Greg Schueler <greg.schueler@gmail.com>
2016-05-11 11:20:37 -0700

    plugin description

--- core/src/main/java/com/dtolabs/rundeck/core/execution/workflow/SequentialWorkflowStrategy.java
+++ core/src/main/java/com/dtolabs/rundeck/core/execution/workflow/SequentialWorkflowStrategy.java
@@ -18,7 +18,7 @@ import static com.dtolabs.rundeck.core.execution.workflow.EngineWorkflowExecutor
  * Created by greg on 5/5/16.
  */
 @Plugin(name = "sequential", service = ServiceNameConstants.WorkflowStrategy)
-@PluginDescription(title = "Sequential", description = "Run each step in order")
+@PluginDescription(title = "Sequential", description = "Run each step in order. Execute a step on all nodes before proceeding to the next step")
 
 public class SequentialWorkflowStrategy implements WorkflowStrategy {
     public static String PROVIDER_NAME = "sequential";
