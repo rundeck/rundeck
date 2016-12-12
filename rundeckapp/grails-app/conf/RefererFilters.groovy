5e0d35da86d15a4344834f45b6fe12edbef3757b
jtobard <jaime.tobar.diaz@gmail.com>
2016-12-12 12:17:59 -0300

    unused import and development println

--- rundeckapp/grails-app/conf/RefererFilters.groovy
+++ rundeckapp/grails-app/conf/RefererFilters.groovy
@@ -1,7 +1,4 @@
-import org.codehaus.groovy.grails.commons.GrailsApplication
-
 class RefererFilters {
-    // referer must match serverURL, optionally https
     def grailsApplication
 
     def filters = {
@@ -9,22 +6,18 @@ class RefererFilters {
             before = {
                 def csrf = grailsApplication.config.rundeck.security.csrf
                 if(csrf && csrf != 'NONE'){
-                    println(csrf)
-                    //grailsApplication.config.rundeck.clusterMode.enabled
                     if(csrf == 'POST') {
                         if (request.method.toUpperCase() == "POST") {
-                            println('filter post')
+                            // referer must match serverURL, optionally https
                             def validRefererPrefix = "^${grailsApplication.config.grails.serverURL}".replace("http", "https?")
                             def referer = request.getHeader('Referer')
                             return referer && referer =~ validRefererPrefix
                         }
                     }else if(csrf == '*'){
-                        println('filter anything')
                         def validRefererPrefix = "^${grailsApplication.config.grails.serverURL}".replace("http", "https?")
                         def referer = request.getHeader('Referer')
                         if(request.method.toUpperCase() == 'GET' && (referer == null || referer.endsWith("user/login"))) {
                             //excluding the user/login screen
-                            println('return true')
                             return true
                         }else{
                             return referer && referer =~ validRefererPrefix
