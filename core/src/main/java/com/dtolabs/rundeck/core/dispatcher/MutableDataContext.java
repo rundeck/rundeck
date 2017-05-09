551b7124ae30b8735c4e90b2cfdcf541f5dbcfc1
Greg Schueler <greg.schueler@gmail.com>
2017-05-09 14:36:23 -0700

    Add utility methods for interface

--- core/src/main/java/com/dtolabs/rundeck/core/dispatcher/MutableDataContext.java
+++ core/src/main/java/com/dtolabs/rundeck/core/dispatcher/MutableDataContext.java
@@ -1,8 +1,31 @@
 package com.dtolabs.rundeck.core.dispatcher;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by greg on 5/26/16.
  */
 public interface MutableDataContext extends DataContext, Mergable<DataContext> {
     void merge(DataContext item);
+
+    /**
+     * Put value
+     *
+     * @param group
+     * @param key
+     * @param value
+     *
+     * @return
+     */
+    public default String put(String group, String key, String value) {
+        return group(group).put(key, value);
+    }
+
+    default Map<String, String> group(final String group) {
+        if (null == get(group)) {
+            put(group, new HashMap<>());
+        }
+        return get(group);
+    }
 }
