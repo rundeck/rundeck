d748fb9e925d3ac6d01e38a122e8b52cd86c65f6
Greg Schueler <greg.schueler@gmail.com>
2017-06-15 15:07:25 -0700

    cleanup

--- rundeckapp/grails-app/services/rundeck/controllers/MenuService.groovy
+++ rundeckapp/grails-app/services/rundeck/controllers/MenuService.groovy
@@ -23,7 +23,7 @@ class MenuService implements RundeckProjectConfigurable {
                 values("projectList", "projectHome")
                 labels([projectList: "Projects List", projectHome: "Project Home Page"])
                 required(false)
-                defaultValue 'false'
+                defaultValue null
             }.build(),
 
     ]
