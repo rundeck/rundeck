83d44e16f1b49792c8c4f1be5dee8d3a215bffe7
jtobard <jaime.tobar.diaz@gmail.com>
2017-06-20 13:05:31 -0400

    on retryAttemp, initial delay to start

--- rundeckapp/grails-app/services/rundeck/services/ExecutionService.groovy
+++ rundeckapp/grails-app/services/rundeck/services/ExecutionService.groovy
@@ -1668,6 +1668,7 @@ class ExecutionService implements ApplicationContextAware, StepExecutor, NodeSte
                                       timeout:params.timeout?:null,
                                       retryAttempt:params.retryAttempt?:0,
                                       retry:params.retry?:null,
+                                      retryDelay: params.retryDelay?:null,
                                       serverNodeUUID: frameworkService.getServerUUID()
             )
 
@@ -1983,7 +1984,8 @@ class ExecutionService implements ApplicationContextAware, StepExecutor, NodeSte
                 'argString',
                 'timeout',
                 'orchestrator',
-                'retry'
+                'retry',
+                'retryDelay'
         ]
         propset.each{k->
             props.put(k,se[k])
@@ -2040,6 +2042,12 @@ class ExecutionService implements ApplicationContextAware, StepExecutor, NodeSte
             //replace data references
             if (optparams) {
                 props.timeout = DataContextUtils.replaceDataReferencesInString(props.timeout, DataContextUtils.addContext("option", optparams, null))
+            }
+        }
+        if (props.retryDelay?.contains('${')) {
+            //replace data references
+            if (optparams) {
+                props.retryDelay = DataContextUtils.replaceDataReferences(props.retryDelay, DataContextUtils.addContext("option", optparams, null))
             }
         }
 
