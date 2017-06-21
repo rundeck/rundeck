f09e8a6167857a1e2019877796ec782f8e93fcca
Greg Schueler <greg.schueler@gmail.com>
2017-06-21 09:41:49 -0700

    plugin name update: Data Node Step

--- plugins/stub-plugin/src/main/java/com/dtolabs/rundeck/plugin/stub/StubDataNodeStep.java
+++ plugins/stub-plugin/src/main/java/com/dtolabs/rundeck/plugin/stub/StubDataNodeStep.java
@@ -40,7 +40,7 @@ import static com.dtolabs.rundeck.plugin.stub.StubDataStep.parseData;
  */
 
 @Plugin(service = ServiceNameConstants.WorkflowNodeStep, name = StubDataNodeStep.PROVIDER_NAME)
-@PluginDescription(title = "Stub Node Step", description = "Produce data values for a node")
+@PluginDescription(title = "Data Node Step", description = "Produce data values for a node")
 public class StubDataNodeStep implements NodeStepPlugin {
     public static final String PROVIDER_NAME = "stub-node-step";
 
