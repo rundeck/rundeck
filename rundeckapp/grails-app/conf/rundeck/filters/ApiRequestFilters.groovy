ca81b251b4c1442339c4e58bba81eef6a4ce8fe5
Greg Schueler <greg.schueler@gmail.com>
2017-09-05 15:17:20 -0700

    Update api to v21, document removed API endpoints

--- rundeckapp/grails-app/conf/rundeck/filters/ApiRequestFilters.groovy
+++ rundeckapp/grails-app/conf/rundeck/filters/ApiRequestFilters.groovy
@@ -64,15 +64,16 @@ public class ApiRequestFilters {
     public static final int V18 = 18
     public static final int V19 = 19
     public static final int V20 = 20
+    public static final int V21 = 21
     public static final Map VersionMap = [:]
-    public static final List Versions = [V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14, V15, V16, V17, V18, V19, V20]
+    public static final List Versions = [V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14, V15, V16, V17, V18, V19, V20, V21]
     static {
         Versions.each { VersionMap[it.toString()] = it }
     }
     public static final Set VersionStrings = new HashSet(VersionMap.values())
 
     public final static int API_EARLIEST_VERSION = V1
-    public final static int API_CURRENT_VERSION = V20
+    public final static int API_CURRENT_VERSION = V21
     public final static int API_MIN_VERSION = API_EARLIEST_VERSION
     public final static int API_MAX_VERSION = API_CURRENT_VERSION
 
