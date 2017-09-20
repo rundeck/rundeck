a4577ae7c491b2d44f3a8aa820397a408b9f446f
Greg Schueler <greg.schueler@gmail.com>
2017-09-20 10:24:21 -0700

    Add tests for acl authorization

--- rundeckapp/test/unit/rundeck/controllers/MenuControllerSpec.groovy
+++ rundeckapp/test/unit/rundeck/controllers/MenuControllerSpec.groovy
@@ -16,16 +16,28 @@
 
 package rundeck.controllers
 
+import com.dtolabs.rundeck.app.support.ProjAclFile
+import com.dtolabs.rundeck.app.support.SaveProjAclFile
+import com.dtolabs.rundeck.app.support.SaveSysAclFile
+import com.dtolabs.rundeck.app.support.SysAclFile
+import com.dtolabs.rundeck.core.authorization.AuthorizationUtil
 import com.dtolabs.rundeck.core.authorization.UserAndRolesAuthContext
+import com.dtolabs.rundeck.core.common.IRundeckProject
+import com.dtolabs.rundeck.server.authorization.AuthConstants
 import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
+import org.codehaus.groovy.grails.web.servlet.mvc.SynchronizerTokensHolder
 import rundeck.CommandExec
 import rundeck.ScheduledExecution
 import rundeck.Workflow
 import rundeck.services.ApiService
+import rundeck.services.AuthorizationService
 import rundeck.services.FrameworkService
 import rundeck.services.ScheduledExecutionService
 import spock.lang.Specification
+import spock.lang.Unroll
+
+import javax.servlet.http.HttpServletResponse
 
 /**
  * Created by greg on 3/15/16.
@@ -191,6 +203,133 @@ class MenuControllerSpec extends Specification {
         1 * controller.frameworkService.authorizeProjectJobAll(_,job2,['read'],'AProject')>>true
         1 * controller.frameworkService.isClusterModeEnabled()>>true
         1 * controller.apiService.renderSuccessXml(_,_,_)
+
+    }
+
+    protected void setupFormTokens(params) {
+        def token = SynchronizerTokensHolder.store(session)
+        params[SynchronizerTokensHolder.TOKEN_KEY] = token.generateToken('/test')
+        params[SynchronizerTokensHolder.TOKEN_URI] = '/test'
+    }
+
+    @Unroll
+    def "required auth for acl action #action"() {
+        given:
+        controller.frameworkService = Mock(FrameworkService)
+        controller.authorizationService = Mock(AuthorizationService)
+        when:
+        if (paramvals) {
+            params.putAll(paramvals)
+        }
+        if (method) {
+            request.method = method
+            setupFormTokens(params)
+        }
+        def result = arg ? controller."$action"(arg) : controller."$action"()
+        then:
+
+        response.status == HttpServletResponse.SC_FORBIDDEN
+        result == null
+        1 * controller.frameworkService.authorizeApplicationResourceAny(_, resource, actions) >> false
+        if (paramvals?.project) {
+            1 * controller.frameworkService.authResourceForProjectAcl(paramvals.project) >>
+                    AuthorizationUtil.resource(AuthConstants.TYPE_PROJECT_ACL, [name: paramvals.project])
+        }
+        controller.authorizationService.existsPolicyFile(_) >> storageExists
+        controller.frameworkService.getFrameworkProject(_) >> Mock(IRundeckProject) {
+            existsFileResource(_) >> storageExists
+        }
+        where:
+        method | action                 |
+                actions             |
+                arg                                                                                         |
+                paramvals             |
+                resource                               |
+                storageExists
+        null   | 'acls'                 |
+                ['read', 'admin']   |
+                null                                                                                        |
+                null                  |
+                [kind: 'system_acl', type: 'resource']     |
+                false
+        null   | 'editSystemAclFile'    |
+                ['update', 'admin'] |
+                new SysAclFile(id: 'test.aclpolicy', fileType: 'fs')                                        |
+                null                  |
+                [kind: 'system_acl', type: 'resource'] |
+                false
+        'POST' | 'saveSystemAclFile'    |
+                ['update', 'admin'] |
+                new SaveSysAclFile(id: 'test.aclpolicy', fileType: 'storage', create: false, fileText: 'z') |
+                null                  |
+                [kind: 'system_acl', type: 'resource'] |
+                false
+        'POST' | 'saveSystemAclFile'    |
+                ['create', 'admin'] |
+                new SaveSysAclFile(id: 'test.aclpolicy', fileType: 'storage', create: true, fileText: 'z')  |
+                null                  |
+                [kind: 'system_acl', type: 'resource'] |
+                true
+        'POST' | 'deleteSystemAclFile'  |
+                ['delete', 'admin'] |
+                new SysAclFile(id: 'test.aclpolicy', fileType: 'storage')                                   |
+                null                  |
+                [kind: 'system_acl', type: 'resource'] |
+                true
+        'POST' | 'deleteSystemAclFile'  |
+                ['delete', 'admin'] |
+                new SysAclFile(id: 'test.aclpolicy', fileType: 'fs')                                        |
+                null                  |
+                [kind: 'system_acl', type: 'resource'] |
+                true
+        null   | 'createSystemAclFile'  |
+                ['create', 'admin'] |
+                null                                                                                        |
+                [fileType: 'storage'] |
+                [kind: 'system_acl', type: 'resource'] |
+                true
+        null   | 'createSystemAclFile'  |
+                ['create', 'admin'] |
+                null                                                                                        |
+                [fileType: 'fs']      |
+                [kind: 'system_acl', type: 'resource'] |
+                true
+        null   | 'projectAcls'          |
+                ['read', 'admin']   |
+                null                                                                                        |
+                [project: 'a']        |
+                [name: 'a', type: 'project_acl']       |
+                false
+        null   | 'editProjectAclFile'   |
+                ['update', 'admin'] |
+                new ProjAclFile(id: 'test.aclpolicy')                                                       |
+                [project: 'a']        |
+                [name: 'a', type: 'project_acl']       |
+                false
+        'POST' | 'saveProjectAclFile'   |
+                ['update', 'admin'] |
+                new SaveProjAclFile(id: 'test.aclpolicy', create: false, fileText: 'x')                     |
+                [project: 'a']        |
+                [name: 'a', type: 'project_acl']       |
+                false
+        'POST' | 'saveProjectAclFile'   |
+                ['create', 'admin'] |
+                new SaveProjAclFile(id: 'test.aclpolicy', create: true, fileText: 'x')                      |
+                [project: 'a']        |
+                [name: 'a', type: 'project_acl']       |
+                true
+        null   | 'createProjectAclFile' |
+                ['create', 'admin'] |
+                null                                                                                        |
+                [project: 'a']        |
+                [name: 'a', type: 'project_acl']       |
+                false
+        'POST' | 'deleteProjectAclFile' |
+                ['delete', 'admin'] |
+                new ProjAclFile(id: 'test.aclpolicy')                                                       |
+                [project: 'a']        |
+                [name: 'a', type: 'project_acl']       |
+                false
 
     }
 }
