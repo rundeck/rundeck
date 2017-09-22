0da70e7adf9db5169dbb43a5f70c1b35e6c5610c
jtobard <jaime.tobar.diaz@gmail.com>
2017-09-22 12:45:33 -0300

    remove println

--- rundeckapp/grails-app/controllers/rundeck/controllers/MenuController.groovy
+++ rundeckapp/grails-app/controllers/rundeck/controllers/MenuController.groovy
@@ -353,10 +353,10 @@ class MenuController extends ControllerBase implements ApplicationContextAware{
                     }
                     if(results.nextExecutions?.get(se.id)){
                         data.nextScheduledExecution=results.nextExecutions?.get(se.id)
-                        if(query.daysAhead) {
+                        if(query.daysAhead != null) {
                             def maxSched = results.nextExecutions?.max{it.value}
                             def to = new Date() + query.daysAhead
-                            if(to>maxSched.value){
+                            if(to<maxSched.value){
                                 to = maxSched.value
                             }
                             data.futureScheduledExecutions = se.nextExecutions(to)
