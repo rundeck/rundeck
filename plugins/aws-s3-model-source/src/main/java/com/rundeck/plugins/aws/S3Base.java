d9612ec1926ecd76875a6c35f915f10d74d065aa
jtobard <jaime.tobar.diaz@gmail.com>
2017-09-28 11:06:48 -0300

    copyright

--- plugins/aws-s3-model-source/src/main/java/com/rundeck/plugins/aws/S3Base.java
+++ plugins/aws-s3-model-source/src/main/java/com/rundeck/plugins/aws/S3Base.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 Rundeck, Inc. (http://rundeck.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.rundeck.plugins.aws;
 
 
@@ -23,7 +38,6 @@ import com.dtolabs.utils.Streams;
 import org.apache.log4j.Logger;
 
 import java.io.*;
-import java.net.URLConnection;
 import java.nio.file.Files;
 
 public class S3Base implements AWSCredentials,ResourceModelSource, WriteableModelSource {
