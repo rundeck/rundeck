c7caf69039f6e4fb296cd5141e1bbdea78615fd1
Greg Schueler <greg.schueler@gmail.com>
2017-10-13 10:44:28 -0700

    Fix #2691 log filters correctly applied in job references

--- core/src/main/java/com/dtolabs/rundeck/core/logging/PluginLoggingManager.java
+++ core/src/main/java/com/dtolabs/rundeck/core/logging/PluginLoggingManager.java
@@ -22,9 +22,8 @@ import java.util.function.Supplier;
 
 
 /**
- * Allows installing plugins into the logging stream, the {@link #installPlugin(LogFilterPlugin)} method
- * must be called before {@link #begin()}, and {@link #end()} must be called to
- * uninstall logging plugins.
+ * Allows installing plugins into the logging stream. The {@link #begin()} must be called before {@link #end()},
+ * or the {@link #runWith(Supplier)} can be used to wrap another action with begin/end.
  *
  * @author greg
  * @since 5/11/17
