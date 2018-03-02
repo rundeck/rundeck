03538a9d68aa471d7e551db48b5236b9982fffb3
Stephen Joyner <stephen@rundeck.com>
2018-03-02 12:11:59 -0600

    Secure application with spring security. Remove obsolete authfilter and rundeckjettyauth plugins.

--- rundeckapp/grails-app/conf/application.groovy
+++ rundeckapp/grails-app/conf/application.groovy
@@ -52,3 +52,61 @@ environments {
         }
     }
 }
+
+grails.plugin.springsecurity.securityConfigType = "InterceptUrlMap"
+
+grails.plugin.springsecurity.interceptUrlMap = [
+        [pattern: '/j_security_check', access: ['permitAll']],
+        [pattern: '/error',          access: ['permitAll']],
+        [pattern: '/error.gsp',      access: ['permitAll']],
+        [pattern: '/404',            access: ['permitAll']],
+        [pattern: '/404.gsp',        access: ['permitAll']],
+        [pattern: '/static/**',      access: ['permitAll']],
+        [pattern: '/assets/**',      access: ['permitAll']],
+        [pattern: '/**/js/**',       access: ['permitAll']],
+        [pattern: '/**/css/**',      access: ['permitAll']],
+        [pattern: '/**/images/**',   access: ['permitAll']],
+        [pattern: '/**/favicon.ico', access: ['permitAll']],
+        [pattern: '/user/login',     access: ['permitAll']],
+        [pattern: '/user/error',     access: ['permitAll']],
+        [pattern: '/user/logout',    access: ['permitAll']],
+        [pattern: '/user/loggedout', access: ['permitAll']],
+        [pattern: '/feed/**',        access: ['permitAll']],
+        [pattern: '/api/**',         access: ['permitAll']],
+        [pattern: '/test/**',        access: ['permitAll']],
+        [pattern: '/**',             access: ['IS_AUTHENTICATED_REMEMBERED']]
+]
+
+grails.plugin.springsecurity.filterChain.chainMap = [
+        [pattern: '/static/**',      filters: 'none'],
+        [pattern: '/assets/**',      filters: 'none'],
+        [pattern: '/feed/**',        filters: 'none'],
+        [pattern: '/test/**',        filters: 'none'],
+        [pattern: '/api/**',         filters: 'none'],
+        [pattern: '/404',            filters: 'none'],
+        [pattern: '/404.gsp',        filters: 'none'],
+        [pattern: '/**/js/**',       filters: 'none'],
+        [pattern: '/**/css/**',      filters: 'none'],
+        [pattern: '/**/images/**',   filters: 'none'],
+        [pattern: '/**/favicon.ico', filters: 'none'],
+        [pattern: '/**',             filters: 'JOINED_FILTERS']
+]
+
+grails.plugin.springsecurity.filterChain.filterNames = [
+        'authFilter','securityContextPersistenceFilter', 'logoutFilter',
+        'authenticationProcessingFilter', 'jaasApiIntegrationFilter',
+        'securityContextHolderAwareRequestFilter',
+        'rememberMeAuthenticationFilter', 'anonymousAuthenticationFilter',
+        'exceptionTranslationFilter', 'filterInvocationInterceptor'
+]
+
+grails.plugin.springsecurity.apf.filterProcessesUrl = "/j_security_check"
+grails.plugin.springsecurity.apf.usernameParameter = "j_username"
+grails.plugin.springsecurity.apf.passwordParameter = "j_password"
+grails.plugin.springsecurity.auth.loginFormUrl = "/user/login"
+grails.plugin.springsecurity.failureHandler.defaultFailureUrl = "/user/error"
+
+grails.plugin.springsecurity.providerNames = [
+        'jaasAuthProvider',
+        'anonymousAuthenticationProvider',
+        'rememberMeAuthenticationProvider']
