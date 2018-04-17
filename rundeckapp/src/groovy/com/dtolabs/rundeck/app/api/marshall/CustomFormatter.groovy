7870ed3ac34a5894c3f32ef9017f80c583841ccf
Valentin Ouvrard <valentin.ouvrard@nautile.sarl>
2018-04-17 13:51:57 +1100

    Upgrade Copyright to timestamp 1514764800

--- rundeckapp/src/groovy/com/dtolabs/rundeck/app/api/marshall/CustomFormatter.groovy
+++ rundeckapp/src/groovy/com/dtolabs/rundeck/app/api/marshall/CustomFormatter.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Rundeck, Inc. (http://rundeck.com)
+ * Copyright 2018 Rundeck, Inc. (http://rundeck.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
