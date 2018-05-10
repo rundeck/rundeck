c15e057aa0998ec3a1b059f215721e1c2995f517
Jaime Tobar <jftobardiaz@gmail.com>
2018-05-10 10:54:59 -0300

    fix test v23 project-sources test

--- test/api/test-v23-project-sources-json.sh
+++ test/api/test-v23-project-sources-json.sh
@@ -3,6 +3,7 @@
 #test listing project sources
 
 DIR=$(cd `dirname $0` && pwd)
+export API_CURRENT_VERSION=${API_VERSION}
 export API_VERSION=23 #/api/23/project/NAME/sources
 source $DIR/include.sh
 
@@ -21,7 +22,7 @@ assert_json_value '1' 'length'  ${file}
 assert_json_value 'file'  '.[0].type' ${file}
 assert_json_value '1'  '.[0].index' ${file}
 assert_json_value 'false'  '.[0].resources.writeable' ${file}
-assert_json_value "${APIURL}/project/${proj}/source/1/resources" '.[0].resources.href' ${file}
+assert_json_value "${RDURL}/api/${API_CURRENT_VERSION}/project/${proj}/source/1/resources" '.[0].resources.href' ${file}
 
 test_succeed
 
