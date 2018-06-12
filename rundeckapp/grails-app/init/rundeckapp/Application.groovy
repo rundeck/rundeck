438a3157c634b4d424cf240315db8fce52d76f94
Stephen Joyner <stephen@rundeck.com>
2018-06-12 08:53:21 -0500

    Fix deb and rpm packaging. Handle non existing lib directory.

--- rundeckapp/grails-app/init/rundeckapp/Application.groovy
+++ rundeckapp/grails-app/init/rundeckapp/Application.groovy
@@ -64,9 +64,11 @@ class Application extends GrailsAutoConfiguration implements EnvironmentAware {
 
     def loadJdbcDrivers() {
         File serverLib = new File(rundeckConfig.serverBaseDir,"lib")
-        serverLib.eachFile { file ->
-            if(!file.name.startsWith("rundeck-core") && file.name.endsWith(".jar")) {
-                Thread.currentThread().contextClassLoader.addURL(file.toURI().toURL())
+        if(serverLib.exists()) {
+            serverLib.eachFile { file ->
+                if (!file.name.startsWith("rundeck-core") && file.name.endsWith(".jar")) {
+                    Thread.currentThread().contextClassLoader.addURL(file.toURI().toURL())
+                }
             }
         }
     }
