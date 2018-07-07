49159c67dc6a8f75564ef28ed106714febc04d75
ProTip <greg.zapp@gmail.com>
2018-07-06 19:29:56 -0500

    Restore deb and rpm publishing.

--- scripts/deploy-rpm-snapshots.sh
+++ scripts/deploy-rpm-snapshots.sh
@@ -9,7 +9,6 @@ bintray_user=$1
 bintray_api_key=$2
 bintray_org=$3
 bintray_repo=$4
-release_tag=$5
 
 VCS_URL=https://github.com/rundeck/rundeck.git
 
@@ -23,22 +22,18 @@ mkdir $WORKSPACE
 
 cp packaging/rpmdist/RPMS/**/*.rpm $WORKSPACE
 
-for rpm in $WORKSPACE/*.rpm ; do
-    bash $DIR/replace-filename-token.sh $rpm SNAPSHOT $release_tag
-done
+(
+    set -e
+    cd $WORKSPACE
+    for rpm in *.rpm ; do
+        PCK_NAME=$(rpm -qp ${rpm} --queryformat "%{NAME}") ;
+        echo "{ \"name\": \"${PCK_NAME}\",
+          \"desc\": \"\",
+          \"vcs_url\": \"${VCS_URL}\",
+          \"licenses\": [\"Apache-2.0\"]}" > bintray-package.json
 
-# (
-#     set -e
-#     cd $WORKSPACE
-#     for rpm in *.rpm ; do
-#         PCK_NAME=$(rpm -qp ${rpm} --queryformat "%{NAME}") ;
-#         echo "{ \"name\": \"${PCK_NAME}\",
-#           \"desc\": \"\",
-#           \"vcs_url\": \"${VCS_URL}\",
-#           \"licenses\": [\"Apache-2.0\"]}" > bintray-package.json
-
-#         bash $DIR/publish-rpm-to-bintray.sh $bintray_user $bintray_api_key $bintray_org $bintray_repo $rpm
-#         rm bintray-package.json
-#     done
-# )
+        bash $DIR/publish-rpm-to-bintray.sh $bintray_user $bintray_api_key $bintray_org $bintray_repo $rpm
+        rm bintray-package.json
+    done
+)
 
