7e86f6c1b1f2295b481971dc3cc38eaf7ce5c308
Stephen Joyner <stephen@rundeck.com>
2018-07-27 09:25:50 -0500

    Fix #3738. Update libpam4j dependency so that it can work with the new jna lib. Add a password callback to Jetty support to retrieve the password from the form and pass it to the PAM module. Add docker test for PAM auth.

---
+++ test/docker/test-pam.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -euo pipefail
+
+. common.sh
+
+export DOCKER_COMPOSE_SPEC=${DOCKER_COMPOSE_SPEC:-docker-compose-pam-test.yaml}
+export CLI_VERS=1.0.29-1
+
+if [ -f rundeck-launcher.war ] ; then
+	mv rundeck-launcher.war dockers/rundeck/data/
+fi
+
+if [ -f rd.deb ] ; then
+	mv rd.deb dockers/rundeck/data/
+fi
+
+build_rdtest_docker
+
+# clean up docker env
+docker-compose -f $DOCKER_COMPOSE_SPEC down --volumes --remove-orphans
+
+set -e
+# re-build docker env
+docker-compose -f $DOCKER_COMPOSE_SPEC build
+
+
+# run docker
+docker-compose -f $DOCKER_COMPOSE_SPEC up -d
+
+echo "up completed, running tests..."
+
+set +e
+
+docker-compose -f $DOCKER_COMPOSE_SPEC exec -T --user rundeck rundeck1 \
+	bash scripts/run_tests.sh /tests/pamtests
+
+EC=$?
+echo "run_tests.sh finished with: $EC"
+
+docker-compose -f $DOCKER_COMPOSE_SPEC logs
+
+# Stop and clean all
+docker-compose -f $DOCKER_COMPOSE_SPEC down --volumes --remove-orphans
+
+exit $EC
