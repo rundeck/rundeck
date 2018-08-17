bc11e32932ea2b1b10c9bd40463611df474a7a87
ProTip <greg.zapp@gmail.com>
2018-08-17 15:23:14 -0500

    Update release.sh to work with nodejs.

--- releaseversion.js
+++ releaseversion.js
@@ -1,5 +1,12 @@
 //run with osascript
-function run(args){
-	var vers=new RundeckVersion({"versionString":args});
-	return 	"export REL_NAME="+vers.name()+"\n"+ "export REL_COLOR="+vers.color() + "\n"+ "export REL_ICON="+vers.icon() + "\n" + "export REL_TEXT=\""+vers.text() +"\""	;
-}
+const fs = require('fs')
+
+
+let VersionFile = fs.readFileSync('rundeckapp/grails-app/assets/javascripts/version.js').toString()
+
+eval(VersionFile)
+
+const version = process.argv[2]
+
+var vers=new RundeckVersion({"versionString":version});
+console.log("export REL_NAME="+vers.name()+"\n"+ "export REL_COLOR="+vers.color() + "\n"+ "export REL_ICON="+vers.icon() + "\n" + "export REL_TEXT=\""+vers.text() +"\"")
