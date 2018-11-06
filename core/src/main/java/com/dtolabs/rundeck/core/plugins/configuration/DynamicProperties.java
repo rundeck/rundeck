064005d5e43d22cb38874d637cee09c2ae996e95
miguelantonio <contacto@migue.cl>
2018-11-05 23:05:33 -0300

    Adding StorageTree to the DynamicProperties interface, in order to access to the key storage on the dynamicProperties plugin implementation.

--- core/src/main/java/com/dtolabs/rundeck/core/plugins/configuration/DynamicProperties.java
+++ core/src/main/java/com/dtolabs/rundeck/core/plugins/configuration/DynamicProperties.java
@@ -1,10 +1,12 @@
 package com.dtolabs.rundeck.core.plugins.configuration;
 
+import com.dtolabs.rundeck.core.storage.StorageTree;
+
 import java.util.Map;
 
 /**
  * Created by carlos on 04/01/18.
  */
 public interface DynamicProperties {
-    Map<String, Object> dynamicProperties (Map<String, Object> projectAndFrameworkValues);
+    Map<String, Object> dynamicProperties (Map<String, Object> projectAndFrameworkValues, StorageTree storageTree);
 }
