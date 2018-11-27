94af14d6badfb9acb498722ac8438000eb55c383
ProTip <greg.zapp@gmail.com>
2018-11-26 19:54:29 -0600

    Use stable UUID default unless random is specified
    fixes #4181

--- docker/official/lib/entry.sh
+++ docker/official/lib/entry.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -eou pipefail
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+for inc in $(ls $DIR/includes | sort -n); do
+    source $DIR/includes/$inc
+done
+
 export HOSTNAME=$(hostname)
 
 export RUNDECK_HOME=/home/rundeck
@@ -17,8 +23,10 @@ mkdir -p ${REMCO_TMP_DIR}/rundeck-config
 remco -config ${REMCO_HOME}/config.toml
 
 # Generate a new server UUID
-GENERATED_UUID=$(uuidgen)
-echo "rundeck.server.uuid = ${RUNDECK_SERVER_UUID:-${GENERATED_UUID}}" > ${REMCO_TMP_DIR}/framework/server-uuid.properties
+if [[ "${RUNDECK_SERVER_UUID}" = "RANDOM" ]] ; then
+    RUNDECK_SERVER_UUID=$(uuidgen)
+fi
+echo "rundeck.server.uuid = ${RUNDECK_SERVER_UUID}" > ${REMCO_TMP_DIR}/framework/server-uuid.properties
 
 # Combine partial config files
 cat ${REMCO_TMP_DIR}/framework/* >> etc/framework.properties
