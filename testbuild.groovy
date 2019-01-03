44e361a941194f4a894eb024d99dc46cc33ec898
Jaime Tobar <jftobardiaz@gmail.com>
2019-01-03 16:26:57 -0300

    fix testbuild file

--- testbuild.groovy
+++ testbuild.groovy
@@ -55,7 +55,7 @@ def coreJarFile = "core/${target}/rundeck-core-${version}.jar"
 //def launcherJarFile = "rundeck-launcher/launcher/${target}/rundeck-launcher-${version}.jar"
 
 //the list of bundled plugins to verify in the war and jar
-def plugins=['script','stub','localexec','copyfile','job-state','flow-control','jasypt-encryption','git','object-store','orchestrator', 'source-refresh','upvar','load-keystorage-value-plugin']
+def plugins=['script','stub','localexec','copyfile','job-state','flow-control','jasypt-encryption','git','object-store','orchestrator', 'source-refresh','upvar','load-keystorage-value']
 def externalPlugins=['rundeck-ansible-plugin','aws-s3-model-source','py-winrm-plugin','openssh-node-execution']
 
 //manifest describing expected build results
