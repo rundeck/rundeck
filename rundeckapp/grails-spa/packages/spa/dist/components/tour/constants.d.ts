b092fea19ad76c2aaa91672002b25bc05b870021
Jesse Marple <jesse@ossium.co>
2019-01-28 13:29:56 -0500

    TS tour app

---
+++ rundeckapp/grails-spa/packages/spa/dist/components/tour/constants.d.ts
@@ -0,0 +1,7 @@
+export declare const tourManifestUrl: string;
+export declare const tourUrl: string;
+declare const _default: {
+    tourManifestUrl: string;
+    tourUrl: string;
+};
+export default _default;
