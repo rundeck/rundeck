af6fc09ada2bb27cac7ddf9df8fc5902aea2b923
Greg Schueler <greg.schueler@gmail.com>
2019-02-28 10:30:23 -0800

    use ui-trellis, typescript

--- rundeckapp/grails-spa/src/pages/project-nodes-config/App.vue
+++ rundeckapp/grails-spa/src/pages/project-nodes-config/App.vue
@@ -1,81 +1,69 @@
 <template>
     <div id="project-nodes-config">
-      <span class="h4">
-        Nodes Plugins
+        <span class="h4">
+          Nodes Plugins
         </span>
+
         <div class="list-group">
-            <div class="list-group-item" v-for="(plugin,index) in plugins" :key="'plugin_'+index">
-                {{index+1}}.
-                <plugin-info :detail="plugin.detail" v-if="plugin.detail"></plugin-info>
-                <span v-else>
-                {{plugin.type}}
-                </span>
-                 <plugin-config :mode="'show'"
+            <div class="list-group-item" v-for="(plugin,index) in nodesPlugins" :key="'plugin_'+index">
+
+                 <plugin-config
+                  :mode="'title'"
+                  :serviceName="'NodeEnhancer'"
+                  :provider="plugin.type"
+                  >
+                  <span slot="titlePrefix">{{index+1}}.</span>
+                  </plugin-config>
+                <plugin-config
+                  :mode="'show'"
                   :serviceName="'NodeEnhancer'"
                   :provider="plugin.type"
                       :config="plugin.config"
                       :show-title="false"
                       :show-description="false"
                   ></plugin-config>
+
             </div>
         </div>
     </div>
 </template>
 
-<script>
+<script lang="ts">
 import axios from 'axios'
 import Vue from 'vue'
 import Trellis, {
     getRundeckContext,
     getSynchronizerToken,
-    RundeckBrowser
+    RundeckBrowser,
+    RundeckContext
 } from "@rundeck/ui-trellis"
 
-import PluginInfo from '@/components/plugins/PluginInfo'
-import PluginConfig from '@/components/plugins/pluginConfig'
+import Expandable from '@rundeck/ui-trellis/src/components/utils/Expandable.vue'
+import PluginInfo from '@rundeck/ui-trellis/src/components/plugins/PluginInfo.vue'
+import PluginConfig from '@rundeck/ui-trellis/src/components/plugins/pluginConfig.vue'
 
-export default {
+export default Vue.extend({
     name: 'App',
     components: {
         PluginInfo,
-        PluginConfig
+        PluginConfig,
+        Expandable
     },
     data () {
         return {
-            project: null,
-            rdBase: null,
-            plugins: [],
-            RundeckContext: null,
-            ServiceProvidersCache: {}
+            project: null as string|null,
+            rdBase: null as string|null,
+            nodesPlugins: [],
+            rundeckContext: {} as RundeckContext,
+            ServiceProvidersCache: {} as {[name:string]:{[provider:string]:{[detail:string]:any}}}
         }
     },
     methods: {
-        getPluginDescription (svcName, provider) {
-            if (this.ServiceProvidersCache[svcName] && this.ServiceProvidersCache[svcName].providers[provider]) {
-                return new Promise((resolve, reject) => {
-                    resolve(this.ServiceProvidersCache[svcName].providers[provider])
-                })
-            }
 
-            return this.RundeckContext.rundeckClient.sendRequest({
-                url: `/plugin/detail/${svcName}/${provider}`,
-                method: 'GET'
-            }).then(resp => {
-                if (!resp.parsedBody) {
-                    throw new Error(`Error getting service provider detail for ${svcName}/${provider}`)
-                }
-                if (!this.ServiceProvidersCache[svcName]) {
-                    this.ServiceProvidersCache[svcName] = {
-                        providers: {}
-                    }
-                }
-                this.ServiceProvidersCache[svcName].providers[provider] = resp.parsedBody
-                return resp.parsedBody
-            }).catch(e => console.warn('Error getting service provider detail', e))
-        }
     },
     mounted () {
-        this.RundeckContext = getRundeckContext()
+        this.rundeckContext = getRundeckContext()
+        const self=this
         if (window._rundeck && window._rundeck.rdBase && window._rundeck.projectName) {
             this.rdBase = window._rundeck.rdBase
             this.project = window._rundeck.projectName
@@ -90,19 +78,13 @@ export default {
                 withCredentials: true
             }).then((response) => {
                 if (response.data && response.data.nodeEnhancerPlugins) {
-                    this.plugins = response.data.nodeEnhancerPlugins
-                    let self = this
-                    console.log("got pugins", this.plugins)
-                    this.plugins.forEach(function (value,x) {
-                        self.getPluginDescription('NodeEnhancer', value.type).then(function (pluginDetail) {
-                            Vue.set(value, 'detail', pluginDetail)
-                        })
-                    })
+                    self.nodesPlugins = response.data.nodeEnhancerPlugins
+
                 }
             })
         }
     }
-}
+})
 </script>
 
 <style>
