0efacef1753ba51cf977aad2e90d0bf11f60a2aa
Greg Schueler <greg.schueler@gmail.com>
2019-04-10 16:42:22 -0700

    don't make '*' glob feature enabled by default in dev mode

--- rundeckapp/grails-app/conf/application.groovy
+++ rundeckapp/grails-app/conf/application.groovy
@@ -31,7 +31,6 @@ environments {
         application.refreshDelay=5000
         grails.profiler.disable=false
         feature.incubator.'*'=true
-        rundeck.feature.'*'.enabled=true
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop','update'
             url = "jdbc:h2:file:./db/devDb"
