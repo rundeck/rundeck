755bb6284aa6687aee9475919b01e086337b4a09
Jesse Marple <jesse@ossium.co>
2019-05-01 15:07:30 -0400

    checkpoint

--- rundeckapp/grails-spa/packages/spa/dist/components/tour/constants.js
+++
@@ -1,9 +0,0 @@
-import { getRundeckContext } from '@rundeck/ui-trellis';
-const context = getRundeckContext();
-export const tourManifestUrl = `${context.rdBase}tour/listAll`;
-export const tourUrl = `${context.rdBase}tour/get/`;
-export default {
-    tourManifestUrl,
-    tourUrl
-};
-//# sourceMappingURL=constants.js.map
