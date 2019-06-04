5945ad96247b49ad2d3a348e78eefdc367d40930
Greg Schueler <greg.schueler@gmail.com>
2019-06-04 14:52:09 -0700

    fix: job with default debug level cannot be set to normal

--- rundeckapp/grails-app/assets/javascripts/scheduledExecution/jobRunFormOptionsKO.js
+++ rundeckapp/grails-app/assets/javascripts/scheduledExecution/jobRunFormOptionsKO.js
@@ -19,6 +19,9 @@ function JobRunFormOptions (data) {
     var self = this
     self.nodeFilter = data.nodeFilter
     self.debug = ko.observable(data.debug === true || data.debug === 'true')
+    self.loglevel = ko.pureComputed(function () {
+        return self.debug() ? 'DEBUG' : 'NORMAL'
+    })
 
     self.canOverrideFilter = ko.observable(data.canOverrideFilter)
     self.changeTargetNodes = ko.observable(data.changeTargetNodes)
