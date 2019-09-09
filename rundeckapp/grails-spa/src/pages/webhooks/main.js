77f6bc24aff5387c59f003ef4e6de90487f9fb37
ProTip <greg.zapp@gmail.com>
2019-09-09 16:02:45 -0500

    Use global Vue for all entry points

--- rundeckapp/grails-spa/src/pages/webhooks/main.js
+++ rundeckapp/grails-spa/src/pages/webhooks/main.js
@@ -5,6 +5,8 @@ import VueScrollTo from 'vue-scrollto'
 import VueFuse from 'vue-fuse'
 import * as uiv from 'uiv'
 import App from './App.vue'
+import VueI18n from 'vue-i18n'
+import uivLang from '../../utilities/uivi18n'
 
 Vue.config.productionTip = false
 
@@ -14,8 +16,24 @@ Vue.use(VueFuse)
 Vue.use(Vue2Filters)
 Vue.use(uiv)
 
+let messages =
+{
+  en_US: Object.assign(
+    {},
+    uivLang['en_US'] || uivLang['en'] || {},
+    window.Messages
+  )
+}
+
+const i18n = new VueI18n({
+  silentTranslationWarn: true,
+  locale: 'en', // set locale
+  messages // set locale messages,
+})
+
 // eslint-disable-next-line no-new
 new Vue({
+  i18n,
   el: '#webhook-vue',
   components: { App },
   template: "<App/>"
