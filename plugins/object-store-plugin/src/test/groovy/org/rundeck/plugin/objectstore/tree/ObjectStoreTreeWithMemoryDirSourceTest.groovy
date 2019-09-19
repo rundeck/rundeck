00282a90e34d05281878458d4a5dea22454d0f0a
Greg Schueler <greg.schueler@gmail.com>
2019-09-18 21:30:31 -0700

    Use containers instead of binaries to run minio
    Fix: groovy changed List.push()/pop() behavior between Groovy 2.4 and
    2.5, modified usage to work in both groovies for test code
    
    (cherry picked from commit 82ab4cc55b5bb50d9fff48d33e8e78a59a1c628e)
    
    # Conflicts:
    #	plugins/object-store-plugin/build.gradle
    #	plugins/object-store-plugin/src/test/groovy/org/rundeck/plugin/objectstore/tree/ObjectStoreTreeWithMemoryDirSourceTest.groovy

--- plugins/object-store-plugin/src/test/groovy/org/rundeck/plugin/objectstore/tree/ObjectStoreTreeWithMemoryDirSourceTest.groovy
+++ plugins/object-store-plugin/src/test/groovy/org/rundeck/plugin/objectstore/tree/ObjectStoreTreeWithMemoryDirSourceTest.groovy
@@ -20,25 +20,38 @@ import com.dtolabs.rundeck.core.storage.StorageUtil
 import com.dtolabs.utils.Streams
 import io.minio.MinioClient
 import io.minio.errors.ErrorResponseException
+<<<<<<< HEAD
 import org.rundeck.plugin.objectstore.directorysource.ObjectStoreDirectAccessDirectorySource
+=======
+>>>>>>> 82ab4cc55b... Use containers instead of binaries to run minio
 import org.rundeck.plugin.objectstore.directorysource.ObjectStoreDirectorySource
 import org.rundeck.plugin.objectstore.directorysource.ObjectStoreMemoryDirectorySource
 import org.rundeck.storage.api.HasInputStream
 import org.rundeck.storage.api.PathUtil
+import org.testcontainers.spock.Testcontainers
+import spock.lang.Shared
 import spock.lang.Specification
+<<<<<<< HEAD
 import testhelpers.MinioTestServer
 
+=======
+import testhelpers.MinioContainer
+>>>>>>> 82ab4cc55b... Use containers instead of binaries to run minio
 
+@Testcontainers
 class ObjectStoreTreeWithMemoryDirSourceTest extends Specification {
     String configBucket = "test-config-bucket"
     ObjectStoreTree store
     ObjectStoreDirectorySource directorySource
     static MinioClient mClient
-    static MinioTestServer server = new MinioTestServer()
+    static final String ACCESS_KEY = 'TEST_KEY'
+    static final String SECRET_KEY = UUID.randomUUID().toString()
+
+    @Shared
+    public MinioContainer minio = new MinioContainer<>().withAccess(ACCESS_KEY, SECRET_KEY)
 
     void setupSpec() {
-        server.start()
-        mClient = new MinioClient("http://localhost:9000", server.accessKey, server.secretKey)
+        mClient = minio.client()
     }
 
     void setup() {
@@ -48,7 +61,7 @@ class ObjectStoreTreeWithMemoryDirSourceTest extends Specification {
     }
 
     void cleanupSpec() {
-        server.stop()
+        minio.stop()
     }
 
     def "Init"() {
@@ -80,6 +93,7 @@ class ObjectStoreTreeWithMemoryDirSourceTest extends Specification {
         ifNotExistAdd(key,"prop1=val1")
 
         expect:
+        store.hasResource(key)
         store.hasDirectory("hasdirectory/isadir")
     }
 
@@ -249,9 +263,16 @@ class ObjectStoreTreeWithMemoryDirSourceTest extends Specification {
         } catch(ErrorResponseException erex) {
             if(erex.response.code() == 404) {
                 ByteArrayInputStream inStream = new ByteArrayInputStream(content.bytes)
-                mClient.putObject(configBucket, key, inStream, content.bytes.length, meta)
+                try {
+                    mClient.putObject(configBucket, key, inStream, content.bytes.length, meta)
+                }catch(Exception e){
+                    System.err.println("Error: " + e)
+                }
                 directorySource.updateEntry(key,meta)
+            } else {
+                System.err.println("Error: " + erex)
             }
+
         }
 
     }
