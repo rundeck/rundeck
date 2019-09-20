db8352daa07fe6bc6c61c50481302d44ba1380a4
Greg Schueler <greg.schueler@gmail.com>
2019-09-20 16:21:14 -0700

    cleanup: use lombok, private fields

--- core/src/main/java/com/dtolabs/rundeck/plugins/jobs/JobOptionImpl.java
+++ core/src/main/java/com/dtolabs/rundeck/plugins/jobs/JobOptionImpl.java
@@ -2,39 +2,41 @@ package com.dtolabs.rundeck.plugins.jobs;
 
 import com.dtolabs.rundeck.core.jobs.JobOption;
 import com.dtolabs.rundeck.core.plugins.configuration.ValidationException;
+import lombok.Data;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.*;
 
+@Data
 public class JobOptionImpl implements JobOption, Comparable {
-
     static final String DEFAULT_DELIMITER = ",";
-    String name;
-    Integer sortIndex;
-    String description;
-    String defaultValue;
-    String defaultStoragePath;
-    Boolean enforced;
-    Boolean required;
-    Boolean isDate;
-    String dateFormat;
-    SortedSet<String> values;
-    URL realValuesUrl;
-    String label;
-    String regex;
-    String valuesList;
-    String valuesListDelimiter;
-    Boolean multivalued;
-    String delimiter;
-    Boolean secureInput;
-    Boolean secureExposed;
-    String optionType;
-    String configData;
-    Boolean multivalueAllSelected;
-    String optionValuesPluginType;
-    Boolean hidden;
-    Boolean sortValues;
-    List<String> optionValues;
+    private String name;
+    private Integer sortIndex;
+    private String description;
+    private String defaultValue;
+    private String defaultStoragePath;
+    private Boolean enforced;
+    private Boolean required;
+    private Boolean isDate;
+    private String dateFormat;
+    private TreeSet values;
+    private URL realValuesUrl;
+    private String label;
+    private String regex;
+    private String valuesList;
+    private String valuesListDelimiter;
+    private Boolean multivalued;
+    private String delimiter;
+    private Boolean secureInput;
+    private Boolean secureExposed;
+    private String optionType;
+    private String configData;
+    private Boolean multivalueAllSelected;
+    private String optionValuesPluginType;
+    private Boolean hidden;
+    private Boolean sortValues;
+    private List<String> optionValues;
 
     public JobOptionImpl(){}
 
@@ -111,238 +113,6 @@ public class JobOptionImpl implements JobOption, Comparable {
         }
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setSortIndex(Integer sortIndex) {
-        this.sortIndex = sortIndex;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setDefaultValue(String defaultValue) {
-        this.defaultValue = defaultValue;
-    }
-
-    public void setDefaultStoragePath(String defaultStoragePath) {
-        this.defaultStoragePath = defaultStoragePath;
-    }
-
-    public void setEnforced(Boolean enforced) {
-        this.enforced = enforced;
-    }
-
-    public void setRequired(Boolean required) {
-        this.required = required;
-    }
-
-    public void setDate(Boolean date) {
-        isDate = date;
-    }
-
-    public void setDateFormat(String dateFormat) {
-        this.dateFormat = dateFormat;
-    }
-
-    public void setValues(SortedSet<String> values) {
-        this.values = values;
-    }
-
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
-    public void setRegex(String regex) {
-        this.regex = regex;
-    }
-
-    public void setValuesList(String valuesList) {
-        this.valuesList = valuesList;
-    }
-
-    public void setValuesListDelimiter(String valuesListDelimiter) {
-        this.valuesListDelimiter = valuesListDelimiter;
-    }
-
-    public void setMultivalued(Boolean multivalued) {
-        this.multivalued = multivalued;
-    }
-
-    public void setDelimiter(String delimiter) {
-        this.delimiter = delimiter;
-    }
-
-    public void setSecureInput(Boolean secureInput) {
-        this.secureInput = secureInput;
-    }
-
-    public void setSecureExposed(Boolean secureExposed) {
-        this.secureExposed = secureExposed;
-    }
-
-    public void setOptionType(String optionType) {
-        this.optionType = optionType;
-    }
-
-    public void setConfigData(String configData) {
-        this.configData = configData;
-    }
-
-    public void setMultivalueAllSelected(Boolean multivalueAllSelected) {
-        this.multivalueAllSelected = multivalueAllSelected;
-    }
-
-    public void setOptionValuesPluginType(String optionValuesPluginType) {
-        this.optionValuesPluginType = optionValuesPluginType;
-    }
-
-    public void setHidden(Boolean hidden) {
-        this.hidden = hidden;
-    }
-
-    public void setSortValues(Boolean sortValues) {
-        this.sortValues = sortValues;
-    }
-
-    public void setOptionValues(List<String> optionValues) {
-        this.optionValues = optionValues;
-    }
-
-    @Override
-    public String getName() {
-        return this.name;
-    }
-
-    @Override
-    public Integer getSortIndex() {
-        return this.sortIndex;
-    }
-
-    @Override
-    public String getDescription() { return this.description; }
-
-    @Override
-    public String getDefaultValue() {
-        return this.defaultValue;
-    }
-
-    @Override
-    public String getDefaultStoragePath() {
-        return this.defaultStoragePath;
-    }
-
-    @Override
-    public Boolean getEnforced() {
-        return this.enforced;
-    }
-
-    @Override
-    public Boolean getRequired() {
-        return this.required;
-    }
-
-    @Override
-    public Boolean getDate() {
-        return this.isDate;
-    }
-
-    @Override
-    public String getDateFormat() {
-        return this.dateFormat;
-    }
-
-    @Override
-    public SortedSet<String> getValues() {
-        return this.values;
-    }
-
-    @Override
-    public URL getValuesUrl() {
-        return this.realValuesUrl;
-    }
-
-    @Override
-    public String getLabel() {
-        return this.label;
-    }
-
-    @Override
-    public URL getValuesUrlLong() {
-        return this.realValuesUrl;
-    }
-
-    @Override
-    public String getRegex() {
-        return this.regex;
-    }
-
-    @Override
-    public String getValuesList() {
-        return this.valuesList;
-    }
-
-    @Override
-    public String getValuesListDelimiter() {
-        return this.valuesListDelimiter;
-    }
-
-    @Override
-    public Boolean getMultivalued() {
-        return this.multivalued;
-    }
-
-    @Override
-    public String getDelimiter() {
-        return this.delimiter;
-    }
-
-    @Override
-    public Boolean getSecureInput() {
-        return this.secureInput;
-    }
-
-    @Override
-    public Boolean getSecureExposed() {
-        return this.secureExposed;
-    }
-
-    @Override
-    public String getOptionType() {
-        return this.optionType;
-    }
-
-    @Override
-    public String getConfigData() {
-        return this.configData;
-    }
-
-    @Override
-    public Boolean getMultivalueAllSelected() {
-        return this.multivalueAllSelected;
-    }
-
-    @Override
-    public String getOptionValuesPluginType() {
-        return this.optionValuesPluginType;
-    }
-
-    @Override
-    public Boolean getHidden() {
-        return this.hidden;
-    }
-
-    @Override
-    public Boolean getSortValues() {
-        return this.sortValues;
-    }
-
-    @Override
-    public List<String> getOptionValues() {
-        return this.optionValues;
-    }
 
     private String produceValuesList(){
         if(this.values != null){
