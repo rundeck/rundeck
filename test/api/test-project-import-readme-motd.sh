282f08e5c3f4de7f25e48a6ba0e7bf47bc549ac5
Luis Toledo <luis@variacode.com>
2019-10-17 17:42:33 -0300

    addin api test for readme import
    (cherry picked from commit e95a21d7b40d343d47812f8b2bf4cfd4e082fbcd)

--- test/api/test-project-import-readme-motd.sh
+++ test/api/test-project-import-readme-motd.sh
@@ -78,3 +78,32 @@ assert_motd(){
     value=$(cat "$DIR/curl.out")
     assert "this is a message of the day" "$value"
 }
+
+test_proj="APIImportTestReame"
+create_proj $test_proj
+
+runurl="${APIURL}/project/$test_proj/import"
+params="jobUuidOption=remove&&importConfig=true"
+
+echo "TEST: PUT $runurl"
+
+archivefile=$SRC_DIR/archive-test-readme.zip
+
+docurl -X PUT -H 'Content-Type: application/zip' -D $DIR/headers.out --data-binary @$archivefile \
+    ${runurl}?${params} > $DIR/curl.out
+if [ 0 != $? ] ; then
+    errorMsg "ERROR: failed query request"
+    exit 2
+fi
+assert_http_status 200 $DIR/headers.out
+
+assert_xml_value 'successful' '/import/@status' $DIR/curl.out
+
+# testing readme and motd
+assert_readme $test_proj
+assert_motd $test_proj
+
+echo "OK"
+
+delete_proj $test_proj
+
