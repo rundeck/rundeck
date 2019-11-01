903e8ce3d9aa68dea3c6fa5d066ed8722ec4a1a6
Christoph Hille <38468371+hille721@users.noreply.github.com>
2019-11-01 11:58:28 +0100

    Corrected home and shell
    Corrected home and shell for new rundeck user passwd entry

--- docker/official/lib/entry.sh
+++ docker/official/lib/entry.sh
@@ -52,7 +52,7 @@ fi
 # Support Arbitrary User IDs on OpenShift
 if ! whoami &> /dev/null; then
   if [ -w /etc/passwd ]; then
-    echo "rundeck:x:$(id -u):0:rundeck user:/home/server:/sbin/nologin" >> /etc/passwd
+    echo "rundeck:x:$(id -u):0:rundeck user:/home/rundeck:/bin/bash" >> /etc/passwd
   fi
 fi
 
