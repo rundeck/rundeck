ac72c3e457e89c0a9b9f9fe934906a51c68d15d6
Stephen Joyner <stephen@rundeck.com>
2019-11-08 16:06:39 -0600

    Enable the repository feature with in-app config. Also fixes issue where repository config previously ignored the boolean value for enable/disable.
    (cherry picked from commit 958b75460b517d964971c34218c1bcba14e7278f)

--- rundeckapp/repository/src/main/groovy/repository/RepositoryGrailsPlugin.groovy
+++ rundeckapp/repository/src/main/groovy/repository/RepositoryGrailsPlugin.groovy
@@ -47,7 +47,7 @@ Brief summary/description of the plugin.
 //    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
 
     Closure doWithSpring() { {->
-            if(application.config.rundeck.feature.repository.enabled == "true") {
+            if(application.config.rundeck.feature.repository.enabled in ["true",true]) {
                 def rdeckBase = System.getProperty('rdeck.base')
 
                 ensureRequiredFilesExist(rdeckBase)
