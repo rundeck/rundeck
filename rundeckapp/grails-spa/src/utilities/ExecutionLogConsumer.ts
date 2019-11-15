ebe52132603015057bbd50a9fea51abf4616d2f8
ProTip <greg.zapp@gmail.com>
2019-11-14 23:12:54 -0600

    Support following running executions and exponential backoff

--- rundeckapp/grails-spa/src/utilities/ExecutionLogConsumer.ts
+++ rundeckapp/grails-spa/src/utilities/ExecutionLogConsumer.ts
@@ -11,11 +11,15 @@ interface IRenderedEntry extends ExecutionOutputEntry {
 
 export type EnrichedExecutionOutput = Omit<ExecutionOutput, 'entries'> & {entries: IRenderedEntry[]}
 
+const BACKOFF_MIN = 100
+const BACKOFF_MAX = 10000
+
 export class ExecutionLog {
     client: Rundeck
 
     offset = '0'
     completed = false
+    backoff = 0
 
     private jobWorkflowProm!: Promise<JobWorkflow>
     private executionStatusProm!: Promise<ExecutionStatusGetResponse>
@@ -45,20 +49,53 @@ export class ExecutionLog {
     }
 
     async getOutput(maxLines: number): Promise<ExecutionOutputGetResponse> {
+        await this.waitBackOff()
+
         const res = await this.client.executionOutputGet(this.id, {offset: this.offset, maxlines: maxLines})
         this.offset = res.offset
-        this.completed = res.completed
+        this.completed = res.completed && res.execCompleted
+
+        if (!this.completed && res.entries.length == 0) {
+            this.increaseBackOff()
+        } else {
+            this.decreaseBackOff()
+        }
+
+        console.log(`Backoff: ${this.backoff}`)
+        console.log(`Results: ${res.entries.length}`)
+
         return res
+    }
+
+    async waitBackOff() {
+        if (this.backoff == 0) {
+            return void(0)
+        } else {
+            return new Promise<void>((res, rej) => {
+                setTimeout(res, this.backoff)
+            })
+        }
+    }
+
+    private increaseBackOff() {
+        // TODO: Jitter https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/
+        this.backoff = Math.min(Math.max(this.backoff, BACKOFF_MIN) * 2, BACKOFF_MAX)
+    }
+
+    private decreaseBackOff() {
+        if (this.backoff == 0)
+            return
+    
+        const backoff = this.backoff / 2
+        this.backoff = backoff < BACKOFF_MIN ? 0 : backoff
     }
 
     async getEnrichedOutput(maxLines: number): Promise<EnrichedExecutionOutput> {
         const [workflow, res] = await Promise.all([
             this.getJobWorkflow(),
-            this.client.executionOutputGet(this.id, {offset: this.offset, maxlines: maxLines})
+            this.getOutput(maxLines)
         ])
-        this.offset = res.offset
-        this.completed = res.completed
-
+        
         const enrichedEntries = res.entries.map(e => ({
             renderedStep: workflow.renderStepsFromContextPath(e.stepctx!),
             ...e
