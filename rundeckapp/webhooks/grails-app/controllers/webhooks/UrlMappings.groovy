fa29702572e85665ca9baa1f890d1fa6acd79c1e
Stephen Joyner <stephen@rundeck.com>
2019-11-18 14:05:00 -0600

    Resolve merge issue.

--- rundeckapp/webhooks/grails-app/controllers/webhooks/UrlMappings.groovy
+++ rundeckapp/webhooks/grails-app/controllers/webhooks/UrlMappings.groovy
@@ -3,16 +3,9 @@ package webhooks
 class UrlMappings {
 
     static mappings = {
-
-<<<<<<< HEAD
-        if(getGrailsApplication().config.rundeck.feature.webhooks.enabled == "true") {
-            "/api/$api_version/webhook/$authtoken"(controller: 'webhook') {
-                action=[POST:"post"]
-            }
-=======
+        
         if(getGrailsApplication().config.rundeck.feature.webhooks.enabled in ["true",true]) {
             "/api/$api_version/webhook/$authtoken"(controller: 'webhook',action:"post")
->>>>>>> 019a7e6293... Constrain webhook post url to POST http method.
 
             "/api/$api_version/project/${project}/webhooks"(controller: 'webhook', action: 'list')
 
