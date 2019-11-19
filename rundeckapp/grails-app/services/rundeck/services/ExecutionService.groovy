cd011b252837e4eb1da0a633698bf3d97b2a0bed
Jaime Tobar <jftobardiaz@gmail.com>
2019-11-19 12:11:35 -0300

    fix #5397 skip the previous validation if the default field contains a variable
    (cherry picked from commit 0d2f4679e002ba1281af8d8503f1d47bf710998a)
    
    # Conflicts:
    #	rundeckapp/src/test/groovy/ExecutionServiceSpec.groovy

--- rundeckapp/grails-app/services/rundeck/services/ExecutionService.groovy
+++ rundeckapp/grails-app/services/rundeck/services/ExecutionService.groovy
@@ -1197,6 +1197,9 @@ class ExecutionService implements ApplicationContextAware, StepExecutor, NodeSte
      * @return
      */
     boolean canReadStoragePassword(AuthContext authContext, String storagePath, boolean failIfMissing){
+        if(storagePath?.contains('${')){
+            return true;//bypass validation if uses an execution variable
+        }
         def keystore = storageService.storageTreeWithContext(authContext)
         try {
             return keystore.hasPassword(storagePath)
