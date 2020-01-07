02e9baffedcd93d39857918e5f6f0b9feb432b77
Greg Schueler <greg.schueler@gmail.com>
2020-01-07 09:19:38 -0800

    update selenium tests
    * add some basic ui tests
    * remove the screenshot comparison

--- test/selenium/src/pages/projectCreate.page.ts
+++ test/selenium/src/pages/projectCreate.page.ts
@@ -2,11 +2,24 @@ import {By} from 'selenium-webdriver'
 
 import {Page} from 'page'
 
-export enum Elems {
+export const Elems= {
+   projectNameInput  : By.css('#createform form input[name="newproject"]'),
+   labelInput  : By.css('#createform form input[name="label"]'),
+   descriptionInput  : By.css('#createform form input[name="description"]'),
    
+
 }
 
 export class ProjectCreatePage extends Page {
     path = '/resources/createProject'
 
+    async projectNameInput(){
+        await this.ctx.driver.findElement(Elems.projectNameInput)
+    }
+    async labelInput(){
+        await this.ctx.driver.findElement(Elems.labelInput)
+    }
+    async descriptionInput(){
+        await this.ctx.driver.findElement(Elems.descriptionInput)
+    }
 }
