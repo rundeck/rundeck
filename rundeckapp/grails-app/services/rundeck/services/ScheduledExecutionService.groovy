52c6e84e29f3c0af46defd78852bffab8ed70852
carlos <carloseduardo.rorizfranco@gmail.com>
2020-01-30 15:15:58 -0300

    Add boolean filters if runJobLaterFilter param is false

--- rundeckapp/grails-app/services/rundeck/services/ScheduledExecutionService.groovy
+++ rundeckapp/grails-app/services/rundeck/services/ScheduledExecutionService.groovy
@@ -356,7 +356,7 @@ class ScheduledExecutionService implements ApplicationContextAware, Initializing
                 }
 
                 add(Restrictions.disjunction().add(Subqueries.exists(subquery)).add(restr))
-            }
+            } else if(boolfilters.any {key, val -> null!=query["${key}Filter"]}){ add(restr) }
 
             if('*'==query["groupPath"] || (('-'==query.groupPath)||!query.groupPath) && !query.groupPathExact){
                 //don't filter out any grouppath
