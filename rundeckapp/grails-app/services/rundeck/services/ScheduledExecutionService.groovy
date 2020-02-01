06dfe5bd7faa7d75d61d342fabe49bf0ea9e12b3
carlos <carloseduardo.rorizfranco@gmail.com>
2020-01-31 22:00:05 -0300

    Refactoring

--- rundeckapp/grails-app/services/rundeck/services/ScheduledExecutionService.groovy
+++ rundeckapp/grails-app/services/rundeck/services/ScheduledExecutionService.groovy
@@ -332,7 +332,9 @@ class ScheduledExecutionService implements ApplicationContextAware, Initializing
                 }
 
                 add(Restrictions.disjunction().add(Subqueries.exists(subquery)).add(restr))
-            } else if(boolfilters.any {key, val -> null!=query["${key}Filter"]}){ add(restr) }
+            } else if(restr.conditions().size() > 0){
+                add(restr)
+            }
 
             if('*'==query["groupPath"]){
                 //don't filter out any grouppath
