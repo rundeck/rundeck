066555f672db131d550e78d9bfbfaf7cb91eb7d3
niravassar <assarn@objectcomputing.com>
2020-04-13 11:14:12 -0500

    repository files upgrade only - cannot compile yet due to :core dependency

--- rundeckapp/repository/src/main/groovy/repository/RepositoryGrailsPlugin.groovy
+++ rundeckapp/repository/src/main/groovy/repository/RepositoryGrailsPlugin.groovy
@@ -11,7 +11,7 @@ import grails.plugins.*
 class RepositoryGrailsPlugin extends Plugin {
 
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "3.3.8 > *"
+    def grailsVersion = "4.0.3 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
