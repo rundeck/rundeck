25af9ce13b73fc948e646cd5eaf1192ad021f161
niravassar <assarn@objectcomputing.com>
2020-04-13 16:15:01 -0500

    rundeckapp files upgrade. gradlew builds, but app does not run

--- rundeckapp/grails-app/init/rundeckapp/Application.groovy
+++ rundeckapp/grails-app/init/rundeckapp/Application.groovy
@@ -97,7 +97,7 @@ class Application extends GrailsAutoConfiguration implements EnvironmentAware {
 
     void loadGroovyRundeckConfigIfExists(final Environment environment) {
         String rundeckGroovyConfigFile = System.getProperty(RundeckInitConfig.SYS_PROP_RUNDECK_SERVER_CONFIG_DIR) +
-                                         "/rundeck-config.groovy"
+                "/rundeck-config.groovy"
 
         if (System.getProperty(RundeckInitConfig.SYS_PROP_RUNDECK_CONFIG_LOCATION).endsWith(".groovy")) {
             // if SYS_PROP_RUNDECK_CONFIG_LOCATION is set, get .groovy file from there
