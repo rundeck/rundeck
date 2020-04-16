42d4a18e1e2828cfeda2d95d18fee89f10c73c10
Stephen Joyner <stephen@rundeck.com>
2020-04-16 12:23:59 -0500

    Remove empty test file

--- rundeckapp/src/test/groovy/rundeck/ProjectPropertiesTagLibSpec.groovy
+++
@@ -1,8 +0,0 @@
-package rundeck
-
-import grails.testing.web.taglib.TagLibUnitTest
-import spock.lang.Specification
-
-class ProjectPropertiesTagLibSpec extends Specification implements TagLibUnitTest<ProjectPropertiesTagLib> {
-    
-}
