3bb3b46dc8c298c8a3076885ba21dfd84fb00adf
ProTip <greg.zapp@gmail.com>
2020-05-19 16:05:44 -0500

    Disable index html generation

--- rundeckapp/grails-spa/vue.config.js
+++ rundeckapp/grails-spa/vue.config.js
@@ -2,6 +2,22 @@ const Path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
+  pages: {
+    'components/central': { entry: './src/components/central/main.ts'},
+    'components/execution-log': { entry: './src/components/execution-log/main.js'},
+    'components/motd': { entry: './src/components/motd/main.js'},
+    'components/tour': { entry: './src/components/tour/main.js'},
+    'components/version-notification': { entry: './src/components/version-notification/main.js'},
+    'components/community-news-notification': { entry: './src/components/community-news-notification/main.js'},
+    'pages/project-dashboard': { entry: './src/pages/project-dashboard/main.js'},
+    'pages/project-activity': { entry: './src/pages/project-activity/main.js'},
+    'pages/repository': { entry: './src/pages/repository/main.js'},
+    'pages/community-news': { entry: './src/pages/community-news/main.js'},
+    'pages/project-nodes-config': { entry: './src/pages/project-nodes-config/main.js'},
+    'pages/webhooks': { entry: './src/pages/webhooks/main.js'},
+    'pages/user-summary': {entry: './src/pages/menu/main.js'}
+  },
+
   outputDir: process.env.VUE_APP_OUTPUT_DIR,
   publicPath: './',
   assetsDir: 'static',
@@ -17,20 +33,13 @@ module.exports = {
       }
     }
   },
-  pages: {
-    'components/central': { entry: './src/components/central/main.ts'},
-    'components/execution-log': { entry: './src/components/execution-log/main.js'},
-    'components/motd': { entry: './src/components/motd/main.js'},
-    'components/tour': { entry: './src/components/tour/main.js'},
-    'components/version-notification': { entry: './src/components/version-notification/main.js'},
-    'components/community-news-notification': { entry: './src/components/community-news-notification/main.js'},
-    'pages/project-dashboard': { entry: './src/pages/project-dashboard/main.js'},
-    'pages/project-activity': { entry: './src/pages/project-activity/main.js'},
-    'pages/repository': { entry: './src/pages/repository/main.js'},
-    'pages/community-news': { entry: './src/pages/community-news/main.js'},
-    'pages/project-nodes-config': { entry: './src/pages/project-nodes-config/main.js'},
-    'pages/webhooks': { entry: './src/pages/webhooks/main.js'},
-    'pages/user-summary': {entry: './src/pages/menu/main.js'}
+
+  chainWebpack: config => {
+    config.entryPoints.store.forEach( (_, entry) => {
+      config.plugins.delete(`html-${entry}`)
+      config.plugins.delete(`preload-${entry}`)
+      config.plugins.delete(`prefetch-${entry}`)
+    })
   },
   configureWebpack: {
     devtool: 'eval-source-map',
