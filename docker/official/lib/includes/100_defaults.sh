90ae9825c699b52e57ce0bd6487e7d1080d8e00d
ProTip <greg.zapp@gmail.com>
2020-05-26 17:14:40 -0500

    Update Docker base image to Ubuntu 18.04
    Update to OpenJDK 11

--- docker/official/lib/includes/100_defaults.sh
+++ docker/official/lib/includes/100_defaults.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 # Hard-coded default for OSS so scheduler is not confused; set manually for multi-node deployments
 RUNDECK_SERVER_UUID="${RUNDECK_SERVER_UUID:-a14bc3e6-75e8-4fe4-a90d-a16dcc976bf6}"
 
-JVM_MAX_RAM_FRACTION="${JVM_MAX_RAM_FRACTION:-1}"
+JVM_MAX_RAM_PERCENTAGE="${JVM_MAX_RAM_FRACTION:-75}"
