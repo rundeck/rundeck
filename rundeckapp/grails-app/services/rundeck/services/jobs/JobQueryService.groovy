665f15eb2c0c4d3cab334458f6ad1466a125291f
carlos <carlos.franco@rundeck.com>
2020-06-04 08:10:04 -0300

    Fix issue/1019 where running jobs is aways zero even if there are jobs running.
    (cherry picked from commit 141afcdfc3b8e65d6f81757d3ebf5129fb133703)

--- rundeckapp/grails-app/services/rundeck/services/jobs/JobQueryService.groovy
+++ rundeckapp/grails-app/services/rundeck/services/jobs/JobQueryService.groovy
@@ -2,8 +2,13 @@ package rundeck.services.jobs
 
 import com.dtolabs.rundeck.app.support.ScheduledExecutionQuery
 import com.dtolabs.rundeck.core.plugins.configuration.Property
+import org.hibernate.criterion.DetachedCriteria
+import org.hibernate.criterion.Projections
+import org.hibernate.criterion.Restrictions
+import org.hibernate.criterion.Subqueries
 import org.rundeck.app.components.jobs.JobQuery
 import org.rundeck.app.components.jobs.JobQueryInput
+import rundeck.Execution
 
 class JobQueryService implements JobQuery {
 
@@ -23,10 +28,23 @@ class JobQueryService implements JobQuery {
 class LocalJobQueryService {
 
     Map extendCriteria(JobQueryInput input, Map params, Object delegate) {
+        def restr = Restrictions.conjunction()
         ScheduledExecutionQuery.IS_SCHEDULED_FILTER.each { key, val ->
             if(null!=input["${key}Filter"]){
-                delegate.eq(val,input["${key}Filter"])
+                restr.add(Restrictions.eq(val,input["${key}Filter"]))
             }
+        }
+        if(params.runJobLaterFilter){
+            Date now = new Date()
+            DetachedCriteria subquery = DetachedCriteria.forClass(Execution.class, "e").with{
+                setProjection Projections.property('e.id')
+                add(Restrictions.gt('e.dateStarted', now))
+                add Restrictions.conjunction().
+                        add(Restrictions.eqProperty('e.scheduledExecution.id', 'this.id'))
+            }
+            delegate.criteria.add(Restrictions.disjunction().add(Subqueries.exists(subquery)).add(restr))
+        } else if(restr.conditions().size() > 0){
+            delegate.criteria.add(restr)
         }
         //this returns an empty map since schedules is already considered as part of the attributes to show as part of the search
         //ergo, no need to add new values
