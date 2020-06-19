ebaab354e134585662f247a00df3a10a15711d38
ProTip <greg.zapp@gmail.com>
2020-06-19 01:11:57 -0500

    Load less styles

--- rundeckapp/grails-spa/packages/ui-trellis/babel.config.js
+++ rundeckapp/grails-spa/packages/ui-trellis/babel.config.js
@@ -5,6 +5,6 @@ module.exports = {
         }]
     ],
     plugins: [
-        ["import", { "libraryName": "ant-design-vue", "libraryDirectory": "es", "style": "css" }]
+        ["import", { "libraryName": "ant-design-vue", "libraryDirectory": "es", "style": true }]
     ]
 };
