0732f617b27a8fea4e3aedb847d84a27ce437e14
ProTip <greg.zapp@gmail.com>
2020-06-29 15:46:29 -0500

    Display no output if step never started

--- rundeckapp/grails-app/assets/javascripts/executionStateKO.js
+++ rundeckapp/grails-app/assets/javascripts/executionStateKO.js
@@ -1129,7 +1129,11 @@ function NodeFlowViewModel(workflow, outputUrl, nodeStateUpdateUrl, multiworkflo
     self.showOutput= function (nodestep) {
         if (!window._rundeck.feature?.legacyExecOutputViewer?.enabled) {
             /** Kick the event out to Vue Land and let the new viewer handle display */
-            window._rundeck.eventBus.$emit('ko-exec-show-output', nodestep)
+            if (nodestep.executionState() != 'NOT_STARTED')
+                window._rundeck.eventBus.$emit('ko-exec-show-output', nodestep)
+            else
+                nodestep.outputLineCount(0)
+
             return
         }
 
