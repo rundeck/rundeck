26828ac5eb6851f5653e31bcb2a2193e1172ff58
Greg Schueler <greg.schueler@gmail.com>
2020-08-21 15:58:06 -0700

    Add taglibs for typed config values...with caveat
    caveat: the taglibs always return Strings in gsps :(
    however, they do parse and validate input from configs so that
    the returned String can be converted back to Integer for example

--- rundeckapp/src/test/groovy/rundeck/ConfigTagLibSpec.groovy
+++ rundeckapp/src/test/groovy/rundeck/ConfigTagLibSpec.groovy
@@ -3,6 +3,7 @@ package rundeck
 import grails.testing.web.taglib.TagLibUnitTest
 import rundeck.services.ConfigurationService
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class ConfigTagLibSpec extends Specification implements TagLibUnitTest<ConfigTagLib> {
 
@@ -26,5 +27,81 @@ class ConfigTagLibSpec extends Specification implements TagLibUnitTest<ConfigTag
         key | val
         "one" | "val-one"
         "two" | ""
+    }
+
+    @Unroll
+    def "get string"() {
+        given:
+            tagLib.configurationService = Mock(ConfigurationService) {
+                getString("test.config.val", sdefVal) >> {
+                    sconfVal != null ? sconfVal : it[1]
+                }
+            }
+        when:
+            def result = tagLib.getString(config: 'test.config.val', default: sdefVal)
+        then:
+            result == sexpected
+
+        where:
+            sconfVal | sdefVal | sexpected
+            'abc'    | null    | 'abc'
+            'xyz'    | 'asdf'  | 'xyz'
+            null     | 'asdf'  | 'asdf'
+    }
+
+    @Unroll
+    def "get boolean"() {
+        given:
+            tagLib.configurationService = Mock(ConfigurationService) {
+                getBoolean("test.config.val", bdefVal) >> {
+                    bconfVal != null ? bconfVal : it[1]
+                }
+            }
+        when:
+            def bresult = tagLib.getBoolean([config: 'test.config.val', default: bdefVal])
+        then:
+            bresult == bexpected
+
+        where:
+            bconfVal | bdefVal | bexpected
+            true     | false   | 'true'
+            false    | true    | 'false'
+            null     | true    | 'true'
+            null     | false   | 'false'
+    }
+    @Unroll
+    def "get integer"() {
+        given:
+            tagLib.configurationService = Mock(ConfigurationService) {
+                getInteger("test.config.val", idefVal) >> {
+                    iconfVal != null ? iconfVal : it[1]
+                }
+            }
+        when:
+            def iresult = tagLib.getInteger([config: 'test.config.val', default: idefVal])
+        then:
+            iresult == iexpected
+
+        where:
+            iconfVal | idefVal | iexpected
+            1        | 2       | '1'
+            null     | 2       | '2'
+    }
+
+    def "get integer numberformat exception"() {
+        given:
+            tagLib.configurationService = Mock(ConfigurationService) {
+                getInteger("test.config.val", idefVal) >> {
+                    throw new NumberFormatException('expected exception')
+                }
+            }
+        when:
+            def iresult = tagLib.getInteger([config: 'test.config.val', default: idefVal])
+        then:
+            iresult == iexpected
+
+        where:
+            idefVal | iexpected
+            2       | '2'
     }
 }
