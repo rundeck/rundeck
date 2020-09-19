de34bc3c59347b835293db1a6da6ffd83c0690c6
Greg Schueler <greg.schueler@gmail.com>
2020-09-18 18:29:27 -0700

    enable notificationsEditorVue feature in production

--- rundeckapp/grails-app/conf/application.groovy
+++ rundeckapp/grails-app/conf/application.groovy
@@ -73,6 +73,7 @@ environments {
         rundeck.feature.cleanExecutionsHistoryJob.enabled = true
         rundeck.feature.executionLifecyclePlugin.enabled = true
         rundeck.feature.legacyExecOutputViewer.enabled = false
+        rundeck.feature.notificationsEditorVue.enabled = true
         dataSource {
             dbCreate = "update"
             url = "jdbc:h2:file:/rundeck/grailsh2"
