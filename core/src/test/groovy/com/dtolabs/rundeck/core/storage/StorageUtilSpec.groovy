05fd690cc302e064142dfe07b751b406934865b5
Greg Schueler <greg.schueler@gmail.com>
2020-11-19 10:10:43 -0800

    test for recursive delete

--- core/src/test/groovy/com/dtolabs/rundeck/core/storage/StorageUtilSpec.groovy
+++ core/src/test/groovy/com/dtolabs/rundeck/core/storage/StorageUtilSpec.groovy
@@ -1,5 +1,8 @@
 package com.dtolabs.rundeck.core.storage
 
+import org.rundeck.storage.api.PathUtil
+import org.rundeck.storage.api.Resource
+import org.rundeck.storage.api.Tree
 import spock.lang.Specification
 
 class StorageUtilSpec extends Specification {
@@ -31,5 +34,43 @@ class StorageUtilSpec extends Specification {
             def content = StorageUtil.withStream(new ByteArrayInputStream(''.bytes), [:])
         expect:
             content.contentLength == -1
+    }
+
+    void " delete path recursively"(){
+        setup:
+            def tree = Stub(Tree){
+                1*hasResource("projects/test1") >> false
+                1*hasResource("projects/test1/etc") >> false
+                1*hasDirectory("projects/test1") >> true
+                1*hasDirectory("projects/test1/etc") >> true
+                1*deleteResource("projects/test1/file1") >> true
+                1*deleteResource("projects/test1/file2") >> true
+                1*deleteResource("projects/test1/etc/project.properties") >> true
+                1*listDirectory("projects/test1") >> [
+                    Stub(Resource){
+                        isDirectory()>>false
+                        getPath()>>PathUtil.asPath("projects/test1/file1")
+                    },
+                    Stub(Resource){
+                        isDirectory()>>false
+                        getPath()>>PathUtil.asPath("projects/test1/file2")
+                    },
+                    Stub(Resource){
+                        isDirectory()>>true
+                        getPath()>>PathUtil.asPath("projects/test1/etc")
+                    }
+                ]
+                1*listDirectory("projects/test1/etc") >> [
+                    Stub(Resource){
+                        isDirectory()>>false
+                        getPath()>>PathUtil.asPath("projects/test1/etc/project.properties")
+                    }
+                ]
+            }
+        when:
+           def result= StorageUtil.deletePathRecursive(tree, PathUtil.asPath('projects/test1'))
+        then:
+            result
+
     }
 }
