99f5f6473b7b3b19484f7ec829c9d1a6c02e2355
carlos <carlos.franco@rundeck.com>
2021-01-12 15:26:57 -0300

    Fixing to get schedules both via addon (enterprise-schedules) and via job scheduling (job edit page)

--- rundeckapp/grails-app/controllers/rundeck/controllers/MenuController.groovy
+++ rundeckapp/grails-app/controllers/rundeck/controllers/MenuController.groovy
@@ -3163,7 +3163,7 @@ class MenuController extends ControllerBase implements ApplicationContextAware{
         }
 
 
-        def list = ScheduledExecution.findAllByServerNodeUUIDAndScheduled(uuid,true)
+        def list = jobSchedulesService.getAllScheduled(uuid, null)
         //filter authorized jobs
         Map<String, UserAndRolesAuthContext> projectAuths = [:]
         def authForProject = { String project ->
