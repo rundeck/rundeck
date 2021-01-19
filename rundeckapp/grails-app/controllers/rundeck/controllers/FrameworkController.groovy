1bb8d802f9b7854b0ccf85b8bb7094d174db075c
chrismcg14 <chrismcgilbert@gmail.com>
2021-01-19 10:16:14 -0700

    uploading changes to allow for node health checks on by default

--- rundeckapp/grails-app/controllers/rundeck/controllers/FrameworkController.groovy
+++ rundeckapp/grails-app/controllers/rundeck/controllers/FrameworkController.groovy
@@ -903,7 +903,7 @@ class FrameworkController extends ControllerBase implements ApplicationContextAw
         def pconfigurable = frameworkService.validateProjectConfigurableInput(
                 params.extraConfig,
                 'extraConfig.',
-                { String category -> category != 'resourceModelSource' }
+                null
         )
         if (pconfigurable.errors) {
             errors.addAll(pconfigurable.errors)
