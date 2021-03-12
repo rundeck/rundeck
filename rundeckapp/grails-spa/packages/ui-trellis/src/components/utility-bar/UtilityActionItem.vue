caccbeaa965d8a645b577b96cba21861642d6185
ProTip <greg.zapp@gmail.com>
2021-03-12 23:05:39 +0000

    Fix for rdicon icon sizes in util bar

--- rundeckapp/grails-spa/packages/ui-trellis/src/components/utility-bar/UtilityActionItem.vue
+++ rundeckapp/grails-spa/packages/ui-trellis/src/components/utility-bar/UtilityActionItem.vue
@@ -1,6 +1,6 @@
 <template>
     <li :id="item.id" class="utility-bar__item" @click="item.action">
-        <i :class="item.class"/>
+        <i class="utility-bar__item-icon" :class="item.class"/>
         <span>{{item.label}}</span>
         <span v-if="item.count" class="utility-bar__item-counter">{{item.count}}</span>
     </li>
--- rundeckapp/grails-spa/packages/ui-trellis/src/components/utility-bar/UtilityWidgetItem.vue
+++ rundeckapp/grails-spa/packages/ui-trellis/src/components/utility-bar/UtilityWidgetItem.vue
@@ -1,6 +1,6 @@
 <template>
     <li :id="item.id" class="utility-bar__item" @click="handleClick">
-        <i :class="item.class"/>
+        <i class="utility-bar__item-icon" :class="item.class"/>
         <span>{{item.label}}</span>
         <span v-if="item.count" class="utility-bar__item-counter">{{item.count}}</span>
         <Popper v-if="open" @close="close">
