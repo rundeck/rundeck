36919a4551a1030f9860ef9823707f16a6501f99
ProTip <greg.zapp@gmail.com>
2021-03-12 01:24:12 +0000

    Optional chaining has poor support ATM

--- rundeckapp/grails-spa/packages/ui-trellis/src/stores/NavBar.ts
+++ rundeckapp/grails-spa/packages/ui-trellis/src/stores/NavBar.ts
@@ -18,7 +18,7 @@ export class NavBar {
     }
 
     constructor(readonly root: RootStore, readonly client: RundeckClient) {
-        if (window._rundeck?.navbar) {
+        if (window._rundeck.navbar) {
             window._rundeck.navbar.items.forEach(i => {
                 this.items.push({...i, visible: true, container: i.container || 'root'})
             })
