ad576f4914a81f3462f27e318dfba4a7acf327db
carlos <carlos.franco@rundeck.com>
2021-03-15 16:01:28 -0300

    Removing codes

--- rundeckapp/src/test/groovy/rundeck/ReferencedExecutionSpec.groovy
+++ rundeckapp/src/test/groovy/rundeck/ReferencedExecutionSpec.groovy
@@ -1,34 +1,6 @@
-/*
- * Copyright 2016 SimplifyOps, Inc. (http://simplifyops.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package rundeck
 
-import com.dtolabs.rundeck.core.common.Framework
-import com.dtolabs.rundeck.core.common.NodeEntryImpl
-import com.dtolabs.rundeck.core.common.NodeSetImpl
 import grails.test.hibernate.HibernateSpec
-import org.rundeck.app.authorization.AppAuthContextProcessor
-import rundeck.services.FrameworkService
-import rundeck.services.NotificationService
-import rundeck.services.OrchestratorPluginService
-import rundeck.services.PluginService
-import rundeck.services.ScheduledExecutionService
-import rundeck.services.feature.FeatureService
-
-import static org.junit.Assert.*
 
 /**
  * Created by greg on 10/21/15.
