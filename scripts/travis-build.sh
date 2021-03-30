cf5578726fb37f4f4c54f9ad00547eba25d79aef
ProTip <greg.zapp@gmail.com>
2021-03-30 22:33:58 +0000

    Setup release publishing of existing artifacts via OSSHR
    Refactor exported libs code
    
    Sign existing artifacts
    
    Upload existing artifacts with signatures
    
    (cherry picked from commit 2097d8eec1aa1e3012ce1883f60723a747957768)

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -34,23 +34,26 @@ buildFork() {
 buildDocker() {
     docker_login
 
-    local CLEAN_TAG=$(echo $RUNDECK_BRANCH | tr '/' '-')
+    local CLEAN_TAG=$(echo "${RUNDECK_BRANCH}" | tr '/' '-')
     local BRANCH_AS_TAG=branch${CLEAN_TAG}
 
     local ECR_BUILD_TAG=${ECR_REPO}:${ECR_IMAGE_PREFIX}-build-${RUNDECK_BUILD_NUMBER}
     local ECR_BRANCH_TAG=${ECR_REPO}:${BRANCH_AS_TAG}
 
-    local CI_BRANCH_TAG=rundeck/ci:${CLEAN_TAG}
-
     ./gradlew officialBuild -Penvironment=${ENV} -PdockerTags=latest,SNAPSHOT
 
     docker tag rundeck/rundeck:latest $ECR_BUILD_TAG
     docker tag rundeck/rundeck:latest $ECR_BRANCH_TAG
-    docker tag rundeck/rundeck:latest $CI_BRANCH_TAG
 
     docker push $ECR_BUILD_TAG
     docker push $ECR_BRANCH_TAG
-    docker push $CI_BRANCH_TAG
+
+    # CircleCI tag builds do not have a branch set
+    if [[ ! -z "${CLEAN_TAG}" ]] ; then
+      local CI_BRANCH_TAG=rundeck/ci:${CLEAN_TAG}
+      docker tag rundeck/rundeck:latest $CI_BRANCH_TAG
+      docker push $CI_BRANCH_TAG
+    fi
 
     if [[ "${RUNDECK_MAIN_BUILD}" = true && -z "${RUNDECK_TAG}" ]] ; then
         ./gradlew officialPush -PdockerTags=SNAPSHOT
@@ -66,13 +69,13 @@ publish() {
     ./gradlew \
         -Penvironment="${ENV}" \
         -PdryRun="${DRY_RUN}" \
-        -PbintrayUseExisting="true" \
-        -PbintrayUser="${BINTRAY_USER}" \
-        -PbintrayApiKey="${BINTRAY_API_KEY}" \
+        -Psigning.secretKeyRingFile="$(realpath ./ci-resources/secring.gpg)" \
+        -Psigning.password="${RUNDECK_SIGNING_PASSWORD}" \
+        -Psigning.keyId="${RUNDECK_SIGNING_KEYID}" \
         -PsigningPassword="${RUNDECK_SIGNING_PASSWORD}" \
         -PsonatypeUsername="${SONATYPE_USERNAME}" \
         -PsonatypePassword="${SONATYPE_PASSWORD}" \
-        bintrayUpload --info
+        uploadExisting --info
 }
 
 publish_oss() {
