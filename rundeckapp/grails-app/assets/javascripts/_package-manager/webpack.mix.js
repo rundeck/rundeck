07d783efcfcfbd79c8dd5719dac6873ab613b561
mishingo <mr@mishingo.com>
2021-04-27 21:58:45 -0400

    add update for bootstra 3.4.0

--- rundeckapp/grails-app/assets/javascripts/_package-manager/webpack.mix.js
+++ rundeckapp/grails-app/assets/javascripts/_package-manager/webpack.mix.js
@@ -4,7 +4,7 @@ let mix = require('laravel-mix');
 
 mix
     .copy('node_modules/jquery/dist/jquery.min.js', '../jquery.js')
-    //.copy('node_modules/bootstrap/dist/js/bootstrap.min.js', '../bootstrap-all.js')
+    .copy('node_modules/bootstrap/dist/js/bootstrap.js', '../bootstrap/bootstrap.js')
     .copy('node_modules/knockout/build/output/knockout-latest.js', '../knockout.min.js')
     .copy('node_modules/knockout-mapping/dist/knockout.mapping.min.js', '../knockout-mapping.js')
     .copy('node_modules/perfect-scrollbar/dist/perfect-scrollbar.min.js', '../perfect-scrollbar.js')
