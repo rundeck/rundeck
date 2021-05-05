511d55c8ab02255ba9432d8abd74d54d672158c6
Greg Schueler <greg.schueler@gmail.com>
2021-05-05 16:27:02 -0700

    Fix: add db migration for preserveQuotes column

--- rundeckapp/grails-app/migrations/core/WorkflowStep.groovy
+++ rundeckapp/grails-app/migrations/core/WorkflowStep.groovy
@@ -9,4 +9,15 @@ databaseChangeLog = {
             column(name: "error_handler_id")
         }
     }
+
+    changeSet(author: "greg (generated)", id: "1620252002774-1") {
+        preConditions(onFail: "MARK_RAN"){
+            not{
+                columnExists (tableName:"workflow_step", columnName: "preserve_quotes")
+            }
+        }
+        addColumn(tableName: "workflow_step") {
+            column(name: "preserve_quotes", type: '${boolean.type}')
+        }
+    }
 }
