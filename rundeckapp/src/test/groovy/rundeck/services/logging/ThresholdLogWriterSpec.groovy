ded58358b78866b38e17f5f33c26e480cd6682fe
Alexander Abarca <alexander@variacode.com>
2021-08-17 18:00:53 -0400

    Changed test from Junit to Spock

--- rundeckapp/src/test/groovy/rundeck/services/logging/ThresholdLogWriterSpec.groovy
+++ rundeckapp/src/test/groovy/rundeck/services/logging/ThresholdLogWriterSpec.groovy
@@ -18,17 +18,11 @@ package rundeck.services.logging
 
 import com.dtolabs.rundeck.core.logging.LogEvent
 import com.dtolabs.rundeck.core.logging.LogUtil
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
 import rundeck.services.execution.ValueHolder
-import org.junit.Assert
+import spock.lang.Specification
 
-/**
- * Created by greg on 9/21/15.
- */
-@RunWith(JUnit4)
-class ThresholdLogWriterTest  {
+class ThresholdLogWriterSpec extends Specification  {
+
     class testLogWriter extends NoopLogWriter {
         List<String> messages = []
 
@@ -42,9 +36,8 @@ class ThresholdLogWriterTest  {
         Long value
     }
 
-
-    @Test
-    void testLogOutputOnThresholdReached() {
+    void "test Log Output On Threshold Reached"() {
+        given:
         def t = new LoggingThreshold()
         t.description = 'thresholddesc'
         t.maxValue = 1
@@ -54,6 +47,7 @@ class ThresholdLogWriterTest  {
         def logger = new testLogWriter()
         def w = new ThresholdLogWriter(logger, t)
 
+        when:
         w.addEvent(LogUtil.logNormal("message1"))
         valueHolder.value = 2 //exceed threshold
         w.addEvent(LogUtil.logNormal("message2"))
@@ -61,15 +55,13 @@ class ThresholdLogWriterTest  {
         w.addEvent(LogUtil.logNormal("message4"))
         w.addEvent(LogUtil.logNormal("message5"))
 
-        Assert.assertEquals(6, logger.messages.size())
-
-        Assert.assertEquals(
-                ["message1", "message2", "Log output limit exceeded: thresholddesc", "message3", "message4", "message5"],
-                logger.messages
-        )
+        then:
+        logger.messages.size() == 6
+        logger.messages == ["message1", "message2", "Log output limit exceeded: thresholddesc, job execution: Unknown", "message3", "message4", "message5"]
     }
-    @Test
-    void testTruncate() {
+
+    void "test Truncate"() {
+        given:
         def t = new LoggingThreshold()
         t.description = 'thresholddesc'
         t.maxValue = 1
@@ -80,6 +72,7 @@ class ThresholdLogWriterTest  {
         def logger = new testLogWriter()
         def w = new ThresholdLogWriter(logger, t)
 
+        when:
         w.addEvent(LogUtil.logNormal("message1"))
         valueHolder.value = 2 //exceed threshold
         w.addEvent(LogUtil.logNormal("message2"))
@@ -87,8 +80,8 @@ class ThresholdLogWriterTest  {
         w.addEvent(LogUtil.logNormal("message4"))
         w.addEvent(LogUtil.logNormal("message5"))
 
-        Assert.assertEquals(3, logger.messages.size())
-        Assert.assertEquals(["message1", "message2", "Log output limit exceeded: thresholddesc"], logger.messages)
-
+        then:
+        logger.messages.size() == 3
+        logger.messages == ["message1", "message2", "Log output limit exceeded: thresholddesc, job execution: Unknown"]
     }
 }
