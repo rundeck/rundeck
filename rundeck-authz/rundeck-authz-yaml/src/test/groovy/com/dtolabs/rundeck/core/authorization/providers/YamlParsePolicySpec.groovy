74e036b06f6a628f3de342f2c10f550f9cf982f1
Greg Schueler <greg.schueler@gmail.com>
2021-08-18 13:14:08 -0700

    Fix: by: urn list is not working

--- rundeck-authz/rundeck-authz-yaml/src/test/groovy/com/dtolabs/rundeck/core/authorization/providers/YamlParsePolicySpec.groovy
+++ rundeck-authz/rundeck-authz-yaml/src/test/groovy/com/dtolabs/rundeck/core/authorization/providers/YamlParsePolicySpec.groovy
@@ -16,9 +16,12 @@
 
 package com.dtolabs.rundeck.core.authorization.providers
 
+import com.dtolabs.rundeck.core.authorization.ValidationSet
+import com.dtolabs.rundeck.core.authorization.providers.yaml.model.ACLPolicyDoc
 import com.dtolabs.rundeck.core.authorization.providers.yaml.model.YamlPolicyDocConstructor
 import org.yaml.snakeyaml.Yaml
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class YamlParsePolicySpec extends Specification {
     final static String buggyDoc = '''
@@ -67,5 +70,47 @@ by:
         where:
         content  | _
         buggyDoc | _
+    }
+
+    @Unroll
+    def "createYamlPolicy by clause validation"() {
+        given:
+            def doc = new ACLPolicyDoc()
+            doc.setDescription("a description")
+
+            def context = new ACLPolicyDoc.Context()
+            context.setProject("project")
+            doc.setContext(context)
+
+            def rule = new ACLPolicyDoc.TypeRule()
+            rule.setAllow("*")
+            doc.setFor([resource:[
+                rule
+            ]])
+            doc.setBy(new ACLPolicyDoc.By())
+            doc.by.setGroup(group)
+            doc.by.setUrn(urn)
+            doc.by.setUsername(username)
+            def validation = new ValidationSet()
+
+        when:
+            Policy result = YamlParsePolicy.createYamlPolicy(null, doc, 'test', 0, validation)
+        then:
+            result.groups==(rGroups?:new HashSet<String>())
+            result.urns==(rUrns?:new HashSet<String>())
+            result.usernames==(rUsers?:new HashSet<String>())
+        where:
+            group | urn  | username           || rGroups | rUrns | rUsers
+            null  | null | 'auser'            || null    | null   | ['auser'].toSet()
+            null  | null | ['auser']          || null    | null   | ['auser'].toSet()
+            null  | null | ['auser', 'buser'] || null    | null   | ['auser', 'buser'].toSet()
+
+            'agroup'             | null | null || ['agroup'].toSet()           | null | null
+            ['agroup']           | null | null || ['agroup'].toSet()           | null | null
+            ['agroup', 'bgroup'] | null | null || ['agroup', 'bgroup'].toSet() | null | null
+
+            null | 'aurn'           | null || null | ['aurn'].toSet()         | null
+            null | ['aurn']         | null || null | ['aurn'].toSet()         | null
+            null | ['aurn', 'burn'] | null || null | ['aurn', 'burn'].toSet() | null
     }
 }
