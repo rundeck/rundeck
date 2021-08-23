f36cb12ce532f91b0a11fa5b4d1cf22223162fc4
Alexander Abarca <alexander@variacode.com>
2021-08-23 16:50:14 -0400

    Added logging threshold unit test

--- rundeckapp/src/test/groovy/rundeck/services/logging/LoggingThresholdSpec.groovy
+++ rundeckapp/src/test/groovy/rundeck/services/logging/LoggingThresholdSpec.groovy
@@ -1,4 +1,193 @@
 package rundeck.services.logging
 
-class LoggingThresholdSpec {
+import rundeck.ScheduledExecution
+import rundeck.services.execution.ValueHolder
+import spock.lang.Specification
+
+class LoggingThresholdSpec extends Specification {
+
+    class tHolder implements ValueHolder<Long> {
+        Long value
+    }
+
+    void "test null global output limit"() {
+        given:
+        def globalLimitMap = null
+        def jobLimitMap    = ScheduledExecution.parseLogOutputThreshold("100KB")
+        when:
+        def thresholdMap   = LoggingThreshold.getOutputLimit(globalLimitMap, jobLimitMap)
+        then:
+        thresholdMap.maxSizeBytes == 102400
+    }
+
+    void "test null job output limit"() {
+        given:
+        def globalLimitMap = ScheduledExecution.parseLogOutputThreshold("100KB")
+        def jobLimitMap    = null
+        when:
+        def thresholdMap   = LoggingThreshold.getOutputLimit(globalLimitMap, jobLimitMap)
+        then:
+        thresholdMap.maxSizeBytes == 102400
+    }
+
+    void "test different units of output limit"() {
+        given:
+        def globalLimitMap = ScheduledExecution.parseLogOutputThreshold("100/node")
+        def jobLimitMap    = ScheduledExecution.parseLogOutputThreshold("100KB")
+        when:
+        def thresholdMap   = LoggingThreshold.getOutputLimit(globalLimitMap, jobLimitMap)
+        then:
+        thresholdMap.maxLines == 100
+    }
+
+    void "test minimum output limit"() {
+        given:
+        def globalLimitMap = ScheduledExecution.parseLogOutputThreshold("100KB")
+        def jobLimitMap    = ScheduledExecution.parseLogOutputThreshold("200KB")
+        when:
+        def thresholdMap   = LoggingThreshold.getOutputLimit(globalLimitMap, jobLimitMap)
+        then:
+        thresholdMap.maxSizeBytes == 102400
+    }
+
+    void "test From Map Blank"() {
+        when:
+        def thresholdBlank = LoggingThreshold.fromMap([:], "blah")
+        def thresholdNull = LoggingThreshold.fromMap(null, "blah")
+        then:
+        thresholdBlank == null
+        thresholdNull  == null
+    }
+
+    void "test From Map No Max Value"() {
+        when:
+        def threshold = LoggingThreshold.fromMap([value: 1], "blah")
+        then:
+        threshold == null
+    }
+
+    void "test From Map Max Size"() {
+        when:
+        def threshold = LoggingThreshold.fromMap([maxSizeBytes: 1], "blah")
+        then:
+        threshold.type     == LoggingThreshold.TOTAL_FILE_SIZE
+        threshold.action   == 'blah'
+        threshold.maxValue == 1
+    }
+
+    void "test From Map Max Lines"() {
+        when:
+        def threshold = LoggingThreshold.fromMap([maxLines: 1], "blah")
+        then:
+        threshold.type     == LoggingThreshold.TOTAL_LINES
+        threshold.action   == 'blah'
+        threshold.maxValue == 1
+    }
+
+    void "test From Map Max Lines per Node False"() {
+        when:
+        def threshold = LoggingThreshold.fromMap(maxLines: 1, perNode: false, "blah")
+        then:
+        threshold.type     == LoggingThreshold.TOTAL_LINES
+        threshold.action   == 'blah'
+        threshold.maxValue == 1
+    }
+
+    void "test From Map Max Node Lines"() {
+        when:
+        def threshold = LoggingThreshold.fromMap(maxLines: 1, perNode: true, "blah")
+        then:
+        threshold.type     == LoggingThreshold.LINES_PER_NODE
+        threshold.action   == 'blah'
+        threshold.maxValue == 1
+    }
+
+    void "test Action Halt"() {
+        when:
+        def threshold = LoggingThreshold.fromMap(maxLines: 1, perNode: true, "halt")
+        then:
+        threshold.truncateOnLimitReached == false
+        threshold.haltOnLimitReached     == true
+    }
+
+    void "test Action Truncate"() {
+        when:
+        def threshold = LoggingThreshold.fromMap(maxLines: 1, perNode: true, "truncate")
+        then:
+        threshold.truncateOnLimitReached == true
+        threshold.haltOnLimitReached     == false
+    }
+
+    void "test Watcher For Type Lines Per Node"() {
+        when:
+        def threshold = LoggingThreshold.fromMap([maxLines: 1, perNode: true], "truncate")
+        then:
+        threshold != null
+        threshold.watcherForType("blah")                     == null
+        threshold.watcherForType(LoggingThreshold.TOTAL_FILE_SIZE) == null
+        threshold.watcherForType(LoggingThreshold.TOTAL_LINES)     == null
+        threshold.watcherForType(LoggingThreshold.LINES_PER_NODE)  != null
+    }
+
+    void "test Watcher For Type Max Size"() {
+        when:
+        def threshold = LoggingThreshold.fromMap([maxSizeBytes: 1], "truncate")
+        then:
+        threshold != null
+        threshold.watcherForType("blah")                     == null
+        threshold.watcherForType(LoggingThreshold.TOTAL_FILE_SIZE) != null
+        threshold.watcherForType(LoggingThreshold.TOTAL_LINES)     == null
+        threshold.watcherForType(LoggingThreshold.LINES_PER_NODE)  == null
+    }
+
+    void "test get Threshold Value noWatcher"() {
+        when:
+        def threshold = LoggingThreshold.fromMap([maxSizeBytes: 1], "truncate")
+        then:
+        threshold != null
+        threshold.value == -1
+    }
+
+    void "test get Threshold Value watcher"() {
+        when:
+        def threshold = LoggingThreshold.fromMap([maxSizeBytes: 1], "truncate")
+        def holder = new tHolder(value: 0)
+        threshold.watch(holder)
+        then:
+        holder.value    == 0
+        threshold.value == 0
+    }
+
+    void "test is Threshold Exceeded less than false"() {
+        when:
+        def threshold = LoggingThreshold.fromMap([maxSizeBytes: 2], "truncate")
+        def holder = new tHolder(value: 1)
+        threshold.watch(holder)
+        then:
+        holder.value    == 1
+        threshold.value == 1
+        threshold.isThresholdExceeded() == false
+    }
+
+    void "test is Threshold Exceeded equals false"() {
+        when:
+        def threshold = LoggingThreshold.fromMap([maxSizeBytes: 1], "truncate")
+        def holder = new tHolder(value: 1)
+        threshold.watch(holder)
+        then:
+        holder.value    == 1
+        threshold.value == 1
+        threshold.isThresholdExceeded() == false
+    }
+
+    void "test is Threshol dExceeded true"() {
+        when:
+        def threshold = LoggingThreshold.fromMap([maxSizeBytes: 1], "truncate")
+        def holder = new tHolder(value: 2)
+        threshold.watch(holder)
+        then:
+        holder.value    == 2
+        threshold.value == 2
+        threshold.isThresholdExceeded() == true
+    }
 }
