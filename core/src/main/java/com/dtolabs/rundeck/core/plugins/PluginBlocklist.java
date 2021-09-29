67dc25a84833202d8e406ac369eb12ff634eb7e2
Greg Schueler <greg.schueler@gmail.com>
2021-09-29 15:50:59 -0700

    add javadoc

--- core/src/main/java/com/dtolabs/rundeck/core/plugins/PluginBlocklist.java
+++ core/src/main/java/com/dtolabs/rundeck/core/plugins/PluginBlocklist.java
@@ -3,10 +3,19 @@ package com.dtolabs.rundeck.core.plugins;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Matches plugin file or providers against a block list
+ */
 public interface PluginBlocklist {
 
+    /**
+     * @return true if file name is present
+     */
     public Boolean isPluginFilePresent(String fileName);
 
+    /**
+     * @return true if provider name and service are present in the list
+     */
     public Boolean isPluginProviderPresent(String service, String providerName);
 
     public Boolean isBlocklistSet();
