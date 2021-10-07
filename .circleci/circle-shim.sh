49f3f8961dfc0b1be4dceec0ba0854651ab6a892
Chris McCarroll-Gilbert <chrismcg@rundeck.com>
2021-10-07 16:11:25 -0600

    delete travis, rename everything

--- .circleci/circle-shim.sh
+++ .circleci/circle-shim.sh
@@ -2,12 +2,12 @@
 # Exports environment variables and functions for use in CI environments
 set -eo pipefail
 
-export TRAVIS_BRANCH=${CIRCLE_BRANCH:-}
-export TRAVIS_TAG=${CIRCLE_TAG:-}
-export TRAVIS_BUILD_NUMBER=${CIRCLE_PIPELINE_NUM:-}
-export TRAVIS_COMMIT=${CIRCLE_SHA1:-}
+export CI_BRANCH=${CIRCLE_BRANCH:-}
+export CI_TAG=${CIRCLE_TAG:-}
+export CI_BUILD_NUMBER=${CIRCLE_PIPELINE_NUM:-}
+export CI_COMMIT=${CIRCLE_SHA1:-}
 
 S3_ARTIFACT_BASE="s3://rundeck-ci-artifacts/oss/circle/rundeck"
 ECR_IMAGE_PREFIX="circle"
 
-source scripts/travis-helpers.sh
+source scripts/circle-helpers.sh
