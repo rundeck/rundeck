5a632c71455265363fd018a5f8a134a405415ed3
Greg Schueler <greg.schueler@gmail.com>
2021-11-12 15:20:27 -0800

    Central interface that provides authorizing checks and AuthorizingAccess to resources

---
+++ rundeck-authz/rundeck-authz-core/src/main/java/org/rundeck/core/auth/app/DomainAuthorizer.java
@@ -0,0 +1,63 @@
+package org.rundeck.core.auth.app;
+
+import org.rundeck.core.auth.access.*;
+import org.rundeck.core.auth.app.type.AuthorizingSystem;
+
+import javax.security.auth.Subject;
+
+/**
+ * Authorizing access to resources
+ */
+public interface DomainAuthorizer {
+    /**
+     * Test authorized access
+     *
+     * @param subject  subject
+     * @param resolver id resolver
+     * @param request  auth request
+     * @return true if authorized
+     * @throws MissingParameter if required id parameter is missing
+     */
+    boolean isAuthorized(
+            Subject subject,
+            ResIdResolver resolver,
+            TypedNamedAuthRequest request
+    )
+            throws MissingParameter;
+
+    /**
+     * authorize access
+     *
+     * @param subject  subject
+     * @param resolver id resolver
+     * @param request  auth request
+     * @return true if authorized
+     * @throws MissingParameter if required id parameter is missing
+     */
+    void authorize(
+            Subject subject,
+            ResIdResolver resolver,
+            TypedNamedAuthRequest request
+    )
+            throws UnauthorizedAccess, NotFound, MissingParameter;
+
+    /**
+     * Get AuthorizingAccess for a resource type
+     *
+     * @param subject  subject
+     * @param resolver id resolver
+     * @param type     resource type
+     * @return authorizing access
+     * @throws MissingParameter if required id param is missing
+     */
+    AuthorizingAccess getAuthorizingAccess(Subject subject, ResIdResolver resolver, String type)
+            throws MissingParameter;
+
+    /**
+     * Authorizing system
+     *
+     * @param subject subject
+     * @return authorizing system
+     */
+    AuthorizingSystem system(Subject subject);
+}
