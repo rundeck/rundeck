5a945ecff464a224d1d74bd4b1dc3fe12a52dfcb
Greg Schueler <greg.schueler@gmail.com>
2021-11-12 15:16:16 -0800

    extend AuthorizingAccess for the System resource

---
+++ rundeck-authz/rundeck-authz-core/src/main/java/org/rundeck/core/auth/app/type/AuthorizingSystem.java
@@ -0,0 +1,11 @@
+package org.rundeck.core.auth.app.type;
+
+import org.rundeck.core.auth.access.AuthorizingAccess;
+
+/**
+ * Authorizing access to "System"
+ */
+public interface AuthorizingSystem
+    extends AuthorizingAccess
+{
+}
