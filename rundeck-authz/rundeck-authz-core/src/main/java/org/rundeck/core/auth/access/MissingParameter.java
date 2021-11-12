146bedffe62cb9931c058a576fbd03fb4763a871
Greg Schueler <greg.schueler@gmail.com>
2021-11-12 15:20:44 -0800

    Add single String constructor

--- rundeck-authz/rundeck-authz-core/src/main/java/org/rundeck/core/auth/access/MissingParameter.java
+++ rundeck-authz/rundeck-authz-core/src/main/java/org/rundeck/core/auth/access/MissingParameter.java
@@ -1,11 +1,16 @@
 package org.rundeck.core.auth.access;
 
+import java.util.Collections;
 import java.util.List;
 
 public class MissingParameter
         extends Exception
 {
     private final List<String> parameters;
+
+    public MissingParameter(String parameter) {
+        this(Collections.singletonList(parameter));
+    }
 
     public MissingParameter(final List<String> parameters) {
         super("Required parameters were missing: " + String.join(", ", parameters));
