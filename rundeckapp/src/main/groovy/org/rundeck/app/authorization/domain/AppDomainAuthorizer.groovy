7d33a0c1f3c102e5b7fce0f7a563da97c1654f02
Greg Schueler <greg.schueler@gmail.com>
2021-11-22 16:36:17 -0800

    cleanup

--- rundeckapp/src/main/groovy/org/rundeck/app/authorization/domain/AppDomainAuthorizer.groovy
+++ rundeckapp/src/main/groovy/org/rundeck/app/authorization/domain/AppDomainAuthorizer.groovy
@@ -1,9 +1,8 @@
 package org.rundeck.app.authorization.domain
 
-import org.rundeck.app.authorization.domain.execution.AuthorizingExecution
 import org.rundeck.app.auth.types.AuthorizingProject
+import org.rundeck.app.authorization.domain.execution.AuthorizingExecution
 import org.rundeck.app.authorization.domain.job.AuthorizingJob
-import org.rundeck.core.auth.app.type.AuthorizingProjectAdhoc
 import org.rundeck.core.auth.access.ResIdResolver
 import org.rundeck.core.auth.app.DomainAuthorizer
 
