bf43ec85e6531c955b699e692690f3aad87a9f4d
L2JE <leojesus.juarez@gmail.com>
2022-02-02 02:31:28 -0300

    Yaml tests refactored

--- rundeckapp/grails-app/domain/rundeck/ScheduledExecution.groovy
+++ rundeckapp/grails-app/domain/rundeck/ScheduledExecution.groovy
@@ -389,8 +389,8 @@ class ScheduledExecution extends ExecutionContext implements EmbeddedJsonData {
                     trigger.add(map1)
                 }
             }
-            map.keySet().findAll{it.startsWith('on')}.each { trigger ->
-                map.notification[trigger].find({ notif -> notif.plugin }).plugin.sort{ a, b -> a.type <=> b.type }
+            map.notification.keySet().findAll{it.startsWith('on')}.each { trigger ->
+                map.notification[trigger].find{ notif -> notif.plugin }.plugin.sort{ a, b -> a.type <=> b.type }
             }
             map.notifyAvgDurationThreshold = notifyAvgDurationThreshold
         }
