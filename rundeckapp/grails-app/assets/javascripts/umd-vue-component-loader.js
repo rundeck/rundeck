fa9a04d8d0db6d1fd9f08755ab75566c91b736a9
Miguel Ramos <mr@mishingo.com>
2022-02-08 16:33:08 -0500

    add storybook

--- rundeckapp/grails-app/assets/javascripts/umd-vue-component-loader.js
+++ rundeckapp/grails-app/assets/javascripts/umd-vue-component-loader.js
@@ -1,5 +1,5 @@
 function createVueComponentsFromUmdModule(moduleName) {
-  //console.log("attempting to init vue components for module: " + moduleName)
+  // console.log("attempting to init vue components for module: " + moduleName)
   for (let [k, comp] of Object.entries(window[moduleName])) {
     //console.log('initing: ' + k); //uncomment for debug message
     Vue.component(k,comp)
