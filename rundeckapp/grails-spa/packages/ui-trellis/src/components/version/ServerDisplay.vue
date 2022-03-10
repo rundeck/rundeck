0e3e6dec832f72480eed4c3ed73bd905f7037db7
Greg Schueler <greg.schueler@gmail.com>
2022-03-10 15:48:52 -0800

    enable some new props for server display
    nameClass: css class for name text
    showId: if false, hide uuid

--- rundeckapp/grails-spa/packages/ui-trellis/src/components/version/ServerDisplay.vue
+++ rundeckapp/grails-spa/packages/ui-trellis/src/components/version/ServerDisplay.vue
@@ -2,9 +2,9 @@
     <span :title="title">
         <span :style="{color}">
             <i class="glyphicon" :class="[`glyphicon-${glyphicon}`]" />
-            {{uuidShort}}
+            <span v-if="showId">{{ uuidShort }}</span>
         </span>
-        <span style="margin-left: 0.5em">{{name}}</span>
+        <span style="margin-left: 0.5em" :class="css">{{name}}</span>
     </span>
 </template>
 
@@ -18,9 +18,14 @@ export default Vue.extend({
     props: {
         glyphicon: String,
         uuid: String,
-        name: String
+        name: String,
+        nameClass: String,
+        showId: {default:true}
     },
     computed: {
+        css(): string {
+            return this.nameClass||''
+        },
         color(): string {
             const ver = new RundeckVersion({})
             return `#${ver.splitUUID(this.uuid)['sixes'][0]}`
