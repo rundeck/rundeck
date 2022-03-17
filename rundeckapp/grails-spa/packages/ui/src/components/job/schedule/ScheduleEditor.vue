3f63d5b13a768bc16f1d9550c92fef931fe73398
Greg Schueler <greg.schueler@gmail.com>
2022-03-17 16:38:08 -0700

    Fix: crontab tab should show by default if crontab is used

--- rundeckapp/grails-spa/packages/ui/src/components/job/schedule/ScheduleEditor.vue
+++ rundeckapp/grails-spa/packages/ui/src/components/job/schedule/ScheduleEditor.vue
@@ -299,6 +299,9 @@ export default class ScheduleEditor extends Vue {
   @Prop({required: true})
   eventBus!: Vue
 
+  @Prop({required: false, default: false})
+  useCrontabString!: boolean
+
   labelColClass = 'col-sm-2 control-label'
   fieldColSize = 'col-sm-10'
 
@@ -330,10 +333,9 @@ export default class ScheduleEditor extends Vue {
   async mounted() {
     this.modelData = Object.assign({
       selectedDays: [],
-      selectedMonths: []
+      selectedMonths: [],
+      useCrontabString: this.useCrontabString
     }, this.value)
-
-    this.showSimpleCron()
   }
 
   loadScheduleIntoSimpleTab (decomposedSchedule:any){
