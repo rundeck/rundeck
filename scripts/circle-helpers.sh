475900a13d75786bef9d3d2690c5edbddff09291
chrismcg14 <chrismcg@rundeck.com>
2022-03-23 09:23:33 -0600

    update to new syntax

--- scripts/circle-helpers.sh
+++ scripts/circle-helpers.sh
@@ -168,8 +168,10 @@ fetch_commit_common_artifacts() {
 }
 
 docker_login() {
-    docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-    $(aws ecr get-login-password --no-include-email --region us-west-2)
+      docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+#    $(aws ecr get-login-password --region us-west-2)
+      aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 481311893001.dkr.ecr.us-west-2.amazonaws.com
+
 }
 
 build_rdtest() {
