887fe6865d96f7df5c3015abd50b2a87bb0395e2
ehe-pd <ehe@pagerduty.com>
2022-04-28 12:28:24 -0700

    Remove unnecessary config

--- rundeckapp/grails-app/services/rundeck/services/actuator/RundeckReadinessHealthIndicatorService.groovy
+++ rundeckapp/grails-app/services/rundeck/services/actuator/RundeckReadinessHealthIndicatorService.groovy
@@ -3,12 +3,9 @@ package rundeck.services.actuator
 import grails.core.GrailsApplication
 import grails.events.annotation.Subscriber
 import grails.events.bus.EventBusAware
-import groovy.transform.CompileDynamic
-import groovy.transform.CompileStatic
 import org.springframework.boot.availability.AvailabilityChangeEvent
 import org.springframework.boot.availability.ReadinessState
 
-@CompileStatic
 class RundeckReadinessHealthIndicatorService implements EventBusAware {
 
     GrailsApplication grailsApplication
