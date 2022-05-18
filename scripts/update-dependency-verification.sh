497a6db680466530673d52d51d8d6f1fe260fcf0
Greg Schueler <greg.schueler@gmail.com>
2022-05-18 13:14:36 -0700

    update script

--- scripts/update-dependency-verification.sh
+++ scripts/update-dependency-verification.sh
@@ -4,17 +4,69 @@ set -euo pipefail
 
 #/ Write Dependency Verification
 #/ Update dependency verification metadata and export any new keys.
+#/ usage: sh update-dependency-verification.sh [-r][-e]
+#/
+#/ use -r to refresh missing keys
+#/ use -r to export keys
+REFRESH=0
+EXPORT=0
 
-main() {
+usage() {
+  grep '^#/' <"$0" | cut -c4- # prints the #/ lines above as usage info
+}
+die() {
+  echo >&2 "$@"
+  exit 2
+}
+
+update_sha() {
   ./gradlew --write-verification-metadata sha256 help
-  ./gradlew --write-verification-metadata pgp,sha256 --refresh-keys --export-keys help
+}
+update_pgp() {
+  ./gradlew --write-verification-metadata pgp,sha256 help
+}
+refresh_keys() {
+  ./gradlew --refresh-keys help
+}
+export_keys() {
+  ./gradlew -export-keys help
+}
+git_add() {
   rm gradle/verification-keyring.gpg
   git add gradle/verification-metadata.xml
   git add gradle/verification-keyring.keys
-
+}
+notify() {
   echo "================================"
   echo "Update complete."
   echo "Next: Commit changes to gradle/verification-metadata.xml and gradle/verification-keyring.keys"
   echo "================================"
 }
-main
+check_args(){
+  while getopts :re flag; do
+    case "${flag}" in
+    r)
+      REFRESH=1
+      ;;
+    e)
+      EXPORT=1
+      ;;
+    ?)
+      usage
+      die "unknown option $OPTARG"
+      ;;
+    esac
+  done
+}
+
+main() {
+  check_args "$@"
+  update_sha
+  update_pgp
+  if [ "$REFRESH" -eq "1" ] ; then refresh_keys ; fi
+  if [ "$EXPORT" -eq "1" ] ; then export_keys ; fi
+  git_add
+  notify
+}
+
+main "$@"
