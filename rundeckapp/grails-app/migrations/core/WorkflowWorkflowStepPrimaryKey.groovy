459eb7b2cdf163dd14d6343995c927b408f31b7e
Greg Schueler <greg.schueler@gmail.com>
2022-05-26 14:32:26 -0700

    Add primary key id to workflow_workflow_step table

---
+++ rundeckapp/grails-app/migrations/core/WorkflowWorkflowStepPrimaryKey.groovy
@@ -0,0 +1,16 @@
+databaseChangeLog = {
+    changeSet(author: "rundeckuser (generated)", id: "1653585641550-1") {
+        comment { 'add primary key to workflow_workflow_step' }
+        preConditions(onFail: 'MARK_RAN') {
+            not {
+                columnExists(tableName: 'workflow_workflow_step', columnName: 'id')
+            }
+        }
+
+        addColumn(tableName: "workflow_workflow_step") {
+            column(autoIncrement: "true", name: "id", type: '${number.type}') {
+                constraints(nullable: "false", primaryKey: "true", primaryKeyName: "workflowworkflowstepPK")
+            }
+        }
+    }
+}
