2e680ebb48e38de2dd92e67ab44a96cfc19294a5
Greg Schueler <greg.schueler@gmail.com>
2022-05-31 11:49:29 -0700

    limit to only mysql,mariadb

--- rundeckapp/grails-app/migrations/core/DBChangelogPrimaryKey.groovy
+++ rundeckapp/grails-app/migrations/core/DBChangelogPrimaryKey.groovy
@@ -1,7 +1,7 @@
 package core
 
 databaseChangeLog = {
-    changeSet(author: "rundeckuser (generated)", id: "1653665507506-1") {
+    changeSet(author: "rundeckuser (generated)", id: "1653665507506-1", dbms: 'mysql,mariadb') {
         comment { 'add primary key to DATABASECHANGELOG' }
         preConditions(onFail: 'MARK_RAN') {
             not {
