ecdee6a1a92506464ad43dc6fdc83c198870120d
siwrad <dnarvaez@ad-cap.com.ar>
2022-06-13 13:26:56 -0300

    RUN-982 Node tags being showed all in uppercase without distinction between each other until selecting them.

--- rundeckapp/grails-app/assets/javascripts/knockout-node-filter-link.js
+++ rundeckapp/grails-app/assets/javascripts/knockout-node-filter-link.js
@@ -59,7 +59,7 @@ ko.components.register('node-filter-link', {
     viewModel:NodeFilterLinkParams,
     template: '<a  class="nodefilterlink link-quiet"  href="#"  data-bind="attr: attributes($root),  css: classnames"  > \
     <span data-bind="if: linkicon"><i data-bind="css: linkicon"></i></span>\
-    <span data-bind="if: !linkicon"><span data-bind="text: viewtext(), css: textcss"></span></span>\
+    <span data-bind="if: !linkicon"><span data-bind="text: viewtext()" style="text-transform: none;"></span></span>\
     <span data-bind="if: count">(<span data-bind="text: count"></span>)</span>\
     </a>'
 });
@@ -68,7 +68,7 @@ ko.components.register('node-exclude-filter-link', {
     viewModel:NodeFilterLinkParams,
     template: '<a  class="nodeexcludefilterlink link-quiet"  href="#"  data-bind="attr: attributes($root),  css: classnames"  > \
     <span data-bind="if: linkicon"><i data-bind="css: linkicon"></i></span>\
-    <span data-bind="if: !linkicon"><span data-bind="text: viewtext(), css: textcss"></span></span> \
+    <span data-bind="if: !linkicon"><span data-bind="text: viewtext()" style="text-transform: none;"></span></span> \
     <span data-bind="if: count">(<span data-bind="text: count"></span>)</span> \
     </a>'
 });
