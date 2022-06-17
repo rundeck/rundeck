4d69cb0b8f0e60a14ab1691afa2e6bfb21800ce0
Greg Schueler <greg.schueler@gmail.com>
2022-06-17 09:30:45 -0700

    Change description
    Co-authored-by: Eric He <96143000+ehe-pd@users.noreply.github.com>

--- rundeckapp/src/main/groovy/org/rundeck/app/api/model/LinkListResponse.groovy
+++ rundeckapp/src/main/groovy/org/rundeck/app/api/model/LinkListResponse.groovy
@@ -4,7 +4,7 @@ import groovy.transform.CompileStatic
 import io.swagger.v3.oas.annotations.media.Schema
 
 @CompileStatic
-@Schema(description = "Links")
+@Schema(description = "API links")
 class LinkListResponse {
 
     @Schema(name = "_links", description = "Available named links")
