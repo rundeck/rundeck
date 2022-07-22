d186b8fa48c0d632d646f35e4ad30302d8bed764
chrismcg14 <chrismcg@rundeck.com>
2022-07-22 10:40:15 -0600

    Update path

--- docker/debinstall/entry.sh
+++ docker/debinstall/entry.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 . /rd-util.sh
 
-test -f "$HOME"/rundeck/packaging/debdist/rundeck*.deb || {
-	echo "debian not found at $HOME/rundeck/packaging/debdist/rundeck*.deb"
+test -f "$HOME"/rundeck/packaging/packaging/debdist/rundeck*.deb || {
+	echo "debian not found at $HOME/rundeck/packaging/packaging/debdist/rundeck*.deb"
 	exit 2
 }
 
-dpkg -i "$HOME"/rundeck/packaging/debdist/rundeck*.deb
+dpkg -i "$HOME"/rundeck/packaging/packaging/debdist/rundeck*.deb
 
 entry_start "$*"
