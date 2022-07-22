8b886a7599cf7a7742a93ffa591bd0206a0d71d8
chrismcg14 <chrismcg@rundeck.com>
2022-07-22 15:28:23 -0600

    add debug points

--- packaging/test/test-docker-install-common.sh
+++ packaging/test/test-docker-install-common.sh
@@ -5,20 +5,24 @@ set -euo pipefail
 readonly ARGS=("$@")
 
 list_debs(){
+  echo "----enter list debs-----"
   echo "$PWD"
 	PATTERN="build/distributions/*.${PACKAGE_TYPE}"
 	echo $PATTERN
+	echo "----leave list debs-----"
 }
 
 build(){
+  echo "----enter build debs-----"
   echo "$PWD"
 	local TAG="rdpro-$COMMON"
 	docker build -t "$TAG-util" -f packaging/test/docker/installcommon/"$COMMON".Dockerfile packaging/test/docker/installcommon
 	docker build "$DIR" -t "$TAG"
+	echo "----leave build debs-----"
 }
 
 run(){
-
+echo "----enter run debs-----"
 	local TAG="rdpro-$COMMON"
 	# for EDITION in cluster dr team ; do
 	DEBS=$(list_debs)
@@ -26,4 +30,5 @@ run(){
 		echo $DEB
 		docker run -it -v "$PWD:/home/rundeck/rundeck" -e "PACKAGE=$DEB" "$TAG":latest -test
 	done
+	echo "----leave run debs-----"
 }
