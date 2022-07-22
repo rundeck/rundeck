8bd0d4547fd5c94ccd0719b838e99f93e5011d3c
chrismcg14 <chrismcg@rundeck.com>
2022-07-22 15:32:16 -0600

    remove debugs, update path

--- packaging/test/test-docker-install-common.sh
+++ packaging/test/test-docker-install-common.sh
@@ -5,24 +5,19 @@ set -euo pipefail
 readonly ARGS=("$@")
 
 list_debs(){
-  echo "----enter list debs-----"
   echo "$PWD"
-	PATTERN="build/distributions/*.${PACKAGE_TYPE}"
+	PATTERN="packaging/packaging/build/distributions/*.${PACKAGE_TYPE}"
 	echo $PATTERN
-	echo "----leave list debs-----"
 }
 
 build(){
-  echo "----enter build debs-----"
   echo "$PWD"
 	local TAG="rdpro-$COMMON"
 	docker build -t "$TAG-util" -f packaging/test/docker/installcommon/"$COMMON".Dockerfile packaging/test/docker/installcommon
 	docker build "$DIR" -t "$TAG"
-	echo "----leave build debs-----"
 }
 
 run(){
-echo "----enter run debs-----"
 	local TAG="rdpro-$COMMON"
 	# for EDITION in cluster dr team ; do
 	DEBS=$(list_debs)
@@ -30,5 +25,4 @@ echo "----enter run debs-----"
 		echo $DEB
 		docker run -it -v "$PWD:/home/rundeck/rundeck" -e "PACKAGE=$DEB" "$TAG":latest -test
 	done
-	echo "----leave run debs-----"
 }
