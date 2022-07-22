4ee0da89162af8fe008daa2f22288260dc83311e
chrismcg14 <chrismcg@rundeck.com>
2022-07-22 10:46:16 -0600

    update deb and rpm paths

--- docker/rpminstall/entry.sh
+++ docker/rpminstall/entry.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 . /rd-util.sh
 
-if ! find "$HOME/rundeck/packaging/rpmdist/RPMS/noarch/" -name '*.rpm' ; then
-	echo "rpm not found at $HOME/rundeck/packaging/rpmdist/RPMS/noarch/rundeck*.rpm"
+if ! find "$HOME/packaging/packaging/rpmdist/RPMS/noarch/" -name '*.rpm' ; then
+	echo "rpm not found at $HOME/packaging/packaging/rpmdist/RPMS/noarch/rundeck*.rpm"
 	exit 2
 fi
 
-rpm -ivh "$HOME"/rundeck/packaging/rpmdist/RPMS/noarch/rundeck*.rpm
+rpm -ivh "$HOME"/packaging/packaging/rpmdist/RPMS/noarch/rundeck*.rpm
 
 entry_start "$*"
