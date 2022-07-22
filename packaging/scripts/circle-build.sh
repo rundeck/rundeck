c725b68924f822a40867421b54fcfd0c52500da7
chrismcg14 <chrismcg@rundeck.com>
2022-07-22 09:58:54 -0600

    update packaging path

--- packaging/scripts/circle-build.sh
+++ packaging/scripts/circle-build.sh
@@ -34,7 +34,7 @@ docker_login() {
 build() {
     local RELEASE_NUM="1"
     echo  "---------Entering fetch artifcts---------"
-    bash packaging/scripts/circle-build.sh fetch_artifacts
+    bash packaging/packaging/scripts/circle-build.sh fetch_artifacts
 #    for file in packaging/*; do
 #      echo "${file##*/}"
 #    done
