4d0e12b26996629a15a1441de18780b3ba6fe835
carlosfranco <cfranco@pagerduty.com>
2022-08-03 11:36:10 -0300

    Fix path for rundeckpro project

--- packaging/test/test-docker-install-deb.sh
+++ packaging/test/test-docker-install-deb.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 LDIR=$( cd $(dirname $0) ; echo $PWD )
-export DIR=packaging/test/docker/debinstall
-export COMMON="ubuntu1604"
-export PACKAGE_TYPE="deb"
-export PACKAGING_DIR_PARENT=""
 
 if [[ "${UPSTREAM_PROJECT}" == "rundeckpro" ]] ; then
   export PACKAGING_DIR_PARENT="rundeck/"
 fi
+
+export DIR="${PACKAGING_DIR_PARENT}"packaging/test/docker/debinstall
+export COMMON="ubuntu1604"
+export PACKAGE_TYPE="deb"
+
 
 echo "$LDIR"
 
--- packaging/test/test-docker-install-rpm.sh
+++ packaging/test/test-docker-install-rpm.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 LDIR=$( cd $(dirname $0) ; echo $PWD )
-export DIR=packaging/test/docker/rpminstall
-export COMMON="centos7"
-export PACKAGE_TYPE="rpm"
 
 if [[ "${UPSTREAM_PROJECT}" == "rundeckpro" ]] ; then
   export PACKAGING_DIR_PARENT="rundeck/"
 fi
+
+export DIR="${PACKAGING_DIR_PARENT}"packaging/test/docker/rpminstall
+export COMMON="centos7"
+export PACKAGE_TYPE="rpm"
+
 
 . "$LDIR/test-docker-install-common.sh"
 
