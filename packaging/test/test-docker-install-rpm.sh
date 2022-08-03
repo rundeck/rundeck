0c85ccd8c6fe9b19dfd1e54fd156be922b5e1d4d
carlosfranco <cfranco@pagerduty.com>
2022-08-03 11:19:48 -0300

    Fix path to work with runedeckpro project

--- packaging/test/test-docker-install-rpm.sh
+++ packaging/test/test-docker-install-rpm.sh
@@ -5,6 +5,10 @@ export DIR=packaging/test/docker/rpminstall
 export COMMON="centos7"
 export PACKAGE_TYPE="rpm"
 
+if [[ "${UPSTREAM_PROJECT}" == "rundeckpro" ]] ; then
+  export PACKAGING_DIR_PARENT="rundeck/"
+fi
+
 . "$LDIR/test-docker-install-common.sh"
 
 build
