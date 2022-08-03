7bdd4d458a8d078ba3ddda1443889eba57316ff6
carlosfranco <cfranco@pagerduty.com>
2022-08-03 11:02:55 -0300

    Fix path to work with runedeckpro project

--- packaging/test/test-docker-install-common.sh
+++ packaging/test/test-docker-install-common.sh
@@ -5,14 +5,14 @@ set -euo pipefail
 readonly ARGS=("$@")
 
 list_debs(){
-	PATTERN="packaging/packaging/build/distributions/*.${PACKAGE_TYPE}"
+	PATTERN="${PACKAGING_DIR_PARENT}packaging/packaging/build/distributions/*.${PACKAGE_TYPE}"
 	echo $PATTERN
 }
 
 build(){
   echo "$PWD"
 	local TAG="rdpro-$COMMON"
-	docker build -t "$TAG-util" -f packaging/test/docker/installcommon/"$COMMON".Dockerfile packaging/test/docker/installcommon
+	docker build -t "$TAG-util" -f "${PACKAGING_DIR_PARENT}"packaging/test/docker/installcommon/"$COMMON".Dockerfile "${PACKAGING_DIR_PARENT}"packaging/test/docker/installcommon
 	docker build "$DIR" -t "$TAG"
 }
 
--- packaging/test/test-docker-install-deb.sh
+++ packaging/test/test-docker-install-deb.sh
@@ -4,6 +4,11 @@ LDIR=$( cd $(dirname $0) ; echo $PWD )
 export DIR=packaging/test/docker/debinstall
 export COMMON="ubuntu1604"
 export PACKAGE_TYPE="deb"
+export PACKAGING_DIR_PARENT=""
+
+if [[ "${UPSTREAM_PROJECT}" != "rundeckpro" ]] ; then
+  export PACKAGING_DIR_PARENT="rundeck/"
+fi
 
 echo "$LDIR"
 
