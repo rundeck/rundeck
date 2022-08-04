cae96c2ffa242e9509ae25bbfffa55207258781a
carlosfranco <cfranco@pagerduty.com>
2022-08-04 11:03:27 -0300

    Bind PACKAGING_DIR_PARENT variable

--- packaging/test/test-docker-install-rpm.sh
+++ packaging/test/test-docker-install-rpm.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 LDIR=$( cd $(dirname $0) ; echo $PWD )
-
-if [[ "${UPSTREAM_PROJECT}" == "rundeckpro" ]] ; then
-  export PACKAGING_DIR_PARENT="rundeck/"
-fi
-
 export DIR="${PACKAGING_DIR_PARENT}"packaging/test/docker/rpminstall
 export COMMON="centos7"
 export PACKAGE_TYPE="rpm"
