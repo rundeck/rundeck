f5a40a36b986164b493c0c48e434bd0e22a062d6
carlosfranco <cfranco@pagerduty.com>
2022-08-22 16:19:54 -0300

    Uncomment Miguel block

--- rundeckapp/grails-spa/packages/ui-trellis-vite/src/app.ts
+++ rundeckapp/grails-spa/packages/ui-trellis-vite/src/app.ts
@@ -7,23 +7,23 @@ Vue.config.productionTip = false;
 Vue.config.devtools = true;
 
 //**** Miguel  ****/
-// import './app/components/copybox/main'
+import './app/components/copybox/main'
 import './app/components/central/main'
-// import './app/components/ko-paginator/main'
-// import './app/components/motd/main'
-// import './app/components/navbar/main'
-// import './app/components/project-picker/main'
-// import './app/components/first-run/main'
-// //import './app/components/theme/main'
-// // import './app/components/tour/main'
-// // import './app/components/version-notification/main'
-//
-// // import './app/pages/login/main'
-// // import './app/pages/project-dashboard/main'
-// import './app/pages/project-activity/main'
-// import './app/pages/repository/main'
-// import './app/pages/command/main'
-// import './app/pages/community-news/main'
+import './app/components/ko-paginator/main'
+import './app/components/motd/main'
+import './app/components/navbar/main'
+import './app/components/project-picker/main'
+import './app/components/first-run/main'
+// import './app/components/theme/main'
+import './app/components/tour/main'
+import './app/components/version-notification/main'
+
+import './app/pages/login/main'
+import './app/pages/project-dashboard/main'
+import './app/pages/project-activity/main'
+import './app/pages/repository/main'
+import './app/pages/command/main'
+import './app/pages/community-news/main'
 
 //**** Carlos  ****/
 import './app/components/version/main'
