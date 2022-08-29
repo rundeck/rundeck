4177f90d0767c4887468e0cbde8d407cd091ef25
Miguel Ramos <mr@mishingo.com>
2022-08-29 12:43:20 -0400

    update vite config and deps

--- rundeckapp/grails-spa/packages/ui-trellis-vite/vite.config.app.js
+++ rundeckapp/grails-spa/packages/ui-trellis-vite/vite.config.app.js
@@ -5,6 +5,7 @@ const pathSrc = path.resolve(__dirname, "./src");
 import { defineConfig, loadEnv } from "vite";
 import { createVuePlugin as Vue2 } from 'vite-plugin-vue2'
 import copy from 'rollup-plugin-copy'
+import { viteExternalsPlugin } from 'vite-plugin-externals'
 
 
 
@@ -29,6 +30,9 @@ export default ({ mode }) => {
   
     plugins: [
       Vue2(),
+      viteExternalsPlugin({
+        vue: 'Vue'
+      }),
       copy({
         targets: [
           { src: 'public/assets/static/ui-trellis-vite/fonts', dest: 'public' },
@@ -37,13 +41,20 @@ export default ({ mode }) => {
       })
     ],
     build: {
+      sourcemap: true,
       outDir: `${process.env.VITE_APP_OUTPUT_DIR}/ui-trellis-vite/`,
       rollupOptions: {
         // input: {
         //   main: resolve(__dirname, 'index.html'),
         //   command: resolve(__dirname, 'sandbox/index.html')
         // },
+        external: ['vue', 'Vue'],
         output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
           assetFileNames: (assetInfo) => {
             let extType = assetInfo.name.split('.').at(1);
             if (/png|jpe?g|svg|gif|tiff|bmp|svg|ico/i.test(extType)) {
