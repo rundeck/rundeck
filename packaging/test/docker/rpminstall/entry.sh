3f8414b326390a4cf7170790603f86fdf1848efe
Greg Schueler <greg.schueler@gmail.com>
2022-09-27 16:54:44 -0700

    cleanup

--- packaging/test/docker/rpminstall/entry.sh
+++ packaging/test/docker/rpminstall/entry.sh
@@ -4,7 +4,7 @@
 set -e
 
 FLAV=${EDITION:-cluster}
-DIR=$HOME/"${PACKAGING_DIR_PARENT}"packaging/packaging/rundeck/build/distributions
+DIR=$HOME/"${PACKAGING_DIR_PARENT}"packaging/packaging/build/distributions
 
 
 install_rundeck(){
