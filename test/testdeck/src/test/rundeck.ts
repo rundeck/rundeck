cb8034fbf4ead64b3607bd321e140f6bc241b362
Greg Schueler <greg.schueler@gmail.com>
2022-10-24 11:44:49 -0700

    enable using a token for testdeck tests

--- test/testdeck/src/test/rundeck.ts
+++ test/testdeck/src/test/rundeck.ts
@@ -2,6 +2,7 @@ import { ParseBool } from "../util/parseBool";
 
 export const envOpts = {
     TESTDECK_RUNDECK_URL: process.env.TESTDECK_RUNDECK_URL || 'http://127.0.0.1:4440',
+    TESTDECK_RUNDECK_TOKEN: process.env.TESTDECK_RUNDECK_TOKEN,
     CI: ParseBool(process.env.CI),
     TESTDECK_HEADLESS: ParseBool(process.env.TESTDECK_HEADLESS) || ParseBool(process.env.CI),
     TESTDECK_S3_UPLOAD: ParseBool(process.env.TESTDECK_S3_UPLOAD),
