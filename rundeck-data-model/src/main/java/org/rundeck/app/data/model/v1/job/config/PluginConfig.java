15c544b4e4f0b6e131a2e41ad1134183913759f6
Imad <ijafir@gmail.com>
2022-11-09 16:45:13 -0500

    refactor Project references to go through the projectProvider data-spi

--- rundeck-data-model/src/main/java/org/rundeck/app/data/model/v1/job/config/PluginConfig.java
+++
@@ -1,6 +0,0 @@
-package org.rundeck.app.data.model.v1.job.config;
-
-import java.util.Map;
-
-public interface PluginConfig extends Map<String, Object> {
-}
--- rundeck-data-model/src/main/java/org/rundeck/app/data/model/v1/job/orchestrator/Orchestrator.java
+++
@@ -1,8 +0,0 @@
-package org.rundeck.app.data.model.v1.job.orchestrator;
-
-import java.util.Map;
-
-public interface Orchestrator {
-    String getType();
-    Map<String,Object> getConfiguration();
-}
