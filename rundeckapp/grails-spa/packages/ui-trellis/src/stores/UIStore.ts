1c0d5064146c21b6ee5fccded1ddd0fe3869657b
Greg Schueler <greg.schueler@gmail.com>
2022-11-30 12:00:27 -0800

    Add manual reactive mechanism
    UI Sockets entries added after the component was mounted will be updated in the UI

--- rundeckapp/grails-spa/packages/ui-trellis/src/stores/UIStore.ts
+++ rundeckapp/grails-spa/packages/ui-trellis/src/stores/UIStore.ts
@@ -3,14 +3,39 @@ import { VueConstructor } from 'vue'
 
 export class UIStore {
   @observable items: Array<UIItem> = []
+  @observable watchers: Array<UIWatcher> = []
+
+  notifyWatchers() {
+    this.watchers.forEach((w) => {
+      w.callback(this.itemsForLocation(w.section, w.location))
+    })
+  }
 
   itemsForLocation(section: string, location: string): Array<UIItem> {
-    return this.items.filter((v) => v.section === section && (location ? v.location == location : true)).sort((a, b) => (a.order||0) - (b.order||0))
+    return this.items.filter((v) => v.section === section && (location ? v.location == location : true)).sort((a, b) => (a.order || 0) - (b.order || 0))
   }
 
   addItems(items: Array<UIItem>) {
     items.forEach(i => this.items.push(i))
+    this.notifyWatchers()
   }
+
+  addWatcher(watch: UIWatcher) {
+    this.watchers.push(watch)
+  }
+
+  removeWatcher(watch: UIWatcher) {
+    let ndx=this.watchers.indexOf(watch)
+    if(ndx>=0){
+      this.watchers.splice(ndx, 1)
+    }
+  }
+}
+
+export interface UIWatcher {
+  section: string
+  location: string
+  callback: (items: Array<UIItem>) => void
 }
 
 export interface UIItem {
@@ -20,6 +45,6 @@ export interface UIItem {
   location: string
   visible: boolean
   widget?: VueConstructor
-  html?:string
-  text?:string
+  html?: string
+  text?: string
 }
--- rundeckapp/grails-spa/packages/ui/src/components/ui/UiSocket.vue
+++ rundeckapp/grails-spa/packages/ui/src/components/ui/UiSocket.vue
@@ -1,8 +1,8 @@
 <template>
-    <span>
+    <span v-if="items">
         <slot v-if="items.length<1"></slot>
         <template v-for="i in items">
-            <template v-if="i.text">{{i.text}}</template>
+            <template v-if="i.text">{{ i.text }}</template>
             <span v-else-if="i.html" v-html="i.html"></span>
             <component :is="i.widget" v-else-if="i.widget"/>
         </template>
@@ -11,14 +11,12 @@
 <script lang="ts">
 
 import Vue from 'vue'
-import VueI18n from 'vue-i18n'
 import {Component, Prop, Watch} from 'vue-property-decorator'
 
 import {
     getRundeckContext,
-    RundeckContext
 } from '@rundeck/ui-trellis'
-import {UIItem} from '@rundeck/ui-trellis/lib/stores/UIStore'
+import {UIItem, UIWatcher} from '@rundeck/ui-trellis/lib/stores/UIStore'
 
 const rootStore = getRundeckContext().rootStore
 
@@ -30,11 +28,29 @@ export default class UiSocket extends Vue {
 
     items: UIItem[] = []
 
-    mounted() {
-        this.items = rootStore.ui.itemsForLocation(this.section, this.location)
-        if(this.eventBus) {
-            this.eventBus.$emit('ui-socket-mounted', {section: this.section, location: this.location})
+    loadItems() {
+        this.items = rootStore.ui.itemsForLocation(this.section, this.location).filter((a) => a.visible)
+    }
+
+    destroyed() {
+        if(this.uiwatcher){
+            rootStore.ui.removeWatcher(this.uiwatcher)
         }
+    }
+
+    uiwatcher: UIWatcher|null=null
+
+
+    mounted() {
+        this.loadItems()
+        this.uiwatcher = {
+            section: this.section,
+            location: this.location,
+            callback: (items: UIItem[])=>{
+                this.items=items
+            }
+        }
+        rootStore.ui.addWatcher(this.uiwatcher)
     }
 }
 </script>
