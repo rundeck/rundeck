8c1c8cc0d006809556ee53f6d21b6c23c9ac3d99
Darwis <darwisnarvaezdev@gmail.com>
2023-01-17 17:10:02 -0300

    minor refactor

--- rundeckapp/src/test/groovy/rundeck/controllers/WorkflowGraphControllerSpec.groovy
+++ rundeckapp/src/test/groovy/rundeck/controllers/WorkflowGraphControllerSpec.groovy
@@ -7,21 +7,21 @@ import spock.lang.Specification
 
 class WorkflowGraphControllerSpec extends Specification implements ControllerUnitTest<WorkflowGraphController> {
 
-    def setup(){
+    def setup() {
     }
 
     def "render config property"() {
         given:
-        controller.configurationService = Mock(ConfigurationService){
+        controller.configurationService = Mock(ConfigurationService) {
             getBoolean("gui.workflowGraph", controller.defaultPropValue) >> controller.defaultPropValue
         }
 
         when:
-        request.method='GET'
-        def result=controller.WorkflowGraph()
+        request.method = 'GET'
+        def result = controller.WorkflowGraph()
 
         then:
-        response.status==200
+        response.status == 200
         response.contentAsString == '{"showGraph":' + controller.defaultPropValue + '}'
     }
-
+}
