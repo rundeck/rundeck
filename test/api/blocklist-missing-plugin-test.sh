01b9ec9813db0dd94b04df7bf596d5d862b74a0e
Chris McCarroll-Gilbert <chrismcg@pagerduty.com>
2023-02-03 11:11:04 -0700

    remove unwanted change

--- test/api/blocklist-missing-plugin-test.sh
+++ test/api/blocklist-missing-plugin-test.sh
@@ -13,8 +13,6 @@ echo "TEST: /api/projects "
 # get listing
 docurl ${runurl}?${params} > $DIR/curl.out
 
-echo $DIR/curl.out
-
 test_plugin_present 'cyberark' $DIR/curl.out
 test_plugin_present 'openssh' $DIR/curl.out
 test_plugin_present 'ansible' $DIR/curl.out
