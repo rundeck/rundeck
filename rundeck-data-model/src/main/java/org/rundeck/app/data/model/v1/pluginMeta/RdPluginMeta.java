7d3971b5bba715e2e57265987dd78466ef17133a
oscar <ocerda@pagerduty.com>
2023-03-08 11:25:40 -0300

    Adding getPluginData

--- rundeck-data-model/src/main/java/org/rundeck/app/data/model/v1/pluginMeta/RdPluginMeta.java
+++ rundeck-data-model/src/main/java/org/rundeck/app/data/model/v1/pluginMeta/RdPluginMeta.java
@@ -1,12 +1,13 @@
 package org.rundeck.app.data.model.v1.pluginMeta;
 
 import java.util.Date;
+import java.util.Map;
 
 public interface RdPluginMeta {
     Long getId();
     String getKey();
     String getProject();
-    String getJsonData();
     Date getDateCreated();
     Date getLastUpdated();
+    Map<String,Object> getPluginData();
 }
