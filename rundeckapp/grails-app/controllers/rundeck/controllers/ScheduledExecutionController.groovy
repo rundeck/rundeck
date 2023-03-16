61352c5604797518eceb262ffca0605f455f0a72
Greg Schueler <greg.schueler@gmail.com>
2023-03-16 16:32:16 -0700

    api docs for /jobs/file/{id}

--- rundeckapp/grails-app/controllers/rundeck/controllers/ScheduledExecutionController.groovy
+++ rundeckapp/grails-app/controllers/rundeck/controllers/ScheduledExecutionController.groovy
@@ -48,6 +48,7 @@ import io.swagger.v3.oas.annotations.ExternalDocumentation
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.enums.ParameterIn
+import io.swagger.v3.oas.annotations.media.ArraySchema
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.ExampleObject
 import io.swagger.v3.oas.annotations.media.Schema
