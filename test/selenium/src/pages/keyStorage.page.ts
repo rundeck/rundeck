ffc46322d81753d6084a31043f1a91e6c6622c2f
Chris McCarroll-Gilbert <chrismcg@pagerduty.com>
2023-04-25 09:05:17 -0600

    Move elems to a map

--- test/selenium/src/pages/keyStorage.page.ts
+++ test/selenium/src/pages/keyStorage.page.ts
@@ -4,15 +4,16 @@ import {Page} from '@rundeck/testdeck/page'
 import {Context} from '@rundeck/testdeck/context'
 
 export const Elems = {
-    modalOptions: By.css('#execDiv #exec_options_form #optionSelect'),
-    searchModalFields: By.css('#jobs_filters form #jobs_filters_content'),
-    runFormButton: By.css('#execDiv #exec_options_form #formbuttons #execFormRunButton'),
-    jobSearchButton: By.css('#subtitlebar  .btn[data-target="#jobs_filters"]'),
-    jobSearchNameField: By.css('#jobs_filters form input[name="jobFilter"]'),
-    jobSearchGroupField: By.css('#jobs_filters form input[name="groupPath"]'),
-    jobSearchSubmitButton: By.css('#jobs_filters form #jobs_filters_footer input[type="submit"][name="_action_jobs"]'),
-    jobRowLinks: By.css('#job_group_tree .jobname.job_list_row[data-job-id] > a[data-job-id]'),
-    optionValidationWarningText: By.css('#execDiv #exec_options_form #optionSelect #_commandOptions div.form-group.has-warning p.text-warning')
+    createButton: By.linkText("Add or Upload a Key"),
+    deleteButton: By.linkText("Delete Selected Item"),
+    deleteDropdown: By.css(".btn-group > .dropdown-toggle"),
+    overwriteButton: By.linkText("Overwrite Key"),
+    deleteConfirmationButton: By.css(".obs-storagedelete-select"),
+    uploadPasswordField: By.id("uploadpasswordfield"),
+    resourceNameField: By.id("uploadResourceName2"),
+    uploadKeyTypeDropdown: By.name("uploadKeyType"),
+    passwordKeyType: By.css('option[value="password"]'),
+    saveButton: By.css(".btn-cta")
 }
 
 export class KeyStoragePage extends Page {
@@ -23,7 +24,7 @@ export class KeyStoragePage extends Page {
     }
 
     async getCreateButton() {
-        return this.ctx.driver.findElement(By.linkText("Add or Upload a Key"))
+        return this.ctx.driver.findElement(Elems.createButton)
     }
 
     async selectKey(keyName: string) {
@@ -33,40 +34,40 @@ export class KeyStoragePage extends Page {
     }
 
     async getDeleteButton(){
-        return this.ctx.driver.findElement(By.linkText("Delete Selected Item"))
+        return this.ctx.driver.findElement(Elems.deleteButton)
     }
 
     async getDeleteDropdown(){
-        return this.ctx.driver.findElement(By.css(".btn-group > .dropdown-toggle"))
+        return this.ctx.driver.findElement(Elems.deleteDropdown)
     }
 
     async getOverwriteButton(){
-        return this.ctx.driver.findElement(By.linkText("Overwrite Key"))
+        return this.ctx.driver.findElement(Elems.overwriteButton)
     }
 
     async getDeleteConfirmButton(){
-        return this.ctx.driver.findElement(By.css(".obs-storagedelete-select"))
+        return this.ctx.driver.findElement(Elems.deleteConfirmationButton)
     }
 
     async getUploadPasswordField() {
-        await this.ctx.driver.wait(until.elementLocated(By.id("uploadpasswordfield")), 10000);
-        return this.ctx.driver.findElement(By.id("uploadpasswordfield"))
+        await this.ctx.driver.wait(until.elementLocated(Elems.uploadPasswordField), 10000);
+        return this.ctx.driver.findElement(Elems.uploadPasswordField)
     }
 
     async getResourceNameField() {
-        return this.ctx.driver.findElement(By.id("uploadResourceName2"))
+        return this.ctx.driver.findElement(Elems.resourceNameField)
     }
 
     async selectKeyType() {
-        const dropdown = await this.ctx.driver.findElement(By.name("uploadKeyType"))
-        return dropdown.findElement(By.css('option[value="password"]'));
+        const dropdown = await this.ctx.driver.findElement(Elems.uploadKeyTypeDropdown)
+        return dropdown.findElement(Elems.passwordKeyType);
     }
 
     async getSaveButton() {
-        return this.ctx.driver.findElement(By.css(".btn-cta"))
+        return this.ctx.driver.findElement(Elems.saveButton)
     }
 
     async getUploadTypeDropdown() {
-        return this.ctx.driver.findElement(By.name("uploadKeyType"))
+        return this.ctx.driver.findElement(Elems.uploadKeyTypeDropdown)
     }
 }
