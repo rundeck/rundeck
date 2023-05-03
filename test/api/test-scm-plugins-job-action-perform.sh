d6e53eca2f54fc790d78a4aa61b2c9b8d349172d
Antony Velasquez Ruiz <avelasquez@pagerduty.com>
2023-05-03 12:11:35 -0500

    Update test-scm-plugins-job-action-perform.sh
    Update test-scm-plugins-job-action-perform.sh
    
    Update test-scm-plugins-job-action-perform.sh
    
    Update test-scm-plugins-job-action-perform.sh
    
    Update test-scm-plugins-job-action-perform.sh

--- test/api/test-scm-plugins-job-action-perform.sh
+++ test/api/test-scm-plugins-job-action-perform.sh
@@ -74,7 +74,7 @@ test_job_action_perform_json(){
 	local JOBID=$(setup_export_job $project)
 	local actionId="job-commit"
 	local commitMessage="test commit message"
-	
+
 	sleep 2
 
 	#get job status
@@ -84,7 +84,7 @@ test_job_action_perform_json(){
 	TYPE=application/json
 	EXPECT_STATUS=200
 	ENDPOINT="${APIURL}/job/$JOBID/scm/$integration/action/$actionId"
-	
+
 	TMPDIR=`tmpdir`
 	tmp=$TMPDIR/commit.json
 	cat >$tmp <<END
@@ -100,7 +100,7 @@ END
 
 	api_request $ENDPOINT $DIR/curl.out
 
-	
+
 	assert_json_value "true" '.success' $DIR/curl.out
 	assert_json_value "SCM export Action was Successful: $actionId" '.message' $DIR/curl.out
 
@@ -109,9 +109,51 @@ END
 	remove_project $project
 }
 
+test_job_import_perform(){
+  local project=$1
+
+  create_project $project
+  do_setup_export_json_valid "import" "git-import" $project
+  local actionId="import-jobs"
+  local integration=import
+
+  sleep 2
+
+  #get job status
+
+  METHOD=POST
+  ACCEPT=application/json
+  TYPE=application/json
+  EXPECT_STATUS=200
+  ENDPOINT="${APIURL}/project/$project/scm/$integration/action/$actionId"
+
+  TMPDIR=`tmpdir`
+  tmp=$TMPDIR/importjob.json
+  cat >$tmp <<END
+{
+    "items": [
+        "$JOBNAME.yaml"
+    ]
+}
+END
+  POSTFILE=$tmp
+
+  test_begin "SCM Job Import Action"
+
+  api_request $ENDPOINT $DIR/curl.out
+
+  assert_json_value "true" '.success' $DIR/curl.out
+  assert_json_value "SCM import Action was Successful: $actionId" '.message' $DIR/curl.out
+
+  test_succeed
+
+  remove_project $project
+}
+
 main(){
 	test_job_action_perform_xml "testscm1"
 	test_job_action_perform_json "testscm2"
+	test_job_import_perform "testscm3"
 }
 
 main
