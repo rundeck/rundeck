5ded0f43e580469340eb0720690d489e2a92c172
Darwis <darwisnarvaezdev@gmail.com>
2023-05-04 15:08:44 -0300

    Refactor

--- rundeckapp/grails-app/controllers/rundeck/controllers/MenuController.groovy
+++ rundeckapp/grails-app/controllers/rundeck/controllers/MenuController.groovy
@@ -3235,8 +3235,9 @@ class MenuController extends ControllerBase implements ApplicationContextAware{
                     if (scmService.projectHasConfiguredImportPlugin(params.project)) {
                         def userHasPathAccessToSshKey = storageService.hasPath(authContext, scmService.loadScmConfig(params.project, 'import')?.config?.sshPrivateKeyPath)
                         if( !userHasPathAccessToSshKey ){
-                            results.warning = "Failed to update SCM Import status; user don't have access rights to SCM's configured key."
-                            log.error("Failed to update SCM Import status; user don't have access rights to SCM's configured key.")
+                            def warning = "Failed to update SCM Import status; user don't have access rights to SCM's configured key."
+                            results.warning = warning
+                            log.error(warning)
                         }else{
                             pluginData.scmImportEnabled = scmService.loadScmConfig(params.project, 'import')?.enabled
                             if (pluginData.scmImportEnabled) {
