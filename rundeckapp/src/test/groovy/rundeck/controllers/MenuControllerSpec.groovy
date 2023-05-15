3962a57c51fabd835560f673094dc66a902ae45e
Darwis <darwisnarvaezdev@gmail.com>
2023-05-15 18:51:29 -0300

    Test normalizing

--- rundeckapp/src/test/groovy/rundeck/controllers/MenuControllerSpec.groovy
+++ rundeckapp/src/test/groovy/rundeck/controllers/MenuControllerSpec.groovy
@@ -34,8 +34,6 @@ import com.dtolabs.rundeck.core.authorization.providers.Policy
 import com.dtolabs.rundeck.core.authorization.providers.PolicyCollection
 import com.dtolabs.rundeck.core.authorization.providers.Validator
 import com.dtolabs.rundeck.core.common.IFramework
-import com.dtolabs.rundeck.plugins.scm.ScmOperationContext
-import com.dtolabs.rundeck.plugins.scm.ScmUserInfo
 import com.dtolabs.rundeck.server.AuthContextEvaluatorCacheManager
 import grails.test.hibernate.HibernateSpec
 import grails.testing.web.controllers.ControllerUnitTest
@@ -86,7 +84,6 @@ import rundeck.services.feature.FeatureService
 import rundeck.services.scm.ScmPluginConfig
 import rundeck.services.scm.ScmPluginConfigData
 import rundeck.services.StorageService
-import rundeck.services.scm.ScmUser
 import spock.lang.Ignore
 import spock.lang.Unroll
 import testhelper.RundeckHibernateSpec
@@ -1588,10 +1585,6 @@ class MenuControllerSpec extends RundeckHibernateSpec implements ControllerUnitT
         controller.storageService = Mock(StorageService)
         def project = 'test'
         def scmConfig = Mock(ScmPluginConfigData)
-        def userAndRolesAuthContext = Mock(UserAndRolesAuthContext){
-            it.getRoles() >> new HashSet<String>(Arrays.asList("role1", "role2"))
-            it.username >> 'test'
-        }
 
         when:
         request.method = 'POST'
@@ -1612,12 +1605,10 @@ class MenuControllerSpec extends RundeckHibernateSpec implements ControllerUnitT
         1 * controller.rundeckAuthContextProcessor.authorizeApplicationResourceAny(_, _, [AuthConstants.ACTION_ADMIN, AuthConstants.ACTION_APP_ADMIN,
                                                                                AuthConstants.ACTION_IMPORT,
                                                                                 AuthConstants.ACTION_SCM_IMPORT]) >> true
-        1 * controller.rundeckAuthContextProcessor.getAuthContextForSubject(_) >> userAndRolesAuthContext
         1 * controller.scmService.projectHasConfiguredExportPlugin(project) >> false
         1 * controller.scmService.projectHasConfiguredImportPlugin(project) >> true
-        2 * controller.scmService.loadScmConfig(project,'import') >> scmConfig
+        1 * controller.scmService.loadScmConfig(project,'import') >> scmConfig
         1 * scmConfig.getEnabled() >> enabled
-        1 * controller.storageService.hasPath(_,_) >> true
         (count) * controller.scmService.getJobsPluginMeta(project, false)
         (count) * controller.scmService.importStatusForJobs(project,_, _, _, _)
         (count) * controller.scmService.importPluginStatus(_,project)
@@ -1634,58 +1625,6 @@ class MenuControllerSpec extends RundeckHibernateSpec implements ControllerUnitT
             enabled | count
             true    | 1
     }
-
-    def "SCM NOT disabled when an unauthorized user request status and import is enabled"(){
-        given:
-        controller.frameworkService = Mock(FrameworkService){
-            isClusterModeEnabled() >> true
-        }
-        controller.rundeckAuthContextProcessor=Mock(AppAuthContextProcessor)
-        controller.aclFileManagerService = Mock(AclFileManagerService)
-        controller.scheduledExecutionService = Mock(ScheduledExecutionService)
-        controller.scmService = Mock(ScmService)
-        controller.storageService = Mock(StorageService) {
-            it.hasPath() >> false
-        }
-        def project = 'test'
-        def scmConfig = Mock(ScmPluginConfigData)
-        def userAndRolesAuthContext = Mock(UserAndRolesAuthContext){
-            it.getRoles() >> new HashSet<String>(Arrays.asList("role1", "role2"))
-            it.username >> 'test'
-        }
-
-        when:
-        request.method = 'POST'
-        request.JSON = []
-        request.format = 'json'
-        params.project = project
-        controller.listExport()
-
-        then:
-        1 * controller.scheduledExecutionService.listWorkflows(_,_) >> [schedlist : []]
-        1 * controller.scheduledExecutionService.finishquery(_,_,_) >> [max: 20,
-                                                                        offset:0,
-                                                                        paginateParams:[:],
-                                                                        displayParams:[:]]
-        1 * controller.rundeckAuthContextProcessor.authorizeApplicationResourceAny(_, _, [AuthConstants.ACTION_ADMIN, AuthConstants.ACTION_APP_ADMIN,
-                                                                                          AuthConstants.ACTION_EXPORT,
-                                                                                          AuthConstants.ACTION_SCM_EXPORT]) >> true
-        1 * controller.rundeckAuthContextProcessor.authorizeApplicationResourceAny(_, _, [AuthConstants.ACTION_ADMIN, AuthConstants.ACTION_APP_ADMIN,
-                                                                                          AuthConstants.ACTION_IMPORT,
-                                                                                          AuthConstants.ACTION_SCM_IMPORT]) >> true
-        1 * controller.rundeckAuthContextProcessor.getAuthContextForSubject(_) >> userAndRolesAuthContext
-        1 * controller.scmService.projectHasConfiguredExportPlugin(project) >> false
-        1 * controller.scmService.projectHasConfiguredImportPlugin(project) >> true
-        1 * controller.scmService.loadScmConfig(project,'import') >> scmConfig
-        1 * controller.storageService.hasPath(_,_) >> false
-        0 * controller.scmService.initProject(project,'export')
-        0 * controller.scmService.initProject(project,'import')
-
-        0 * controller.scmService.disablePlugin(_,_,_)
-        response.json.warning !== null
-
-    }
-
 
     def "project Toggle SCM off"(){
         given:
