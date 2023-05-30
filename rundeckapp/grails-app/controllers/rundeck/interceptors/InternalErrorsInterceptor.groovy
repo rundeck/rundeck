e2f7159cc6b09166687577398d5e9bb9d4f50a92
L2JE <leojesus.juarez@gmail.com>
2023-05-30 18:56:18 -0300

    removed logouts

--- rundeckapp/grails-app/controllers/rundeck/interceptors/InternalErrorsInterceptor.groovy
+++ rundeckapp/grails-app/controllers/rundeck/interceptors/InternalErrorsInterceptor.groovy
@@ -1,7 +1,6 @@
 package rundeck.interceptors
 
 import groovy.transform.CompileStatic
-import groovy.util.logging.Slf4j
 import org.apache.http.HttpStatus
 import org.springframework.beans.factory.annotation.Autowired
 import rundeck.services.ConfigurationService
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletRequest
 /**
  * Intercept all responses with 5xx status and removes stacktrace data
  */
-@Slf4j
 @CompileStatic
 class InternalErrorsInterceptor {
     private final ConfigurationService configurationService
@@ -33,29 +31,17 @@ class InternalErrorsInterceptor {
         Enumeration<String> reqAttributes = requestForRendering.getAttributeNames()
         boolean throwableFound = false
 
-        log.error(":::::::::::::::::::::::::::::ENTERING ERRORS INTERCEPTOR:::::::::::::")
-        log.error("Request URI: " + request.getRequestURI())
-        log.error("Status: " + response.getStatus())
-        log.error("response: " + response.getOutputStream().toString())
-        log.error("---------------------------------------------------------------------")
-
         reqAttributes.each { String attributeName ->
             def attribute = requestForRendering.getAttribute(attributeName)
             if(attribute instanceof Throwable && attribute != null) {
-                throwableFound = true
                 Throwable serverException = (attribute as Throwable)
-                log.error("Has a throwable instance!")
-                log.error("Attr Name: " + attributeName)
-                log.error("SERVER ERROR:::::::::: ")
-                attribute.printStackTrace()
+                throwableFound = true
 
                 requestForRendering.removeAttribute(attributeName)
                 cleanStackTraces(serverException)
                 requestForRendering.setAttribute(attributeName, serverException)
             }
         }
-
-        log.error(":::::::::::::::::::::::::::::EXITING ERRORS INTERCEPTOR:::::::::::::")
         if(throwableFound)
             render(view: '/error.gsp')
         return !throwableFound
