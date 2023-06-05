7ed100a88165a7a1f10c2ad22da85f82d7966da0
ronaveva <rnavarro@pagerduty.com>
2023-06-05 11:13:21 -0400

    add preconditions and set dbCreate property as none

--- rundeckapp/grails-app/migrations/changelog.groovy
+++ rundeckapp/grails-app/migrations/changelog.groovy
@@ -82,6 +82,13 @@ databaseChangeLog = {
         property name: "varchar3072.type", global: "true", value: "VARCHAR(3072)", dbms: "mysql,postgresql,mssql,h2,mariadb"
         property name: "varchar3072.type", global: "true", value: "VARCHAR2(3072 CHAR)", dbms: "oracle"
 
+        if('${catalogName}' == '${catalogName}'){
+                //this is liquibase default catalog name
+                property name: "catalogName", global: "true", value: '${database.defaultCatalogName}'
+        }else{
+                property name: "catalogName", global: "true", value: '${catalogName}'
+        }
+
         include file: 'core/HibernateIndex.groovy'
         include file: 'core/AuthToken.groovy'
         include file: 'core/BaseReport.groovy'
