75c39774da3bd609ccbcdce2a46dcf44a506fef8
oscar <ocerda@pagerduty.com>
2023-06-07 12:42:50 -0400

    BaseReport migration
    (cherry picked from commit 65b0380532659d30fc26ac1e3db73382097a6943)

--- rundeckapp/grails-app/migrations/core/BaseReport.groovy
+++ rundeckapp/grails-app/migrations/core/BaseReport.groovy
@@ -83,28 +83,4 @@ databaseChangeLog = {
             column(name: "filter_applied", type: '${text.type}')
         }
     }
-    changeSet(author: "rundeckdev", id: "Add-job-uuid-to-base-report") {
-        preConditions(onFail: "MARK_RAN") {
-            not {
-                columnExists(tableName: "base_report", columnName: 'job_uuid')
-            }
-        }
-        addColumn(tableName: "base_report") {
-            column(name: 'job_uuid', type: '${varchar255.type}')
-        }
-    }
-
-    changeSet(author: "rundeckdev", id: "Populate-base-report-job-uuid") {
-        preConditions(onFail: "MARK_RAN") {
-            tableExists(tableName: "base_report")
-            tableExists(tableName: "execution")
-            tableExists(tableName: "scheduled_execution")
-        }
-        sql("update base_report\n" +
-                "set job_uuid =\n" +
-                "        (select scheduled_execution.uuid\n" +
-                "         from scheduled_execution\n" +
-                "                  inner join execution ON execution.scheduled_execution_id = scheduled_execution.id\n" +
-                "         WHERE base_report.execution_id = execution.id)")
-    }
 }
