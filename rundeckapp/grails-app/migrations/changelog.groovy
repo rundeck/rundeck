2eff301a035c0c115082c9b2de5040ca5967cabd
ronaveva <rnavarro@pagerduty.com>
2023-06-23 16:54:54 -0400

    remove unnecesary code to apply custom catalog name

--- rundeckapp/grails-app/migrations/changelog.groovy
+++ rundeckapp/grails-app/migrations/changelog.groovy
@@ -82,12 +82,7 @@ databaseChangeLog = {
         property name: "varchar3072.type", global: "true", value: "VARCHAR(3072)", dbms: "mysql,postgresql,mssql,h2,mariadb"
         property name: "varchar3072.type", global: "true", value: "VARCHAR2(3072 CHAR)", dbms: "oracle"
 
-        if('${catalogName}' == '${catalogName}'){
-                //this is liquibase default catalog name
-                property name: "catalogName", global: "true", value: '${database.defaultCatalogName}'
-        }else{
-                property name: "catalogName", global: "true", value: '${catalogName}'
-        }
+        property name: "catalogName", global: "true", value: '${database.defaultCatalogName}'
 
         include file: 'core/HibernateIndex.groovy'
         include file: 'core/AuthToken.groovy'
