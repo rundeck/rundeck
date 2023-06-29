a7cce62bc6e4f6c96285f9125a24ae7c710f96fd
Chris McCarroll-Gilbert <chrismcg@pagerduty.com>
2023-06-29 12:51:30 -0600

    remove unneeded file

--- core/src/main/java/com/dtolabs/rundeck/core/plugins/StoragePluginProviderConfiguration.java
+++
@@ -1,6 +0,0 @@
-package com.dtolabs.rundeck.core.plugins;
-
-interface StoragePluginProviderConfiguration extends PluginProviderConfiguration {
-    String getPath();
-    Boolean getPathPrefix();
-}
