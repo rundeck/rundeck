6d581496b6377a75cce94ceec9886f8acebf0730
Luis Toledo <luis@variacode.com>
2023-07-10 13:52:01 -0400

    share services with node source loader

---
+++ core/src/main/java/com/dtolabs/rundeck/core/common/NodeSourceLoaderConfig.java
@@ -0,0 +1,12 @@
+package com.dtolabs.rundeck.core.common;
+
+import com.dtolabs.rundeck.core.plugins.CloseableProvider;
+import com.dtolabs.rundeck.core.resources.ResourceModelSource;
+import org.rundeck.app.spi.Services;
+
+public interface NodeSourceLoaderConfig {
+
+    CloseableProvider<ResourceModelSource> getCloseableProvider();
+
+    Services getServices();
+}
