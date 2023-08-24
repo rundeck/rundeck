6ba1bf9cb8b787f06606a32b5f4f483efb33a043
smartinellibenedetti <139791797+smartinellibenedetti@users.noreply.github.com>
2023-08-24 11:55:58 -0600

    get storybook running

--- rundeckapp/grails-spa/packages/ui-trellis/src/library/components/containers/tabs/tabs.stories.ts
+++ rundeckapp/grails-spa/packages/ui-trellis/src/library/components/containers/tabs/tabs.stories.ts
@@ -21,62 +21,64 @@ function setupStory(vue) {
     document.body.style.overflow = 'hidden'
 }
 
-export const tabs: StoryFn<typeof Tab> = (args) => {
-    const chan = addons.getChannel()
-
-    return {
-        setup() {
-            return { args }
-        },
-        template: `
-        <Tabs class="card" v-bind="args" style="padding: 20px;">
-            <Tab :index="0" title="Tab"><TabContent>Foo Content</TabContent></Tab>
-            <Tab :index="1" title="Tab Bar"><TabContent><input type="text"/></TabContent></Tab>
-            <Tab :index="2" title="Tab Baz">Baz Content</Tab>
-            <Tab :index="3" title="Tab Batch Processing">Batch Content</Tab>
-        </Tabs>`,
-        components: {Tabs, Tab, TabContent},
-        mounted() {
-            setupStory(this)
-        },
-        methods: {
-            handleChecked(val: boolean) {
-                chan.emit('checked',val)
-            }
-        }
-    }
-}
-tabs.args = {
-    type: 'standard'
-}
-
-export const cardTabs = (args) => {
-    const chan = addons.getChannel()
-
-    return {
-        setup() {
-            return { args }
-        },
-        template: `
-        <div class="card" style="border-width: 0.1em;">
-        <Tabs v-bind="args">
-            <Tab :index="0" title="Tab"><TabContent>Foo Content</TabContent></Tab>
-            <Tab :index="1" title="Tab Bar"><TabContent><input type="text"/></TabContent></Tab>
-            <Tab :index="2" title="Tab Baz">Baz Content</Tab>
-            <Tab :index="3" title="Tab Batch Processing">Batch Content</Tab>
-        </Tabs>
-        </div>`,
-        components: {Tabs, Tab, TabContent},
-        mounted() {
-            setupStory(this)
-        },
-        methods: {
-            handleChecked(val: boolean) {
-                chan.emit('checked', val)
-            }
-        }
-    }
-}
-cardTabs.args = {
-    type: 'card',
-}
+// export const tabs: StoryFn<typeof Tab> = (args) => {
+//     const chan = addons.getChannel()
+//
+//     return {
+//         setup() {
+//             return { args }
+//         },
+//         template: `
+//         <div>
+//           <Tabs class="card" v-bind="args" style="padding: 20px;">
+//             <Tab :index="0" title="Tab"><TabContent>Foo Content</TabContent></Tab>
+//             <Tab :index="1" title="Tab Bar"><TabContent><input type="text"/></TabContent></Tab>
+//             <Tab :index="2" title="Tab Baz">Baz Content</Tab>
+//             <Tab :index="3" title="Tab Batch Processing">Batch Content</Tab>
+//           </Tabs>
+//         </div>`,
+//         components: {Tabs, Tab, TabContent},
+//         mounted() {
+//             setupStory(this)
+//         },
+//         methods: {
+//             handleChecked(val: boolean) {
+//                 chan.emit('checked',val)
+//             }
+//         }
+//     }
+// }
+// tabs.args = {
+//     type: 'standard'
+// }
+//
+// export const cardTabs = (args) => {
+//     const chan = addons.getChannel()
+//
+//     return {
+//         setup() {
+//             return { args }
+//         },
+//         template: `
+//         <div class="card" style="border-width: 0.1em;">
+//         <Tabs v-bind="args">
+//             <Tab :index="0" title="Tab"><TabContent>Foo Content</TabContent></Tab>
+//             <Tab :index="1" title="Tab Bar"><TabContent><input type="text"/></TabContent></Tab>
+//             <Tab :index="2" title="Tab Baz">Baz Content</Tab>
+//             <Tab :index="3" title="Tab Batch Processing">Batch Content</Tab>
+//         </Tabs>
+//         </div>`,
+//         components: {Tabs, Tab, TabContent},
+//         mounted() {
+//             setupStory(this)
+//         },
+//         methods: {
+//             handleChecked(val: boolean) {
+//                 chan.emit('checked', val)
+//             }
+//         }
+//     }
+// }
+// cardTabs.args = {
+//     type: 'card',
+// }
