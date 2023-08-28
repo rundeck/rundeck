e6ecff1e77975fe7e3349e704f79a017c1105766
Osman Albarran <osman@Osmans-MacBook-Pro.local>
2023-08-28 12:49:37 -0400

    removing comments

--- core/src/main/java/com/dtolabs/rundeck/core/plugins/AbstractDescribableScriptPlugin.java
+++ core/src/main/java/com/dtolabs/rundeck/core/plugins/AbstractDescribableScriptPlugin.java
@@ -157,18 +157,6 @@ public abstract class AbstractDescribableScriptPlugin implements Describable {
 
                     pbuild.required(required);
 
-
-//                    final Object blankIfUnexpandedValue = itemmeta.get(CONFIG_BLANK_IF_UNEXPANDED);
-//                    final boolean blankIfUnexpanded;
-//                    if (blankIfUnexpandedValue instanceof Boolean) {
-//                        blankIfUnexpanded = (Boolean) blankIfUnexpandedValue;
-//                    } else {
-//                        blankIfUnexpanded = blankIfUnexpandedValue instanceof String && Boolean.parseBoolean((String) blankIfUnexpandedValue);
-//                    }
-//
-//                    pbuild.blankIfUnexpandable(blankIfUnexpanded);
-
-
                     final Object defObj = itemmeta.get(CONFIG_DEFAULT);
 
                     pbuild.defaultValue(null != defObj ? defObj.toString() : null);
