d162199b402932b95a2797e6ba90c2070380c79d
Greg Schueler <greg.schueler@gmail.com>
2023-09-19 14:12:32 -0700

    remove job filters from rdUser datatype

--- rundeckapp/grails-app/taglib/rundeck/UserDataTagLib.groovy
+++ rundeckapp/grails-app/taglib/rundeck/UserDataTagLib.groovy
@@ -7,14 +7,6 @@ class UserDataTagLib {
 
     UserDataProvider userDataProvider
 
-    def getJobFilters = { attrs, body ->
-        def jobFilters = userDataProvider.findByLogin(attrs.user)?.jobfilters
-        def var = attrs.var ?: "filters"
-        if(jobFilters) {
-            out << body((var): jobFilters)
-        }
-    }
-
     def getReportFilters = { attrs, body ->
         def reportFilters = userDataProvider.findByLogin(attrs.user)?.reportfilters
         def var = attrs.var ?: "filters"
