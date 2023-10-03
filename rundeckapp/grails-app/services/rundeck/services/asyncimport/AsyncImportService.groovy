2614a42b0fbf61c88ae3be5b2c4838f82256d2a6
Darwis <darwisnarvaezdev@gmail.com>
2023-10-03 15:36:26 -0300

    Progress on the status informer endpoint

--- rundeckapp/grails-app/services/rundeck/services/asyncimport/AsyncImportService.groovy
+++ rundeckapp/grails-app/services/rundeck/services/asyncimport/AsyncImportService.groovy
@@ -1,6 +1,7 @@
 package rundeck.services.asyncimport
 
 import grails.converters.JSON
+import groovy.json.JsonSlurper
 import rundeck.services.FrameworkService
 
 import java.nio.charset.StandardCharsets
@@ -14,17 +15,21 @@ class AsyncImportService implements AsyncImportStatusFileOperations {
     static final String JSON_FILE_EXT = '.json'
 
     @Override
-    Long createStatusFile(AsyncImportStatusDTO newStatus) {
+    Long createStatusFile(String projectName) {
         def resource
         try {
-            def jsonStatus = newStatus as JSON
+            def statusPersist = new AsyncImportStatusDTO()
+                statusPersist.projectName = projectName
+                statusPersist.lastUpdated = new Date().toString()
+                statusPersist.lastUpdate = AsyncImportMilestone.M1_CREATED.name
+            def jsonStatus = statusPersist as JSON
             def inputStream = new ByteArrayInputStream(jsonStatus.toString().getBytes(StandardCharsets.UTF_8));
-            if (!newStatus.projectName || newStatus.projectName.size() <= 0) {
+            if (!statusPersist.projectName || statusPersist.projectName.size() <= 0) {
                 log.error("No project name provided in new status.")
                 throw new MissingPropertyException("No project name provided in new status.")
             }
-            final def fwkProject = frameworkService.getFrameworkProject(newStatus.projectName)
-            final def filename = JSON_FILE_PREFFIX + newStatus.projectName + JSON_FILE_EXT
+            final def fwkProject = frameworkService.getFrameworkProject(statusPersist.projectName)
+            final def filename = JSON_FILE_PREFFIX + statusPersist.projectName + JSON_FILE_EXT
             resource = fwkProject.storeFileResource(filename, inputStream)
             inputStream.close();
             return resource
@@ -32,5 +37,20 @@ class AsyncImportService implements AsyncImportStatusFileOperations {
             e.printStackTrace();
             throw e
         }
+    }
+
+    @Override
+    AsyncImportStatusDTO getAsyncImportStatusForProject(String projectName) {
+        try{
+            final def fwkProject = frameworkService.getFrameworkProject(projectName)
+            ByteArrayOutputStream output = new ByteArrayOutputStream()
+            fwkProject.loadFileResource(JSON_FILE_PREFFIX + projectName + JSON_FILE_EXT, output)
+            def obj = new JsonSlurper().parseText(output.toString()) as AsyncImportStatusDTO
+            log.debug("Object extracted: ${obj.toString()}")
+            return obj
+        }catch(Exception e){
+            log.error("Error during the async import file extraction process: ${e.message}")
+        }
+        return null
     }
 }
