d64df1fe72918e51ad21a122c4b002e052b170fb
Darwis <darwisnarvaezdev@gmail.com>
2023-10-03 18:34:19 -0300

    Progress on status file pipeline: added update method to service and a api mapping just for tests.

--- rundeckapp/grails-app/services/rundeck/services/asyncimport/AsyncImportStatusFileOperations.groovy
+++ rundeckapp/grails-app/services/rundeck/services/asyncimport/AsyncImportStatusFileOperations.groovy
@@ -3,4 +3,5 @@ package rundeck.services.asyncimport
 interface AsyncImportStatusFileOperations {
     Long createStatusFile(String projectName);
     AsyncImportStatusDTO getAsyncImportStatusForProject(String projectName);
+    Long updateAsyncImportStatus(AsyncImportStatusDTO updatedStatus);
 }
