e5d2289836b5f6a9320ebd6596364c3c26d64f42
Greg Schueler <greg.schueler@gmail.com>
2023-10-20 16:08:01 -0700

    update

--- functional-test/src/test/groovy/org/rundeck/tests/functional/selenium/pages/LoginPage.groovy
+++ functional-test/src/test/groovy/org/rundeck/tests/functional/selenium/pages/LoginPage.groovy
@@ -2,9 +2,8 @@ package org.rundeck.tests.functional.selenium.pages
 
 import groovy.transform.CompileStatic
 import org.openqa.selenium.By
-import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
-
+import org.rundeck.util.container.SeleniumContext
 /**
  * Login page
  */
@@ -14,12 +13,15 @@ class LoginPage extends BasePage {
     By passwordFieldBy = By.id("password")
     By loginBtnBy = By.id("btn-login")
 
-    public static final String PAGE_PATH = "/user/login"
+    static final String PAGE_PATH = "/user/login"
 
-    LoginPage(final WebDriver driver) {
-        super(driver)
+    LoginPage(final SeleniumContext context) {
+        super(context, "/")
+    }
+
+    void validatePage() {
         if (!driver.currentUrl.contains(PAGE_PATH)) {
-            throw new IllegalStateException("Not on login page")
+            throw new IllegalStateException("Not on login page: " + driver.currentUrl)
         }
     }
 
