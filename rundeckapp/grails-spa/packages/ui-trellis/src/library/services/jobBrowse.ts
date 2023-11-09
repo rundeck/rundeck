8c9a4b8290ac0f3b74123343904b86ebb7e59e4d
Greg Schueler <greg.schueler@gmail.com>
2023-11-08 16:57:55 -0800

    add job page store and bulk actions

--- rundeckapp/grails-spa/packages/ui-trellis/src/library/services/jobBrowse.ts
+++ rundeckapp/grails-spa/packages/ui-trellis/src/library/services/jobBrowse.ts
@@ -9,3 +9,61 @@ export async function browsePath(project: string, path: string,meta: string='*')
     return resp.data
   }
 }
+
+export interface ResultItem {
+    id: string;
+    message?: string;
+    errorCode?: string;
+}
+
+export interface BulkDeleteResponse {
+    failures: ResultItem[];
+    failedCount: number;
+    successCount: number;
+    requestCount: number;
+    allsuccessful: boolean;
+}
+
+export interface BulkToggleResponse {
+    failured: ResultItem[];
+    succeeded: ResultItem[];
+    enabled: boolean;
+    requestCount: number;
+    allsuccessful: boolean;
+}
+
+export async function bulkDeleteJobs(
+    project: string,
+    ids: string[]
+): Promise<BulkDeleteResponse> {
+    const resp = await api.post(`jobs/delete`, { ids });
+    if (resp.status !== 200) {
+        throw { message: resp.data.message, response: resp };
+    } else {
+        return resp.data;
+    }
+}
+export async function bulkScheduleEnableDisable(
+    project: string,
+    ids: string[],
+    enabled:boolean
+): Promise<BulkToggleResponse> {
+    const resp = await api.post(`jobs/schedule/${enabled?'enable':'disable'}`, { ids });
+    if (resp.status !== 200) {
+        throw { message: resp.data.message, response: resp };
+    } else {
+        return resp.data;
+    }
+}
+export async function bulkExecutionEnableDisable(
+    project: string,
+    ids: string[],
+    enabled:boolean
+): Promise<BulkToggleResponse> {
+    const resp = await api.post(`jobs/execution/${enabled?'enable':'disable'}`, { ids });
+    if (resp.status !== 200) {
+        throw { message: resp.data.message, response: resp };
+    } else {
+        return resp.data;
+    }
+}
