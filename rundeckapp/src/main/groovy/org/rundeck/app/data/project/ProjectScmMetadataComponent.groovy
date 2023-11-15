4bfea1e00a791c02dae381e756cdf733e94875d0
Greg Schueler <greg.schueler@gmail.com>
2023-11-15 11:51:13 -0800

    project scm metadata component

---
+++ rundeckapp/src/main/groovy/org/rundeck/app/data/project/ProjectScmMetadataComponent.groovy
@@ -0,0 +1,106 @@
+package org.rundeck.app.data.project
+
+import com.dtolabs.rundeck.core.authorization.AuthContextProcessor
+import com.dtolabs.rundeck.core.authorization.UserAndRolesAuthContext
+import com.dtolabs.rundeck.plugins.scm.ScmImportSynchState
+import com.dtolabs.rundeck.plugins.scm.ScmPluginException
+import groovy.transform.CompileStatic
+import org.rundeck.app.components.jobs.ComponentMeta
+import org.rundeck.app.components.project.ProjectMetadataComponent
+import org.rundeck.core.auth.AuthConstants
+import org.springframework.beans.factory.annotation.Autowired
+import rundeck.services.ScmService
+
+@CompileStatic
+class ProjectScmMetadataComponent implements ProjectMetadataComponent {
+    public static final String SCM_EXPORT = 'scmExport'
+    public static final String SCM_IMPORT = 'scmImport'
+    @Autowired
+    ScmService scmService
+    @Autowired
+    AuthContextProcessor rundeckAuthContextProcessor
+    final Set<String> availableMetadataNames = Collections.unmodifiableSet(
+        new HashSet<String>(
+            [SCM_EXPORT, SCM_IMPORT]
+        )
+    )
+
+    @Override
+    Optional<List<ComponentMeta>> getMetadataForProject(
+        final String project,
+        final Set<String> names,
+        final UserAndRolesAuthContext authContext
+    ) {
+
+        List<ComponentMeta> metaItems = new ArrayList<>()
+        if (
+            (names.contains(SCM_EXPORT) || names.contains('*')) &&
+            checkAuth(authContext, project, [AuthConstants.ACTION_EXPORT, AuthConstants.ACTION_SCM_EXPORT])
+        ) {
+            Map<String, Object> results = new HashMap<>()
+            try {
+                if (scmService.projectHasConfiguredExportPlugin(project)) {
+                    results.configured = true
+                    results.enabled = scmService.loadScmConfig(project, 'export')?.enabled
+                    if (results.enabled) {
+                        def validation = scmService
+                            .userHasAccessToScmConfiguredKeyOrPassword(authContext, ScmService.EXPORT, project)
+                        if (validation) {
+
+                            results.status = scmService.exportPluginStatus(authContext, project)
+                            results.actions = scmService.exportPluginActions(authContext, project)
+                            results.renamed = scmService.getRenamedJobPathsForProject(project)
+                        } else {
+                            results.valid = false
+                        }
+                    }
+                }
+            } catch (ScmPluginException e) {
+                results.warning = "Failed to update SCM Export status: ${e.message}".toString()
+            }
+            metaItems.add(ComponentMeta.with(SCM_EXPORT, results))
+        }
+        if (
+            (names.contains(SCM_IMPORT) || names.contains('*')) &&
+            checkAuth(authContext, project, [AuthConstants.ACTION_IMPORT, AuthConstants.ACTION_SCM_IMPORT])
+        ) {
+            Map<String, Object> results = new HashMap<>()
+            try {
+                if (scmService.projectHasConfiguredImportPlugin(project)) {
+                    results.configured = true
+                    results.enabled = scmService.loadScmConfig(project, 'import')?.enabled
+                    if (results.enabled) {
+                        def validation = scmService
+                            .userHasAccessToScmConfiguredKeyOrPassword(authContext, ScmService.IMPORT, project)
+                        if (validation) {
+                            ScmImportSynchState status = scmService.importPluginStatus(authContext, project)
+                            results.status = status
+                            results.actions = scmService.importPluginActions(authContext, project, status)
+                        } else {
+                            results.valid = false
+                        }
+                    }
+                }
+            } catch (ScmPluginException e) {
+                results.warning = "Failed to update SCM Import status: ${e.message}"
+            }
+            metaItems.add(ComponentMeta.with(SCM_IMPORT, results))
+        }
+
+        if (metaItems?.size()) {
+            return Optional.of(metaItems)
+        } else {
+            return Optional.empty()
+        }
+    }
+
+    private boolean checkAuth(UserAndRolesAuthContext authContext, String project, List<String> actions) {
+        rundeckAuthContextProcessor.authorizeApplicationResourceAny(
+            authContext,
+            rundeckAuthContextProcessor.authResourceForProject(
+                project
+            ),
+            [AuthConstants.ACTION_ADMIN, AuthConstants.ACTION_APP_ADMIN] + actions
+        )
+    }
+}
