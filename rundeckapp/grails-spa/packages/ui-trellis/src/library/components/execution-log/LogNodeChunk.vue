f3b14455e2f4d14ee2db459b4583182cdc733bed
Jesus Osuna <jesus.m.osuna.m@gmail.com>
2023-11-23 14:57:02 -0300

    follow buttom on log view page

--- rundeckapp/grails-spa/packages/ui-trellis/src/library/components/execution-log/LogNodeChunk.vue
+++ rundeckapp/grails-spa/packages/ui-trellis/src/library/components/execution-log/LogNodeChunk.vue
@@ -6,13 +6,15 @@
                      class="scroller execution-log__chunk"
                      key-field="lineNumber"
                      ref="scroller"
-                     @resize="scrollToLine(jumpToLine)"
+
     >
       <template v-slot="{ item, index, active}">
         <DynamicScrollerItem :item="item"
                              :data-index="index"
                              :active="active"
                              :size-dependencies="[item.log, item.logHtml]"
+                             :emit-resize="true"
+                             @resize="scrollToLine()"
         >
           <LogEntryFlex :eventBus="eventBus"
                         :key="index"
@@ -180,13 +182,7 @@ export default defineComponent({
     onSelectLine(index: number) {
       this.$emit('line-select', index)
     },
-    scrollToLine(jumpToLine: number) {
-      if (jumpToLine && jumpToLine <= this.entries.length && !this.jumped) {
-        this.$emit('line-select', jumpToLine)
-        this.$refs.scroller.scrollToItem(jumpToLine-1)
-        this.$emit('jumped')
-        return
-      }
+    scrollToLine() {
       if (this.follow) {
         this.$refs.scroller.scrollToBottom()
       }
