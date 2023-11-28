987acc8c107c2820fec6f325c6d1eec3ee2dc378
Greg Schueler <greg.schueler@gmail.com>
2023-11-27 16:32:19 -0800

    fix key for job item

--- rundeckapp/grails-spa/packages/ui-trellis/src/app/pages/job/browse/tree/Browser.vue
+++ rundeckapp/grails-spa/packages/ui-trellis/src/app/pages/job/browse/tree/Browser.vue
@@ -49,7 +49,7 @@
                   itemTag="li"
                   page-mode
               >
-                <browser-job-item :job="item" v-if="item.job" :key="item.job.id" :active="active" :load-meta="this.jobBrowserStore.findPath(this.browsePath).bpHit"/>
+                <browser-job-item :job="item" v-if="item.job" :key="item.id" :active="active" :load-meta="this.jobBrowserStore.findPath(this.browsePath).bpHit"/>
               </RecycleScroller>
 
             <li v-if="items.length === 0">
