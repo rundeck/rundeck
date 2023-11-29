f7b6b61158df2fea96ad0edd685b1431e15b98bd
L2JE <leojesus.juarez@gmail.com>
2023-11-29 17:45:32 -0300

    Loading scm actions and status badge for job asynchronously using vue sockets
    fixed unnecessary updates on JobScmStatus component caused by JobBrowserStore.loadJobMeta
    
    Fix job scm actions not showing in job list page
    
    use runClusterFix=true for getting scm status from scmMetadataComponent so it retrieves the same status as ScheduledExecutionService.scmActionMenuOptions
    
    Removed scm authorizing mocks from test for ScheduledExecutionController.show
    
    add a wait for JobCreatePage.adhocRemoteStringField to be clickable using locator to avoid error finding element

---
+++ rundeckapp/grails-spa/packages/ui-trellis/src/app/pages/job/head/scm/scm-action-buttons.ts
@@ -0,0 +1,43 @@
+import { defineComponent, markRaw, provide, reactive } from 'vue'
+import { getRundeckContext } from '../../../../../library'
+
+import moment from 'moment'
+import JobScmActions from '@/app/pages/job/browse/tree/JobScmActions.vue'
+import { JobBrowseItem } from "../../../../../library/types/jobs/JobBrowse";
+import { JobPageStoreInjectionKey } from "../../../../../library/stores/JobPageStore";
+import { observer } from "../../../../utilities/uiSocketObserver";
+
+const rootStore = getRundeckContext().rootStore;
+const jobPageStore = reactive(rootStore.jobPageStore);
+moment.locale(getRundeckContext().locale||'en_US')
+rootStore.ui.addItems([
+    {
+        section: "job-head",
+        location: "job-action-button",
+        visible: true,
+        widget: markRaw(
+            defineComponent({
+                name: "JobHeadScmActions",
+                components: { JobScmActions },
+                props: ["itemData"],
+                setup(props){
+                    let job : JobBrowseItem = reactive({ job: true, groupPath: "", id: props.itemData.jobUuid })
+                    provide(JobPageStoreInjectionKey, jobPageStore);
+
+                    jobPageStore.load()
+                    jobPageStore.getJobBrowser().loadJobMeta(job.id).then(jobMeta => job.meta = jobMeta)
+
+                    return { jobPageStore, job }
+                },
+                template: `<job-scm-actions :job="job"></job-scm-actions>`
+            })
+        ),
+    },
+]);
+
+window.addEventListener('DOMContentLoaded', () => {
+    let elem = document.getElementById("job_group_tree")
+    if (elem) {
+        observer.observe(elem, { subtree: true, childList: true })
+    }
+})
