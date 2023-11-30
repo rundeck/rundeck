2cfb46f312d5c92e7e2720e738a5aaeadd3f82c3
Darwis Narvaez <darwisnarvaezdev@gmail.com>
2023-11-30 11:57:50 -0300

    Added tests schematics

---
+++ functional-test/src/test/groovy/org/rundeck/tests/functional/selenium/aceEditor/AceEditorSpec.groovy
@@ -0,0 +1,24 @@
+package org.rundeck.tests.functional.selenium.aceEditor
+
+import org.rundeck.util.annotations.SeleniumCoreTest
+import org.rundeck.util.container.SeleniumBase
+
+@SeleniumCoreTest
+class AceEditorSpec extends SeleniumBase{
+
+    def "Edit json file resource model with indented text"(){
+
+        // 1. Logs, create a project
+        // 2. Adds a json file resource model
+        // 3. Attempt to edit the file, asses the ace editor rendered lines based on JSON text (must be > 1).
+
+        setup:
+
+        when:
+
+        then:
+
+
+    }
+
+}
