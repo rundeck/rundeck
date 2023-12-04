86514a6c31b535db170204126b9d6e4c56fdb513
carlosrfranco <carloseduardo.rorizfranco@gmail.com>
2023-12-04 14:01:29 -0300

    Fix: icon plugins

--- plugins/script-node-step-plugin/src/main/groovy/org/rundeck/plugin/scriptnodestep/CommandNodeStepPlugin.groovy
+++ plugins/script-node-step-plugin/src/main/groovy/org/rundeck/plugin/scriptnodestep/CommandNodeStepPlugin.groovy
@@ -3,6 +3,8 @@ package org.rundeck.plugin.scriptnodestep
 import com.dtolabs.rundeck.core.common.INodeEntry
 import com.dtolabs.rundeck.core.dispatcher.DataContextUtils
 import com.dtolabs.rundeck.core.execution.ExecArgList
+import com.dtolabs.rundeck.core.plugins.PluginException
+import com.dtolabs.rundeck.core.plugins.PluginResourceLoader
 import com.dtolabs.rundeck.core.utils.OptsUtil
 import org.rundeck.core.execution.ExecCommand
 import com.dtolabs.rundeck.core.execution.service.NodeExecutorResult
@@ -16,7 +18,7 @@ import com.dtolabs.rundeck.plugins.step.PluginStepContext
 
 @Plugin(service = ServiceNameConstants.WorkflowNodeStep, name = EXEC_COMMAND_TYPE)
 @PluginDescription(title = "Command", description = "Execute a remote command", isHighlighted = true, order = 0)
-class CommandNodeStepPlugin extends ScriptProxyRunner implements NodeStepPlugin, ExecCommand {
+class CommandNodeStepPlugin extends ScriptProxyRunner implements NodeStepPlugin, ExecCommand, PluginResourceLoader {
 
     @PluginProperty(title = "Command",
             description = "Enter the shell command, e.g.: echo this is a test",
@@ -40,4 +42,13 @@ class CommandNodeStepPlugin extends ScriptProxyRunner implements NodeStepPlugin,
         }
     }
 
+    @Override
+    List<String> listResources() throws PluginException, IOException {
+        ['WorkflowNodeStep.exec-command.icon.png']
+    }
+
+    @Override
+    InputStream openResourceStreamFor(String name) throws PluginException, IOException {
+        return this.getClass().getResourceAsStream("/" + name)
+    }
 }
