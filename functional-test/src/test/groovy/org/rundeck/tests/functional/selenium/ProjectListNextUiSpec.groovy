5fc44c2cfd2dc97fe24278a0b77a50fae8ed672a
smartinellibenedetti <139791797+smartinellibenedetti@users.noreply.github.com>
2023-12-08 13:55:11 -0700

    add ids for selenium test, and add initial structure for tests

---
+++ functional-test/src/test/groovy/org/rundeck/tests/functional/selenium/ProjectListNextUiSpec.groovy
@@ -0,0 +1,41 @@
+package org.rundeck.tests.functional.selenium
+
+import org.openqa.selenium.By
+import org.rundeck.tests.functional.selenium.pages.ProjectListPage
+import org.rundeck.tests.functional.selenium.pages.ProjectListNextUiPage
+import org.rundeck.tests.functional.selenium.pages.LoginPage
+import org.rundeck.util.annotations.SeleniumCoreTest
+import org.rundeck.util.container.SeleniumBase
+import spock.lang.Unroll
+
+@SeleniumCoreTest
+class ProjectListNextUiSpec extends SeleniumBase {
+    public static final String TEST_PROJECT = "SeleniumBasic"
+
+    def setupSpec() {
+        setupProject(TEST_PROJECT, "/projects-import/SeleniumBasic.zip")
+    }
+
+    @Unroll
+    def "view project list page"() {
+        setup:
+            ProjectListPage listPage = page ProjectListPage
+            LoginPage loginPage = page LoginPage
+            ProjectListNextUiPage projectListNextUiPage = page ProjectListNextUiPage
+        when:
+            loginPage.go()
+            loginPage.login(TEST_USER, TEST_PASS)
+        then:
+            listPage.validatePage()
+
+        when:
+            projectListNextUiPage.go()
+        then:
+            //validate ui type next
+            verify {
+                driver.currentUrl.contains('nextUi=true')
+            }
+
+            projectListNextUiPage.getFirstRunCreateBtn().isDisplayed()
+    }
+}
