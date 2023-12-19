5f4d8b495f330277e45660c47cabae48a00a6c1c
Alberto Hormazabal <ahormazabal@exatech.cl>
2023-12-19 11:57:29 -0300

    No more separate testdeck image.

--- scripts/circleci/testdeck-functions.sh
+++ scripts/circleci/testdeck-functions.sh
@@ -14,23 +14,23 @@ testdeck_build_rdtest() {
         build_rdtest_docker
     )
 
-    docker tag rdtest:latest "${ECR_TEST_REPO}:${ECR_IMAGE_TAG}"
+#    docker tag rdtest:latest "${ECR_TEST_REPO}:${ECR_IMAGE_TAG}"
 
 }
 
-testdeck_push_rdtest() {
+#testdeck_push_rdtest() {
+#
+#    # Tag and push to be used as cache source in later test builds
+#    docker_ecr_login
+#    docker push "${ECR_TEST_REPO}:${ECR_IMAGE_TAG}"
+#
+#}
 
-    # Tag and push to be used as cache source in later test builds
-    docker_ecr_login
-    docker push "${ECR_TEST_REPO}:${ECR_IMAGE_TAG}"
-
-}
-
-testdeck_pull_rdtest() {
-   docker_ecr_login
-   docker pull "${ECR_TEST_REPO}:${ECR_IMAGE_TAG}"
-   docker tag "${ECR_TEST_REPO}:${ECR_IMAGE_TAG}" rdtest:latest
-}
+#testdeck_pull_rdtest() {
+#   docker_ecr_login
+#   docker pull "${ECR_TEST_REPO}:${ECR_IMAGE_TAG}"
+#   docker tag "${ECR_TEST_REPO}:${ECR_IMAGE_TAG}" rdtest:latest
+#}
 
 
 testdeck_pull_rundeck() {
