3cfee99305f98802111d19481b0e967563a828af
JoseOrtiz <jortiz@pagerduty.com>
2023-12-20 11:48:17 -0300

    Use transaction for project and storage write to database

--- rundeckapp/src/main/groovy/org/rundeck/app/data/providers/storage/GormStorageDataProvider.groovy
+++ rundeckapp/src/main/groovy/org/rundeck/app/data/providers/storage/GormStorageDataProvider.groovy
@@ -32,68 +32,71 @@ class GormStorageDataProvider implements StorageDataProvider {
 
     @Override
     Long create(RundeckStorage data) throws DataAccessException {
-
-        def parent = PathUtil.parentPath(data.getPath())
-        String dir = parent?parent.path:''
-        String name = data.getPath().name
-        Storage s = new Storage(namespace: data.getNamespace(),
-                                name: name,
-                                dir: dir,
-                                data: data.getData())
-        s.setStorageMeta(data.getStorageMeta())
-        try {
-            if (storageDataService.save(s)) {
-                return s.getId()
-            } else {
-                log.warn(s.errors.allErrors.collect { messageSource.getMessage(it, null) }.join(","))
-                throw new DataAccessException(s.errors.allErrors.collect { messageSource.getMessage(it, null) }.join(","))
+        Storage.withTransaction {
+            def parent = PathUtil.parentPath(data.getPath())
+            String dir = parent ? parent.path : ''
+            String name = data.getPath().name
+            Storage s = new Storage(namespace: data.getNamespace(),
+                    name: name,
+                    dir: dir,
+                    data: data.getData())
+            s.setStorageMeta(data.getStorageMeta())
+            try {
+                if (storageDataService.save(s)) {
+                    return s.getId()
+                } else {
+                    log.warn(s.errors.allErrors.collect { messageSource.getMessage(it, null) }.join(","))
+                    throw new DataAccessException(s.errors.allErrors.collect { messageSource.getMessage(it, null) }.join(","))
+                }
+            } catch (Exception e) {
+                throw new DataAccessException("Failed to create storage: ${name}: ${e}", e)
             }
-        } catch (Exception e) {
-            throw new DataAccessException("Failed to create storage: ${name}: ${e}", e)
         }
     }
 
     @Override
     void update(final RundeckStorage source, final RundeckStorage data, Map<String, String> metadata) throws DataAccessException {
-        def sourceParent = PathUtil.parentPath(source.getPath())
-        String sourceDir = sourceParent?sourceParent.path:''
-        String sourceName = source.getPath().name
-        Storage storage = storageDataService.findByNamespaceAndDirAndName(source.namespace, sourceDir, sourceName)
-        if (!storage) {
-            throw new DataAccessException("Not found: storage with ID: ${source.id}")
-        }
-        def parent = PathUtil.parentPath(data.getPath())
-        String dir = parent?parent.path:''
-        String name = data.getPath().name
-        Map<String, String> existingMeta = storage.storageMeta?:[:]
-        storage.storageMeta = existingMeta + metadata
-
-        storage.namespace = data.getNamespace()
-        storage.name = name
-        storage.dir = dir
-        storage.data= data.getData()
-        storage.lastUpdated = new Date()
-
-         try {
-             storage.save(flush: true)
-        } catch (Exception e) {
-            throw new DataAccessException("Error: could not update project ${source.id}: ${e}", e)
+        Storage.withTransaction {
+            def sourceParent = PathUtil.parentPath(source.getPath())
+            String sourceDir = sourceParent ? sourceParent.path : ''
+            String sourceName = source.getPath().name
+            Storage storage = storageDataService.findByNamespaceAndDirAndName(source.namespace, sourceDir, sourceName)
+            if (!storage) {
+                throw new DataAccessException("Not found: storage with ID: ${source.id}")
+            }
+            def parent = PathUtil.parentPath(data.getPath())
+            String dir = parent ? parent.path : ''
+            String name = data.getPath().name
+            Map<String, String> existingMeta = storage.storageMeta ?: [:]
+            storage.storageMeta = existingMeta + metadata
+            storage.namespace = data.getNamespace()
+            storage.name = name
+            storage.dir = dir
+            storage.data = data.getData()
+            storage.lastUpdated = new Date()
+            try {
+                storage.save(flush: true)
+            } catch (Exception e) {
+                throw new DataAccessException("Error: could not update project ${source.id}: ${e}", e)
+            }
         }
     }
 
     @Override
     void delete(final RundeckStorage source) throws DataAccessException {
-        def sourceParent = PathUtil.parentPath(source.getPath())
-        String sourceDir = sourceParent?sourceParent.path:''
-        String sourceName = source.getPath().name
-        def storage = storageDataService.findByNamespaceAndDirAndName(source.namespace, sourceDir, sourceName)
-        if (!storage) {
-            throw new DataAccessException("Not found: storage with ID: ${source.id}")
-        }
-        try {
-            storage.delete(flush:true)
-         } catch (Exception e) {
-            throw new DataAccessException("Could not delete storage ${source.id}: ${e}", e)
+        Storage.withTransaction {
+            def sourceParent = PathUtil.parentPath(source.getPath())
+            String sourceDir = sourceParent ? sourceParent.path : ''
+            String sourceName = source.getPath().name
+            def storage = storageDataService.findByNamespaceAndDirAndName(source.namespace, sourceDir, sourceName)
+            if (!storage) {
+                throw new DataAccessException("Not found: storage with ID: ${source.id}")
+            }
+            try {
+                storage.delete(flush: true)
+            } catch (Exception e) {
+                throw new DataAccessException("Could not delete storage ${source.id}: ${e}", e)
+            }
         }
     }
     List<RundeckStorage> findAllByNamespaceAndDir(String namespace, String path) {
