6116b8ca9233f3efe68967400174524293f9e572
Antony Velasquez Ruiz <avelasquez@pagerduty.com>
2023-12-21 18:21:24 -0500

    Add APi Tests
    test/api/test-execution-state-404.sh
    test/api/test-execution-state.sh

--- functional-test/src/test/groovy/org/rundeck/tests/functional/api/job/JobExecutionSpec.groovy
+++ functional-test/src/test/groovy/org/rundeck/tests/functional/api/job/JobExecutionSpec.groovy
@@ -4,7 +4,12 @@ import org.rundeck.util.annotations.APITest
 import org.rundeck.util.container.BaseContainer
 
 @APITest
-class JobExecutionCleanerSpec extends BaseContainer {
+class JobExecutionSpec extends BaseContainer {
+
+    def setupSpec() {
+        startEnvironment()
+        setupProject()
+    }
 
     def "import project with configs and clean executions"() {
         given:
@@ -45,7 +50,7 @@ class JobExecutionCleanerSpec extends BaseContainer {
                 def json2 = client.jsonValue(response.body(), Map)
                 json2.executions.size() == 6
             }
-            sleep 40000
+            sleep 50000
             def responseClean = client.doGet("/project/${projectName}/executions")
             verifyAll {
                 responseClean.successful
@@ -59,6 +64,62 @@ class JobExecutionCleanerSpec extends BaseContainer {
             14      | "APIImportAndCleanHistoryTest"
             45      | "APIImportAndCleanHistoryTest45"
 
+    }
+
+    def "execution state not found"() {
+        given:
+            def client = getClient()
+        when:
+            def response = client.doGet("/execution/000/state")
+        then:
+            verifyAll {
+                !response.successful
+                response.code() == 404
+                def json = client.jsonValue(response.body(), Map)
+                json.errorCode == 'api.error.item.doesnotexist'
+                json.message == 'Execution does not exist: 000'
+            }
+    }
+
+    def "execution state OK"() {
+        given:
+            def client = getClient()
+        when:
+            def runCommand = client.post("/project/${PROJECT_NAME}/run/command?exec=echo+testing+execution+api", null, Map)
+            def idExec = runCommand.execution.id
+            def response = client.doGet("/execution/${idExec}/state")
+        then:
+            verifyAll {
+                response.successful
+                response.code() == 200
+                def json = client.jsonValue(response.body(), Map)
+                json.executionState == 'RUNNING'
+                json.targetNodes.size() == 1
+                json.targetNodes.get(0) == 'localhost'
+            }
+    }
+
+    def "POST job/id/run should succeed"() {
+        given:
+            def client = getClient()
+            def pathXmlFile = getClass().getResource("/projects-import/api-test-execution-state.xml").getPath()
+            def xmlProjectContent = new File(pathXmlFile).text
+            def xmlProject = xmlProjectContent.replaceAll("api-test-execution-state.xml", PROJECT_NAME)
+            new File(pathXmlFile).text = xmlProject
+        when:
+            def responseImport = client.doPost("/project/${PROJECT_NAME}/jobs/import", new File(pathXmlFile), "application/xml")
+            def json = client.jsonValue(responseImport.body(), Map)
+            def jobId = json.succeeded.get(2).id
+            def adhoc = runJobAndWait(jobId, ["options":["opt1": "foobar"]])
+            def exec = client.get("/execution/${adhoc.id}/state", Map)
+        then:
+            verifyAll {
+                responseImport.successful
+                responseImport.code() == 200
+                json.succeeded.size() == 3
+                adhoc.execState == 'succeeded'
+                exec.steps.get(0).nodeStates.localhost.executionState == 'SUCCEEDED'
+            }
     }
 
 }
