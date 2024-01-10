2a24f74ca635636e08fd7d909819d8b9f4d21053
L2JE <leojesus.juarez@gmail.com>
2024-01-10 14:51:33 -0300

    added test to validate QuartzSchedulerService throws when trying to schedule job while having executions mode active

--- rundeckapp/src/test/groovy/rundeck/services/QuartzJobScheduleManagerServiceSpec.groovy
+++ rundeckapp/src/test/groovy/rundeck/services/QuartzJobScheduleManagerServiceSpec.groovy
@@ -1,24 +1,42 @@
 package rundeck.services
 
 import com.dtolabs.rundeck.core.schedule.JobScheduleFailure
-import com.dtolabs.rundeck.core.schedule.JobScheduleManager
 import grails.testing.services.ServiceUnitTest
 import org.grails.spring.beans.factory.InstanceFactoryBean
+import org.quartz.JobKey
 import org.quartz.Scheduler
-import org.quartz.core.QuartzScheduler
 import rundeck.data.quartz.QuartzJobSpecifier
+import rundeck.quartzjobs.ExecutionJob
 import spock.lang.Specification
 
 class QuartzJobScheduleManagerServiceSpec extends Specification implements ServiceUnitTest<QuartzJobScheduleManagerService> {
-    def "interrupt schedule job NOW if executions are disabled"(){
+    def "interrupt schedule job NOW if execution mode is PASSIVE"(){
+        given:
+        defineBeans{
+            quartzScheduler(InstanceFactoryBean, Mock(Scheduler))
+            frameworkService(InstanceFactoryBean, Mock(FrameworkService){
+                configurationService >> Mock(ConfigurationService){
+                    isExecutionModeActive() >> false
+                }
+            })
+        }
         when:
         service.scheduleJobNow('a', 'b', [:], true)
         then:
         JobScheduleFailure e = thrown(JobScheduleFailure)
         e.getMessage() == 'Execution mode is PASSIVE'
-
     }
-    def "interrupt schedule job if executions are disabled"(){
+
+    def "interrupt schedule job if execution mode is PASSIVE"(){
+        given:
+        defineBeans{
+            quartzScheduler(InstanceFactoryBean, Mock(Scheduler))
+            frameworkService(InstanceFactoryBean, Mock(FrameworkService){
+                configurationService >> Mock(ConfigurationService){
+                    isExecutionModeActive() >> false
+                }
+            })
+        }
         when:
         service.scheduleJob('a', 'b', [:], new Date(), true)
         then:
@@ -26,18 +44,55 @@ class QuartzJobScheduleManagerServiceSpec extends Specification implements Servi
         e.getMessage() == 'Execution mode is PASSIVE'
     }
 
-    def setup(){
-        defineBeans {
+    def "schedule job NOW if execution mode is ACTIVE"(){
+        given:
+        defineBeans{
             quartzScheduler(InstanceFactoryBean, Mock(Scheduler))
             frameworkService(InstanceFactoryBean, Mock(FrameworkService){
                 configurationService >> Mock(ConfigurationService){
-                    isExecutionModeActive() >> false
+                    isExecutionModeActive() >> true
                 }
             })
-            scheduledExecutionService(InstanceFactoryBean, Mock(ScheduledExecutionService))
-            quartzJobSpecifier(InstanceFactoryBean, Mock(QuartzJobSpecifier))
         }
+        when:
+        service.scheduleJobNow('a', 'b', [:], true)
+        then:
+        notThrown(JobScheduleFailure)
+        1 * service.quartzScheduler.scheduleJob(_,_)
     }
 
+    def "schedule job if execution mode is ACTIVE"(){
+        given:
+        JobKey jobKey = new JobKey('a', 'b')
+        defineBeans{
+            quartzScheduler(InstanceFactoryBean, Mock(Scheduler){
+                checkExists(jobKey) >> scheduleExists
+            })
+            frameworkService(InstanceFactoryBean, Mock(FrameworkService){
+                configurationService >> Mock(ConfigurationService){
+                    isExecutionModeActive() >> true
+                }
+            })
+        }
+        when:
+        service.scheduleJob(jobKey.getName(), jobKey.getGroup(), [:], new Date(), true)
+        then:
+        notThrown(JobScheduleFailure)
+        scheduleJobCalls * service.quartzScheduler.scheduleJob(_,_)
+        rescheduleJobCalls * service.quartzScheduler.rescheduleJob(_,_)
 
+        where:
+        scheduleExists | rescheduleJobCalls | scheduleJobCalls
+        true           | 1                  | 0
+        false          | 0                  | 1
+    }
+
+    def setup(){
+        defineBeans {
+            scheduledExecutionService(InstanceFactoryBean, Mock(ScheduledExecutionService))
+            quartzJobSpecifier(InstanceFactoryBean, Mock(QuartzJobSpecifier){
+                getJobClass() >> ExecutionJob
+            })
+        }
+    }
 }
