85addc33311e9aca255f69eae6876310cf5f1508
Darwis Narvaez <darwisnarvaezdev@gmail.com>
2024-01-10 13:40:39 -0300

    reviewer requested changes

--- rundeckapp/grails-app/controllers/rundeckapp/UrlMappings.groovy
+++ rundeckapp/grails-app/controllers/rundeckapp/UrlMappings.groovy
@@ -149,7 +149,7 @@ class UrlMappings {
                 action: 'apiProjectExportAsyncDownload'
         )
         "/api/$api_version/project/$project/import"(controller: 'project',action: 'apiProjectImport')
-        "/api/$api_version/project/$project/async/import-status"(controller: 'project',action: 'apiProjectAsyncImportStatus')
+        "/api/$api_version/project/$project/import/status"(controller: 'project',action: 'apiProjectAsyncImportStatus')
 //        "/api/$api_version/project/$project/resources/refresh"(controller: 'framework', action: 'apiProjectResourcesRefresh')
         "/api/$api_version/project/$project/sources"(controller: 'framework') {
             action = [GET: "apiSourcesList"]
