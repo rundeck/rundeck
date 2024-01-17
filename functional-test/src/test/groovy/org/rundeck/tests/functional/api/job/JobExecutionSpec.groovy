16bbd084a6168789ba74bdd3f16495860b64cf65
Antony Velasquez Ruiz <avelasquez@pagerduty.com>
2024-01-17 09:05:21 -0500

    Add test
    test-job-executions.sh

--- functional-test/src/test/groovy/org/rundeck/tests/functional/api/job/JobExecutionSpec.groovy
+++ functional-test/src/test/groovy/org/rundeck/tests/functional/api/job/JobExecutionSpec.groovy
@@ -11,8 +11,10 @@ class JobExecutionSpec extends BaseContainer {
 
     @Shared String jobId
     @Shared String jobId2
+    @Shared String jobId3
     @Shared int execId
     @Shared int execId2
+    @Shared int execId3
 
     def setupSpec() {
         startEnvironment()
@@ -21,6 +23,8 @@ class JobExecutionSpec extends BaseContainer {
         jobId = jobImportFile(pathFile).succeeded[0].id
         def pathFile2 = updateFile("job-template-common.xml", null, "test job", "test/api/executions 2", "Test the /job/ID/executions API endpoint", "/bin/false this should fail")
         jobId2 = jobImportFile(pathFile2).succeeded[0].id
+        def pathFile3 = updateFile("job-template-common-2.xml", null, "test job", "test/api/executions 3", "Test the /job/ID/executions API endpoint", "echo this job will be killed...", "sleep 240")
+        jobId3 = jobImportFile(pathFile3).succeeded[0].id
     }
 
     def "job/jobId/executions should succeed with 0 results"() {
@@ -155,6 +159,35 @@ class JobExecutionSpec extends BaseContainer {
             json.executions.size() == 1
             json.executions[0].id == execId2
         }
+    }
+
+    def "job/id/executions?status=running with 1 results"() {
+        when:
+            execId3 = runJob(jobId3, ["options":["opt2": "a"]])
+        then:
+            def response = doGet("/job/${jobId3}/executions?status=running")
+            verifyAll {
+                response.successful
+                response.code() == 200
+                def json = jsonValue(response.body())
+                json.executions.size() == 1
+                json.executions[0].id == execId3
+            }
+    }
+
+    def "job/jobid/executions?status=aborted with 1 results"() {
+        when:
+            doPost("/execution/${execId3}/abort")
+            sleep 5000
+        then:
+            def response = doGet("/job/${jobId3}/executions?status=aborted")
+            verifyAll {
+                response.successful
+                response.code() == 200
+                def json = jsonValue(response.body())
+                json.executions.size() == 1
+                json.executions[0].id == execId3
+            }
     }
 
 }
