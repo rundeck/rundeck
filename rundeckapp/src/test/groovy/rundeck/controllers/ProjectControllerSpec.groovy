8125a043dfbc6f7c2acece86d66017fb7caaec74
Jesus Osuna <jesus.m.osuna.m@gmail.com>
2024-01-18 11:55:10 -0300

    fix test

--- rundeckapp/src/test/groovy/rundeck/controllers/ProjectControllerSpec.groovy
+++ rundeckapp/src/test/groovy/rundeck/controllers/ProjectControllerSpec.groovy
@@ -354,7 +354,7 @@ class ProjectControllerSpec extends Specification implements ControllerUnitTest<
         true | false | false | false   | false   | false
     }
 
-    def "api v34 exportAll include webhooks auth tokens when whkIncludeAuthTokens is set to true"(){
+    def "api v34 exportAll include webhooks auth tokens when whkIncludeAuthTokens && whkRegenUuid are set to true"(){
 
         given:"a project to be exported"
         controller.projectService = Mock(ProjectService)
@@ -362,10 +362,12 @@ class ProjectControllerSpec extends Specification implements ControllerUnitTest<
         controller.frameworkService = Mock(FrameworkService)
         setupGetResource()
         params.project = 'aproject'
+        Map<String, String> exportOpts = [(WebhooksProjectExporter.INLUDE_AUTH_TOKENS):"true",(WebhooksProjectExporter.WHK_REGEN_UUID):"true"]
 
         when:"exporting the project using the API"
         params.exportAll = "true"
         params.whkIncludeAuthTokens = "true"
+        params.whkRegenUuid = "true"
         request.api_version = ApiVersions.V34
         controller.apiProjectExport()
 
@@ -374,7 +376,7 @@ class ProjectControllerSpec extends Specification implements ControllerUnitTest<
         1 * controller.apiService.requireApi(_, _) >> true
         1 * controller.projectService.exportProjectToOutputStream(_, _, _, _, { ArchiveOptions opts ->
                     opts.all == true &&
-                    opts.exportOpts[WebhooksProjectComponent.COMPONENT_NAME]==[(WebhooksProjectExporter.INLUDE_AUTH_TOKENS):"true"]
+                    opts.exportOpts[WebhooksProjectComponent.COMPONENT_NAME]==exportOpts
         },_
         )
     }
@@ -428,11 +430,14 @@ class ProjectControllerSpec extends Specification implements ControllerUnitTest<
         controller.apiService = Mock(ApiService)
         controller.frameworkService = Mock(FrameworkService)
         setupGetResource()
+        Map<String, String> exportOpts = [(WebhooksProjectExporter.INLUDE_AUTH_TOKENS):whinclude.toString(),(WebhooksProjectExporter.WHK_REGEN_UUID):regenUuid.toString()]
+
 
         params.project = 'aproject'
 
         params.exportWebhooks=whenable.toString()
         params.whkIncludeAuthTokens=whinclude.toString()
+        params.whkRegenUuid=regenUuid.toString()
         request.api_version = 34
 
         when:
@@ -442,15 +447,15 @@ class ProjectControllerSpec extends Specification implements ControllerUnitTest<
         1 * controller.apiService.requireApi(_, _) >> true
 
         1 * controller.projectService.exportProjectToOutputStream(_, _, _, _, { ArchiveOptions opts ->
-                    opts.exportComponents[WebhooksProjectComponent.COMPONENT_NAME] == whenable &&
-                    opts.exportOpts[WebhooksProjectComponent.COMPONENT_NAME]==[(WebhooksProjectExporter.INLUDE_AUTH_TOKENS):whinclude.toString()]
+                    opts.exportComponents[WebhooksProjectComponent.COMPONENT_NAME] == true &&
+                    opts.exportOpts[WebhooksProjectComponent.COMPONENT_NAME]==exportOpts
         },_
         )
 
         where:
-            whenable | whinclude
-            true     | true
-            true     | false
+            whenable | whinclude    | regenUuid
+            true     | true         | true
+            true     | false        | false
     }
 
     def "api project delete error"() {
