8bfee1480dc3487bac4ac1c93ed075a780b1d4e2
smartinellibenedetti <139791797+smartinellibenedetti@users.noreply.github.com>
2024-01-22 17:41:25 -0700

    clean up

--- functional-test/src/test/groovy/org/rundeck/tests/functional/selenium/pages/JobCreatePage.groovy
+++ functional-test/src/test/groovy/org/rundeck/tests/functional/selenium/pages/JobCreatePage.groovy
@@ -137,9 +137,6 @@ class JobCreatePage extends BasePage {
         new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.elementToBeClickable(jobAddStepButton))
     }
 
-    void waitForURL(String url){
-        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.urlContains(url))
-    }
 }
 
 enum NotificationType {
