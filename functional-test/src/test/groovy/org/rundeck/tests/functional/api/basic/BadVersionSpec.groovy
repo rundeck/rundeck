07db24dec75fd138523da3a88d2447abd382664f
Darwis Narvaez <darwisnarvaezdev@gmail.com>
2024-01-26 15:29:51 -0300

    Bad version test

---
+++ functional-test/src/test/groovy/org/rundeck/tests/functional/api/basic/BadVersionSpec.groovy
@@ -0,0 +1,29 @@
+package org.rundeck.tests.functional.api.basic
+
+import org.rundeck.tests.functional.api.ResponseModels.ErrorResponse
+import org.rundeck.util.annotations.APITest
+import org.rundeck.util.container.BaseContainer
+
+@APITest
+class BadVersionSpec extends BaseContainer {
+
+    def setupSpec() {
+        startEnvironment()
+    }
+
+    def "test-project-bad-version"() {
+        when:
+        def projectName = PROJECT_NAME
+        def apiVersion = 10
+        def client = getClient()
+        client.apiVersion = apiVersion
+        def response = doGet("project/${projectName}")
+        ErrorResponse errorResponse = ErrorResponse.fromJson(response.body().string())
+
+        then:
+        errorResponse.errorCode == "api.error.api-version.unsupported"
+        errorResponse.error
+        errorResponse.message.contains("Unsupported API Version")
+
+    }
+}
