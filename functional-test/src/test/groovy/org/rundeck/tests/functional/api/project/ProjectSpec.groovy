ea39375b779777e90d8b9cca3eda65816f469790
Osmar Perez <operez@pagerduty.com>
2024-02-05 16:06:33 -0400

    Migrate Test Groovy
    Delete & migrate  test   test-project.sh & test-projects.sh

---
+++ functional-test/src/test/groovy/org/rundeck/tests/functional/api/project/ProjectSpec.groovy
@@ -0,0 +1,22 @@
+package org.rundeck.tests.functional.api.project
+
+import org.rundeck.util.annotations.APITest
+import org.rundeck.util.container.BaseContainer
+
+@APITest
+class ProjectSpec extends BaseContainer {
+    def setupSpec() {
+        startEnvironment()
+        setupProject("TestProject", null)
+    }
+
+    def "Test project" () {
+        given:
+            def projectName = "TestProject"
+            def response = getProject(projectName)
+        expect:
+            response.name == projectName
+            (response.url as String).endsWith("/project/$projectName")
+    }
+
+}
