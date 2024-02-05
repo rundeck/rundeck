2687aa4964d85a9c7adc0681cbbece9eaa89af13
Osmar Perez <operez@pagerduty.com>
2024-02-05 19:30:48 -0400

    Migration  Test  groovy
    Migration  Test

---
+++ functional-test/src/test/groovy/org/rundeck/tests/functional/api/project/ProjectsSpec.groovy
@@ -0,0 +1,20 @@
+package org.rundeck.tests.functional.api.project
+
+import org.rundeck.util.annotations.APITest
+import org.rundeck.util.container.BaseContainer
+
+@APITest
+class ProjectsSpec extends BaseContainer{
+    def setupSpec() {
+        startEnvironment()
+        setupProject("ProjectsSpec", null)
+    }
+
+    def "Test ProjectsSpec" () {
+        given:
+        def response = getProjects()
+        expect:
+        response.size() > 0
+    }
+
+}
