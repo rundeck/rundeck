d8e918a83aee5f44b69a02bfcad062afc7ff1214
smartinellibenedetti <139791797+smartinellibenedetti@users.noreply.github.com>
2024-02-09 15:45:46 -0700

    fix old tests

--- rundeckapp/grails-spa/packages/ui-trellis/src/library/components/widgets/theme-select/ThemeSelect.test.ts
+++ rundeckapp/grails-spa/packages/ui-trellis/src/library/components/widgets/theme-select/ThemeSelect.test.ts
@@ -1,27 +1,52 @@
-import { mount } from '@vue/test-utils';
-import ThemeSelect from './ThemeSelect.vue';
+import { shallowMount } from "@vue/test-utils";
+import ThemeSelect from "./ThemeSelect.vue";
+import { PrefersColorScheme, Theme } from "../../../stores/Theme";
+import { RundeckContext } from "@rundeck/client";
+import { EventBus } from "../../../utilities/vueEventBus";
+import { RootStore } from "../../../stores/RootStore";
+import { RundeckToken } from "../../../interfaces/rundeckWindow";
 
-describe('ThemeSelect', () => {
-    it('calls a method when an option is selected', async () => {
-        const mockMethod = jest.fn(); // Create a mock function to track method calls
-        window._rundeck = {
-            rootStore: {
-                theme: {
-                    setUserTheme: mockMethod,
-                    userPreferences: {
-                        theme: 'system'
-                    }
-                }
-            }
-        }
-        const wrapper = mount(ThemeSelect);
+describe("ThemeSelect", () => {
+  it("calls a method when an option is selected", async () => {
+    const mockMethod = jest.fn(); // Create a mock function to track method calls
+    window._rundeck = {
+      eventBus: jest.fn() as unknown as typeof EventBus,
+      rdBase: "mock_rdBase",
+      apiVersion: "mock_apiVersion",
+      projectName: "mock_projectName",
+      activeTour: "mock_activeTour",
+      activeTourStep: "mock_activeTourStep",
+      appMeta: {},
+      token: {} as RundeckToken,
+      tokens: {},
+      navbar: { items: [] as any },
+      feature: {},
+      data: {},
+      rundeckClient: {} as any,
+      rootStore: {
+        theme: {
+          setUserTheme: mockMethod,
+          theme: {} as Theme,
+          handleSystemChange: jest.fn(),
+          setTheme: jest.fn(),
+          loadConfig: jest.fn(),
+          saveConfig: jest.fn(),
+          userPreferences: {
+            theme: Theme.system,
+          },
+          prefersColorScheme: PrefersColorScheme.dark,
+          themeMediaQuery: jest.fn() as unknown as MediaQueryList,
+        },
+      } as unknown as RootStore,
+    };
+    const wrapper = shallowMount(ThemeSelect);
 
-        // Simulate selecting an option
-        const optionIndex = 1;
-        const option = wrapper.find(`option:nth-child(${optionIndex + 1})`);
-        await option.setValue(true);
+    // Simulate selecting an option
+    const optionIndex = 1;
+    const option = wrapper.find(`option:nth-child(${optionIndex + 1})`);
+    await option.setValue(true);
 
-        // Assert that the method was called with the correct option
-        expect(mockMethod).toHaveBeenCalledWith('light');
-    });
+    // Assert that the method was called with the correct option
+    expect(mockMethod).toHaveBeenCalledWith("light");
+  });
 });
--- rundeckapp/grails-spa/packages/ui-trellis/tests/unit/ExecutionOutput.spec.ts
+++ rundeckapp/grails-spa/packages/ui-trellis/tests/unit/ExecutionOutput.spec.ts
@@ -1,83 +1,125 @@
-import 'isomorphic-fetch'
+import "isomorphic-fetch";
 
-console.log(global.fetch)
+console.log(global.fetch);
 
-import {ExecutionOutput, ExecutionOutputStore} from '../../src/library/stores/ExecutionOutput'
-import {ExecutionLog} from '../../src/library/utilities/ExecutionLogConsumer'
+import {
+  ExecutionOutput,
+  ExecutionOutputStore,
+} from "../../src/library/stores/ExecutionOutput";
+import { ExecutionLog } from "../../src/library/utilities/ExecutionLogConsumer";
 
-import {observe, autorun, intercept} from 'mobx'
-import { PasswordCredentialProvider, passwordAuthPolicy, rundeckPasswordAuth, RundeckClient, TokenCredentialProvider } from '@rundeck/client'
-import {RundeckVcr, Cassette} from '@rundeck/client/dist/util/RundeckVcr'
-import { BtoA, AtoB } from '../utilities/Base64'
-import { RootStore } from '../../src/library/stores/RootStore'
-import fetchMock from 'fetch-mock'
+import { observe, autorun, intercept } from "mobx";
+import {
+  PasswordCredentialProvider,
+  passwordAuthPolicy,
+  rundeckPasswordAuth,
+  RundeckClient,
+  TokenCredentialProvider,
+  RundeckBrowser,
+} from "@rundeck/client";
+import { RundeckVcr, Cassette } from "@rundeck/client/dist/util/RundeckVcr";
+import { BtoA, AtoB } from "../utilities/Base64";
+import { RootStore } from "../../src/library/stores/RootStore";
+import fetchMock from "fetch-mock";
+import { RundeckToken } from "../../src/library/interfaces/rundeckWindow";
+import { EventBus } from "../../src/library";
 
-jest.setTimeout(60000)
+jest.setTimeout(60000);
 
-describe('ExecutionOutput Store', () => {
-    beforeEach(() => {
-        window._rundeck = {
-            navbar: {
-                items: []
-            }
-        }
-    })
-    it('Loads Output', async () => {
-        const client = new RundeckClient(new TokenCredentialProvider('foo'), {baseUri: '/'})
-        fetchMock.mock('path:/api/43/execution/900/output', { entries: [], completed: true, execCompleted: true })
+jest.mock("../../src/library/stores/RootStore", () => {
+  return {
+    __esModule: true,
+    RootStore: jest.fn().mockImplementation(() => {
+      return {};
+    }),
+  };
+});
 
-        const vcr = new RundeckVcr(fetchMock)
-        const cassette = await Cassette.Load('./tests/data/fixtures/ExecRunningOutput.json')
-        vcr.play(cassette, fetchMock)
+describe("ExecutionOutput Store", () => {
+  beforeEach(() => {
+    window._rundeck = {
+      activeTour: "",
+      activeTourStep: "",
+      apiVersion: "43",
+      appMeta: {},
+      data: {},
+      eventBus: {} as typeof EventBus,
+      feature: {},
+      navbar: {
+        items: [],
+      },
+      projectName: "test",
+      rdBase: "/",
+      rootStore: {} as unknown as RootStore,
+      rundeckClient: {} as RundeckBrowser,
+      token: {} as RundeckToken,
+      tokens: {},
+    };
+  });
+  it("Loads Output", async () => {
+    const client = new RundeckClient(new TokenCredentialProvider("foo"), {
+      baseUri: "/",
+    });
+    fetchMock.mock("path:/api/43/execution/900/output", {
+      entries: [],
+      completed: true,
+      execCompleted: true,
+    });
 
-        const rootStore = new RootStore(client)
+    const vcr = new RundeckVcr(fetchMock);
+    const cassette = await Cassette.Load(
+      "./tests/data/fixtures/ExecRunningOutput.json",
+    );
+    vcr.play(cassette, fetchMock);
 
-        const {executionOutputStore} = rootStore
+    const executionOutputStore = new ExecutionOutputStore(
+      window._rundeck.rootStore,
+      client,
+    );
 
-        const output = executionOutputStore.createOrGet('900')
+    const output = executionOutputStore.createOrGet("900");
 
-        const intercepter = observe(output.entries, change => {
-            console.log(change.type == 'splice')
-            if (change.type == 'splice') {
-                console.log(change.index)
-                console.log(change.addedCount)
-            }
-        })
+    const intercepter = observe(output.entries, (change) => {
+      console.log(change.type == "splice");
+      if (change.type == "splice") {
+        console.log(change.index);
+        console.log(change.addedCount);
+      }
+    });
 
-        let finished = false
-        while (!finished) {
-            const output = await executionOutputStore.getOutput('900', 200)
-            finished = output.completed
-        }
+    let finished = false;
+    while (!finished) {
+      const output = await executionOutputStore.getOutput("900", 200);
+      finished = output.completed;
+    }
 
-        // console.log(executionOutputStore.executionOutputsById.get('900')?.entries)
+    // console.log(executionOutputStore.executionOutputsById.get('900')?.entries)
+  });
+  // it('Saves', async () => {
+  // jest.setTimeout(60000)
+  // const client = rundeckPasswordAuth('admin', 'admin', {baseUri: 'http://xubuntu:4440'})
 
-    })
-    // it('Saves', async () => {
-        // jest.setTimeout(60000)
-        // const client = rundeckPasswordAuth('admin', 'admin', {baseUri: 'http://xubuntu:4440'})
+  // const vcr = new RundeckVcr()
 
-        // const vcr = new RundeckVcr()
+  // // const run = await client.jobExecutionRun('825b3ed7-3d40-418f-bfb4-313ff4d50577')
 
-        // // const run = await client.jobExecutionRun('825b3ed7-3d40-418f-bfb4-313ff4d50577')
+  // const cassette = new Cassette([/execution/, /job/], './tests/data/fixtures/ExecAnsiColorOutput.json')
 
-        // const cassette = new Cassette([/execution/, /job/], './tests/data/fixtures/ExecAnsiColorOutput.json')
+  // vcr.record(cassette)
 
-        // vcr.record(cassette)
-    
-        // const consumer = new ExecutionLog('912', client)
+  // const consumer = new ExecutionLog('912', client)
 
-        // await consumer.init()
+  // await consumer.init()
 
-        // await consumer.getJobWorkflow()
+  // await consumer.getJobWorkflow()
 
-        // let finished: boolean = false
-        // while (!finished) {
-        //     let resp = await consumer.getOutput(200)
-        //     finished = consumer.completed
-        // }
+  // let finished: boolean = false
+  // while (!finished) {
+  //     let resp = await consumer.getOutput(200)
+  //     finished = consumer.completed
+  // }
 
-        // await cassette.store()
+  // await cassette.store()
 
-    // })
-})
+  // })
+});
