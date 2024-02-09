c7fcb8d11b4e08b19b6728bbb1d26689202e9e0f
Greg Schueler <greg.schueler@gmail.com>
2024-02-09 11:12:45 -0800

    add i18n

--- rundeckapp/grails-spa/packages/ui-trellis/src/app/components/util/UndoRedo.vue
+++ rundeckapp/grails-spa/packages/ui-trellis/src/app/components/util/UndoRedo.vue
@@ -2,10 +2,10 @@
   <span>
     <btn :class="{ disabled: !hasUndo }" @click="doUndo" size="xs">
       <i class="glyphicon glyphicon-step-backward"></i>
-      Undo
+      {{ $t("util.undoredo.undo")}}
     </btn>
     <btn :class="{ disabled: !hasRedo }" @click="doRedo" size="xs">
-      Redo
+      {{ $t("util.undoredo.redo")}}
       <i class="glyphicon glyphicon-step-forward"></i>
     </btn>
   </span>
