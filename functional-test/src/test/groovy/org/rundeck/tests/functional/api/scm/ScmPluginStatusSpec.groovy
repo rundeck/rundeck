acb73003f9831ad332bbfcddb6fa31dd1bb8f7dd
L2JE <leojesus.juarez@gmail.com>
2024-02-13 17:20:47 -0300

    Migrated first scm test test-scm-plugin-status.sh

---
+++ functional-test/src/test/groovy/org/rundeck/tests/functional/api/scm/ScmPluginStatusSpec.groovy
@@ -0,0 +1,49 @@
+package org.rundeck.tests.functional.api.scm
+
+import org.rundeck.util.annotations.APITest
+import org.rundeck.util.api.scm.GitLocalServerRepoCreator
+import org.rundeck.util.api.scm.GitScmApiClient
+import org.rundeck.util.api.scm.IntegrationStatusResponse
+import org.rundeck.util.api.scm.SetupGitIntegrationRequest
+import org.rundeck.util.container.BaseContainer
+
+@APITest
+class ScmPluginStatusSpec extends BaseContainer {
+
+    static final String PROJECT_NAME = "ScmPluginStatusSpec-project"
+    def setupSpec() {
+        startEnvironment()
+        setupProject(PROJECT_NAME)
+    }
+
+    def "project scm export status must be clean after setup on empty project"(){
+        given:
+        String integration = "export"
+        GitScmApiClient scmClient = new GitScmApiClient(clientProvider).forIntegration(integration).forProject(PROJECT_NAME)
+
+        SetupGitIntegrationRequest requestBody = new SetupGitIntegrationRequest([ config: [
+                dir : "/home/rundeck/projects/${PROJECT_NAME}/ScmExport",
+                url :"${GitLocalServerRepoCreator.REPO_TEMPLATE_PATH}",
+                committerName :"Git Test",
+                committerEmail :"A@test.com",
+                pathTemplate :'${job.group}${job.name}-${job.id}.${config.format}',
+                format :"xml",
+                branch :"master",
+                strictHostKeyChecking :"yes"
+        ]])
+
+        expect:
+        scmClient.setupIntegration(requestBody).success
+
+        when:
+        IntegrationStatusResponse resp = scmClient.getIntegrationStatus()
+
+        then:
+        verifyAll {
+            resp != null
+            resp.project == PROJECT_NAME
+            resp.integration == integration
+            resp.synchState == "CLEAN"
+        }
+    }
+}
---
+++ functional-test/src/test/groovy/org/rundeck/util/api/scm/GitLocalServerRepoCreator.groovy
@@ -0,0 +1,5 @@
+package org.rundeck.util.api.scm
+
+class GitLocalServerRepoCreator {
+    static final REPO_TEMPLATE_PATH = "/tmp/repoTemplate"
+}
