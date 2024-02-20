86e9fcddd74fad3a37ea8e4bdc1fad2df5e844c3
Jaya Singh <jaya@catalytic.com>
2024-02-20 12:44:21 -0600

    fixed the eslint, indent lines

--- rundeckapp/grails-spa/packages/ui-trellis/src/app/components/readme-motd/EditProjectFile.vue
+++ rundeckapp/grails-spa/packages/ui-trellis/src/app/components/readme-motd/EditProjectFile.vue
@@ -16,45 +16,70 @@
       <div class="container-fluid">
         <div class="row">
           <div class="col-xs-12">
-            <div class="card" id="createform">
+            <div id="createform" class="card">
               <div class="card-header">
-                <h3 class="card-title">{{  $t("edit.file.project", [this.filename, this.project]) }}</h3>
-
+                <h3 class="card-title">
+                  {{ $t("edit.file.project", [filename, project]) }}
+                </h3>
               </div>
               <div class="card-content">
                 <div class="help-block">
                   <details class="details-reset more-info">
                     <summary>
                       <span v-html="$t('file.readme.help.markdown')"></span>
-                      <span class="more-indicator-verbiage more-info-icon"><i class="glyphicon glyphicon-chevron-right "></i></span>
-                      <span class="less-indicator-verbiage more-info-icon"><i class="glyphicon glyphicon-chevron-down "></i></span>
+                      <span class="more-indicator-verbiage more-info-icon"
+                        ><i class="glyphicon glyphicon-chevron-right"></i
+                      ></span>
+                      <span class="less-indicator-verbiage more-info-icon"
+                        ><i class="glyphicon glyphicon-chevron-down"></i
+                      ></span>
                     </summary>
-                    <span v-html="$t('file.readme.help.html')"></span> <a href="https://en.wikipedia.org/wiki/Markdown" target="_blank" rel="noopener noreferrer">
-                    {{ $t('file.readme.help.linkText') }}
-                  </a>
+                    <span v-html="$t('file.readme.help.html')"></span>
+                    <a
+                      href="https://en.wikipedia.org/wiki/Markdown"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {{ $t("file.readme.help.linkText") }}
+                    </a>
                   </details>
                 </div>
-                <ace-editor v-model="fileText"
-                            :soft-wrap-control="true"
-                            height="250"
-                            width="100%"
-                            lang="markdown"
-                            :readOnly="false"
+                <ace-editor
+                  v-model="fileText"
+                  :soft-wrap-control="true"
+                  height="250"
+                  width="100%"
+                  lang="markdown"
+                  :read-only="false"
                 />
               </div>
               <div class="card-footer">
-                <button type="button" class="btn btn-default reset_page_confirm" @click="createProjectHomeLink">Cancel</button>
-                <button type="submit" class="btn btn-cta reset_page_confirm" @click="saveProjectFile">Save</button>
+                <button
+                  type="button"
+                  class="btn btn-default reset_page_confirm"
+                  @click="createProjectHomeLink"
+                >
+                  Cancel
+                </button>
+                <button
+                  type="submit"
+                  class="btn btn-cta reset_page_confirm"
+                  @click="saveProjectFile"
+                >
+                  Save
+                </button>
                 <template v-if="displayConfig.includes('none')">
-                    <span class="text-warning text-right">
-                      <template v-if="authAdmin">
-                        {{ $t("file.warning.not.displayed.admin.message") }}
-                        <a :href="createProjectConfigureLink"> {{ $t("project.configuration.label") }} </a>
-                      </template>
-                      <template v-else>
-                        {{ $t("file.warning.not.displayed.nonadmin.message") }}
-                      </template>
-                    </span>
+                  <span class="text-warning text-right">
+                    <template v-if="authAdmin">
+                      {{ $t("file.warning.not.displayed.admin.message") }}
+                      <a :href="createProjectConfigureLink">
+                        {{ $t("project.configuration.label") }}
+                      </a>
+                    </template>
+                    <template v-else>
+                      {{ $t("file.warning.not.displayed.nonadmin.message") }}
+                    </template>
+                  </span>
                 </template>
               </div>
             </div>
@@ -66,72 +91,78 @@
 </template>
 
 <script>
-import {defineComponent} from "vue";
+import { defineComponent } from "vue";
 import AceEditorVue from "@/library/components/utils/AceEditorVue.vue";
 import AceEditor from "@/library/components/utils/AceEditor.vue";
-import {getRundeckContext} from "@/library";
-import {url} from  "@/library/rundeckService"
-import {Notification} from "uiv";
-import {saveProjectFile, getFileText} from "@/app/components/readme-motd/editProjectFileService";
+import { getRundeckContext } from "@/library";
+import { url } from "@/library/rundeckService";
+import { Notification } from "uiv";
+import {
+  saveProjectFile,
+  getFileText,
+} from "@/app/components/readme-motd/editProjectFileService";
 
-const rundeckClient = getRundeckContext().rundeckClient
+const rundeckClient = getRundeckContext().rundeckClient;
 export default defineComponent({
   name: "EditProjectFile",
-  components: {AceEditor, AceEditorVue},
-  data() {
-    return {
-      fileText: '',
-      markdownSectionOpen: false,
-      errorMsg: ''
-    };
-  },
+  components: { AceEditor, AceEditorVue },
   props: {
     filename: {
       type: String,
-      default: ''
+      default: "",
     },
     displayConfig: {
       type: Array,
-      default: []
+      default: [],
     },
     project: {
       type: String,
-      default: ''
+      default: "",
     },
     authAdmin: {
       type: Boolean,
-      default: false
+      default: false,
     },
+  },
+  data() {
+    return {
+      fileText: "",
+      markdownSectionOpen: false,
+      errorMsg: "",
+    };
   },
   computed: {
     createProjectConfigureLink() {
-      return url('project/' + this.project + '/configure').href
+      return url("project/" + this.project + "/configure").href;
     },
   },
   mounted() {
-    this.getFileText()
+    this.getFileText();
   },
   methods: {
     async saveProjectFile() {
-      try{
-        const resp = await saveProjectFile(this.project, this.filename, this.fileText)
-        if(resp.success){
-          this.notifySuccess("Success", "Saved Project File " + this.filename )
+      try {
+        const resp = await saveProjectFile(
+          this.project,
+          this.filename,
+          this.fileText,
+        );
+        if (resp.success) {
+          this.notifySuccess("Success", "Saved Project File " + this.filename);
         }
-      }
-      catch (e) {
-        this.notifyError(e.message)
+      } catch (e) {
+        this.notifyError(e.message);
       }
     },
     createProjectHomeLink() {
-      document.location = url('project/' + this.project + '/home').href
+      document.location = url("project/" + this.project + "/home").href;
     },
     notifyError(msg) {
       Notification.notify({
         type: "danger",
         title: "An Error Occurred",
         content: msg,
-        duration: 0
+        duration: 0,
       });
     },
 
@@ -140,7 +171,7 @@ export default defineComponent({
         type: "success",
         title: title,
         content: msg,
-        duration: 5000
+        duration: 5000,
       });
     },
     notifyWarning(message) {
@@ -148,25 +179,29 @@ export default defineComponent({
         type: "info",
         title: "Warning",
         content: message,
-        duration: 0
+        duration: 0,
       });
     },
-    async getFileText(){
-      try{
-        const response = await getFileText(this.project, this.filename)
-        if(response.success){
-          this.fileText = response.contents
+    async getFileText() {
+      try {
+        const response = await getFileText(this.project, this.filename);
+        if (response.success) {
+          this.fileText = response.contents;
+        }
+      } catch (e) {
+        if (e.warning) {
+          this.notifyWarning(
+            "The file " +
+              this.filename +
+              " does not exist in project " +
+              this.project +
+              " yet. Please save to create it.",
+          );
+        } else {
+          this.notifyError(e.message);
         }
       }
-      catch (e) {
-        if(e.warning){
-          this.notifyWarning("The file " + this.filename + " does not exist in project " + this.project + " yet. Please save to create it.");
-        }
-        else{
-          this.notifyError(e.message)
-        }
-      }
-    }
+    },
   },
 });
 </script>
--- rundeckapp/grails-spa/packages/ui-trellis/src/app/components/readme-motd/main.ts
+++ rundeckapp/grails-spa/packages/ui-trellis/src/app/components/readme-motd/main.ts
@@ -1,63 +1,64 @@
-
-import {defineComponent, markRaw} from 'vue'
-import {getRundeckContext} from "../../../library";
+import { defineComponent, markRaw } from "vue";
+import { getRundeckContext } from "../../../library";
 import EditProjectFile from "./EditProjectFile.vue";
 
+import messages from "./i18n";
 
-import messages from './i18n'
-
-const _i18n = messages as any
+const _i18n = messages as any;
 
 // Create VueI18n instance with options
-let locale = getRundeckContext().locale || 'en_US'
-let lang = getRundeckContext().language || 'en'
+const locale = getRundeckContext().locale || "en_US";
+const lang = getRundeckContext().language || "en";
 
 // include any i18n injected in the page by the app
-let i18nMessages =
-    {
-        [locale]: Object.assign(
-            {},
-            _i18n[locale] || _i18n[lang] || _i18n['en_US'] || {}
-        )
-    }
+const i18nMessages = {
+  [locale]: Object.assign(
+    {},
+    _i18n[locale] || _i18n[lang] || _i18n["en_US"] || {},
+  ),
+};
 
-let rundeckContext = getRundeckContext()
+const rundeckContext = getRundeckContext();
 
 rundeckContext.rootStore.ui.addItems([
-    {
-        section: 'edit-project-file',
-        location: 'main',
-        visible: true,
-        widget: markRaw(defineComponent({
-            components: {
-                EditProjectFile
-            },
-            props: ['itemData'],
-            inject: ['addUiMessages'],
+  {
+    section: "edit-project-file",
+    location: "main",
+    visible: true,
+    widget: markRaw(
+      defineComponent({
+        components: {
+          EditProjectFile,
+        },
+        inject: ["addUiMessages"],
+        props: ["itemData"],
 
-            data() {
-                return {
-                    filename: '',
-                    displayConfig: [],
-                    project: '',
-                    authAdmin: false,
-                };
-            },
-            created() {
-                this.addUiMessages([i18nMessages[locale]]);
-                this.filename = this.itemData.filename;
-                // code to handle displayConfig
-                if (typeof this.itemData.displayConfig === 'string') {
-                    this.displayConfig = this.itemData.displayConfig.replace(/^\[|\]$/g, '').split(', ')
-                }
+        data() {
+          return {
+            filename: "",
+            displayConfig: [],
+            project: "",
+            authAdmin: false,
+          };
+        },
+        created() {
+          this.addUiMessages([i18nMessages[locale]]);
+          this.filename = this.itemData.filename;
+          // code to handle displayConfig
+          if (typeof this.itemData.displayConfig === "string") {
+            this.displayConfig = this.itemData.displayConfig
+              .replace(/^\[|\]$/g, "")
+              .split(", ");
+          }
 
-                this.project = this.itemData.project;
-                this.authAdmin = Boolean(this.itemData.authAdmin);
-            },
-            template: `
+          this.project = this.itemData.project;
+          this.authAdmin = Boolean(this.itemData.authAdmin);
+        },
+        template: `
               <edit-project-file :filename="filename" :display-config="displayConfig" :project="project"
                                  :auth-admin="authAdmin"/>\`
             `,
-        }))
-    }
-])
+      }),
+    ),
+  },
+]);
