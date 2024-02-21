f4b078a73d49e0240fd2c063a52f4d39a51f8755
Stephen Joyner <sjoyner@pagerduty.com>
2024-02-21 15:38:46 -0600

    RCLOUD-1182: Add ability to configure path prefixes for web requests that should be excluded from web request log

--- rundeckapp/grails-app/controllers/rundeck/interceptors/ZZ_TimerInterceptor.groovy
+++ rundeckapp/grails-app/controllers/rundeck/interceptors/ZZ_TimerInterceptor.groovy
@@ -3,6 +3,7 @@ package rundeck.interceptors
 class ZZ_TimerInterceptor {
 
     int order = HIGHEST_PRECEDENCE + 500
+    def configurationService
 
     ZZ_TimerInterceptor() {
         matchAll()
@@ -11,6 +12,11 @@ class ZZ_TimerInterceptor {
     boolean before() { true }
 
     boolean after() {
+        def ignorePrefixes = configurationService.getValue("web.logging.ignorePrefixes", [])
+        String testUri = request[AA_TimerInterceptor._REQ_URI]
+        if(ignorePrefixes.any { testUri.startsWith(it) }) {
+            return true
+        }
         AA_TimerInterceptor.afterRequest(request,response,session)
         true
     }
