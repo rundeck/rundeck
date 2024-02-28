aa00c84b468fccdffa21e2564554651aeaa80f7f
Stephen Joyner <sjoyner@pagerduty.com>
2024-02-28 10:10:36 -0600

    All constants should be final

--- grails-rundeck-data-shared/src/main/groovy/rundeck/data/constants/ExecutionConstants.groovy
+++ grails-rundeck-data-shared/src/main/groovy/rundeck/data/constants/ExecutionConstants.groovy
@@ -1,22 +1,22 @@
 package rundeck.data.constants
 
 class ExecutionConstants {
-    public static String EXECUTION_RUNNING = "running"
-    public static String EXECUTION_SUCCEEDED = "succeeded"
-    public static String EXECUTION_FAILED = "failed"
-    public static String EXECUTION_ABORTED = "aborted"
-    public static String EXECUTION_TIMEDOUT = "timedout"
-    public static String EXECUTION_FAILED_WITH_RETRY = "failed-with-retry"
-    public static String EXECUTION_STATE_OTHER = "other"
-    public static String EXECUTION_SCHEDULED = "scheduled"
-    public static String EXECUTION_MISSED = "missed"
-    public static String EXECUTION_QUEUED = "queued"
-    public static String AVERAGE_DURATION_EXCEEDED = "average-duration-exceeded"
+    public static final String EXECUTION_RUNNING = "running"
+    public static final String EXECUTION_SUCCEEDED = "succeeded"
+    public static final String EXECUTION_FAILED = "failed"
+    public static final String EXECUTION_ABORTED = "aborted"
+    public static final String EXECUTION_TIMEDOUT = "timedout"
+    public static final String EXECUTION_FAILED_WITH_RETRY = "failed-with-retry"
+    public static final String EXECUTION_STATE_OTHER = "other"
+    public static final String EXECUTION_SCHEDULED = "scheduled"
+    public static final String EXECUTION_MISSED = "missed"
+    public static final String EXECUTION_QUEUED = "queued"
+    public static final String AVERAGE_DURATION_EXCEEDED = "average-duration-exceeded"
 
-    public static String ABORT_PENDING = "pending"
-    public static String ABORT_ABORTED = "aborted"
-    public static String ABORT_FAILED = "failed"
+    public static final String ABORT_PENDING = "pending"
+    public static final String ABORT_ABORTED = "aborted"
+    public static final String ABORT_FAILED = "failed"
 
-    public static String ABORT_EVENT = "execution-abort"
+    public static final String ABORT_EVENT = "execution-abort"
 
 }
