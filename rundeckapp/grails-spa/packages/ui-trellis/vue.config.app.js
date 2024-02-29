77264c8fece2a92119d3486eec5800423d0ac632
smartinellibenedetti <139791797+smartinellibenedetti@users.noreply.github.com>
2024-02-29 11:13:14 -0700

    remove duplicated vueloaderplugin as vue-cli-service adds it under the hood

--- rundeckapp/grails-spa/packages/ui-trellis/vue.config.app.js
+++ rundeckapp/grails-spa/packages/ui-trellis/vue.config.app.js
@@ -1,6 +1,5 @@
 const Path = require("path");
 const webpack = require("webpack");
-const { VueLoaderPlugin } = require("vue-loader");
 
 const BUILD_COPYRIGHT = `© ${new Date().getFullYear()} PagerDuty, Inc. All Rights Reserved.`;
 
@@ -127,7 +126,6 @@ module.exports = {
         filename: "[file].map",
         include: [/\.css$/],
       }),
-      new VueLoaderPlugin(),
     ],
   },
   transpileDependencies: ["uiv"],
