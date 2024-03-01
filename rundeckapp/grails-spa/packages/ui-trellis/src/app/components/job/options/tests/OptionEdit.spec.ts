caff63379be935e523c6b00602e68e8989f627b6
Greg Schueler <greg.schueler@gmail.com>
2024-02-29 16:56:16 -0800

    test for file type in option edit

---
+++ rundeckapp/grails-spa/packages/ui-trellis/src/app/components/job/options/tests/OptionEdit.spec.ts
@@ -0,0 +1,115 @@
+import { mount, shallowMount, VueWrapper } from "@vue/test-utils";
+import { action } from "mobx";
+import { Btn } from "uiv";
+import { OptionValidation } from "../../../../../library/types/jobs/JobEdit";
+import OptionEdit from "../OptionEdit.vue";
+
+jest.mock("@/library/modules/rundeckClient", () => ({
+  client: {},
+}));
+// Mock validateJobOption method
+jest.mock("@/library/services/jobEdit", () => ({
+  validateJobOption: jest.fn().mockResolvedValue({
+    valid: true,
+    message: "",
+    messages: {},
+  } as OptionValidation),
+}));
+const mountOptionEdit = async (options: any): Promise<VueWrapper<any>> => {
+  const wrapper = mount(OptionEdit, {
+    props: {
+      ...options,
+    },
+    global: {
+      mocks: {
+        $t: (msg: string) => msg,
+      },
+    },
+    components: {
+      Btn,
+    },
+  });
+
+  // Wait for the next Vue tick to allow for asynchronous rendering
+  await wrapper.vm.$nextTick();
+
+  return wrapper;
+};
+const shallowMountOptionEdit = async (
+  options: any,
+): Promise<VueWrapper<any>> => {
+  const wrapper = shallowMount(OptionEdit, {
+    props: {
+      ...options,
+    },
+    global: {
+      mocks: {
+        $t: (msg: string) => msg,
+      },
+      stubs: {
+        PluginConfig: {
+          name: "PluginConfig",
+          props: [
+            "mode",
+            "serviceName",
+            "modelValue",
+            "provider",
+            "showTitle",
+            "showDescription",
+            "scope",
+          ],
+          template:
+            "<div data-test-component='pluginConfig' :data-test-svc-name='serviceName' :data-test-provider='provider'>" +
+            "Plugin Config Component" +
+            "</div>",
+        },
+      },
+    },
+    components: {
+      Btn,
+    },
+  });
+
+  // Wait for the next Vue tick to allow for asynchronous rendering
+  await wrapper.vm.$nextTick();
+
+  return wrapper;
+};
+
+describe("OptionEdit", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it.each([
+    [false, "pluginType", false],
+    [true, null, false],
+    [true, "", false],
+    [true, "pluginType", true],
+  ])(
+    "file select option presence if file plugin enabled %p and pluginType %p expect %p",
+    async (enabled: boolean, pluginType: any, expected: boolean) => {
+      const wrapper = await shallowMountOptionEdit({
+        features: { fileUploadPlugin: enabled },
+        fileUploadPluginType: pluginType,
+      });
+
+      const sel = wrapper.get("#opttype_");
+      expect(sel.findAll("option").length).toBe(expected ? 2 : 1);
+      expect(sel.findAll("option[value=text]").length).toBe(1);
+      expect(sel.findAll("option[value=file]").length).toBe(expected ? 1 : 0);
+    },
+  );
+  it("file input type shows file plugin config", async () => {
+    const wrapper = await mountOptionEdit({
+      modelValue: { name: "a_test_option", optionType: "file" },
+      editable: true,
+      features: { fileUploadPlugin: true },
+      fileUploadPluginType: "pluginType",
+    });
+
+    let element = wrapper.get(
+      ".form-group plugin-config[servicename=FileUpload][provider=pluginType]",
+    );
+  });
+});
