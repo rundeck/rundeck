17a779226eb7ac6830310b0de72319fbc0ace8db
Rodrigo Navarro <rnavarro@pagerduty.com>
2024-03-13 11:16:04 -0300

    instad of going directly to home, it uses the mainbar

--- functional-test/src/test/groovy/org/rundeck/tests/functional/selenium/project/EditProjectSpec.groovy
+++ functional-test/src/test/groovy/org/rundeck/tests/functional/selenium/project/EditProjectSpec.groovy
@@ -3,6 +3,7 @@ package org.rundeck.tests.functional.selenium.project
 import org.rundeck.util.annotations.SeleniumCoreTest
 import org.rundeck.util.container.SeleniumBase
 import org.rundeck.util.gui.common.navigation.NavLinkTypes
+import org.rundeck.util.gui.pages.TopMenuPage
 import org.rundeck.util.gui.pages.home.HomePage
 import org.rundeck.util.gui.pages.login.LoginPage
 import org.rundeck.util.gui.pages.project.DashboardPage
@@ -75,10 +76,12 @@ class EditProjectSpec extends SeleniumBase {
             setupProject(projectName)
             def projectEditPage = page ProjectEditPage
             def projectDashboard = page DashboardPage
-            def loginPage = go LoginPage
+            def topMenu = page TopMenuPage
             def sideBarPage = page SideBarPage
             def homePage = page HomePage
+            def loginPage = page LoginPage
         when:
+            loginPage.go()
             loginPage.login(TEST_USER, TEST_PASS)
             projectEditPage.go("/project/${projectName}/configure")
             projectEditPage.clickEditConfigurationFile()
@@ -89,7 +92,7 @@ class EditProjectSpec extends SeleniumBase {
             projectDashboard.expectProjectDescriptionToBe(projectDescription)
             projectDashboard.expectProjectLabelToBe(projectLabel)
         when: "go to homepage and check for description and label to be shown"
-            homePage.go()
+            topMenu.clickHomeButton()
         then:
             homePage.expectPartialLinkToExist(projectDescription)
             homePage.expectPartialLinkToExist(projectLabel)
@@ -98,7 +101,7 @@ class EditProjectSpec extends SeleniumBase {
             projectEditPage.setProjectLabel projectLabel+"edited"
             projectEditPage.setProjectDescription projectDescription+"edited"
             projectEditPage.save()
-            homePage.go()
+            topMenu.clickHomeButton()
         then: "validate that the label and description are shown with the new values"
             homePage.expectPartialLinkToExist(projectDescription+"edited")
             homePage.expectPartialLinkToExist(projectLabel+"edited")
@@ -107,7 +110,7 @@ class EditProjectSpec extends SeleniumBase {
             projectEditPage.setProjectLabel ""
             projectEditPage.setProjectDescription ""
             projectEditPage.save()
-            homePage.go()
+            topMenu.clickHomeButton()
         then: "validate that the project name is shown"
             homePage.expectPartialLinkToExist(projectName)
         cleanup:
