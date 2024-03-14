08d60dae53b184b3f946fa12479eb80d8f99ae0c
Darwis Narvaez <darwisnarvaezdev@gmail.com>
2024-03-14 12:30:20 -0300

    Create project with label and description

--- functional-test/src/test/groovy/org/rundeck/tests/functional/selenium/project/CreateProjectSpec.groovy
+++ functional-test/src/test/groovy/org/rundeck/tests/functional/selenium/project/CreateProjectSpec.groovy
@@ -66,13 +66,37 @@ class CreateProjectSpec extends SeleniumBase {
         deleteProject(projectName)
     }
 
-    def "Create project with invalid characters"(){
+    def "Create simple project | label | description"(){
+        given:
+        def projectName = "simple-project"
+        def loginPage = go LoginPage
+        def homePage = page HomePage
+        def projectCreatePage = page ProjectCreatePage
+        NodeSourcePage nodeSourcePage = page NodeSourcePage
+
+        when:
+        loginPage.login(TEST_USER, TEST_PASS)
+        homePage.createProjectButton()
+        currentUrl.contains("/resources/createProject")
+        projectCreatePage.projectNameInput.sendKeys(projectName)
+        projectCreatePage.projectDescriptionInput.sendKeys("a description")
+        projectCreatePage.labelInput.sendKeys("label")
+        projectCreatePage.createField.click()
+
+        then:
+        nodeSourcePage.validatePage()
+
+        cleanup:
+        deleteProject(projectName)
+    }
+
+    def "Create project with invalid characters in description | name"(){
         given:
         def loginPage = go LoginPage
         def homePage = page HomePage
         def projectCreatePage = page ProjectCreatePage
 
-        when:
+        when: "IC in desc"
         loginPage.login(TEST_USER, TEST_PASS)
         homePage.createProjectButton()
         currentUrl.contains("/resources/createProject")
@@ -84,6 +108,17 @@ class CreateProjectSpec extends SeleniumBase {
         projectCreatePage.projectCreateDangerAlert.displayed
         projectCreatePage.projectCreateDangerAlertContent.text.contains(
                 "Project description can only contain these characters: [a-zA-Z0-9p{L}p{M}\\s\\.,\\(\\)_-]."
+        )
+
+        when: "IC in name"
+        projectCreatePage.go()
+        projectCreatePage.projectNameInput.sendKeys("!\$%&")
+        projectCreatePage.createField.click()
+
+        then:
+        projectCreatePage.projectCreateDangerAlert.displayed
+        projectCreatePage.projectCreateDangerAlertContent.text.contains(
+                "Project name can only contain these characters: [a-zA-Z0-9_-+.] and cannot start with a '.'."
         )
     }
 }
