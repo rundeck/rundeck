ca17a48a9c035717278d190e5ffff554f63b68ff
Greg Schueler <greg.schueler@gmail.com>
2024-03-15 12:25:29 -0700

    cleanup: switch to typescript

--- rundeckapp/grails-spa/packages/ui-trellis/src/app/pages/job/editor/OtherEditorSection.vue
+++ rundeckapp/grails-spa/packages/ui-trellis/src/app/pages/job/editor/OtherEditorSection.vue
@@ -9,7 +9,7 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from "vue";
 import * as _ from "lodash";
 import OtherEditor from "../../../components/job/other/OtherEditor.vue";
