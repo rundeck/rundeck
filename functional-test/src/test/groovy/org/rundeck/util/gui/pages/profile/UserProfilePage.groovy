8bb8636ec725f2b2386451718e2c2ca3b10e40ed
Antony Velasquez Ruiz <avelasquez@pagerduty.com>
2024-03-15 10:07:03 -0500

    add new test
    Test migrated:
    LoginAsAdminTest
    LoginAsUserTest
    LoginCorrectUserInvalidPasswordTest
    LoginIncorrectUserTest
    LoginLogoutTest
    
    ExecutionDeleteTest

--- functional-test/src/test/groovy/org/rundeck/util/gui/pages/profile/UserProfilePage.groovy
+++ functional-test/src/test/groovy/org/rundeck/util/gui/pages/profile/UserProfilePage.groovy
@@ -15,6 +15,8 @@ class UserProfilePage extends BasePage {
     String loadPath = "/user/profile"
 
     By languageBy = By.cssSelector("#layoutBody div.form-inline label[for=language]")
+    By editBy = By.linkText("Edit")
+    By userLoginBy = By.cssSelector(".form-control-static")
 
     UserProfilePage(final SeleniumContext context) {
         super(context)
@@ -22,5 +24,13 @@ class UserProfilePage extends BasePage {
 
     WebElement getLanguageLabel() {
         el languageBy
+    }
+
+    WebElement getEditLink() {
+        el editBy
+    }
+
+    WebElement getUserLogin() {
+        el userLoginBy
     }
 }
