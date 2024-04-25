a52ffef500890701abf2af83b0c53185b7c78baf
Greg Schueler <greg.schueler@gmail.com>
2024-04-25 13:07:31 -0700

    wip; Delete test/testdeck directory

--- test/testdeck/src/commands/Cluster.ts
+++
@@ -1,5 +0,0 @@
-exports.command = 'cluster <command>'
-exports.desc = 'Manage Rundeck clusters'
-exports.builder = function(yargs: any) {
-    yargs.commandDir('cluster_commands', {extensions: ['ts']}).demandCommand().scriptName('deck').help()
-}
