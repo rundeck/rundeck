c009e336e9b2fb77b69015bf3ce456710e9e389f
Jaya Singh <jaya@catalytic.com>
2024-05-08 10:55:05 -0500

    cleaned EditProjectFile test

--- rundeckapp/grails-spa/packages/ui-trellis/src/app/components/readme-motd/EditProjectFile.spec.ts
+++ rundeckapp/grails-spa/packages/ui-trellis/src/app/components/readme-motd/EditProjectFile.spec.ts
@@ -1,14 +1,12 @@
 import { mount } from "@vue/test-utils";
 import EditProjectFile from "./EditProjectFile.vue";
 import * as editProjectFileService from "./editProjectFileService";
-import AceEditor from "../../../library/components/utils/AceEditor.vue";
 
 jest.mock("@/app/components/readme-motd/editProjectFileService", () => ({
   getFileText: jest.fn().mockResolvedValue({
     success: true,
     contents: "sample file content",
   }),
-
   saveProjectFile: jest.fn().mockResolvedValue({
     success: true,
     message: "File saved successfully.",
@@ -31,107 +29,83 @@ jest.mock("@/library/rundeckService", () => ({
     },
     rdBase: "http://localhost:4440",
   }),
-  url: jest.fn().mockReturnValue("http://localhost:4440"),
+  url: jest.fn().mockReturnValue({
+    href: "http://localhost:4440/project/default/home",
+  }),
 }));
+
 jest.mock("../../../library/components/utils/AceEditor.vue", () => ({
+  name: "AceEditor",
   render: () => {},
   methods: {
     getValue: jest.fn().mockReturnValue("sample file content"),
   },
 }));
-let originalLocation;
-
-beforeAll(() => {
-  originalLocation = window.location;
-  delete (window as any).location;
-  window.location = {
-    ...originalLocation,
-    assign: jest.fn(),
-    href: jest.fn(),
-    replace: jest.fn(),
-
-    toString: () => (window.location as any)._href || "http://localhost:4440",
-  };
-});
-
-afterAll(() => {
-  window.location = originalLocation;
-});
-
-let wrapper;
-const mountEditProjectFile = async (props = {}) => {
-  wrapper = mount(EditProjectFile, {
-    props: {
-      filename: "readme.md",
-      project: "default",
-      authAdmin: true,
-      displayConfig: ["none"],
-      ...props,
-    },
-    global: {
-      mocks: {
-        $t: (msg) => msg,
-      },
-      stubs: {
-        "ace-editor": AceEditor,
-      },
-    },
-  });
-  await wrapper.vm.$nextTick();
-};
 
 describe("EditProjectFile", () => {
+  let wrapper;
+  let originalLocation;
+
+  const mountEditProjectFile = async (props = {}) => {
+    wrapper = mount(EditProjectFile, {
+      props: {
+        filename: "readme.md",
+        project: "default",
+        authAdmin: true,
+        displayConfig: ["none"],
+        ...props,
+      },
+      global: {
+        mocks: {
+          $t: (msg) => msg,
+        },
+      },
+    });
+    await wrapper.vm.$nextTick();
+  };
+
   beforeEach(async () => {
+    originalLocation = window.location;
+    delete (window as any).location;
+    window.location = {
+      ...originalLocation,
+      assign: jest.fn(),
+      href: jest.fn(),
+      replace: jest.fn(),
+      toString: () => (window.location as any)._href || "http://localhost:4440",
+    };
+
     await mountEditProjectFile();
   });
 
   afterEach(() => {
     jest.clearAllMocks();
+    window.location = originalLocation;
   });
+
   it.each([
     ["readme.md", "edit.readme.label"],
     ["motd.md", "edit.motd.label"],
   ])("renders the correct title for %s", async (filename, expectedTitle) => {
     await mountEditProjectFile({ filename });
     expect(wrapper.find('[data-test-id="title"]').text()).toContain(
-      expectedTitle,
+      expectedTitle
     );
   });
-  // it("renders file content when getFileText method returns successfully", async () => {
-  //   wrapper.vm.getFileText = jest.fn().mockResolvedValue("sample file content");
-  //   await wrapper.vm.getFileText();
 
-  //   await wrapper.vm.$nextTick();
-
-  //   const aceEditor = wrapper.findComponent({ ref: "aceEditor" });
-  //   expect(aceEditor.exists()).toBe(true);
-  //   if (aceEditor.exists()) {
-  //     await wrapper.vm.$nextTick();
-
-  //     const span = aceEditor.find(".ace_text.ace_xml");
-  //     if (span.exists()) {
-  //       expect(span.text()).toBe("sample file content");
-  //     }
-  //   }
-  // });
   it("renders file content when getFileText method returns successfully", async () => {
-    const expectedContent = "sample file content";
-    wrapper.vm.getFileText = jest.fn().mockResolvedValue(expectedContent);
+    wrapper.vm.getFileText = jest.fn().mockResolvedValue("sample file content");
     await wrapper.vm.getFileText();
-
     await wrapper.vm.$nextTick();
-    await wrapper.vm.$nextTick();
-
-    const aceEditor = wrapper.findComponent({ ref: "aceEditor" });
+    const aceEditor = wrapper.findComponent({ name: "AceEditor" });
     expect(aceEditor.exists()).toBe(true);
-
     const editorContent = aceEditor.vm.getValue();
-    expect(editorContent).toBe(expectedContent);
+    expect(editorContent).toBe("sample file content");
   });
 
   it("handles failure when getFileText method fails", async () => {
     (editProjectFileService.getFileText as jest.Mock).mockImplementationOnce(
-      () => Promise.reject(new Error("Failed to fetch file")),
+      () => Promise.reject(new Error("Failed to fetch file"))
     );
     jest.spyOn(wrapper.vm, "notifyError");
     await wrapper.vm.getFileText();
@@ -140,7 +114,7 @@ describe("EditProjectFile", () => {
 
   it("handles failure when user edits the file and fails to save it", async () => {
     (editProjectFileService.saveProjectFile as jest.Mock).mockRejectedValue(
-      new Error("Failed to save file"),
+      new Error("Failed to save file")
     );
     jest.spyOn(wrapper.vm, "notifyError");
     wrapper.vm.fileText = "new content";
@@ -151,7 +125,7 @@ describe("EditProjectFile", () => {
     wrapper.vm.fileText = "new content";
     await wrapper.find('[data-test-id="save"]').trigger("click");
     expect(
-      editProjectFileService.saveProjectFile as jest.Mock,
+      editProjectFileService.saveProjectFile as jest.Mock
     ).toHaveBeenCalledWith("default", "readme.md", "new content");
   });
 
@@ -159,7 +133,7 @@ describe("EditProjectFile", () => {
     const footerText = wrapper.find(".card-footer").text();
     expect(footerText).toContain("file.warning.not.displayed.admin.message");
     expect(wrapper.find(".card-footer a").text()).toBe(
-      "project.configuration.label",
+      "project.configuration.label"
     );
   });
 
@@ -168,16 +142,15 @@ describe("EditProjectFile", () => {
       authAdmin: false,
     });
     expect(
-      wrapper.find('[data-test-id="nonadmin-warning-message"]').text(),
+      wrapper.find('[data-test-id="nonadmin-warning-message"]').text()
     ).toContain("file.warning.not.displayed.nonadmin.message");
   });
-
   it("navigates to the home page when the cancel button is clicked", async () => {
     await mountEditProjectFile();
-    const expectedUrl = `http://localhost:4440/project/${wrapper.vm.project}/home`;
+    const button = wrapper.find('[data-test-id="cancel"]');
 
-    await wrapper.find('[data-test-id="cancel"]').trigger("click");
-    await wrapper.vm.$nextTick();
-    expect(window.location.href).toBe(expectedUrl);
+    await button.trigger("click");
+
+    expect(window.location).toBe("http://localhost:4440/project/default/home");
   });
 });
--- rundeckapp/grails-spa/packages/ui-trellis/src/app/components/readme-motd/EditProjectFile.vue
+++ rundeckapp/grails-spa/packages/ui-trellis/src/app/components/readme-motd/EditProjectFile.vue
@@ -36,7 +36,6 @@
                   </details>
                 </div>
                 <ace-editor
-                  ref="aceEditor"
                   v-model="fileText"
                   :soft-wrap-control="true"
                   height="250"
@@ -154,8 +153,7 @@ export default defineComponent({
       }
     },
     createProjectHomeLink() {
-      window.location.href =
-        "http://localhost:4440/project/" + this.project + "/home";
+      window.location = url("project/" + this.project + "/home").href;
     },
     notifyError(msg) {
       Notification.notify({
@@ -188,7 +186,6 @@ export default defineComponent({
 
         if (response.success) {
           this.fileText = response.contents;
-          console.log(this.fileText);
         }
       } catch (e) {
         if (e.warning) {
