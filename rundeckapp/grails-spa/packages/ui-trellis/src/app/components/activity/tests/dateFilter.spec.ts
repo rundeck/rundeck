e461524bc908f32d13e625d58c903a8e48679716
Jaya Singh <jaya@catalytic.com>
2024-05-17 17:55:48 -0500

    removed console.log

--- rundeckapp/grails-spa/packages/ui-trellis/src/app/components/activity/tests/dateFilter.spec.ts
+++ rundeckapp/grails-spa/packages/ui-trellis/src/app/components/activity/tests/dateFilter.spec.ts
@@ -127,7 +127,6 @@ describe("DateFilter", () => {
         },
       });
 
-      console.log(wrapper.html());
       const dropdown = wrapper.find(".dropdown");
       await dropdown.trigger("click");
       await wrapper.vm.$nextTick();
