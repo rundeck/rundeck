5cef8f34ba5f5af5ac43b3efa8ff02809e5c04fc
Rodrigo Navarro <rnavarro@pagerduty.com>
2024-05-20 16:31:45 -0400

    modify execution mode to move back to enable

--- functional-test/src/test/groovy/org/rundeck/tests/functional/selenium/project/ExecutionModeSpec.groovy
+++ functional-test/src/test/groovy/org/rundeck/tests/functional/selenium/project/ExecutionModeSpec.groovy
@@ -7,6 +7,7 @@ import org.rundeck.util.common.jobs.JobUtils
 import org.rundeck.util.container.SeleniumBase
 import org.rundeck.util.gui.common.navigation.NavLinkTypes
 import org.rundeck.util.gui.common.navigation.NavProjectSettings
+import org.rundeck.util.gui.pages.activity.ActivityPage
 import org.rundeck.util.gui.pages.home.HomePage
 import org.rundeck.util.gui.pages.jobs.JobListPage
 import org.rundeck.util.gui.pages.login.LoginPage
@@ -18,6 +19,7 @@ class ExecutionModeSpec extends SeleniumBase{
 
     /**
      * It disables executions via edit configuration file, then checks for the job to have the run button disabled
+     * then enables executions and checks for the run job button to be shown
      */
     def "disable execution at project level"(){
         given:
@@ -60,12 +62,23 @@ class ExecutionModeSpec extends SeleniumBase{
             sideBarPage.goTo NavLinkTypes.JOBS
         then:
             jobListPage.expectExecutionsDisabled()
+            jobListPage.getCountJobList() == 0
+        when:
+            projectEditPage.go("/project/${projectName}/configure")
+            projectEditPage.clickEditConfigurationFile()
+            projectEditPage.replaceConfiguration("project.disable.executions=true", "project.disable.executions=false")
+            projectEditPage.save()
+            projectEditPage.validateConfigFileSave()
+            sideBarPage.goTo NavLinkTypes.JOBS
+        then:
+            jobListPage.getCountJobList() == 1
         cleanup:
         deleteProject(projectName)
     }
 
     /**
      * It disables executions via UI, then checks for the job to have the run button disabled
+     * then enables executions and checks for the run job button to be shown
      */
     def "disable execution at project level UI"(){
         given:
@@ -105,6 +118,15 @@ class ExecutionModeSpec extends SeleniumBase{
         sideBarPage.goTo NavLinkTypes.JOBS
         then:
         jobListPage.expectExecutionsDisabled()
+        jobListPage.getCountJobList() == 0
+        when:
+        projectEditPage.go("/project/${projectName}/configure")
+        projectEditPage.clickNavLink(NavProjectSettings.EXEC_MODE)
+        projectEditPage.clickExecutionMode()
+        projectEditPage.save()
+        sideBarPage.goTo NavLinkTypes.JOBS
+        then:
+        jobListPage.getCountJobList() == 1
         cleanup:
         deleteProject(projectName)
     }
@@ -112,6 +134,7 @@ class ExecutionModeSpec extends SeleniumBase{
     /**
      * It disables schedules via edit configuration file, then checks for the job to have the run button to be enabled
      * and pause icon to be shown (indicating that schedules are disabled)
+     * then enables schedules and check the activity page
      */
     def "disable schedules at project level"(){
         given:
@@ -122,6 +145,8 @@ class ExecutionModeSpec extends SeleniumBase{
         def loginPage = page LoginPage
         def jobListPage = page JobListPage
         def sideBarPage = page SideBarPage
+        def activityPage = page ActivityPage
+        def jobName = "test-job-schedules"
         def yamlJob = """
                         -
                           project: ${projectName}
@@ -132,13 +157,13 @@ class ExecutionModeSpec extends SeleniumBase{
                             commands:
                             - exec: echo hello there
                           description: ''
-                          name: test-job-schedules
+                          name: ${jobName}
                           schedule:
                             month: '*'
                             time:
                               hour: '*'
-                              minute: '0'
-                              seconds: '0'
+                              minute: '*'
+                              seconds: '*/5'
                             weekday:
                               day: '*'
                             year: '*'
@@ -164,23 +189,45 @@ class ExecutionModeSpec extends SeleniumBase{
         sideBarPage.goTo NavLinkTypes.JOBS
         then:
         jobListPage.expectScheduleDisabled()
+        when:
+        hold 10 //This is to allow executions to finish
+        sideBarPage.goTo NavLinkTypes.ACTIVITY
+        def executions = activityPage.getActivityRowsByJobName(jobName).size()
+        projectEditPage.go("/project/${projectName}/configure")
+        projectEditPage.clickEditConfigurationFile()
+        projectEditPage.replaceConfiguration("project.disable.schedule=true", "project.disable.schedule=false")
+        projectEditPage.save()
+        projectEditPage.validateConfigFileSave()
+        hold 10 //This is to allow at least one execution to finish
+        sideBarPage.goTo NavLinkTypes.ACTIVITY
+        then:
+        executions < activityPage.getActivityRowsByJobName(jobName).size()
         cleanup:
+        updateConfigurationProject(projectName, [
+                "project.disable.schedule": "true",
+                "project.later.schedule.enable": "false",
+                "project.disable.executions": "true"
+        ])
+        hold 10
         deleteProject(projectName)
     }
 
     /**
      * It disables schedules via UI, then checks for the job to have the run button to be enabled
      * and pause icon to be shown (indicating that schedules are disabled)
+     * then enables schedules and check the activity page
      */
     def "disable schedules at project level UI"(){
         given:
-        def projectName = "disabledSchedulesProject"
+        def projectName = "disabledSchedulesProjectUI"
         setupProject(projectName)
         def projectEditPage = page ProjectEditPage
         def homePage = page HomePage
         def loginPage = page LoginPage
         def jobListPage = page JobListPage
         def sideBarPage = page SideBarPage
+        def activityPage = page ActivityPage
+        def jobName = "test-job-schedules"
         def yamlJob = """
                         -
                           project: ${projectName}
@@ -191,13 +238,13 @@ class ExecutionModeSpec extends SeleniumBase{
                             commands:
                             - exec: echo hello there
                           description: ''
-                          name: test-job-schedules
+                          name: ${jobName}
                           schedule:
                             month: '*'
                             time:
                               hour: '*'
-                              minute: '0'
-                              seconds: '0'
+                              minute: '*'
+                              seconds: '*/5'
                             weekday:
                               day: '*'
                             year: '*'
@@ -220,7 +267,25 @@ class ExecutionModeSpec extends SeleniumBase{
         sideBarPage.goTo NavLinkTypes.JOBS
         then:
         jobListPage.expectScheduleDisabled()
+        when:
+        hold 10 //This is to allow executions to finish
+        sideBarPage.goTo NavLinkTypes.ACTIVITY
+        def executions = activityPage.getActivityRowsByJobName(jobName).size()
+        projectEditPage.go("/project/${projectName}/configure")
+        projectEditPage.clickNavLink(NavProjectSettings.EXEC_MODE)
+        projectEditPage.clickScheduleMode()
+        projectEditPage.save()
+        hold 10 //This is to allow executions to run and finish
+        sideBarPage.goTo NavLinkTypes.ACTIVITY
+        then:
+        executions < activityPage.getActivityRowsByJobName(jobName).size()
         cleanup:
+        updateConfigurationProject(projectName, [
+                "project.disable.schedule": "true",
+                "project.later.schedule.enable": "false",
+                "project.disable.executions": "true"
+        ])
+        hold 10
         deleteProject(projectName)
     }
 }
