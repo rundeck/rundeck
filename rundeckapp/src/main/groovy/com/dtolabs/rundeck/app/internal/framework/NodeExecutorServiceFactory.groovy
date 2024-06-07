374390ac41c675a9bc3b92df436a096e94e61640
Greg Schueler <greg.schueler@gmail.com>
2024-06-07 10:31:10 -0700

    update NodeExecutorService definition
    use Profile beans to define behavior
    use a feature flag bean to toggle defaults for NewLocalNodeExecutor

--- rundeckapp/src/main/groovy/com/dtolabs/rundeck/app/internal/framework/NodeExecutorServiceFactory.groovy
+++ rundeckapp/src/main/groovy/com/dtolabs/rundeck/app/internal/framework/NodeExecutorServiceFactory.groovy
@@ -1,13 +1,12 @@
 package com.dtolabs.rundeck.app.internal.framework
 
 import com.dtolabs.rundeck.core.common.Framework
-import com.dtolabs.rundeck.core.config.FeatureService
 import com.dtolabs.rundeck.core.execution.service.NodeExecutorService
+import com.dtolabs.rundeck.core.execution.service.NodeExecutorServiceProfile
 import groovy.transform.CompileStatic
 import org.springframework.beans.factory.FactoryBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Lazy
-
 /**
  * Factory to create NodeExecutorService, required to lazily load injection dependencies
  */
@@ -15,12 +14,12 @@ import org.springframework.context.annotation.Lazy
 class NodeExecutorServiceFactory implements FactoryBean<NodeExecutorService> {
     @Autowired @Lazy
     Framework rundeckFramework
-    @Autowired @Lazy
-    FeatureService featureService
+    @Autowired
+    NodeExecutorServiceProfile rundeckNodeExecutorProfile
 
     @Override
     NodeExecutorService getObject() throws Exception {
-        return new NodeExecutorService(rundeckFramework, featureService)
+        return new NodeExecutorService(rundeckFramework, rundeckNodeExecutorProfile)
     }
     Class<?> objectType = NodeExecutorService
 }
