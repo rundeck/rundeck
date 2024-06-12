568056b3a74852b7298ffa7752ebd0ff11271f44
Jaya Singh <jaya@catalytic.com>
2024-06-12 16:01:40 -0500

    updated test but still need more work

--- rundeckapp/grails-spa/packages/ui-trellis/src/app/components/activity/tests/mock.ts
+++ rundeckapp/grails-spa/packages/ui-trellis/src/app/components/activity/tests/mock.ts
@@ -12,12 +12,19 @@ export const i18nMocks = {
       "delete.selected.executions": "Delete Selected Executions",
       "cancel.bulk.delete": "Cancel Bulk Delete",
       "bulk.delete": "Bulk Delete",
+      "clearselected.confirm.text":
+        "Clear all {0} selected items, or only items shown on this page?",
     };
     return translations[msg] || msg;
   },
   $tc: (msg, count) => {
     const translations = {
       execution: `${count} executions`,
+      "info.newexecutions.since.0":
+        count > 1
+          ? `${count} New Results. Click to load.`
+          : "1 New Result. Click to load.",
+      "In the last Day": "In the last Day",
     };
     return translations[msg] || msg;
   },
@@ -25,7 +32,7 @@ export const i18nMocks = {
 
 export const rundeckServiceMock = {
   getRundeckContext: jest.fn().mockReturnValue({ projectName: "test" }),
-  url: jest.fn().mockReturnValue("http://localhost"),
+  url: jest.fn().mockImplementation((path) => `http://localhost${path}`),
 };
 
 export const rundeckClientMock = {
@@ -35,8 +42,10 @@ export const rundeckClientMock = {
   })),
 };
 
-export const axiosMock = jest.mock("axios");
-
+export const axiosMock = {
+  get: jest.fn(),
+  post: jest.fn(),
+};
 export const mockEventBus = {
   on: jest.fn(),
   emit: jest.fn(),
