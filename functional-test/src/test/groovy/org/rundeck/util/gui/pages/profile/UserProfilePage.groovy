0dadcd72eddc8e26306ac1462a17ba46330cf444
Jesus Osuna <jesus.m.osuna.m@gmail.com>
2024-06-14 18:27:23 -0400

    migarte tests

--- functional-test/src/test/groovy/org/rundeck/util/gui/pages/profile/UserProfilePage.groovy
+++ functional-test/src/test/groovy/org/rundeck/util/gui/pages/profile/UserProfilePage.groovy
@@ -17,6 +17,17 @@ class UserProfilePage extends BasePage {
     By languageBy = By.cssSelector("#layoutBody div.form-inline label[for=language]")
     By editBy = By.linkText("Edit")
     By userLoginBy = By.cssSelector(".form-control-static")
+    By genTokenButtonBy = By.xpath("//a[@class='btn btn-default btn-xs' and @data-toggle='modal' and @href='#gentokenmodal']")
+    By modalGenerateNewTokenHeaderBy = By.xpath("//h4[@class='modal-title' and @id='gentokenLabel']")
+    By tokenTimeInputBy = By.xpath("//input[@type='number' and @name='tokenTime' and @class='form-control']")
+    By tokenNameInputBy = By.xpath("//input[@type='text' and @name='tokenName' and @class='form-control']")
+    By modalGenerateTokenBtnBy = By.xpath("//a[@class='genusertokenbtn btn btn-cta' and normalize-space()='Generate New Token']")
+    By timeUntilBy = By.xpath("//span[@class='timeuntil']")
+    By modalCloseBtnBy = By.xpath("//button[@class='btn btn-danger' and @type='button' and normalize-space()='Close']")
+    By deleteTokenButtonBy = By.xpath("//a[@title='Delete']")
+    By modalDeleteInputBy = By.xpath("//input[@type='submit' and @class='btn btn-danger yes' and @value='Delete']")
+
+
 
     UserProfilePage(final SeleniumContext context) {
         super(context)
@@ -33,4 +44,42 @@ class UserProfilePage extends BasePage {
     WebElement getUserLogin() {
         el userLoginBy
     }
+
+    WebElement getGenTokenButton(){
+        el genTokenButtonBy
+    }
+
+    WebElement getModalGenerateNewTokenHeader(){
+        el modalGenerateNewTokenHeaderBy
+    }
+
+    WebElement getTokenTimeInput(){
+        el tokenTimeInputBy
+    }
+
+    WebElement getModalGenerateTokenBtnBy(){
+        el modalGenerateTokenBtnBy
+    }
+
+    WebElement getTimeUntil(){
+        el timeUntilBy
+    }
+
+    WebElement getModalCloseBtn(){
+        el modalCloseBtnBy
+    }
+
+    WebElement getDeleteTokenButton(){
+        el deleteTokenButtonBy
+    }
+
+    WebElement  getModalDeleteInput(){
+        el modalDeleteInputBy
+    }
+
+    WebElement getTokenNameInput(){
+        el tokenNameInputBy
+    }
+
+
 }
