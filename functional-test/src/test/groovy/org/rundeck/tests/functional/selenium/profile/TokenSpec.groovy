7aad55280010ee2d8492f588f132af214b7b0d93
jmanuelosunamoreno <jmanuelosunamoreno@pagerduty.com>
2024-06-18 14:41:33 -0400

    improve comments

--- functional-test/src/test/groovy/org/rundeck/tests/functional/selenium/profile/TokenSpec.groovy
+++ functional-test/src/test/groovy/org/rundeck/tests/functional/selenium/profile/TokenSpec.groovy
@@ -13,42 +13,50 @@ class TokenSpec extends SeleniumBase{
         loginPage.login(TEST_USER, TEST_PASS)
     }
 
-    def "validate default token duration is 0"(){
-        when:
+    def "validate default token duration is 0"() {
+        when: "User navigates to the User Profile Page and generates a token"
         def userProfilePage = go UserProfilePage
         userProfilePage.waitForElementToBeClickable(userProfilePage.genTokenButton)
         userProfilePage.genTokenButton.click()
         userProfilePage.waitForElementVisible(userProfilePage.modalGenerateNewTokenHeader)
 
-        then:
-        userProfilePage.waitForTextToBePresentInElement(userProfilePage.tokenTimeInput,"0")
+        then: "The default token duration should be '0'"
+        userProfilePage.waitForTextToBePresentInElement(userProfilePage.tokenTimeInput, "0")
+
+        and: "User generates a token with name 'TEST_TOKEN_1'"
         userProfilePage.tokenNameInput.clear()
         userProfilePage.tokenNameInput.sendKeys("TEST_TOKEN_1")
         userProfilePage.modalGenerateTokenBtnBy.click()
-        userProfilePage.waitForElementToBeClickable (userProfilePage.modalCloseBtn)
+        userProfilePage.waitForElementToBeClickable(userProfilePage.modalCloseBtn)
         userProfilePage.modalCloseBtn.click()
+
+        and: "The token's time until expiration should be shown as '29d'"
         userProfilePage.waitForElementVisible(userProfilePage.timeUntil)
         userProfilePage.timeUntil.getText().contains("29d")
+
+        and: "User deletes the generated token"
         userProfilePage.deleteTokenButton.click()
         userProfilePage.waitForElementToBeClickable(userProfilePage.modalDeleteInput)
         userProfilePage.modalDeleteInput.submit()
     }
 
-    def "validate default token duration can be modified"(){
-        when:
+    def "validate default token duration can be modified"() {
+        when: "User navigates to the User Profile Page and generates a token"
         def userProfilePage = go UserProfilePage
         userProfilePage.waitForElementToBeClickable(userProfilePage.genTokenButton)
         userProfilePage.genTokenButton.click()
         userProfilePage.waitForElementVisible(userProfilePage.modalGenerateNewTokenHeader)
 
-        then:
+        then: "User modifies the default token duration to '50m'"
         userProfilePage.tokenNameInput.clear()
         userProfilePage.tokenNameInput.sendKeys("TEST_TOKEN_2")
-        userProfilePage.waitForTextToBePresentInElement(userProfilePage.tokenTimeInput,"0")
+        userProfilePage.waitForTextToBePresentInElement(userProfilePage.tokenTimeInput, "0")
         userProfilePage.tokenTimeInput.sendKeys("50m")
         userProfilePage.modalGenerateTokenBtnBy.click()
-        userProfilePage.waitForElementToBeClickable (userProfilePage.modalCloseBtn)
+        userProfilePage.waitForElementToBeClickable(userProfilePage.modalCloseBtn)
         userProfilePage.modalCloseBtn.click()
+
+        and: "The token's time until expiration should show 'm' (minutes)"
         userProfilePage.waitForElementVisible(userProfilePage.timeUntil)
         userProfilePage.timeUntil.getText().contains("m")
     }
