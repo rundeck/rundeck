a2c49b551f8ff285b955c2cd0f9b08320af2c9c9
jmanuelosunamoreno <jmanuelosunamoreno@pagerduty.com>
2024-06-18 17:07:28 -0400

    move userSummary test to oss

---
+++ functional-test/src/test/groovy/org/rundeck/tests/functional/selenium/users/UserSummarySpec.groovy
@@ -0,0 +1,71 @@
+package org.rundeck.tests.functional.selenium.users
+
+import org.rundeck.util.annotations.SeleniumCoreTest
+import org.rundeck.util.container.SeleniumBase
+import org.rundeck.util.gui.pages.TopMenuPage
+import org.rundeck.util.gui.pages.login.LoggedOutPage
+import org.rundeck.util.gui.pages.login.LoginPage
+import org.rundeck.util.gui.pages.usersummary.UserSummaryPage
+
+@SeleniumCoreTest
+class UserSummarySpec extends SeleniumBase {
+
+    def "login a first time with two different users"() {
+        when: "Login with two different users consecutively"
+        def loginPage = go LoginPage
+        def topMenuPage = page TopMenuPage
+        def loggedOutPage = page LoggedOutPage
+        def userSummaryPage = page UserSummaryPage
+
+        and: "Login with the first user"
+        loginPage.login("user", "user")
+
+        and: "Log out the first user"
+        topMenuPage.logOut()
+        loggedOutPage.getLoginAgainField().click()
+
+        and: "Login with the second user"
+        loginPage.login(TEST_USER, TEST_PASS)
+
+        and: "Navigate to the User Manager Page"
+        userSummaryPage.go()
+
+        and: "Get the user count"
+        def userCount = userSummaryPage.userCountField.getText().toInteger()
+
+        then: "At least two users should appear on the user summary page"
+        userCount >= 2
+    }
+
+    def "search user by name on user summary list"() {
+        when: "Login and search for a user by name"
+        def loginPage = go LoginPage
+        def userSummaryPage = page UserSummaryPage
+        def topMenuPage = page TopMenuPage
+        def loggedOutPage = page LoggedOutPage
+
+        and: "Login with the first user"
+        loginPage.login("user", "user")
+
+        and: "Log out the first user"
+        topMenuPage.logOut()
+        loggedOutPage.getLoginAgainField().click()
+
+        and: "Login with the second user"
+        loginPage.login(TEST_USER, TEST_PASS)
+
+        and: "Navigate to the User Manager Page"
+        userSummaryPage.go()
+
+        and: "Search for the user by name 'admin'"
+        userSummaryPage.waitForElementToBeClickable(userSummaryPage.searchButton)
+        userSummaryPage.searchButton.click()
+        userSummaryPage.waitForElementVisible(userSummaryPage.loginInput)
+        userSummaryPage.loginInput.sendKeys("admin")
+        userSummaryPage.waitForElementToBeClickable(userSummaryPage.modalSearchButton)
+        userSummaryPage.modalSearchButton.click()
+
+        then: "The user count field should show '1'"
+        userSummaryPage.waitForTextToBePresentBySelector(userSummaryPage.userCountFieldBy, "1", 5)
+    }
+}
