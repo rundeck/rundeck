40ecfd7b47a65d1591e04ce4a737878f4cabf2b3
smartinellibenedetti <139791797+smartinellibenedetti@users.noreply.github.com>
2024-07-11 10:29:46 -0600

    fix v-for location

--- rundeckapp/grails-spa/packages/ui-trellis/src/app/components/job/execution/ExecutionEditor.vue
+++ rundeckapp/grails-spa/packages/ui-trellis/src/app/components/job/execution/ExecutionEditor.vue
@@ -4,100 +4,101 @@
       $t("scheduledExecution.property.executionLifecyclePluginConfig.help.text")
     }}
   </div>
-  <div
-    v-for="(plugin, index) in executionLifecyclePlugins"
-    :key="`plugin_${index}`"
-    class="list-group"
-  >
-    <input
-      type="hidden"
-      name="executionLifecyclePlugins.keys"
-      :value="pluginKey"
-    />
-    <input
-      type="hidden"
-      :name="`executionLifecyclePlugins.type.${pluginKey}`"
-      :value="pluginType"
-    />
-    <div class="list-group-item">
-      <div class="form-group">
-        <div class="col-sm-12">
-          <div class="checkbox">
-            <!--                <g:set var="prkey" value="${rkey()}" />-->
-            <input
-              :id="`executionLifecyclePluginEnabled_${prkey}`"
-              :name="`executionLifecyclePlugins.enabled.${pluginKey}`"
-              type="checkbox"
-              class="form-control"
-            />
-            <!--                <g:checkBox-->
-            <!--                  id="executionLifecyclePluginEnabled_${prkey}"-->
-            <!--                  name="executionLifecyclePlugins.enabled.${pluginKey}"-->
-            <!--                  value="true"-->
-            <!--                  class="form-control"-->
-            <!--                  checked="${pluginConfig != null}"-->
-            <!--                />-->
+  <div class="list-group">
+    <template
+      v-for="(plugin, index) in executionLifecyclePlugins"
+      :key="`plugin_${index}`"
+    >
+      <input
+        type="hidden"
+        name="executionLifecyclePlugins.keys"
+        :value="pluginKey"
+      />
+      <input
+        type="hidden"
+        :name="`executionLifecyclePlugins.type.${pluginKey}`"
+        :value="pluginType"
+      />
+      <div class="list-group-item">
+        <div class="form-group">
+          <div class="col-sm-12">
+            <div class="checkbox">
+              <!--                <g:set var="prkey" value="${rkey()}" />-->
+              <input
+                :id="`executionLifecyclePluginEnabled_${prkey}`"
+                :name="`executionLifecyclePlugins.enabled.${pluginKey}`"
+                type="checkbox"
+                class="form-control"
+              />
+              <!--                <g:checkBox-->
+              <!--                  id="executionLifecyclePluginEnabled_${prkey}"-->
+              <!--                  name="executionLifecyclePlugins.enabled.${pluginKey}"-->
+              <!--                  value="true"-->
+              <!--                  class="form-control"-->
+              <!--                  checked="${pluginConfig != null}"-->
+              <!--                />-->
 
-            <label :for="`executionLifecyclePluginEnabled_${prkey}`">
-              <plugin-info
-                :show-title="true"
-                :show-icon="true"
-                :show-extended="true"
-                :show-description="true"
-                :detail="plugin"
-              >
-                <template #description>
-                  <scheduled-execution-details
-                    :allow-html="true"
-                    :description="plugin.description"
-                    mode="collapsed"
-                  />
-                </template>
-              </plugin-info>
-              <!--                  <g:render-->
-              <!--                    template="/framework/renderPluginDesc"-->
-              <!--                    model="${[-->
-              <!--                                                          serviceName    : ServiceNameConstants.ExecutionLifecycle,-->
-              <!--                                                          description    : pluginDescription,-->
-              <!--                                                          showPluginIcon : true,-->
-              <!--                                                          showNodeIcon   : false,-->
-              <!--                                                          hideTitle      : false,-->
-              <!--                                                          hideDescription: false,-->
-              <!--                                                          fullDescription: false-->
-              <!--                                                  ]}"-->
-              <!--                  />-->
-            </label>
+              <label :for="`executionLifecyclePluginEnabled_${prkey}`">
+                <plugin-info
+                  :show-title="true"
+                  :show-icon="true"
+                  :show-extended="true"
+                  :show-description="true"
+                  :detail="plugin"
+                >
+                  <template #description>
+                    <scheduled-execution-details
+                      :allow-html="true"
+                      :description="plugin.description"
+                      mode="collapsed"
+                    />
+                  </template>
+                </plugin-info>
+                <!--                  <g:render-->
+                <!--                    template="/framework/renderPluginDesc"-->
+                <!--                    model="${[-->
+                <!--                                                          serviceName    : ServiceNameConstants.ExecutionLifecycle,-->
+                <!--                                                          description    : pluginDescription,-->
+                <!--                                                          showPluginIcon : true,-->
+                <!--                                                          showNodeIcon   : false,-->
+                <!--                                                          hideTitle      : false,-->
+                <!--                                                          hideDescription: false,-->
+                <!--                                                          fullDescription: false-->
+                <!--                                                  ]}"-->
+                <!--                  />-->
+              </label>
+            </div>
           </div>
         </div>
+        <template v-if="plugin?.properties">
+          <plugin-config
+            mode="edit"
+            service-name="ExecutionLifecycle"
+            :provider="plugin.name"
+            :model-value="test"
+            :plugin-config="plugin.properties"
+          ></plugin-config>
+          <!--            <g:set-->
+          <!--              var="prefix"-->
+          <!--              value="executionLifecyclePlugins.${pluginKey}.configMap."-->
+          <!--            />-->
+          <!--            <g:render-->
+          <!--              template="/framework/pluginConfigPropertiesInputs"-->
+          <!--              model="${[-->
+          <!--                                              service:ServiceNameConstants.ExecutionLifecycle,-->
+          <!--                                              provider:pluginDescription.name,-->
+          <!--                                              properties:pluginDescription?.properties,-->
+          <!--                                              report: params.executionLifecyclePluginValidation?.get(pluginType),-->
+          <!--                                              prefix:prefix,-->
+          <!--                                              values:pluginConfig?:[:],-->
+          <!--                                              fieldnamePrefix:prefix,-->
+          <!--                                              origfieldnamePrefix:'orig.' + prefix,-->
+          <!--                                              allowedScope:com.dtolabs.rundeck.core.plugins.configuration.PropertyScope.Instance-->
+          <!--                                      ]}"-->
+          <!--            />-->
+        </template>
       </div>
-      <template v-if="plugin?.properties">
-        <plugin-config
-          mode="edit"
-          service-name="ExecutionLifecycle"
-          :provider="plugin.name"
-          :model-value="test"
-          :plugin-config="plugin.properties"
-        ></plugin-config>
-        <!--            <g:set-->
-        <!--              var="prefix"-->
-        <!--              value="executionLifecyclePlugins.${pluginKey}.configMap."-->
-        <!--            />-->
-        <!--            <g:render-->
-        <!--              template="/framework/pluginConfigPropertiesInputs"-->
-        <!--              model="${[-->
-        <!--                                              service:ServiceNameConstants.ExecutionLifecycle,-->
-        <!--                                              provider:pluginDescription.name,-->
-        <!--                                              properties:pluginDescription?.properties,-->
-        <!--                                              report: params.executionLifecyclePluginValidation?.get(pluginType),-->
-        <!--                                              prefix:prefix,-->
-        <!--                                              values:pluginConfig?:[:],-->
-        <!--                                              fieldnamePrefix:prefix,-->
-        <!--                                              origfieldnamePrefix:'orig.' + prefix,-->
-        <!--                                              allowedScope:com.dtolabs.rundeck.core.plugins.configuration.PropertyScope.Instance-->
-        <!--                                      ]}"-->
-        <!--            />-->
-      </template>
-    </div>
+    </template>
   </div>
 </template>
 
