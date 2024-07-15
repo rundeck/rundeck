ce706dd2c0b0a1f4b113fc1aed643f7a84f5e2e8
Jaya Singh <jaya@catalytic.com>
2024-07-15 13:00:25 -0500

    set the rdBase in window._rundeck

--- rundeckapp/grails-spa/packages/ui-trellis/src/app/components/job/resources/tests/NodeFilterLink.spec.ts
+++ rundeckapp/grails-spa/packages/ui-trellis/src/app/components/job/resources/tests/NodeFilterLink.spec.ts
@@ -1,12 +1,37 @@
 import { mount } from "@vue/test-utils";
 import NodeFilterLink from "../NodeFilterLink.vue";
+import { getRundeckContext, url } from "../../../../../library";
 jest.mock("@/library/rundeckService", () => ({
-  getRundeckContext: jest.fn().mockReturnValue({
-    rdBase: "mockRdBase",
-  }),
+  getRundeckContext: jest.fn().mockReturnValue({}),
   url: jest.fn().mockReturnValue({ href: "mockHref" }),
 }));
 
+beforeEach(() => {
+  delete window._rundeck;
+
+  // Set the rdBase in the window._rundeck object
+  window._rundeck = {
+    activeTour: "",
+    activeTourStep: "",
+    apiVersion: "",
+    appMeta: {},
+    data: {},
+    eventBus: undefined,
+    feature: {},
+    navbar: { items: undefined },
+    projectName: "",
+    rootStore: undefined,
+    rundeckClient: undefined,
+    token: undefined,
+    tokens: {},
+    rdBase: "mockRdBase",
+  };
+});
+
+afterEach(() => {
+  // Clean up the window._rundeck object after each test to ensure no test pollution
+  delete window._rundeck;
+});
 const mountNodeFilterLink = async (propsData = {}) => {
   const wrapper = mount(NodeFilterLink, {
     props: {
@@ -53,5 +78,15 @@ describe("NodeFilterLink Component", () => {
     const wrapper = await mountNodeFilterLink();
     const link = wrapper.find("a");
     expect(link.text()).toContain("testText");
+  });
+  it("handles nodeFilter prop correctly", async () => {
+    const wrapper = await mountNodeFilterLink({
+      nodeFilter: "hostname: node1",
+    });
+    await wrapper.trigger("click");
+    await wrapper.vm.$nextTick();
+    expect(wrapper.emitted().nodefilterclick[0][0]).toEqual({
+      filter: "hostname: node1",
+    });
   });
 });
