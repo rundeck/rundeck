d280f9dc40150312abd0b8e9ac39c9a1417c8816
Jaya Singh <jaya@catalytic.com>
2024-07-15 21:16:29 -0500

    removed mock and set the rdbase in the window._rundeck object

--- rundeckapp/grails-spa/packages/ui-trellis/src/app/components/job/resources/tests/NodeFilterLink.spec.ts
+++ rundeckapp/grails-spa/packages/ui-trellis/src/app/components/job/resources/tests/NodeFilterLink.spec.ts
@@ -1,56 +1,52 @@
 import { mount } from "@vue/test-utils";
-import NodeFilterLink from "../NodeFilterLink.vue";
-import { getRundeckContext, url } from "../../../../../library";
-jest.mock("@/library/rundeckService", () => ({
-  getRundeckContext: jest.fn().mockReturnValue({}),
-  url: jest.fn().mockReturnValue({ href: "mockHref" }),
-}));
-
-beforeEach(() => {
-  delete window._rundeck;
-
-  // Set the rdBase in the window._rundeck object
-  window._rundeck = {
-    activeTour: "",
-    activeTourStep: "",
-    apiVersion: "",
-    appMeta: {},
-    data: {},
-    eventBus: undefined,
-    feature: {},
-    navbar: { items: undefined },
-    projectName: "",
-    rootStore: undefined,
-    rundeckClient: undefined,
-    token: undefined,
-    tokens: {},
-    rdBase: "mockRdBase",
-  };
-});
-
-afterEach(() => {
-  // Clean up the window._rundeck object after each test to ensure no test pollution
-  delete window._rundeck;
-});
-const mountNodeFilterLink = async (propsData = {}) => {
-  const wrapper = mount(NodeFilterLink, {
-    props: {
-      filterKey: "tags",
-      filterVal: "testTag",
-      text: "testText",
-      exclude: false,
-      ...propsData,
-    },
-    global: {
-      mocks: {
-        $t: (msg) => msg,
-      },
-    },
-  });
-  await wrapper.vm.$nextTick();
-  return wrapper;
-};
+import { EventBus } from "../../../../../library";
+import { RootStore } from "../../../../../library/stores/RootStore";
+import { RundeckToken } from "../../../../../library/interfaces/rundeckWindow";
 describe("NodeFilterLink Component", () => {
+  beforeAll(() => {
+    window._rundeck = {
+      eventBus: jest.fn() as unknown as typeof EventBus,
+      rdBase: "http://localhost",
+      apiVersion: "mockApiVersion",
+      projectName: "test-project",
+      activeTour: "mockActiveTour",
+      activeTourStep: "mockActiveTourStep",
+      appMeta: {},
+      token: { TOKEN: "mockToken", URI: "mockUri" } as RundeckToken,
+      tokens: {
+        mockToken1: { TOKEN: "mockToken1", URI: "mockUri1" },
+        mockToken2: { TOKEN: "mockToken2", URI: "mockUri2" },
+      },
+      navbar: { items: [] as any },
+      feature: {},
+      data: {},
+      rundeckClient: {} as any,
+      rootStore: {} as unknown as RootStore,
+    };
+  });
+  afterAll(() => {
+    delete window._rundeck;
+  });
+  const mountNodeFilterLink = async (propsData = {}) => {
+    // Dynamically import the component after setting up window._rundeck
+    const NodeFilterLink = (await import("../NodeFilterLink.vue")).default;
+    const wrapper = mount(NodeFilterLink, {
+      props: {
+        filterKey: "tags",
+        filterVal: "testTag",
+        text: "testText",
+        exclude: false,
+        ...propsData,
+      },
+      global: {
+        mocks: {
+          $t: (msg) => msg,
+        },
+      },
+    });
+    await wrapper.vm.$nextTick();
+    return wrapper;
+  };
   it("emits nodefilterclick event with the correct payload when clicked", async () => {
     const wrapper = await mountNodeFilterLink();
     await wrapper.trigger("click");
@@ -63,9 +59,12 @@ describe("NodeFilterLink Component", () => {
   it("renders correct href attribute", async () => {
     const wrapper = await mountNodeFilterLink();
     const link = wrapper.find("a");
-    expect(link.attributes("href")).toBe("mockHref");
+    const expectedHref = new URL(
+      "/project/test-project/nodes?filter=tags%3A%20%22testTag%22",
+      "http://localhost",
+    ).href;
+    expect(link.attributes("href")).toBe(expectedHref);
   });
-
   it("handles exclude prop correctly", async () => {
     const wrapper = await mountNodeFilterLink({ exclude: true });
     await wrapper.trigger("click");
