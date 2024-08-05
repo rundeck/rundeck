19ab5552e2b998fde3ce931e1a1e7fcace18617e
Greg Schueler <greg.schueler@gmail.com>
2024-08-05 09:51:06 -0700

    enable shard plugin for core selenium tests

--- functional-test/src/test/resources/spock-configs/IncludeSeleniumCoreTestsConfig.groovy
+++ functional-test/src/test/resources/spock-configs/IncludeSeleniumCoreTestsConfig.groovy
@@ -2,4 +2,8 @@ import org.rundeck.util.annotations.SeleniumCoreTest
 
 runner {
     include SeleniumCoreTest
+    shard.enabled = true
+    shard.global = true
+    shard.indexEnvVar = 'SPOCK_SHARD_INDEX'
+    shard.countEnvVar = 'SPOCK_SHARD_COUNT'
 }
