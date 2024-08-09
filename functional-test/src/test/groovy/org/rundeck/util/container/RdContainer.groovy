4a4b80c857ef1b04e5e9be9af3c461ce03fa91a5
Alberto Hormazabal <ahormazabal@exatech.cl>
2024-08-09 16:21:56 -0400

    Allow setting a custom architecture for test docker images.

--- functional-test/src/test/groovy/org/rundeck/util/container/RdContainer.groovy
+++ functional-test/src/test/groovy/org/rundeck/util/container/RdContainer.groovy
@@ -19,7 +19,8 @@ class RdContainer extends DockerComposeContainer<RdContainer> implements ClientP
     public static final String STATIC_TOKEN = System.getenv("TEST_RUNDECK_CONTAINER_TOKEN") ?: 'admintoken'
     public static final String RUNDECK_IMAGE = System.getenv("TEST_IMAGE") ?: System.getProperty("TEST_IMAGE")
     public static final String LICENSE_LOCATION = System.getenv("LICENSE_LOCATION")
-    public static final String TEST_RUNDECK_GRAILS_URL = System.getenv("TEST_RUNDECK_GRAILS_URL") ?: "http://localhost:4440"
+    public static final String TEST_RUNDECK_GRAILS_URL = System.getenv("TEST_RUNDECK_GRAILS_URL") ?: "http://rundeck:4440"
+    public static final String TEST_TARGET_PLATFORM = System.getenv("TEST_TARGET_PLATFORM") ?: "linux/amd64"
 
     private final Map<String, Integer> clientConfig
 
@@ -38,6 +39,7 @@ class RdContainer extends DockerComposeContainer<RdContainer> implements ClientP
         withEnv("TEST_IMAGE", RUNDECK_IMAGE)
         withEnv("LICENSE_LOCATION", LICENSE_LOCATION)
         withEnv("TEST_RUNDECK_GRAILS_URL", TEST_RUNDECK_GRAILS_URL)
+        withEnv("TEST_TARGET_PLATFORM", TEST_TARGET_PLATFORM)
         withEnv("TEST_RUNDECK_FEATURE_NAME", featureName ?: 'placeholderFeatureName')
         withLogConsumer(DEFAULT_SERVICE_TO_EXPOSE, new Slf4jLogConsumer(log))
         waitingFor(DEFAULT_SERVICE_TO_EXPOSE,
