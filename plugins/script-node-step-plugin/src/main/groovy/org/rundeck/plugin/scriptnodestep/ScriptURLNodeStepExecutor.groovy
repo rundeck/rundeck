cd1adaa97c41739c3295304a67b44a761ff5aeda
Greg Schueler <greg.schueler@gmail.com>
2024-08-14 15:39:36 -0700

    cleanup: remove semicolons

--- plugins/script-node-step-plugin/src/main/groovy/org/rundeck/plugin/scriptnodestep/ScriptURLNodeStepExecutor.groovy
+++ plugins/script-node-step-plugin/src/main/groovy/org/rundeck/plugin/scriptnodestep/ScriptURLNodeStepExecutor.groovy
@@ -36,25 +36,25 @@ import java.security.NoSuchAlgorithmException
 
 @CompileStatic
 class ScriptURLNodeStepExecutor {
-    static Logger logger = LoggerFactory.getLogger(ScriptURLNodeStepExecutor.class);
+    static Logger logger = LoggerFactory.getLogger(ScriptURLNodeStepExecutor.class)
 
-    private final String scriptInterpreter;
-    private final boolean interpreterArgsQuoted;
-    private final String fileExtension;
-    private final String argString;
-    private final String adhocFilepath;
-    private final boolean expandTokenInScriptFile;
+    private final String scriptInterpreter
+    private final boolean interpreterArgsQuoted
+    private final String fileExtension
+    private final String argString
+    private final String adhocFilepath
+    private final boolean expandTokenInScriptFile
 
 
-    ScriptFileNodeStepUtils scriptUtils = new DefaultScriptFileNodeStepUtils();
+    ScriptFileNodeStepUtils scriptUtils = new DefaultScriptFileNodeStepUtils()
 
     PluginStepContext context
     FileCopierUtil.ContentModifier modifier
-    private File cacheDir;
-    private static final int DEFAULT_TIMEOUT = 30;
-    private static final boolean USE_CACHE = true;
-    public static final String UTF_8 = "UTF-8";
-    URLDownloader downloader = new Downloader();
+    private File cacheDir
+    private static final int DEFAULT_TIMEOUT = 30
+    private static final boolean USE_CACHE = true
+    public static final String UTF_8 = "UTF-8"
+    URLDownloader downloader = new Downloader()
 
     ScriptURLNodeStepExecutor(
         PluginStepContext context,
@@ -77,36 +77,36 @@ class ScriptURLNodeStepExecutor {
 
         cacheDir = new File(
             Constants.getBaseVar(context.getFramework().getFilesystemFramework().getBaseDir().getAbsolutePath())
-                + "/cache/ScriptURLNodeStepExecutor");
+                + "/cache/ScriptURLNodeStepExecutor")
     }
 
     void executeScriptURL(INodeEntry entry, InputStream inputStream) {
-        File destinationTempFile = downloader.downloadURLToTempFile(adhocFilepath, cacheDir, context, entry);
+        File destinationTempFile = downloader.downloadURLToTempFile(adhocFilepath, cacheDir, context, entry)
         if (!USE_CACHE) {
-            destinationTempFile.deleteOnExit();
+            destinationTempFile.deleteOnExit()
         }
-        boolean expandTokens = true;
+        boolean expandTokens = true
         if (context.getIFramework().getPropertyLookup().hasProperty("execution.script.tokenexpansion.enabled")) {
             expandTokens = "true".equals(
                 context.getIFramework().getPropertyLookup().getProperty(
                     "execution.script.tokenexpansion.enabled"
                 )
-            );
+            )
         }
 
-        final String[] args;
+        final String[] args
         if (null != argString) {
-            args = OptsUtil.burst(argString);
+            args = OptsUtil.burst(argString)
         } else {
-            args = new String[0];
+            args = new String[0]
         }
 
         if(expandTokens) {
-            expandTokens = expandTokenInScriptFile;
+            expandTokens = expandTokenInScriptFile
         }
 
         StepExecutionContext stepExecutionContext = context.getExecutionContext() as StepExecutionContext
-        final ExecutionService executionService = context.getIFramework().getExecutionService();
+        final ExecutionService executionService = context.getIFramework().getExecutionService()
 
         NodeStepResult nodeExecutorResult = scriptUtils.executeScriptFile(
                 stepExecutionContext,
@@ -122,21 +122,21 @@ class ScriptURLNodeStepExecutor {
                 executionService,
                 expandTokens,
                 modifier
-        );
+        )
 
         Util.handleFailureResult(nodeExecutorResult, entry)
     }
 
     private static String hashURL(final String url) {
         try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-1");
-            digest.reset();
-            digest.update(url.getBytes(Charset.forName(UTF_8)));
-            return new String(Hex.encodeHex(digest.digest()));
+            MessageDigest digest = MessageDigest.getInstance("SHA-1")
+            digest.reset()
+            digest.update(url.getBytes(Charset.forName(UTF_8)))
+            return new String(Hex.encodeHex(digest.digest()))
         } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+            e.printStackTrace()
         }
-        return Integer.toString(url.hashCode());
+        return Integer.toString(url.hashCode())
     }
 
     static enum Reason implements FailureReason{
@@ -152,7 +152,7 @@ class ScriptURLNodeStepExecutor {
             final File cacheDir,
             final PluginStepContext context,
             final INodeEntry node
-        ) throws NodeStepException;
+        ) throws NodeStepException
     }
 
     static class Downloader implements URLDownloader {
@@ -164,49 +164,49 @@ class ScriptURLNodeStepExecutor {
             final INodeEntry node
         ) throws NodeStepException {
             if (!cacheDir.isDirectory() && !cacheDir.mkdirs()) {
-                throw new RuntimeException("Unable to create cachedir: " + cacheDir.getAbsolutePath());
+                throw new RuntimeException("Unable to create cachedir: " + cacheDir.getAbsolutePath())
             }
             //create node context for node and substitute data references in command
-            WFSharedContext sharedContext = new WFSharedContext(context.getExecutionContext().getSharedDataContext());
-            sharedContext.merge(ContextView.global(), context.getDataContextObject());
+            WFSharedContext sharedContext = new WFSharedContext(context.getExecutionContext().getSharedDataContext())
+            sharedContext.merge(ContextView.global(), context.getDataContextObject())
             sharedContext.merge(
                 ContextView.node(node.getNodename()),
                 new BaseDataContext("node", DataContextUtils.nodeData(node))
-            );
+            )
 
 
-            final String finalUrl = expandUrlString(urlString, sharedContext, node.getNodename());
-            final URL url;
+            final String finalUrl = expandUrlString(urlString, sharedContext, node.getNodename())
+            final URL url
             try {
-                url = new URL(finalUrl);
+                url = new URL(finalUrl)
             } catch (MalformedURLException e) {
-                throw new NodeStepException(e, StepFailureReason.ConfigurationFailure, node.getNodename());
+                throw new NodeStepException(e, StepFailureReason.ConfigurationFailure, node.getNodename())
             }
             if (null != context.getExecutionContext().getExecutionListener()) {
-                context.getExecutionContext().getExecutionListener().log(4, "Requesting URL: " + url.toExternalForm());
+                context.getExecutionContext().getExecutionListener().log(4, "Requesting URL: " + url.toExternalForm())
             }
 
-            String cleanUrl = url.toExternalForm().replaceAll("^(https?://)([^:@/]+):[^@/]*@", "\$1\$2:****@");
-            String tempFileName = hashURL(url.toExternalForm()) + ".temp";
-            File destinationTempFile = new File(cacheDir, tempFileName);
-            File destinationCacheData = new File(cacheDir, tempFileName + ".cache.properties");
+            String cleanUrl = url.toExternalForm().replaceAll("^(https?://)([^:@/]+):[^@/]*@", "\$1\$2:****@")
+            String tempFileName = hashURL(url.toExternalForm()) + ".temp"
+            File destinationTempFile = new File(cacheDir, tempFileName)
+            File destinationCacheData = new File(cacheDir, tempFileName + ".cache.properties")
 
             //update from URL if necessary
             final URLFileUpdaterBuilder urlFileUpdaterBuilder = new URLFileUpdaterBuilder()
                 .setUrl(url)
                 .setAcceptHeader("*/*")
-                .setTimeout(DEFAULT_TIMEOUT);
+                .setTimeout(DEFAULT_TIMEOUT)
             if (USE_CACHE) {
                 urlFileUpdaterBuilder
                     .setCacheMetadataFile(destinationCacheData)
                     .setCachedContent(destinationTempFile)
-                    .setUseCaching(true);
+                    .setUseCaching(true)
             }
-            final URLFileUpdater updater = urlFileUpdaterBuilder.createURLFileUpdater();
+            final URLFileUpdater updater = urlFileUpdaterBuilder.createURLFileUpdater()
             try {
-                UpdateUtils.update(updater, destinationTempFile);
+                UpdateUtils.update(updater, destinationTempFile)
 
-                logger.debug("Updated nodes resources file: " + destinationTempFile);
+                logger.debug("Updated nodes resources file: " + destinationTempFile)
             } catch (UpdateUtils.UpdateException e) {
                 if (!destinationTempFile.isFile() || destinationTempFile.length() < 1) {
                     throw new NodeStepException(
@@ -221,19 +221,19 @@ class ScriptURLNodeStepExecutor {
                     )
                 }
             }
-            return destinationTempFile;
+            return destinationTempFile
         }
     }
 
 
     public static final Converter<String, String> urlQueryEncoder = s -> {
         try {
-            return URLEncoder.encode(s.toString(),UTF_8).replace("+","%20");
+            return URLEncoder.encode(s.toString(),UTF_8).replace("+","%20")
         } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-            return s;
+            e.printStackTrace()
+            return s
         }
-    };
+    }
 
     /**
      * Expand data references in a URL string, using proper encoding for path and query parts.
@@ -249,8 +249,8 @@ class ScriptURLNodeStepExecutor {
             final String nodename
     )
     {
-        final int qindex = urlString.indexOf('?');
-        final StringBuilder builder = new StringBuilder();
+        final int qindex = urlString.indexOf('?')
+        final StringBuilder builder = new StringBuilder()
 
         builder.append(
                 SharedDataContextUtils.replaceDataReferences(
@@ -263,7 +263,7 @@ class ScriptURLNodeStepExecutor {
                         false
                 ))
         if (qindex > 0) {
-            builder.append("?");
+            builder.append("?")
             if (qindex < urlString.length() - 1) {
                 builder.append(SharedDataContextUtils.replaceDataReferences(
                         urlString.substring(qindex + 1),
@@ -273,11 +273,11 @@ class ScriptURLNodeStepExecutor {
                         urlQueryEncoder,
                         true,
                         false
-                ));
+                ))
             }
 
         }
 
-        return builder.toString();
+        return builder.toString()
     }
 }
