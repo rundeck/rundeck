da2d149b272856417baa2cbbeddd55a8c36b4ab5
Greg Schueler <greg.schueler@gmail.com>
2024-08-14 14:34:57 -0700

    update to use modifier, add tests

--- plugins/script-node-step-plugin/src/main/groovy/org/rundeck/plugin/scriptnodestep/ScriptFileNodeStepExecutor.groovy
+++ plugins/script-node-step-plugin/src/main/groovy/org/rundeck/plugin/scriptnodestep/ScriptFileNodeStepExecutor.groovy
@@ -5,9 +5,11 @@ import com.dtolabs.rundeck.core.data.SharedDataContextUtils
 import com.dtolabs.rundeck.core.dispatcher.ContextView
 import com.dtolabs.rundeck.core.dispatcher.DataContextUtils
 import com.dtolabs.rundeck.core.execution.ExecutionService
+import com.dtolabs.rundeck.core.execution.impl.common.FileCopierUtil
 import com.dtolabs.rundeck.core.execution.workflow.StepExecutionContext
 import com.dtolabs.rundeck.core.execution.workflow.steps.node.NodeStepResult
 import com.dtolabs.rundeck.core.execution.workflow.steps.node.impl.DefaultScriptFileNodeStepUtils
+import com.dtolabs.rundeck.core.execution.workflow.steps.node.impl.ScriptFileNodeStepUtils
 import com.dtolabs.rundeck.core.utils.OptsUtil
 import com.dtolabs.rundeck.plugins.step.PluginStepContext
 import groovy.transform.CompileStatic
@@ -17,23 +19,25 @@ import java.util.function.BiFunction
 @CompileStatic
 class ScriptFileNodeStepExecutor {
     private final String scriptInterpreter;
-    private final Boolean interpreterArgsQuoted;
+    private final boolean interpreterArgsQuoted;
     private final String fileExtension;
     private final String argString;
     private final String adhocFilepath;
     private final String adhocLocalString;
     private final boolean expandTokenInScriptFile;
+    private final FileCopierUtil.ContentModifier modifier;
 
-    protected DefaultScriptFileNodeStepUtils scriptUtils = new DefaultScriptFileNodeStepUtils();
+    protected ScriptFileNodeStepUtils scriptUtils = new DefaultScriptFileNodeStepUtils();
 
     ScriptFileNodeStepExecutor(
-            String scriptInterpreter,
-            Boolean interpreterArgsQuoted,
-            String fileExtension,
-            String argString,
-            String adhocFilepath,
-            String adhocLocalString,
-            boolean expandTokenInScriptFile
+        String scriptInterpreter,
+        boolean interpreterArgsQuoted,
+        String fileExtension,
+        String argString,
+        String adhocFilepath,
+        String adhocLocalString,
+        boolean expandTokenInScriptFile,
+        FileCopierUtil.ContentModifier modifier
     ) {
         this.scriptInterpreter = scriptInterpreter;
         this.interpreterArgsQuoted = interpreterArgsQuoted;
@@ -42,19 +46,26 @@ class ScriptFileNodeStepExecutor {
         this.adhocFilepath = adhocFilepath;
         this.adhocLocalString = adhocLocalString;
         this.expandTokenInScriptFile = expandTokenInScriptFile;
+        this.modifier = modifier
     }
 
     void executeScriptFile(PluginStepContext context, INodeEntry entry, InputStream input) {
         boolean expandTokens = true;
-        if (context.getFramework().hasProperty("execution.script.tokenexpansion.enabled")) {
-            expandTokens = "true".equals(context.getFramework().getProperty("execution.script.tokenexpansion.enabled"));
+        if (context.getIFramework().getPropertyLookup().hasProperty("execution.script.tokenexpansion.enabled")) {
+            expandTokens = "true".equals(
+                context.getIFramework().getPropertyLookup().getProperty(
+                    "execution.script.tokenexpansion.enabled"
+                )
+            )
         }
         if (null != adhocFilepath) {
             expandTokens = expandTokenInScriptFile;
         }
 
-        String expandedVarsInURL = SharedDataContextUtils.replaceDataReferences(
-                adhocFilepath ?: "",
+        String expandedVarsInURL = null
+        if (adhocFilepath) {
+            expandedVarsInURL = SharedDataContextUtils.replaceDataReferences(
+                adhocFilepath,
                 context.getExecutionContext().getSharedDataContext(),
                 //add node name to qualifier to read node-data first
                 ContextView.node(entry.getNodename()),
@@ -62,12 +73,13 @@ class ScriptFileNodeStepExecutor {
                 DataContextUtils.replaceMissingOptionsWithBlank,
                 false,
                 false
-        );
+            )
 
-        if (DataContextUtils.hasOptionsInString(expandedVarsInURL)) {
-            Map<String, Map<String, String>> optionsContext = new HashMap();
-            optionsContext.put("option", context.getDataContext().get("option"));
-            expandedVarsInURL = DataContextUtils.replaceDataReferencesInString(expandedVarsInURL, optionsContext);
+            if (DataContextUtils.hasOptionsInString(expandedVarsInURL)) {
+                Map<String, Map<String, String>> optionsContext = new HashMap()
+                optionsContext.put("option", context.getDataContext().get("option"))
+                expandedVarsInURL = DataContextUtils.replaceDataReferencesInString(expandedVarsInURL, optionsContext)
+            }
         }
 
         final String[] args;
@@ -78,24 +90,23 @@ class ScriptFileNodeStepExecutor {
         }
 
         StepExecutionContext stepExecutionContext = context.getExecutionContext() as StepExecutionContext
-        final ExecutionService executionService = context.getFramework().getExecutionService();
-
-        boolean argsQuoted = interpreterArgsQuoted != null ? interpreterArgsQuoted : false;
+        final ExecutionService executionService = context.getIFramework().getExecutionService()
 
         NodeStepResult nodeExecutorResult = scriptUtils.executeScriptFile(
-                stepExecutionContext,
-                entry,
-                this.adhocLocalString,
-                expandedVarsInURL,
-                null,
-                fileExtension,
-                args,
-                scriptInterpreter,
-                input,
-                argsQuoted,
-                executionService,
-                expandTokens
-        );
+            stepExecutionContext,
+            entry,
+            this.adhocLocalString,
+            expandedVarsInURL,
+            null,
+            fileExtension,
+            args,
+            scriptInterpreter,
+            input,
+            interpreterArgsQuoted,
+            executionService,
+            expandTokens,
+            modifier
+        )
 
         Util.handleFailureResult(nodeExecutorResult, entry)
     }
---
+++ plugins/script-node-step-plugin/src/test/groovy/org/rundeck/plugin/scriptnodestep/ScriptFileNodeStepExecutorSpec.groovy
@@ -0,0 +1,172 @@
+package org.rundeck.plugin.scriptnodestep
+
+import com.dtolabs.rundeck.core.common.INodeEntry
+import com.dtolabs.rundeck.core.data.BaseDataContext
+import com.dtolabs.rundeck.core.dispatcher.ContextView
+import com.dtolabs.rundeck.core.execution.ExecutionContext
+import com.dtolabs.rundeck.core.execution.workflow.WFSharedContext
+import com.dtolabs.rundeck.core.execution.workflow.steps.node.NodeStepResult
+import com.dtolabs.rundeck.core.execution.workflow.steps.node.impl.ScriptFileNodeStepUtils
+import com.dtolabs.rundeck.core.utils.PropertyLookup
+import com.dtolabs.rundeck.plugins.step.PluginStepContext
+import spock.lang.Specification
+
+class ScriptFileNodeStepExecutorSpec extends Specification {
+    def "basic execute test"() {
+        given:
+            def scriptFileNodeStepExecutor = new ScriptFileNodeStepExecutor(
+                "scriptInterpreter",
+                true,
+                "fileExtension",
+                "argString",
+                null,
+                "adhocLocalString",
+                true,
+                null
+            )
+            scriptFileNodeStepExecutor.scriptUtils = Mock(ScriptFileNodeStepUtils)
+            def context = Mock(PluginStepContext) {
+                _ * getIFramework() >> Mock(com.dtolabs.rundeck.core.common.IFramework) {
+                    _ * getPropertyLookup() >> Mock(PropertyLookup) {
+                        _ * hasProperty('execution.script.tokenexpansion.enabled') >> { expansionEnabled != null }
+                        _ * getProperty('execution.script.tokenexpansion.enabled') >> expansionEnabled.toString()
+                    }
+                }
+                _ * getExecutionContext() >> Mock(ExecutionContext)
+            }
+            def node = Mock(INodeEntry)
+
+        when:
+            scriptFileNodeStepExecutor.executeScriptFile(context, node, null)
+
+        then:
+            1 * scriptFileNodeStepExecutor.scriptUtils.executeScriptFile(
+                _,
+                node,
+                'adhocLocalString',
+                null,
+                null,
+                'fileExtension',
+                ['argString'].toArray(),
+                'scriptInterpreter',
+                null,
+                true,
+                _,
+                expectedEnabled,
+                null
+            ) >> Mock(NodeStepResult) {
+                isSuccess() >> true
+            }
+        where:
+            expansionEnabled | expectedEnabled
+            null             | true
+            true             | true
+            false            | false
+    }
+
+    def "file path"() {
+        given:
+            def scriptFileNodeStepExecutor = new ScriptFileNodeStepExecutor(
+                "scriptInterpreter",
+                true,
+                "fileExtension",
+                "argString",
+                'some/path',
+                null,
+                expandTokens,
+                null
+            )
+            scriptFileNodeStepExecutor.scriptUtils = Mock(ScriptFileNodeStepUtils)
+            def context = Mock(PluginStepContext) {
+                _ * getIFramework() >> Mock(com.dtolabs.rundeck.core.common.IFramework) {
+                    _ * getPropertyLookup() >> Mock(PropertyLookup) {
+
+                    }
+                }
+                _ * getExecutionContext() >> Mock(ExecutionContext)
+            }
+            def node = Mock(INodeEntry)
+
+        when:
+            scriptFileNodeStepExecutor.executeScriptFile(context, node, null)
+
+        then:
+            1 * scriptFileNodeStepExecutor.scriptUtils.executeScriptFile(
+                _,
+                node,
+                null,
+                'some/path',
+                null,
+                'fileExtension',
+                ['argString'].toArray(),
+                'scriptInterpreter',
+                null,
+                true,
+                _,
+                expectedEnabled,
+                null
+            ) >> Mock(NodeStepResult) {
+                isSuccess() >> true
+            }
+        where:
+            expandTokens | expectedEnabled
+            true         | true
+            false        | false
+    }
+
+    def "file path shared data variable expansion"() {
+        given:
+            def scriptFileNodeStepExecutor = new ScriptFileNodeStepExecutor(
+                "scriptInterpreter",
+                true,
+                "fileExtension",
+                "argString",
+                filePath,
+                null,
+                true,
+                null
+            )
+            scriptFileNodeStepExecutor.scriptUtils = Mock(ScriptFileNodeStepUtils)
+            def dataContext = WFSharedContext.with(ContextView.global(), new BaseDataContext(contextData))
+            def context = Mock(PluginStepContext) {
+                _ * getIFramework() >> Mock(com.dtolabs.rundeck.core.common.IFramework) {
+                    _ * getPropertyLookup() >> Mock(PropertyLookup) {
+
+                    }
+                }
+                _ * getExecutionContext() >> Mock(ExecutionContext) {
+                    getSharedDataContext() >> dataContext
+                }
+            }
+            def node = Mock(INodeEntry)
+
+        when:
+            scriptFileNodeStepExecutor.executeScriptFile(context, node, null)
+
+        then:
+            1 * scriptFileNodeStepExecutor.scriptUtils.executeScriptFile(
+                _,
+                node,
+                null,
+                result,
+                null,
+                'fileExtension',
+                ['argString'].toArray(),
+                'scriptInterpreter',
+                null,
+                true,
+                _,
+                true,
+                null
+            ) >> Mock(NodeStepResult) {
+                isSuccess() >> true
+            }
+        where:
+            filePath                  | contextData                | result
+            'some/path'               | [:]                        | 'some/path'
+            'some/path${option.key1}' | [:]                        | 'some/path'
+            'some/path${option.key1}' | [option: [key1: '/bingo']] | 'some/path/bingo'
+            'some/path${data.key1}'   | [data: [key1: '/bingo']]   | 'some/path/bingo'
+    }
+
+}
