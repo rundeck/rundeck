56f1a36fe79044449083a491eebf18d00854d796
Greg Schueler <greg.schueler@gmail.com>
2024-08-14 15:40:13 -0700

    unused

--- plugins/script-node-step-plugin/src/main/groovy/org/rundeck/plugin/scriptnodestep/ScriptModifier.groovy
+++
@@ -1,8 +0,0 @@
-package org.rundeck.plugin.scriptnodestep
-
-/**
- * Modify a script to add secre input behavior
- */
-interface ScriptModifier {
-    String modifyScriptForSecureInput(String script)
-}
