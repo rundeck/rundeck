c4c82592a004b4faffcdf769dbea4b3e370dd871
Greg Schueler <greg.schueler@gmail.com>
2024-08-14 15:40:06 -0700

    change to content modifier

--- plugins/script-node-step-plugin/src/main/groovy/org/rundeck/plugin/scriptnodestep/ShellScriptModifier.groovy
+++ plugins/script-node-step-plugin/src/main/groovy/org/rundeck/plugin/scriptnodestep/ShellScriptModifier.groovy
@@ -1,31 +1,25 @@
 package org.rundeck.plugin.scriptnodestep
 
+import com.dtolabs.rundeck.core.execution.impl.common.FileCopierUtil
 import groovy.transform.CompileStatic
-
 /**
  * Modify a shell script to include the eval line to consume stdin data as rundeck variables.
  * Appends the eval line after the first line if it a shebang line, otherwise prepends it to the script.
  */
 @CompileStatic
-class ShellScriptModifier implements ScriptModifier {
-    static String EVAL_STRING = 'eval $(</dev/stdin) #consume rundeck variables\n'
+class ShellScriptModifier implements FileCopierUtil.ContentModifier {
+    static String EVAL_STRING = 'eval $(</dev/stdin) #consume rundeck variables'
+    static final String SHEBANG = '#!'
 
-    String modifyScriptForSecureInput(String script) {
-        StringBuilder sb = new StringBuilder()
-        int index = 0
-        if (script.startsWith('#!')) {
-            //skip shebang
-            index = script.indexOf('\n')
-            if (index > 0) {
-                sb.append(script.substring(0, index + 1))
-                index++
-            } else {
-                index = 0
-            }
+    @Override
+    boolean process(final String s, final Sink sink) throws IOException {
+        if (s.startsWith(SHEBANG)) {
+            sink.writeLine(s)
+            sink.writeLine(EVAL_STRING)
+        } else {
+            sink.writeLine(EVAL_STRING)
+            sink.writeLine(s)
         }
-        sb.append(EVAL_STRING)
-        sb.append(script.substring(index))
-
-        return sb.toString()
+        return false
     }
 }
