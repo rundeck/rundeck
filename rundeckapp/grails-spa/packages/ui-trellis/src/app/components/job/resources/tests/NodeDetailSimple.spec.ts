70939482dd88075382fec11b5fd21d91cbf0a0e3
Jaya Singh <jaya@catalytic.com>
2024-08-20 23:05:36 +0530

    Refactored it.each test to improve clarity and remove redundancy

--- rundeckapp/grails-spa/packages/ui-trellis/src/app/components/job/resources/tests/NodeDetailSimple.spec.ts
+++ rundeckapp/grails-spa/packages/ui-trellis/src/app/components/job/resources/tests/NodeDetailSimple.spec.ts
@@ -1,6 +1,5 @@
 import { mount } from "@vue/test-utils";
 import NodeDetailsSimple from "../NodeDetailsSimple.vue";
-
 jest.mock("@/library/rundeckService", () => ({
   getRundeckContext: jest.fn().mockReturnValue({
     rdBase: "mockRdBase",
@@ -8,6 +7,7 @@ jest.mock("@/library/rundeckService", () => ({
   }),
   url: jest.fn().mockReturnValue({ href: "mockHref" }),
 }));
+
 const mockAttributes = {
   osFamily: "Linux",
   osName: "Ubuntu",
@@ -29,6 +29,7 @@ const mockAttributes = {
   username: "user",
   hostname: "host",
 };
+
 const mountNodeDetailsSimple = async (propsData = {}) => {
   const wrapper = mount(NodeDetailsSimple, {
     props: {
@@ -47,31 +48,99 @@ const mountNodeDetailsSimple = async (propsData = {}) => {
   await wrapper.vm.$nextTick();
   return wrapper;
 };
+
 describe("NodeDetailsSimple Component", () => {
-  it("renders node attributes", async () => {
-    const wrapper = await mountNodeDetailsSimple();
-    const attributes = wrapper.findAll("td.value, td.key, .setting");
-    const attributeTexts = attributes.map((attr) => attr.text());
-    const expectedAttributes = {
-      osFamily: "Linux",
-      osName: "Ubuntu",
-      osVersion: "20.04",
-      osArch: "x86_64",
-      description: "Test Node 1",
-      "ui:status:text": "Healthy",
-      "ui:status:icon": "fa-check",
-      "ns1:attr1": "value1",
-      "ns1:attr2": "value2",
-      "ns1:attr3": "value3",
-      "ns1:attr4": "value4",
-    };
-    Object.entries(expectedAttributes).forEach(([key, value]) => {
-      const attributeFound = attributeTexts.some((text) =>
-        text.includes(value),
-      );
-      expect(attributeFound).toBe(true);
-    });
-  });
+  it.each([
+    {
+      description: "with useNamespace and nodeColumns set to false",
+      props: {
+        useNamespace: false,
+        nodeColumns: false,
+        showExcludeFilterLinks: true,
+      },
+      expectedPlusCount: 19,
+      expectedMinusCount: 19,
+      expectedChevronCount: 0,
+    },
+    {
+      description: "with useNamespace and nodeColumns set to true",
+      props: {
+        useNamespace: true,
+        nodeColumns: true,
+        showExcludeFilterLinks: true,
+      },
+      expectedPlusCount: 4,
+      expectedMinusCount: 4,
+      expectedChevronCount: 3,
+    },
+  ])(
+    "renders node attributes correctly $description",
+    async ({
+      props,
+      expectedPlusCount,
+      expectedMinusCount,
+      expectedChevronCount,
+    }) => {
+      let wrapper = await mountNodeDetailsSimple(props);
+      // General attributes
+      const generalAttributes = {
+        description: "Test Node 1",
+        "ui:status:text": "Healthy",
+        "ui:status:icon": "fa-check",
+        osFamily: "Linux",
+        osName: "Ubuntu",
+        osVersion: "20.04",
+        osArch: "x86_64",
+      };
+      Object.entries(generalAttributes).forEach(([key, value]) => {
+        const attributeElement = wrapper.find(`td.value:contains(${value})`);
+        expect(attributeElement.exists()).toBe(true);
+      });
+      // Attributes without namespaces
+      const attributesWithNoNamespaces = {
+        key3: "value3",
+        key4: "value4",
+        key5: "value5",
+        key6: "value6",
+      };
+      Object.entries(attributesWithNoNamespaces).forEach(([key, value]) => {
+        const attributeElement = wrapper.find(`td.setting:contains(${value})`);
+        expect(attributeElement.exists()).toBe(true);
+      });
+      // Check for .glyphicon-plus elements related to attributes without namespaces
+      const plusLinks = wrapper.findAll(".setting .glyphicon-plus");
+      expect(plusLinks.length).toBe(expectedPlusCount);
+      plusLinks.forEach((link) => {
+        expect(link.classes()).toContain("glyphicon-plus");
+      });
+      // Check for .glyphicon-minus elements related to attributes without namespaces
+      const minusLinks = wrapper.findAll(".setting .glyphicon-minus");
+      expect(minusLinks.length).toBe(expectedMinusCount);
+      minusLinks.forEach((link) => {
+        expect(link.classes()).toContain("glyphicon-minus");
+      });
+      // Attributes with namespaces (only when useNamespace is true)
+      if (props.useNamespace) {
+        const attributesWithNamespaces = {
+          "namespace:key1": "value1",
+          "namespace:key2": "value2",
+        };
+        Object.entries(attributesWithNamespaces).forEach(([key, value]) => {
+          const attributeElement = wrapper.find(
+            `tbody.subattrs .value:contains(${value})`,
+          );
+          expect(attributeElement.exists()).toBe(true);
+        });
+        // Check for glyphicon-chevron to ensure proper rendering of namespaces
+        const chevrons = wrapper.findAll(".namespace .auto-caret");
+        expect(chevrons.length).toBe(expectedChevronCount); // Adjusted based on prop combinations
+        chevrons.forEach((chevron) => {
+          expect(chevron.exists()).toBe(true);
+        });
+      }
+    },
+  );
+
   it("renders tags", async () => {
     const wrapper = await mountNodeDetailsSimple();
     const tags = wrapper.findAll(".label-muted");
@@ -79,25 +148,28 @@ describe("NodeDetailsSimple Component", () => {
     expect(tags.at(0).text()).toContain("Tag1");
     expect(tags.at(1).text()).toContain("Tag2");
   });
+
   it("renders expandable attributes using class", async () => {
     const wrapper = await mountNodeDetailsSimple();
     const toggleButton = wrapper.find(".textbtn");
     await toggleButton.trigger("click");
     await wrapper.vm.$nextTick();
-    // Adjust the selector if necessary based on the actual HTML structure
     const expandedAttributes = wrapper.findAll(".hover-action-holder");
-    expect(expandedAttributes.length).toBe(12);
+    expect(expandedAttributes.length).toBe(12); // Adjust based on your expected output
   });
 
-  it("renders correctly with useNamespace and nodeColumns set to true", async () => {
-    const wrapper = await mountNodeDetailsSimple();
+  it("renders node filter links when useDefaultColumns is true", async () => {
+    const wrapper = await mountNodeDetailsSimple({
+      useDefaultColumns: true,
+    });
     const usernameLink = wrapper.find(
       "[data-testid='node-attribute-link-username']",
     );
     const hostnameLink = wrapper.find(
       "[data-testid='node-attribute-link-hostname']",
     );
-    await wrapper.vm.$nextTick();
+    expect(usernameLink.exists()).toBe(true);
+    expect(hostnameLink.exists()).toBe(true);
     await usernameLink.trigger("click");
     expect(wrapper.emitted().filter).toBeTruthy();
     expect(wrapper.emitted().filter[0][0]).toEqual({
@@ -105,7 +177,10 @@ describe("NodeDetailsSimple Component", () => {
     });
     await hostnameLink.trigger("click");
     expect(wrapper.emitted().filter).toBeTruthy();
-    expect(wrapper.emitted().filter.length).toBe(2);
+    expect;
+    await hostnameLink.trigger("click");
+    expect(wrapper.emitted().filter).toBeTruthy();
+    expect(wrapper.emitted().filter.length).toBe(3);
     expect(wrapper.emitted().filter[1][0]).toEqual({
       filter: 'hostname: "host"',
     });
