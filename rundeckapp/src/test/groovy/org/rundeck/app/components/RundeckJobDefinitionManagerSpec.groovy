d330b587764bed8e7d3c7f621d8931b6be1d4534
Greg Schueler <greg.schueler@gmail.com>
2024-08-26 16:02:39 -0700

    add additional test for decoding basic step types

--- rundeckapp/src/test/groovy/org/rundeck/app/components/RundeckJobDefinitionManagerSpec.groovy
+++ rundeckapp/src/test/groovy/org/rundeck/app/components/RundeckJobDefinitionManagerSpec.groovy
@@ -2,6 +2,8 @@ package org.rundeck.app.components
 
 import grails.testing.gorm.DataTest
 import org.rundeck.app.components.jobs.ImportedJob
+import org.rundeck.core.execution.ExecCommand
+import org.rundeck.core.execution.ScriptCommand
 import org.rundeck.core.execution.ScriptFileCommand
 import rundeck.CommandExec
 import rundeck.PluginStep
@@ -17,7 +19,7 @@ class RundeckJobDefinitionManagerSpec extends Specification implements DataTest 
     }
 
 
-    def "test decode format file with/without expandTokenInScriptFile field"(){
+    def "test decode script file step with/without expandTokenInScriptFile field"() {
         when:
         List<ImportedJob<ScheduledExecution>> jobList = rundeckJobDefinitionManager.decodeFormat(format, new ByteArrayInputStream(input.getBytes()))
 
@@ -36,17 +38,167 @@ class RundeckJobDefinitionManagerSpec extends Specification implements DataTest 
         ] + (emptyargs ? [argString: ''] : [:])
 
         where:
-        format | input             | expandTokenInScriptFile | emptyargs
-        "xml"  | getJobXml(true)   | true                    | true
-        "xml"  | getJobXml(false)  | false                   | true
-        "xml"  | getJobXml(null)   | false                   | true
-        "yaml" | getJobYaml(true)  | true                    | false
-        "yaml" | getJobYaml(false) | false                   | false
-        "yaml" | getJobYaml(null)  | false                   | false
-        "json" | getJobJson(true)  | true                    | false
-        "json" | getJobJson(false) | false                   | false
-        "json" | getJobJson(null)  | false                   | false
+        format | input                       | expandTokenInScriptFile | emptyargs
+        "xml"  | getJobXmlScriptfile(true)   | true                    | true
+        "xml"  | getJobXmlScriptfile(false)  | false                   | true
+        "xml"  | getJobXmlScriptfile(null)   | false                   | true
+        "yaml" | getJobYamlScriptfile(true)  | true                    | false
+        "yaml" | getJobYamlScriptfile(false) | false                   | false
+        "yaml" | getJobYamlScriptfile(null)  | false                   | false
+        "json" | getJobJsonScriptfile(true)  | true                    | false
+        "json" | getJobJsonScriptfile(false) | false                   | false
+        "json" | getJobJsonScriptfile(null)  | false                   | false
     }
+    def "test decode script URL step with/without expandTokenInScriptFile field"() {
+        when:
+        List<ImportedJob<ScheduledExecution>> jobList = rundeckJobDefinitionManager.decodeFormat(format, new ByteArrayInputStream(input.getBytes()))
+
+        then:
+        jobList.size() == 1
+        ScheduledExecution se = jobList.first().getJob()
+        se
+        se.workflow.commands.size() == 1
+        se.workflow.commands.first() instanceOf PluginStep
+        se.workflow.commands.first().type == ScriptFileCommand.SCRIPT_FILE_COMMAND_TYPE
+
+        se.workflow.commands.first().configuration == [
+            adhocFilepath          : 'http://example.com',
+            adhocExecution         : true,
+            expandTokenInScriptFile: expandTokenInScriptFile
+        ] + (emptyargs ? [argString: ''] : [:])
+
+        where:
+        format | input                                            | expandTokenInScriptFile | emptyargs
+        "xml"  | getJobXmlScriptURL('http://example.com', true)   | true                    | true
+        "xml"  | getJobXmlScriptURL('http://example.com', false)  | false                   | true
+        "xml"  | getJobXmlScriptURL('http://example.com', null)   | false                   | true
+        "yaml" | getJobYamlScriptURL('http://example.com', true)  | true                    | false
+        "yaml" | getJobYamlScriptURL('http://example.com', false) | false                   | false
+        "yaml" | getJobYamlScriptURL('http://example.com', null)  | false                   | false
+        "json" | getJobJsonScriptURL('http://example.com', true)  | true                    | false
+        "json" | getJobJsonScriptURL('http://example.com', false) | false                   | false
+        "json" | getJobJsonScriptURL('http://example.com', null)  | false                   | false
+    }
+
+    def "test decode command step"() {
+        when:
+            List<ImportedJob<ScheduledExecution>> jobList = rundeckJobDefinitionManager
+                .decodeFormat(format, new ByteArrayInputStream(input.getBytes()))
+
+        then:
+            jobList.size() == 1
+            ScheduledExecution se = jobList.first().getJob()
+            se
+            se.workflow.commands.size() == 1
+            se.workflow.commands.first() instanceOf PluginStep
+            se.workflow.commands.first().type == ExecCommand.EXEC_COMMAND_TYPE
+
+            se.workflow.commands.first().configuration == [
+                adhocRemoteString: 'some command',
+                adhocExecution   : true,
+            ]
+
+        where:
+            format | input
+            "xml"  | getJobXmlCommand('some command')
+            "yaml" | getJobYamlCommand('some command')
+            "json" | getJobJsonCommand('some command')
+    }
+
+    def "test decode script step"() {
+        when:
+            List<ImportedJob<ScheduledExecution>> jobList = rundeckJobDefinitionManager
+                .decodeFormat(format, new ByteArrayInputStream(input.getBytes()))
+
+        then:
+            jobList.size() == 1
+            ScheduledExecution se = jobList.first().getJob()
+            se
+            se.workflow.commands.size() == 1
+            se.workflow.commands.first() instanceOf PluginStep
+            se.workflow.commands.first().type == ScriptCommand.SCRIPT_COMMAND_TYPE
+
+            se.workflow.commands.first().configuration == [
+                adhocLocalString: 'some script',
+                adhocExecution  : true,
+            ] + (emptyargs ? [argString: ''] : [:])
+
+        where:
+            format | input              | emptyargs
+            "xml"  | getJobXmlScript()  | true
+            "yaml" | getJobYamlScript() | false
+            "json" | getJobJsonScript() | false
+    }
+
+    def "test decode script step multiline"() {
+        when:
+            List<ImportedJob<ScheduledExecution>> jobList = rundeckJobDefinitionManager
+                .decodeFormat(format, new ByteArrayInputStream(input.getBytes()))
+
+        then:
+            jobList.size() == 1
+            ScheduledExecution se = jobList.first().getJob()
+            se
+            se.workflow.commands.size() == 1
+            se.workflow.commands.first() instanceOf PluginStep
+            se.workflow.commands.first().type == ScriptCommand.SCRIPT_COMMAND_TYPE
+
+            se.workflow.commands.first().configuration == [
+                adhocLocalString: script,
+                adhocExecution  : true,
+            ] + (emptyargs ? [argString: ''] : [:])
+
+        where:
+            script = 'some\n\nscript\n'
+            format | input | emptyargs
+            "xml" | getJobXmlScriptMultiline('some\n\nscript\n') | true
+            "yaml" | getJobYamlScriptMulti('some\n          \n          script\n') | false
+            "json" | getJobJsonScriptMulti('some\\n\\nscript\\n') | false
+    }
+
+    def "Test hasMultiNewlineEnding regex"() {
+        expect: "internal method to determine if script content has multiple newlines at the end"
+            hasMultiNewlineEnding(script) == expected
+
+        where:
+            script                 | expected
+            'some\n\nscript'       | false
+            'some\n\nscript\n'     | false
+            'some\n\nscript\n\n'   | true
+            'some\n\nscript\n\n\n' | true
+            'some\nscript'         | false
+            'some\nscript\n'       | false
+            'some\nscript\n\n'     | true
+            'some\nscript\n\n\n'   | true
+    }
+
+    def "test decode yaml script step multiline preserve newlines"() {
+        when:
+            List<ImportedJob<ScheduledExecution>> jobList = rundeckJobDefinitionManager
+                .decodeFormat(format, new ByteArrayInputStream(input.getBytes()))
+
+        then:
+
+            jobList.size() == 1
+            ScheduledExecution se = jobList.first().getJob()
+            se
+            se.workflow.commands.size() == 1
+            se.workflow.commands.first() instanceOf PluginStep
+            se.workflow.commands.first().type == ScriptCommand.SCRIPT_COMMAND_TYPE
+
+            se.workflow.commands.first().configuration == [
+                adhocLocalString: script,
+                adhocExecution  : true,
+            ]
+
+        where:
+            format = 'yaml'
+            script | input
+            'some\n\nscript' | getJobYamlScriptMulti('some\n          \n          script')
+            'some\n\nscript\n' | getJobYamlScriptMulti('some\n          \n          script\n')
+            'some\n\nscript\n\n' | getJobYamlScriptMulti('some\n          \n          script\n\n')
+    }
+
 
     def "export job to a format with expandTokenInScriptFile"(){
         given:
@@ -74,12 +226,10 @@ class RundeckJobDefinitionManagerSpec extends Specification implements DataTest 
 
         where:
         format | jobDefinition
-        "xml"  | getJobXml(true)
-        "yaml" | getJobYaml(true)
+            "xml"  | getJobXmlScriptfile(true)
+            "yaml" | getJobYamlScriptfile(true)
     }
-
-    private static String getJobXml(Boolean expandTokenInScriptFile){
-        return """<joblist>
+    static final String XML_PREFIX = '''<joblist>
   <job>
     <description>a job</description>
     <executionEnabled>true</executionEnabled>
@@ -95,19 +245,56 @@ class RundeckJobDefinitionManagerSpec extends Specification implements DataTest 
       <year year='*' />
     </schedule>
     <scheduleEnabled>true</scheduleEnabled>
-    <sequence keepgoing='true' strategy='node-first'>
-      <command>
-        ${expandTokenInScriptFile!=null ? "<expandTokenInScriptFile>${expandTokenInScriptFile}</expandTokenInScriptFile>" : ""}
-        <scriptargs />
-        <scriptfile>path/to/file.sh</scriptfile>
-      </command>
+    <sequence keepgoing='true' strategy='node-first'>'''
+
+    static final String XML_SUFFIX = '''
     </sequence>
   </job>
-</joblist>"""
+</joblist>'''
+
+    private static String getJobXmlScriptfile(Boolean expandTokenInScriptFile){
+        return """${XML_PREFIX}
+      <command>
+        ${expandTokenInScriptFile!=null ? "<expandTokenInScriptFile>${expandTokenInScriptFile}</expandTokenInScriptFile>" : ""
+        }
+        <scriptargs />
+        <scriptfile>path/to/file.sh</scriptfile>
+      </command>${XML_SUFFIX}"""
+    }
+    private static String getJobXmlScriptURL(String url,Boolean expandTokenInScriptFile){
+        return """${XML_PREFIX}
+      <command>
+        ${expandTokenInScriptFile!=null ? "<expandTokenInScriptFile>${expandTokenInScriptFile}</expandTokenInScriptFile>" : ""
+        }
+        <scriptargs />
+        <scripturl>${url}</scripturl>
+      </command>${XML_SUFFIX}"""
     }
 
-    private static String getJobYaml(Boolean expandTokenInScriptFile){
-        return """- description: a job
+    private static String getJobXmlCommand(String command) {
+        return """${XML_PREFIX}
+      <command>
+        <exec>${command}</exec>
+      </command>${XML_SUFFIX}"""
+    }
+
+    private static String getJobXmlScript() {
+        return """${XML_PREFIX}
+      <command>
+        <scriptargs />
+        <script>some script</script>
+      </command>${XML_SUFFIX}"""
+    }
+
+    private static String getJobXmlScriptMultiline(String data) {
+        return """${XML_PREFIX}
+      <command>
+        <scriptargs />
+        <script><![CDATA[${data}]]></script>
+      </command>${XML_SUFFIX}"""
+    }
+
+    static final String YAML_PREFIX = '''- description: a job
   executionEnabled: true
   group: some/where
   id: IDSUB
@@ -115,25 +302,74 @@ class RundeckJobDefinitionManagerSpec extends Specification implements DataTest 
   name: blue
   nodeFilterEditable: false
   schedule:
-    month: '*'
+    month: '*\'
     time:
-      hour: '0'
-      minute: '0'
-      seconds: '0'
+      hour: '0\'
+      minute: '0\'
+      seconds: '0\'
     weekday:
-      day: '*'
-    year: '*'
+      day: '*\'
+    year: '*\'
   scheduleEnabled: true
   sequence:
-    commands:
+    commands:'''
+
+    static final String YAML_SUFFIX = '''    keepgoing: true
+    strategy: node-first
+'''
+
+    private static String getJobYamlScriptfile(Boolean expandTokenInScriptFile){
+        return """${YAML_PREFIX}
     ${expandTokenInScriptFile!=null ? """- expandTokenInScriptFile: ${expandTokenInScriptFile}
       scriptfile: path/to/file.sh""" : "- scriptfile: path/to/file.sh"}
-    keepgoing: true
-    strategy: node-first
+${YAML_SUFFIX}"""
+    }
+
+    private static String getJobYamlScriptURL(String url, Boolean expandTokenInScriptFile){
+        return """${YAML_PREFIX}
+    ${expandTokenInScriptFile!=null ? """- expandTokenInScriptFile: ${expandTokenInScriptFile}
+      scripturl: """+url : "- scripturl: "+url}
+${YAML_SUFFIX}"""
+    }
+
+    private static String getJobYamlCommand(String command) {
+        return """${YAML_PREFIX}
+      - exec: ${command}
+${YAML_SUFFIX}
 """
     }
-    private static String getJobJson(Boolean expandTokenInScriptFile){
-        return """[
+
+    private static String getJobYamlScript() {
+        return """${YAML_PREFIX}
+      - script: some script
+${YAML_SUFFIX}
+"""
+    }
+
+    private static String getJobYamlScriptMulti(String script) {
+        def sigil = ""
+        if (hasMultiNewlineEnding(script)) {
+            //2+ newlines at the end
+            sigil = "+"
+        } else if (script.endsWith("\n")) {
+            //1 newline at the end
+        } else {
+            sigil = "-"
+            //no newlines at the end, insert one to correctly format
+            script = script + "\n"
+        }
+
+        return """${YAML_PREFIX}
+      - script: |${sigil}
+          ${script}${YAML_SUFFIX}
+"""
+    }
+
+    static boolean hasMultiNewlineEnding(String script) {
+        script.matches('(?s)^.*\\n{2,}$')
+    }
+
+    static final String JSON_PREFIX = '''[
   {
     "description": "a job",
     "executionEnabled": true,
@@ -156,15 +392,48 @@ class RundeckJobDefinitionManagerSpec extends Specification implements DataTest 
     },
     "scheduleEnabled": true,
     "sequence": {
-      "commands": [{
-      """+(expandTokenInScriptFile!=null?"""\"expandTokenInScriptFile\": ${expandTokenInScriptFile},""":"")+"""
-        "scriptfile": "path/to/file.sh"
-      }],
+      "commands": '''
+
+    static final String JSON_SUFFIX = ''',
       "keepgoing": true,
       "strategy": "node-first"
     }
   }
-]
+]'''
+
+    private static String getJobJsonScriptfile(Boolean expandTokenInScriptFile){
+        return """${JSON_PREFIX}[{
+      """+(expandTokenInScriptFile!=null?"""\"expandTokenInScriptFile\": ${expandTokenInScriptFile},""":"")+"""
+        "scriptfile": "path/to/file.sh"
+      }]${JSON_SUFFIX}
+"""
+    }
+    private static String getJobJsonScriptURL(String url, Boolean expandTokenInScriptFile){
+        return """${JSON_PREFIX}[{
+      """+(expandTokenInScriptFile!=null?"""\"expandTokenInScriptFile\": ${expandTokenInScriptFile},""":"")+"""
+        "scripturl": "${url}"
+      }]${JSON_SUFFIX}
+"""
+    }
+
+    private static String getJobJsonCommand(String command) {
+        return """${JSON_PREFIX}[{
+        "exec": "${command}"
+      }]${JSON_SUFFIX}
+"""
+    }
+
+    private static String getJobJsonScript() {
+        return """${JSON_PREFIX}[{
+        "script": "some script"
+      }]${JSON_SUFFIX}
+"""
+    }
+
+    private static String getJobJsonScriptMulti(String script) {
+        return """${JSON_PREFIX}[{
+        "script": "${script}"
+      }]${JSON_SUFFIX}
 """
     }
 }
