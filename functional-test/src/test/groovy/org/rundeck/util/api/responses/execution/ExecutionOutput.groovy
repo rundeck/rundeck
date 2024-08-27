bd1ddc77f2d46299b821b0c8e5704a107fc23522
Charlie Crawford <ccrawford@pagerduty.com>
2024-08-27 13:42:22 -0400

    Removes unneeded test.

--- functional-test/src/test/groovy/org/rundeck/util/api/responses/execution/ExecutionOutput.groovy
+++ functional-test/src/test/groovy/org/rundeck/util/api/responses/execution/ExecutionOutput.groovy
@@ -2,7 +2,6 @@ package org.rundeck.util.api.responses.execution
 
 import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import com.fasterxml.jackson.annotation.JsonProperty
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 class LogEntry {
@@ -32,47 +31,4 @@ class ExecutionOutput {
     String serverNodeUUID
     boolean compacted
     List<LogEntry> entries
-}
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-class ExecutionInfo {
-    String id;
-    String href;
-    String permalink;
-    String status;
-    String project;
-    String executionType;
-    String user;
-
-    @JsonProperty("date-started")
-    DateInfo dateStarted;
-    @JsonProperty("date-ended")
-    DateInfo dateEnded;
-
-    JobInfo job;
-    String description;
-    String argstring;
-    String serverUUID;
-    List<String> successfulNodes;
-}
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-class DateInfo {
-    long unixtime;
-
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
-    String date;
-}
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-class JobInfo {
-    String id;
-    int averageDuration;
-    String name;
-    String group;
-    String project;
-    String description;
-    Map<String, String> options;
-    String href;
-    String permalink;
 }
