3c62cb413d633321a1ac146d09e91df3dfd19daf
carlosrfranco <carloseduardo.rorizfranco@gmail.com>
2024-10-14 19:20:01 -0300

    Fix test

--- rundeckapp/src/test/groovy/rundeck/interceptors/AuditInterceptorSpec.groovy
+++ rundeckapp/src/test/groovy/rundeck/interceptors/AuditInterceptorSpec.groovy
@@ -1,12 +1,18 @@
 package rundeck.interceptors
 
 import grails.testing.web.interceptor.InterceptorUnitTest
+import rundeck.services.ConfigurationService
 import spock.lang.Specification
 
 class AuditInterceptorSpec  extends Specification implements InterceptorUnitTest<AuditInterceptor> {
     def "Skip AuditInterceptor if requesting fiveHundred action on error page"() {
         given:
         withRequest(controller: 'error', action: 'fiveHundred')
+        defineBeans {
+            configurationService(ConfigurationService) {
+                grailsApplication = grailsApplication
+            }
+        }
         when: "The interceptor check if request matches the interceptor"
 
         def result = interceptor.doesMatch()
