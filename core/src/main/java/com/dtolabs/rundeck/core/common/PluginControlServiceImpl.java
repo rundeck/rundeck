b0fcdd48531a65a9f2be649cc1126b84be59c5a0
Charlie <104772503+ChuckCrawford@users.noreply.github.com>
2024-11-11 09:39:26 -0500

    RPL-82: Ensures blocked plugins do not show up in Rundeck (#9432)
    * Cleans up how PluginApiService builds the plugin list.
    * Introduces global plugin blocking to PluginControlService.

--- core/src/main/java/com/dtolabs/rundeck/core/common/PluginControlServiceImpl.java
+++ core/src/main/java/com/dtolabs/rundeck/core/common/PluginControlServiceImpl.java
@@ -1,9 +1,12 @@
 package com.dtolabs.rundeck.core.common;
 
+import com.dtolabs.rundeck.core.plugins.PluginRegistry;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.Predicate;
 
@@ -12,16 +15,30 @@ public class PluginControlServiceImpl implements PluginControlService {
     public static final String DISABLED_PLUGINS = "disabled.plugins";
     private HashSet<String> disabledPlugins;
     private final IFramework framework;
+    private final PluginRegistry pluginRegistry;
     private final String project;
 
-    private PluginControlServiceImpl(IFramework framework, String project) {
+    private PluginControlServiceImpl(IFramework framework, PluginRegistry pluginRegistry, String project) {
         this.framework = framework;
+        this.pluginRegistry = pluginRegistry;
         this.project = project;
     }
 
 
+    /**
+     * Creates a PluginControlService that only checks for disabled plugins at the project level. This version of the
+     * service is deprecated and the PluginRegistry aware version of the service should be preferred.
+     */
+    @Deprecated
     public static PluginControlService forProject(IFramework framework, String project) {
-        return new PluginControlServiceImpl(framework, project);
+        return new PluginControlServiceImpl(framework, null, project);
+    }
+
+    /**
+     * Creates a PluginControlService that checks for disabled plugins at both the global and project levels.
+     */
+    public static PluginControlService forProject(IFramework framework, PluginRegistry pluginRegistry, String project) {
+        return new PluginControlServiceImpl(framework, pluginRegistry, project);
     }
 
     private Set<String> getDisabledPlugins() {
@@ -55,8 +72,20 @@ public class PluginControlServiceImpl implements PluginControlService {
     public Predicate<String> enabledPredicateForService(
         final String serviceName
     ) {
-        Set<String> strings = getDisabledPlugins();
-        return name -> !strings.contains(serviceName + ":" + name);
+        Optional<PluginRegistry> globalRegistry = Optional.ofNullable(pluginRegistry);
+        Set<String> pluginsDisabledByProject = getDisabledPlugins();
+
+        return (String name) -> {
+            boolean blockedByRegistry = globalRegistry
+                    .map(r -> r.isBlockedPlugin(serviceName, name))
+                    .orElse(false);
+
+            if (blockedByRegistry) {
+                return false;
+            }
+
+            return !pluginsDisabledByProject.contains(serviceName + ":" + name);
+        };
     }
 
     /**
@@ -66,6 +95,14 @@ public class PluginControlServiceImpl implements PluginControlService {
      */
     @Override
     public boolean isDisabledPlugin(String pluginName, final String serviceName) {
+        boolean blockedByRegistry = Optional.ofNullable(pluginRegistry)
+                .map(r -> r.isBlockedPlugin(serviceName, pluginName))
+                .orElse(false);
+
+        if (blockedByRegistry) {
+            return true;
+        }
+
         return getDisabledPlugins().contains(serviceName + ":" + pluginName);
     }
 
