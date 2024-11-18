35d80fb7e25cc454b6e29849749aa824cc8aa678
smartinellibenedetti <139791797+smartinellibenedetti@users.noreply.github.com>
2024-11-18 08:38:16 -0700

    update urlMapping

--- grails-migrationwizard/grails-app/controllers/com/rundeck/plugins/migwiz/UrlMappings.groovy
+++ grails-migrationwizard/grails-app/controllers/com/rundeck/plugins/migwiz/UrlMappings.groovy
@@ -3,7 +3,7 @@ package com.rundeck.plugins.migwiz
 class UrlMappings {
 
     static mappings = {
-        "/migWiz"(controller: 'MigWiz')
+        "/migWiz"(controller: 'MigWiz', action: "index")
         "/api/$api_version/priv/migWiz/migrate/$project"(controller: 'MigWiz', action: 'migrateProjectToRBA', method: 'POST')
 
         "/$controller/$action?/$id?(.$format)?"{
