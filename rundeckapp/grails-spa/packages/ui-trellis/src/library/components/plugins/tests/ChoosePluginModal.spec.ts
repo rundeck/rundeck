720b49fc30dfc54c25a4f1636ce6aa3fa645659a
smartinellibenedetti <139791797+smartinellibenedetti@users.noreply.github.com>
2024-11-27 14:32:15 -0700

    add unit tests and small fixes

--- rundeckapp/grails-spa/packages/ui-trellis/src/library/components/plugins/tests/ChoosePluginModal.spec.ts
+++ rundeckapp/grails-spa/packages/ui-trellis/src/library/components/plugins/tests/ChoosePluginModal.spec.ts
@@ -1,53 +1,63 @@
-import { shallowMount, VueWrapper, flushPromises } from "@vue/test-utils";
+import { mount, VueWrapper, flushPromises } from "@vue/test-utils";
 import ChoosePluginModal from "../ChoosePluginModal.vue";
-import { Modal, Btn, Tabs, Tab } from "uiv";
-const mockServices = [
-  {
-    service: "mockService1",
-    providers: [{ name: "provider1" }, { name: "provider2" }],
-  },
-  {
-    service: "mockService2",
-    providers: [{ name: "provider3" }],
-  },
-];
+import PluginSearch from "../PluginSearch.vue";
+import { Popover, Tabs, Tab } from "uiv";
+
 jest.mock("@/library", () => ({
   getRundeckContext: jest.fn(() => ({
     rootStore: {
       plugins: {
         load: jest.fn(),
         getServicePlugins: jest.fn((service) =>
-          service === "mockService1"
-            ? [{ name: "provider1" }, { name: "provider2" }]
-            : [{ name: "provider3" }],
+          service === "WorkflowStep"
+            ? [
+                {
+                  name: "provider1",
+                  title: "plugin1",
+                  description: "done by John Doe",
+                  isHighlighted: true,
+                  highlightedOrder: 0,
+                },
+                {
+                  name: "provider2",
+                  title: "plugin2",
+                  description: "done by Jane Doe",
+                  isHighlighted: false,
+                  highlightedOrder: 0,
+                },
+              ]
+            : [
+                {
+                  name: "provider3",
+                  title: "plugin3",
+                  description: "run a script locally",
+                  isHighlighted: false,
+                },
+              ],
         ),
       },
     },
   })),
 }));
+
 const createWrapper = async (props = {}): Promise<VueWrapper<any>> => {
-  const wrapper = shallowMount(ChoosePluginModal, {
+  const wrapper = mount(ChoosePluginModal, {
     props: {
       title: "Test Title",
-      services: mockServices.map((s) => s.service),
+      services: ["WorkflowStep", "mockService2"],
       modelValue: true,
       tabNames: ["Service", "Provider"],
       ...props,
     },
     global: {
-      components: { Modal, Btn, Tabs, Tab },
-      stubs: {
-        Modal: false,
-        Btn: false,
-        Tabs: false,
-        Tab: false,
-      },
+      components: { Tabs, Tab, Popover },
     },
     attachTo: document.body,
   });
   await flushPromises();
   return wrapper;
 };
+
 describe("ChoosePluginModal", () => {
   afterEach(() => {
     jest.clearAllMocks();
@@ -55,25 +65,54 @@ describe("ChoosePluginModal", () => {
 
   it('emits "selected" event with correct data when a provider button is clicked', async () => {
     const wrapper = await createWrapper();
-    await flushPromises();
-    const providerButtons = wrapper.findAll('[data-testid="provider-button"]');
-    expect(providerButtons.length).toBe(3);
-    await providerButtons[0].trigger("click");
-    const emittedEvents = wrapper.emitted("selected");
-    expect(emittedEvents).toBeTruthy();
-    expect(emittedEvents?.[0]?.[0]).toEqual({
-      provider: "provider1",
-      service: "mockService1",
-    });
+    await wrapper.find('[data-test="provider-button"]').trigger("click");
+    expect(wrapper.emitted("selected")[0]).toEqual([
+      {
+        provider: "provider1",
+        service: "WorkflowStep",
+      },
+    ]);
   });
+
   it('emits "cancel" event when cancel button is clicked', async () => {
     const wrapper = await createWrapper();
-    await flushPromises();
     const cancelButton = wrapper.find('[data-testid="cancel-button"]');
-    expect(wrapper.text()).toContain("Cancel");
     await cancelButton.trigger("click");
-    const emittedEvents = wrapper.emitted("cancel");
-    expect(emittedEvents).toBeTruthy();
-    expect(emittedEvents?.length).toBe(1);
+    expect(wrapper.emitted("cancel")).toBeTruthy();
+  });
+
+  it("renders a list when there is a single service", async () => {
+    const wrapper = await createWrapper({ services: ["WorkflowStep"] });
+    expect(wrapper.findComponent(Tabs).exists()).toBeFalsy();
+    expect(wrapper.find('[data-testid="list-view"]').exists()).toBeTruthy();
+  });
+
+  it("renders an input for search when showSearch is true and correctly handles searches", async () => {
+    const wrapper = await createWrapper({ showSearch: true });
+    const searchComponent = wrapper.findComponent(PluginSearch);
+    expect(searchComponent.exists()).toBeTruthy();
+    searchComponent.vm.$emit("search", "plugin2");
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.findAll("[data-test='provider-button']").length).toBe(1);
+    expect(wrapper.find("[data-test='provider-button']").text()).toBe(
+      "plugin2 - done by Jane Doe",
+    );
+    // when user erases the search query, reset results back to original
+    searchComponent.vm.$emit("search", "");
+    await wrapper.vm.$nextTick();
+
+    const tabsComponent = wrapper.findComponent(Tabs);
+    const tabTitles = tabsComponent.findAll('[role="tab"]');
+    expect(tabTitles[0].text()).toBe("Service (2)");
+    expect(tabTitles[1].text()).toBe("Provider (1)");
+    expect(wrapper.findAll("[data-test='provider-button']").length).toBe(3);
+  });
+
+  it("renders dividers if showDividers is true and dividerIndex for a service is different than 0", async () => {
+    const wrapper = await createWrapper({ showDivider: true });
+    const divider = wrapper.findAll('[data-testid="divider"]');
+    expect(divider.length).toBe(1);
+    expect(divider[0].text()).toBe("workflow.step.plugin.plural");
   });
 });
