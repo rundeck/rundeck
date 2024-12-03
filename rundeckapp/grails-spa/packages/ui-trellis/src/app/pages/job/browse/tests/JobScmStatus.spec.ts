faea24c92dd923d31f4787aafbd7d5869c7a580e
smartinellibenedetti <139791797+smartinellibenedetti@users.noreply.github.com>
2024-12-03 09:58:50 -0700

    clean up test files and fix 2 broken tests

--- rundeckapp/grails-spa/packages/ui-trellis/src/app/pages/job/browse/tests/JobScmStatus.spec.ts
+++ rundeckapp/grails-spa/packages/ui-trellis/src/app/pages/job/browse/tests/JobScmStatus.spec.ts
@@ -10,12 +10,6 @@ jest.mock("@/library/rundeckService.ts", () => ({
 const mountJobScmStatus = async (props: any): Promise<VueWrapper<any>> => {
   const wrapper = mount(JobScmStatus, {
     props,
-    global: {
-      mocks: {
-        $t: jest.fn().mockImplementation((msg) => msg),
-        $tc: jest.fn().mockImplementation((msg) => msg),
-      },
-    },
   });
 
   // Wait for the next Vue tick to allow for asynchronous rendering
@@ -28,14 +22,14 @@ describe("JobScmStatus", () => {
   it("no content if there is no scm status", async () => {
     const wrapper = await mountJobScmStatus({ itemData: {} });
 
-    let detail = wrapper.findAll("*");
+    const detail = wrapper.findAll("*");
     expect(detail.length).toBe(0);
     expect(wrapper.text()).toEqual("");
   });
   it("spinner icon shown if loading prop is true", async () => {
     const wrapper = await mountJobScmStatus({ itemData: {}, loading: true });
 
-    let detail = wrapper.findAll("span > i");
+    const detail = wrapper.findAll("span > i");
     expect(detail.length).toBe(1);
     expect(detail[0].classes()).toContain("fas");
     expect(detail[0].classes()).toContain("fa-spinner");
