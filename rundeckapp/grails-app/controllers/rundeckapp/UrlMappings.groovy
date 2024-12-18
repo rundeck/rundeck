c28933ace4b0e5225c89ad727b3349c136fa1fb2
mrdubr <mr.dubr@gmail.com>
2024-12-18 16:18:52 -0500

    Add /authorizations/application/* and /authorizations/project/* API endpoints

--- rundeckapp/grails-app/controllers/rundeckapp/UrlMappings.groovy
+++ rundeckapp/grails-app/controllers/rundeckapp/UrlMappings.groovy
@@ -251,14 +251,22 @@ class UrlMappings {
             }
         }
 
-
-
         "/api/$api_version/metrics/$name**?"(controller: 'api', action: 'apiMetrics')
 
         "/api/$api_version/plugin/list"(controller: 'plugin', action: 'listPlugins')
         "/api/$api_version/plugin/detail/$service/$provider"(controller: 'plugin', action: 'apiPluginDetail')
 
         "/api/$api_version"(controller: 'api', action: 'info')
+
+        // Project context
+        "/api/$api_version/authorizations/project/$project/job/$specifier"(controller:'authorizations',action:'projectContextAuthorizationsForJob')
+        "/api/$api_version/authorizations/project/$project/$type/$specifier"(controller:'authorizations',action:'projectContextAuthorizationsForTypeWithSpecifier')
+        "/api/$api_version/authorizations/project/$project/$kind"(controller:'authorizations',action:'projectContextAuthorizationsForResourceKind')
+
+        // Application context
+        "/api/$api_version/authorizations/application/$type/$specifier"(controller:'authorizations',action:'appContextAuthorizationsForTypeWithSpecifier')
+        "/api/$api_version/authorizations/application/$kind"(controller:'authorizations',action:'appContextAuthorizationsForResourceKind')
+
         //catchall
         "/api/$api_version/$other/$extra**?"(controller: 'api', action: 'invalid')
 
