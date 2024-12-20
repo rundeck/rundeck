069162863b67df5dd4e480b8ee10ac44391cfa3e
Jaya Singh <jaya@catalytic.com>
2024-12-20 11:11:37 -0600

    added required props

--- rundeckapp/grails-spa/packages/ui-trellis/src/library/components/primeVue/PtAutoComplete/PtAutoComplete.vue
+++ rundeckapp/grails-spa/packages/ui-trellis/src/library/components/primeVue/PtAutoComplete/PtAutoComplete.vue
@@ -1,14 +1,18 @@
 <template>
   <AutoComplete
-    v-model="value"
-    :suggestions="suggestions"
-    @complete="search"
+      v-model="value"
+      :suggestions="suggestions"
+      :optionLabel="optionLabel"
+      :name="name"
+      @complete="onComplete"
+      @change="onChange"
+      @input="updateValue"
   ></AutoComplete>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import AutoComplete, { AutoCompleteCompleteEvent } from "primevue/autocomplete";
+import AutoComplete, { AutoCompleteCompleteEvent, AutoCompleteChangeEvent } from "primevue/autocomplete";
 
 export default defineComponent({
   name: "PtAutoComplete",
@@ -23,11 +27,23 @@ export default defineComponent({
       type: Array,
       default: () => [],
     },
+    defaultValue:{
+      type: String,
+      default: "",
+    },
+    name: {
+      type: String,
+      default: "",
+    },
+    optionLabel: {
+      type: String,
+      default: "label",
+    },
   },
-  emits: ["search"],
+  emits: ["update:modelValue","onChange","onComplete","search"],
   data() {
     return {
-      value: this.modelValue,
+      value: this.modelValue || this.defaultValue,
     };
   },
   watch: {
@@ -36,9 +52,16 @@ export default defineComponent({
     },
   },
   methods: {
-    search(event: AutoCompleteCompleteEvent) {
-      this.$emit("search", event);
+    onComplete(event: AutoCompleteCompleteEvent) {
+      this.$emit("onComplete", event);
     },
+    onChange(event: AutoCompleteChangeEvent) {
+      this.$emit("onChange", event);
+    },
+    updateValue() {
+      this.$emit("update:modelValue", this.value);
+    },
+
   },
 });
 </script>
@@ -46,5 +69,7 @@ export default defineComponent({
 <style lang="scss">
 .p-inputtext {
   background: var(--color-white);
+  //border: 1px solid var(--color-gray-500);
+  //color: var(--color-gray-900);
 }
 </style>
