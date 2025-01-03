61debf670fabd2fc3172be4260b0b032c2d04835
mrdubr <mr.dubr@gmail.com>
2025-01-03 16:33:47 -0500

    snyk convert to build step

--- scripts/circleci/build-functions.sh
+++ scripts/circleci/build-functions.sh
@@ -57,12 +57,6 @@ rundeck_verify_build() {
         verifyBuild
 }
 
-rundeck_assemble_build() {
-    ./gradlew ${GRADLE_BASE_OPTS} \
-        -Penvironment="${ENV}" \
-        assemble
-}
-
 rundeck_gradle_functional_tests() {
     TEST_IMAGE=${TEST_IMAGE:-} ./gradlew :functional-test:${GRADLE_TASK} -Penvironment="${ENV}" -PtestFiles="${TEST_FILES}" ${GRADLE_BUILD_OPTS} --info
 }
