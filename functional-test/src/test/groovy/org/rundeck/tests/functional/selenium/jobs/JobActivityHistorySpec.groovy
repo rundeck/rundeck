be9cf67f5296abdd233a9873955ff1a584313947
Jaya Singh <jaya@catalytic.com>
2025-01-03 15:59:35 -0600

    cleaned tests

--- functional-test/src/test/groovy/org/rundeck/tests/functional/selenium/jobs/JobActivityHistorySpec.groovy
+++ functional-test/src/test/groovy/org/rundeck/tests/functional/selenium/jobs/JobActivityHistorySpec.groovy
@@ -1,6 +1,6 @@
 
 package org.rundeck.tests.functional.selenium.jobs
-import org.openqa.selenium.By
+
 import org.openqa.selenium.support.ui.WebDriverWait
 import org.rundeck.util.annotations.SeleniumCoreTest
 import org.rundeck.util.container.SeleniumBase
@@ -24,7 +24,7 @@ class JobActivityHistorySpec extends SeleniumBase {
      * Setup the test environment.
      */
     def setup() {
-        // Set up WebDriverWait to wait up to 40 seconds for elements that take time to appear or change
+        // Set up WebDriverWait to wait up to 30 seconds for elements that take time to appear or change
         wait = new WebDriverWait(getDriver(), Duration.ofSeconds(30))
         (go LoginPage).login(TEST_USER, TEST_PASS)
         def jobDefinition = JobUtils.generateScheduledExecutionXml("TestJobActivityHistory")
@@ -42,12 +42,14 @@ class JobActivityHistorySpec extends SeleniumBase {
         when: "Navigate to the Job List Page"
         def activityPage = go ActivityPage, SELENIUM_BASIC_PROJECT
         activityPage.loadActivityPageForProject(SELENIUM_BASIC_PROJECT)
+
         then: "Validate job execution is listed in Activity History"
         activityPage.validatePage()
         wait.until {
             def activityList = activityPage.getActivityRows()
             !activityList.isEmpty()
         }
+
         def firstActivityRow = activityPage.getActivityRows().get(0)
         def statusIcon = ExecutionShowPage.getActivityExecStatusIcon(firstActivityRow)
         wait.until {
@@ -59,33 +61,17 @@ class JobActivityHistorySpec extends SeleniumBase {
     def "validate activity history from Job List Page Saved Filters"() {
         when: "Navigate to the Job List Page and apply a saved filter"
         def jobListPage = go JobListPage, SELENIUM_BASIC_PROJECT
-        println "Navigated to Job List Page"
-
         jobListPage.clickAnyTimeButton()
-        println "Clicked 'AnyTime' button"
-
-        jobListPage.clickLastWeekButton()
-        println "Clicked 'Last Week' button"
-
-        jobListPage.clickSaveFilterButton()
-        println "Clicked 'Save Filter' button"
-
-        jobListPage.enterFilterName("testFilter")
-        println "Entered filter name 'testFilter'"
-
-        jobListPage.confirmFilterSave()
-        println "Confirmed filter save"
-
-        jobListPage.openFilterDropdown()
-        println "Opened filter dropdown"
-
-        jobListPage.selectSavedFilter()
-        println "Selected saved filter"
+                .clickLastWeekButton()
+                .clickSaveFilterButton()
+                .enterFilterName("testFilter")
+                .confirmFilterSave()
+                .openFilterDropdown()
+                .selectSavedFilter()
 
         then: "Wait for the job to complete and validate the saved filter is applied"
         wait.until {
             def status = jobListPage.getFirstRowStatus()
-            println "First row status: ${status}"
             status.equalsIgnoreCase('SUCCEEDED')
         }
 
@@ -100,11 +86,12 @@ class JobActivityHistorySpec extends SeleniumBase {
     def "review job activity history from Activity Page"() {
         when: "Navigate to the Activity Page"
         def activityPage = go ActivityPage, SELENIUM_BASIC_PROJECT
+        activityPage.validatePage()
         activityPage.loadActivityPageForProject(SELENIUM_BASIC_PROJECT)
+
         then: "Validate job execution is listed in Activity History"
         activityPage.validatePage()
         def activityList = activityPage.getActivityRows()
         activityPage.validateFirstActivityRow(activityList)
-
     }
 }
