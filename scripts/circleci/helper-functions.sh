fdf2c9af2f837b6543079fda1a394e2c4bbbe38d
mrdubr <mr.dubr@gmail.com>
2025-01-07 15:34:01 -0500

    added monitoring

--- scripts/circleci/helper-functions.sh
+++ scripts/circleci/helper-functions.sh
@@ -136,7 +136,7 @@ snyk_tests() {
     find "${RUNDECK_WAR_DIR}" -name '*.war' -exec jar xvf \{\} \;
 
     # Execute snyk tests
-#    snyk monitor --all-projects --detection-depth=10 --scan-all-unmanaged # run monitor first to push results to webui
+    snyk monitor --detection-depth=10 --scan-all-unmanaged # run monitor first to push results to webui
     snyk test --severity-threshold=low --detection-depth=10 --scan-all-unmanaged
 
      # Just in case current path is important to the subsequent steps
