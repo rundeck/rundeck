cb4834f58a0db73d9d52e544dcf9fed1a8d4e74e
Alexander Grachtchouk <agrachtchouk@pagerduty.com>
2025-01-21 14:00:46 -0500

    Add snyk security scanning
    Add the Snyk security scan to the build pipeline. To avoid adding additional time to the build, the snyk scan is done in parallel with the fat war build.

--- scripts/circleci/build-functions.sh
+++ scripts/circleci/build-functions.sh
@@ -12,6 +12,17 @@ rundeck_war_build() {
     ./gradlew -Penvironment="${ENV}" ${GRADLE_BUILD_OPTS} publishToMavenLocal build -x check
 }
 
+rundeck_assemble_build() {
+    echo "== Versions =="
+    java -version
+    echo "NPM=$(npm -version)"
+    echo "Node=$(node --version)"
+    echo "Groovy=$(groovy --version)"
+
+    echo "== Assemble build =="
+    ./gradlew -Penvironment="${ENV}" ${GRADLE_BUILD_OPTS} rundeckapp:assemble
+}
+
 rundeck_gradle_tests() {
     ./gradlew -Penvironment="${ENV}" ${GRADLE_BUILD_OPTS} check
 }
