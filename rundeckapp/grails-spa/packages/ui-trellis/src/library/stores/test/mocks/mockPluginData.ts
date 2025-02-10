05f5fa1c988caf22f2b5f07bf10aa4bd3dd2c023
Greg Schueler <greg.schueler@gmail.com>
2025-02-10 10:47:41 -0800

    update tests for Plugins store

--- rundeckapp/grails-spa/packages/ui-trellis/src/library/stores/test/mocks/mockPluginData.ts
+++ rundeckapp/grails-spa/packages/ui-trellis/src/library/stores/test/mocks/mockPluginData.ts
@@ -1,6 +1,100 @@
 // @ts-ignore
 import { Plugin, ServiceType } from "../../Plugins";
 
+export const mockPluginDetail:any = {
+  "title": "Script",
+  "targetHostCompatibility": "all",
+  "ver": null,
+  "id": "21a0270b7f66",
+  "providerMetadata": {
+    "faicon": "file-alt"
+  },
+  "dynamicProps": null,
+  "thirdPartyDependencies": null,
+  "name": "script",
+  "desc": null,
+  "iconUrl": null,
+  "projectMapping": {},
+  "vueConfigComponent": null,
+  "props": [
+    {
+      "allowed": [
+        "resourcexml",
+        "resourcejson",
+        "resourceyaml"
+      ],
+      "defaultValue": null,
+      "desc": "Resources document format that the script will produce",
+      "name": "format",
+      "options": null,
+      "required": true,
+      "scope": null,
+      "selectLabels": null,
+      "staticTextDefaultValue": "",
+      "title": "Resource Format",
+      "type": "FreeSelect"
+    },
+    {
+      "allowed": null,
+      "defaultValue": null,
+      "desc": "Path to script file to execute",
+      "name": "file",
+      "options": null,
+      "required": true,
+      "scope": null,
+      "selectLabels": null,
+      "staticTextDefaultValue": "",
+      "title": "Script File Path",
+      "type": "String"
+    },
+    {
+      "allowed": null,
+      "defaultValue": null,
+      "desc": "Command interpreter to use (optional)",
+      "name": "interpreter",
+      "options": null,
+      "required": false,
+      "scope": null,
+      "selectLabels": null,
+      "staticTextDefaultValue": "",
+      "title": "Interpreter",
+      "type": "String"
+    },
+    {
+      "allowed": null,
+      "defaultValue": null,
+      "desc": "Arguments to pass to the script (optional)",
+      "name": "args",
+      "options": null,
+      "required": false,
+      "scope": null,
+      "selectLabels": null,
+      "staticTextDefaultValue": "",
+      "title": "Arguments",
+      "type": "String"
+    },
+    {
+      "allowed": null,
+      "defaultValue": "false",
+      "desc": "If true, pass script file and args as a single argument to interpreter, otherwise, pass as multiple arguments",
+      "name": "argsQuoted",
+      "options": null,
+      "required": false,
+      "scope": null,
+      "selectLabels": null,
+      "staticTextDefaultValue": "false",
+      "title": "Quote Interpreter Args",
+      "type": "Boolean"
+    }
+  ],
+  "rundeckCompatibilityVersion": "unspecified",
+  "license": "unspecified",
+  "pluginVersion": "5.10.0-SNAPSHOT",
+  "description": "Run a script to produce resource model data",
+  "sourceLink": null,
+  "fwkMapping": {},
+  "dynamicDefaults": null
+}
 export const mockWorkflowStepPlugins: Plugin[] = [
   {
     id: "f39ba031afe7",
