c442ff7d8d91d0920616e2868dbb388c64ec23b2
smartinellibenedetti <139791797+smartinellibenedetti@users.noreply.github.com>
2025-02-12 14:44:21 -0700

    fix alphaUI test and reorganize tests for workflow

--- functional-test/src/test/groovy/org/rundeck/tests/functional/selenium/alphaUi/jobs/JobsSpec.groovy
+++ functional-test/src/test/groovy/org/rundeck/tests/functional/selenium/alphaUi/jobs/JobsSpec.groovy
@@ -3,6 +3,7 @@ package org.rundeck.tests.functional.selenium.alphaUi.jobs
 import org.openqa.selenium.By
 import org.rundeck.util.annotations.AlphaUiSeleniumCoreTest
 import org.rundeck.util.gui.pages.jobs.JobCreatePage
+import org.rundeck.util.gui.pages.jobs.JobShowPage
 import org.rundeck.util.gui.pages.jobs.JobTab
 import org.rundeck.util.container.SeleniumBase
 import org.rundeck.util.gui.pages.login.LoginPage
@@ -27,8 +28,79 @@ class JobsSpec extends SeleniumBase {
         jobCreatePage.go()
         then:
         jobCreatePage.tab JobTab.WORKFLOW click()
-        jobCreatePage.waitForTextToBePresentBySelector(By.xpath("//section[@id='workflowContent2']//div[contains(@class, 'control-label')]"), "Workflow",60)
+        jobCreatePage.waitForTextToBePresentBySelector(By.xpath("//section[@id='workflowContent']//div[contains(@class, 'control-label')]"), "Workflow",60)
         expect:
         jobCreatePage.workflowAlphaUiContainer.isDisplayed()
+    }
+
+    def "Duplicate option create form next ui"() {
+        when:
+        def jobCreatePage = page JobCreatePage, SELENIUM_BASIC_PROJECT
+        jobCreatePage.nextUi=true
+        jobCreatePage.go()
+        def optName = 'test'
+        jobCreatePage.fillBasicJob specificationContext.currentIteration.name+" ${nextUi ? "next ui" : "old ui"}"
+        jobCreatePage.optionButton.click()
+        jobCreatePage.optionNameNew() sendKeys optName
+        jobCreatePage.waitForElementVisible jobCreatePage.separatorOption
+        jobCreatePage.executeScript "window.location.hash = '#workflowKeepGoingFail'"
+        jobCreatePage.saveOptionButton.click()
+        jobCreatePage.waitFotOptLi 0
+
+        jobCreatePage.duplicateButton( optName, 0) click()
+
+
+        then: "create form is shown"
+        jobCreatePage.optionNameNew() displayed
+        jobCreatePage.optionNameNew().getAttribute('value') == optName + '_copy'
+        jobCreatePage.saveOptionButton.displayed
+        when:
+        jobCreatePage.executeScript "arguments[0].scrollIntoView(true);", jobCreatePage.saveOptionButton
+        jobCreatePage.saveOptionButton.click()
+        jobCreatePage.waitFotOptLi 1
+
+        then:
+        jobCreatePage.optionNameSaved 0 getText() equals optName
+        jobCreatePage.optionNameSaved 1 getText() equals optName + '_copy'
+
+        where:
+        nextUi = true
+    }
+
+    def "Create option form next ui"() {
+        when:
+        def jobCreatePage = page JobCreatePage, SELENIUM_BASIC_PROJECT
+        jobCreatePage.nextUi=true
+        jobCreatePage.go()
+        def optName = 'test'
+        jobCreatePage.fillBasicJob specificationContext.currentIteration.name+" ${nextUi ? "next ui" : "old ui"}"
+        jobCreatePage.optionButton.click()
+
+        then: "create form is shown"
+        jobCreatePage.optionNameNew() displayed
+        jobCreatePage.saveOptionButton.displayed
+        where:
+        nextUi = true
+    }
+
+    def "create valid job basic options"() {
+        when:
+        def jobCreatePage = page JobCreatePage, SELENIUM_BASIC_PROJECT
+        jobCreatePage.nextUi=true
+        jobCreatePage.go()
+        def jobShowPage = page JobShowPage
+        def optionName = 'seleniumOption1'
+        then:
+        jobCreatePage.fillBasicJob specificationContext.currentIteration.name+" ${nextUi ? "next ui" : "old ui"}"
+        jobCreatePage.optionButton.click()
+        jobCreatePage.optionNameNew() sendKeys optionName
+        jobCreatePage.executeScript "arguments[0].scrollIntoView(true);", jobCreatePage.saveOptionButton
+        jobCreatePage.saveOptionButton.click()
+        jobCreatePage.waitFotOptLi 0
+        jobCreatePage.createJobButton.click()
+        then:
+        jobCreatePage.waitForUrlToContain('/job/show')
+        jobShowPage.jobLinkTitleLabel.getText().contains('create valid job basic options')
+        jobShowPage.optionInputText(optionName) != null
     }
 }
--- functional-test/src/test/groovy/org/rundeck/tests/functional/selenium/jobs/JobsSpec.groovy
+++ functional-test/src/test/groovy/org/rundeck/tests/functional/selenium/jobs/JobsSpec.groovy
@@ -128,56 +128,6 @@ class JobsSpec extends SeleniumBase {
             jobCreatePage.optionNameSaved 2 getText() equals optName + '_2'
     }
 
-    def "Duplicate option create form next ui"() {
-        when:
-            def jobCreatePage = page JobCreatePage, SELENIUM_BASIC_PROJECT
-            jobCreatePage.nextUi=true
-            jobCreatePage.go()
-            def optName = 'test'
-            jobCreatePage.fillBasicJob specificationContext.currentIteration.name+" ${nextUi ? "next ui" : "old ui"}"
-            jobCreatePage.optionButton.click()
-            jobCreatePage.optionNameNew() sendKeys optName
-            jobCreatePage.waitForElementVisible jobCreatePage.separatorOption
-            jobCreatePage.executeScript "window.location.hash = '#workflowKeepGoingFail'"
-            jobCreatePage.saveOptionButton.click()
-            jobCreatePage.waitFotOptLi 0
-
-            jobCreatePage.duplicateButton( optName, 0) click()
-
-
-        then: "create form is shown"
-            jobCreatePage.optionNameNew() displayed
-            jobCreatePage.optionNameNew().getAttribute('value') == optName + '_copy'
-            jobCreatePage.saveOptionButton.displayed
-        when:
-            jobCreatePage.executeScript "arguments[0].scrollIntoView(true);", jobCreatePage.saveOptionButton
-            jobCreatePage.saveOptionButton.click()
-            jobCreatePage.waitFotOptLi 1
-
-        then:
-            jobCreatePage.optionNameSaved 0 getText() equals optName
-            jobCreatePage.optionNameSaved 1 getText() equals optName + '_copy'
-
-        where:
-            nextUi = true
-    }
-
-    def "Create option form next ui"() {
-        when:
-            def jobCreatePage = page JobCreatePage, SELENIUM_BASIC_PROJECT
-            jobCreatePage.nextUi=true
-            jobCreatePage.go()
-            def optName = 'test'
-            jobCreatePage.fillBasicJob specificationContext.currentIteration.name+" ${nextUi ? "next ui" : "old ui"}"
-            jobCreatePage.optionButton.click()
-
-        then: "create form is shown"
-            jobCreatePage.optionNameNew() displayed
-            jobCreatePage.saveOptionButton.displayed
-        where:
-            nextUi = true
-    }
-
     def "create job with dispatch to nodes"() {
         when:
             def jobCreatePage = go JobCreatePage, SELENIUM_BASIC_PROJECT
