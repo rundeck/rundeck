332c84983400736e7f05050b36dd4385fa26fbe8
smartinellibenedetti <139791797+smartinellibenedetti@users.noreply.github.com>
2025-07-01 09:44:43 -0600

    Update ExecutionSpec.groovy

--- functional-test/src/test/groovy/org/rundeck/tests/functional/selenium/execution/ExecutionSpec.groovy
+++ functional-test/src/test/groovy/org/rundeck/tests/functional/selenium/execution/ExecutionSpec.groovy
@@ -146,6 +146,7 @@ class ExecutionSpec extends SeleniumBase {
         then:
         executionShowPage.waitForNumberOfElementsToBe executionShowPage.autoCaretBy, 2
         executionShowPage.autoCaret.get(1).click()
+        executionShowPage.waitForElementVisible  executionShowPage.execLogNode
         executionShowPage.execLogNode.isDisplayed()
         executionShowPage.execLogNode.text == "Hello world"
         executionShowPage.execLogGutterEntryAttribute.matches("\\d{2}:\\d{2}:\\d{2}")
