745218a8e625a53e3ea69da53a79882ab9f4edb5
Jesus Osuna <jesus.m.osuna.m@gmail.com>
2025-07-14 16:30:09 -0400

    validate permisions on artifactController

--- grails-repository/grails-app/controllers/repository/ArtifactController.groovy
+++ grails-repository/grails-app/controllers/repository/ArtifactController.groovy
@@ -1,6 +1,36 @@
 package repository
 
+import com.dtolabs.rundeck.core.authorization.AuthContext
+import com.dtolabs.rundeck.core.authorization.AuthContextProcessor
+import grails.converters.JSON
+import groovy.transform.PackageScope
+import org.rundeck.core.auth.AuthConstants
+
 class ArtifactController {
 
-    def index() { }
+    AuthContextProcessor rundeckAuthContextProcessor
+
+
+    def index() {
+        if(!authorized(AuthConstants.RESOURCE_TYPE_PLUGIN, AuthConstants.ACTION_READ)) {
+            specifyUnauthorizedError()
+            return
+        }
+        render(view: "index")
+    }
+
+
+    @PackageScope
+    boolean authorized(Map resourceType = AuthConstants.RESOURCE_TYPE_SYSTEM, String action = AuthConstants.ACTION_ADMIN) {
+        List authorizedActions = [AuthConstants.ACTION_ADMIN, AuthConstants.ACTION_OPS_ADMIN]
+        if(action != AuthConstants.ACTION_ADMIN) authorizedActions.add(action)
+        AuthContext authContext = rundeckAuthContextProcessor.getAuthContextForSubject(session.subject)
+        rundeckAuthContextProcessor.authorizeApplicationResourceAny(authContext, resourceType, authorizedActions)
+    }
+
+    private def specifyUnauthorizedError() {
+        response.setStatus(400)
+        def err = [error: "You are not authorized to perform this action"]
+        render err as JSON
+    }
 }
