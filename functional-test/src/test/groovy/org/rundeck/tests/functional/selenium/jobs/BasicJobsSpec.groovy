652c79179119ac7310c3cd74f837141950709a1e
Jaya Singh <jaya@catalytic.com>
2025-07-21 17:52:36 -0500

    testing the fix

--- functional-test/src/test/groovy/org/rundeck/tests/functional/selenium/jobs/BasicJobsSpec.groovy
+++ functional-test/src/test/groovy/org/rundeck/tests/functional/selenium/jobs/BasicJobsSpec.groovy
@@ -266,9 +266,10 @@ class BasicJobsSpec extends SeleniumBase {
         jobShowPage.dropDownToggle.click()
         jobShowPage.waitForElementToBeClickable jobShowPage.selectAllNodesLink
         jobShowPage.selectAllNodesLink.click()
-        Thread.sleep(3000)
-        jobShowPage.waitForElementToBeClickable jobShowPage.localhostNode
-        jobShowPage.localhostNode.click()
+        Thread.sleep(2500)
+        def localhostElement = driver.findElement(jobShowPage.localhostNodeBy)
+        localhostElement.click()
+        Thread.sleep(2000)
         jobShowPage.waitForElementToBeClickable jobShowPage.nodeFilterArrowIcon
         jobShowPage.nodeFilterArrowIcon.click()
         expect:
