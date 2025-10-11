90b99b62c6bfaa8dbcdd652c6a2457cc70548558
Greg Schueler <greg.schueler@gmail.com>
2025-10-10 18:10:26 -0700

    update job store with current options data when editing

--- rundeckapp/grails-spa/packages/ui-trellis/src/app/pages/job/editor/OptionsEditorSection.vue
+++ rundeckapp/grails-spa/packages/ui-trellis/src/app/pages/job/editor/OptionsEditorSection.vue
@@ -7,8 +7,10 @@
   <json-embed :output-data="updatedData.options" field-name="jobOptionsJson" />
 </template>
 <script lang="ts">
+import { useJobStore } from "@/library/stores/JobsStore";
 import { cloneDeep } from "lodash";
 import * as _ from "lodash";
+import { mapActions } from "pinia";
 import OptionsEditor from "../../../components/job/options/OptionsEditor.vue";
 import JsonEmbed from "./JsonEmbed.vue";
 import { defineComponent } from "vue";
@@ -29,10 +31,13 @@ export default defineComponent({
         options: [],
       },
       subs: {},
+      uuid: "",
     };
   },
   async mounted() {
     if (getRundeckContext() && getRundeckContext().data) {
+      this.uuid = getRundeckContext().data.otherData?.uuid;
+
       this.optionsData = getRundeckContext().data.optionsData;
       this.updatedData = this.optionsData;
       this.subs["job-edit-schedules-changed"] = eventBus.on(
@@ -41,6 +46,7 @@ export default defineComponent({
           this.optionsData.jobWasScheduled = data.scheduled;
         },
       );
+      await this.updateStore();
     }
   },
   async beforeUnmount() {
@@ -50,9 +56,20 @@ export default defineComponent({
     );
   },
   methods: {
-    changed(data) {
+    async updateStore() {
+      await this.setActiveId(this.uuid);
+      await this.updateJobDefinition(
+        {
+          options: this.updatedData.options,
+        },
+        this.uuid,
+      );
+    },
+    async changed(data) {
       if (!_.isEqual(data, this.updatedData.options)) {
         this.updatedData.options = cloneDeep(data);
+
+        await this.updateStore();
         //nb: hook to indicate job was editted, defined in jobedit.js
         //@ts-ignore
         if (
@@ -64,6 +81,7 @@ export default defineComponent({
         }
       }
     },
+    ...mapActions(useJobStore, ["updateJobDefinition", "setActiveId"]),
   },
 });
 </script>
