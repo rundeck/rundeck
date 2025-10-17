53c470230d49ef022ece2e61107ff7de3ef80b14
smartinellibenedetti <139791797+smartinellibenedetti@users.noreply.github.com>
2025-10-17 13:35:12 -0600

    undo changes

--- packaging/test/docker/installcommon/scripts/rd-util.sh
+++ packaging/test/docker/installcommon/scripts/rd-util.sh
@@ -43,7 +43,7 @@ wait_for_start(){
 	        exit 1
 	    }
 	    #tail -n 5 "$LOGFILE"
-	    /etc/init.d/rundeckd status || {
+	    service rundeckd status || {
 	        echo >&2 "FAIL: rundeckd is not running. Exiting."
 		cat "${LOGFILE}"
 	        exit 1
@@ -56,7 +56,7 @@ wait_for_start(){
 }
 entry_start(){
 	if [ "$1" != "-skipstart" ]; then
-		/etc/init.d/rundeckd start
+		service rundeckd start
 		wait_for_start /var/log/rundeck/service.log
 	else
 		shift
--- packaging/test/docker/installcommon/scripts/rpm-tests.sh
+++ packaging/test/docker/installcommon/scripts/rpm-tests.sh
@@ -6,13 +6,13 @@ set -e
 
 test_status(){
 	echo "test_status"
-	/etc/init.d/rundeckd status | grep 'is running' || { echo "FAILED" ; exit 2 ; }
+	service rundeckd status | grep 'is running' || { echo "FAILED" ; exit 2 ; }
 	echo "OK"
 }
 
 test_start_twice(){
 	echo "test_start_twice"
-	/etc/init.d/rundeckd start | grep 'Already started' || { echo "FAILED" ; exit 2 ; }
+	service rundeckd start | grep 'Already started' || { echo "FAILED" ; exit 2 ; }
 
 	echo "OK"
 }
