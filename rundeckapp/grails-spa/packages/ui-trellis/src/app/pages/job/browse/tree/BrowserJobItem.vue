765dc900b49475257f7e92924ebb477aa3e79c35
Greg Schueler <greg.schueler@gmail.com>
2025-10-21 14:34:48 -0700

    fix: long job description in nextui job list

--- rundeckapp/grails-spa/packages/ui-trellis/src/app/pages/job/browse/tree/BrowserJobItem.vue
+++ rundeckapp/grails-spa/packages/ui-trellis/src/app/pages/job/browse/tree/BrowserJobItem.vue
@@ -21,7 +21,8 @@
       v-if="showJobsAsLinks"
       :href="jobLinkHref(job)"
       :data-job-id="job.uuid"
-      class="link-quiet"
+      class="link-quiet job-name"
+      :title="job.jobName"
     >
       {{ job.jobName }}
     </a>
@@ -33,7 +34,11 @@
     >
       {{ job.jobName }}
     </button>
-    <span v-if="job.description" class="text-secondary job-description">
+    <span
+      v-if="job.description"
+      class="text-secondary job-description"
+      :title="job.description"
+    >
       {{ shortDescription }}
     </span>
     <ui-socket
@@ -143,15 +148,34 @@ export default defineComponent({
 </script>
 
 <style scoped lang="scss">
-.job-description {
-  margin-left: var(--spacing-2);
-}
 .job-list-row-item {
   padding: 1px 0 1px 3px;
   border-style: solid;
   border-width: 0 0 3px 0;
   border-color: transparent;
   cursor: pointer;
+  display: flex;
+  align-content: flex-start;
+  flex-wrap: nowrap;
+  gap: var(--spacing-2);
+  .job-name,
+  .job-description {
+    flex: 1 2 auto;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  .job-name {
+    min-width: 300px;
+    flex: 1 1 auto;
+  }
+  .btn.btn-xs {
+    margin-right: 0;
+  }
+  .job-actions {
+    margin-left: auto;
+    flex: 0 0 auto;
+  }
 }
 .hover .job-list-row-item {
   background: var(--background-color-accent-lvl2);
