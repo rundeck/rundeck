67fbd439fa147365608fe231cd331efde79964c5
Greg Schueler <greg.schueler@gmail.com>
2025-11-07 11:12:19 -0800

    Update API version variable to use constant

--- rundeckapp/grails-app/init/rundeckapp/Application.groovy
+++ rundeckapp/grails-app/init/rundeckapp/Application.groovy
@@ -64,7 +64,7 @@ import java.nio.file.Paths
                 @ServerVariable(name = "protocol", defaultValue = "https", allowableValues = ["http", "https"], description = "Protocol (http or https)"),
                 @ServerVariable(name = "host", defaultValue = "localhost", description = "Server hostname or IP address"),
                 @ServerVariable(name = "port", defaultValue = "4440", description = "Server port number"),
-                @ServerVariable(name = "version", defaultValue = "44", description = "API version number")
+                @ServerVariable(name = "version", defaultValue = ApiVersions.API_CURRENT_VERSION_STR, description = "API version number")
             ]
         )
     ],
