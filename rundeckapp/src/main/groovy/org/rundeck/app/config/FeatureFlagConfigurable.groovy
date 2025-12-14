86a84681836bb2a8dc0958badbbc0f16f7b6ab48
smartinellibenedetti <139791797+smartinellibenedetti@users.noreply.github.com>
2025-12-13 22:29:46 -0700

    move flag to core

--- rundeckapp/src/main/groovy/org/rundeck/app/config/FeatureFlagConfigurable.groovy
+++ rundeckapp/src/main/groovy/org/rundeck/app/config/FeatureFlagConfigurable.groovy
@@ -15,17 +15,28 @@ class FeatureFlagConfigurable implements SystemConfigurable {
             "(Beta Feature) Enable support for multiline job options in job definitions and the GUI.",
             'app_admin'
         ),
+        featureConfig(
+            Features.EARLY_ACCESS_JOB_CONDITIONAL,
+            "Enable Job Conditional Step (Experimental)",
+            "(Beta Feature) Enable support for conditional steps in job definitions and the GUI.",
+            'app_admin',
+            'Early Access'
+        ),
         //TODO: include additional feature flags here
     ]
 
     private static SysConfigProp featureConfig(Features feature, String label, String description, String auth) {
+        featureConfig(feature, label, description, auth, "Feature")
+    }
+
+    private static SysConfigProp featureConfig(Features feature, String label, String description, String auth, String category) {
         SystemConfig.builder().with {
             key("rundeck.feature.${feature.propertyName}.enabled")
                 .datatype("Boolean")
                 .label(label)
                 .description(description)
                 .defaultValue("false")
-                .category("Feature")
+                .category(category)
                 .visibility("Advanced")
                 .strata("default")
                 .required(false)
