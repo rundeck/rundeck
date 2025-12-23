40580e2775271f99fdc6e8bcadbfa136b9081f8d
Luis Toledo <luis@variacode.com>
2025-12-23 16:36:27 -0300

    fix issue with date-complete filter

--- rundeckapp/src/main/groovy/com/dtolabs/rundeck/app/support/ExecutionQuery.groovy
+++ rundeckapp/src/main/groovy/com/dtolabs/rundeck/app/support/ExecutionQuery.groovy
@@ -388,7 +388,11 @@ class ExecutionQuery extends ScheduledExecutionQuery implements Validateable{
         // project filter is not applied when the jobUUID filter is set to improve the response time of that case
         if(query.projFilter && !query.jobIdListFilter) {
           eq('project', query.projFilter)
-          isNotNull('dateCompleted')
+
+          //use the filter to optimize the filter by project
+          if(!query.statusFilter){
+              isNotNull('dateCompleted')
+          }
         }
         if (query.userFilter) {
           eq('user', query.userFilter)
