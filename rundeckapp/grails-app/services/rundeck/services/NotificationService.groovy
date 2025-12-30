f1646af14526819160db1541956b7b7d03cea760
Rundeck CI <fdevans@gmail.com>
2025-12-30 13:00:57 -0800

    Fix: Add missing EventBusAware import to NotificationService

--- rundeckapp/grails-app/services/rundeck/services/NotificationService.groovy
+++ rundeckapp/grails-app/services/rundeck/services/NotificationService.groovy
@@ -39,6 +39,7 @@ import com.dtolabs.rundeck.server.plugins.services.NotificationPluginProviderSer
 import grails.async.Promises
 import grails.converters.JSON
 import grails.events.annotation.Subscriber
+import grails.events.bus.EventBusAware
 import grails.gorm.transactions.Transactional
 import grails.util.Holders
 import grails.web.JSONBuilder
