cee1c71dd95a1f959e57347f8f361c1bf9948049
smartinellibenedetti <139791797+smartinellibenedetti@users.noreply.github.com>
2026-01-07 14:14:43 -0700

    initial commit

---
+++ rundeckapp/grails-spa/packages/ui-trellis/src/library/components/primeVue/PtToggleButton/PtToggleButton.spec.ts
@@ -0,0 +1,8 @@
+import PtToggleButton from './PtToggleButton.vue';
+
+describe('PtToggleButton', () => {
+  // TODO: Replace this test with a suite of tests appropriate for the component you are building.
+  it('renders the provided content', () => {
+    return true;
+  });
+});
--- rundeckapp/grails-spa/packages/ui-trellis/src/library/components/primeVue/index.ts
+++ rundeckapp/grails-spa/packages/ui-trellis/src/library/components/primeVue/index.ts
@@ -1,3 +1,4 @@
 import PtAutoComplete from "./PtAutoComplete/PtAutoComplete.vue";
+import PtToggleButton from "./PtToggleButton/PtToggleButton.vue";
 
-export { PtAutoComplete };
+export { PtAutoComplete, PtToggleButton };
