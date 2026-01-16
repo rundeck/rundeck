adbb6f1cd5067fdf3c24a1a06129a52369eac912
Rundeck CI <fdevans@gmail.com>
2026-01-16 11:40:41 -0800

    RUN-4056: Move jointjs workflow designer components to open source
    - Moved WorkflowGraph.vue and supporting files from rundeckpro to ui-trellis
    - Added jointjs, dagre, and graphlib dependencies to ui-trellis package.json
    - Created workflow-designer directory in ui-trellis components/job
    - Moved supporting files: Graph.ts, RuleSetParser.ts, RuleSetLex.ts
    - Moved RulesetStrategyEditor.vue and i18n.ts
    - Moved WorkflowStep (Step.ts) and helper components
    - Added index.ts for easier imports
    - This resolves MPL license concerns by moving jointjs to open source

---
+++ rundeckapp/grails-spa/packages/ui-trellis/src/app/components/job/workflow-designer/index.ts
@@ -0,0 +1,8 @@
+// Export workflow designer components for use in rundeckpro
+export { default as WorkflowGraph } from './WorkflowGraph.vue'
+export { default as RulesetStrategyEditor } from './RulesetStrategyEditor.vue'
+export { default as GeneratedRules } from './GeneratedRules.vue'
+export { default as NodeEditor } from './components/NodeEditor.vue'
+export { WorkflowStep } from './elements/Step'
+export { GNode, NodeDescription, WorkflowGraph as WorkflowGraphClass } from './Graph'
+export { RuleSetParser } from './RuleSetParser'
