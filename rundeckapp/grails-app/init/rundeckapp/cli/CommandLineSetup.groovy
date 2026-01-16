635b18642faff8472fe24625095d669bc58f40a3
Greg Schueler <greg.schueler@gmail.com>
2026-01-16 09:43:19 -0800

    replace commons-cli with picocli

--- rundeckapp/grails-app/init/rundeckapp/cli/CommandLineSetup.groovy
+++ rundeckapp/grails-app/init/rundeckapp/cli/CommandLineSetup.groovy
@@ -16,173 +16,142 @@
 package rundeckapp.cli
 
 import grails.util.Environment
-import org.apache.commons.cli.CommandLine
-import org.apache.commons.cli.CommandLineParser
-import org.apache.commons.cli.GnuParser
-import org.apache.commons.cli.HelpFormatter
-import org.apache.commons.cli.Option
-import org.apache.commons.cli.OptionBuilder
-import org.apache.commons.cli.Options
-import org.apache.commons.cli.ParseException
+import groovy.transform.CompileStatic
 import com.dtolabs.rundeck.core.encrypter.PasswordUtilityEncrypter
 import rundeckapp.Application
 import rundeckapp.init.RundeckInitConfig
-import liquibase.util.StringUtil
+import picocli.CommandLine
+import picocli.CommandLine.Command
+import picocli.CommandLine.Option
 
+import java.util.function.IntConsumer
 
-class CommandLineSetup {
+/**
+ * CommandLineSetup parses and holds command-line options for Rundeck / Runbook Automation server startup.
+ */
+@CompileStatic
+@Command(
+    name = 'rundeck-server',
+    mixinStandardHelpOptions = true,
+    description = [
+        'Run the Rundeck / Runbook Automation server, installing the necessary components if they do not exist.'
+    ]
+)
+class CommandLineSetup implements Runnable {
     private static final String TOOL_ROOT_DIR = "tools"
     //system props for launcher config
-    private static final String SYS_PROP_RUNDECK_LAUNCHER_DEBUG     = "rundeck.launcher.debug";
-    private static final String SYS_PROP_RUNDECK_LAUNCHER_REWRITE   = "rundeck.launcher.rewrite";
-    public static final String SYS_PROP_MIGRATE_ONLY               = "migrate.only";
+    private static final String SYS_PROP_RUNDECK_LAUNCHER_DEBUG     = "rundeck.launcher.debug"
+    private static final String SYS_PROP_RUNDECK_LAUNCHER_REWRITE   = "rundeck.launcher.rewrite"
+    public static final String SYS_PROP_MIGRATE_ONLY               = "migrate.only"
 
-    public static final String FLAG_INSTALLONLY = "installonly";
-    public static final String FLAG_SKIPINSTALL = "skipinstall";
-    public static final String FLAG_ENCRYPT_PWD = "encryptpwd";
-    public static final String FLAG_TEST_AUTH   = "testauth";
+    public static final String FLAG_INSTALLONLY = "installonly"
+    public static final String FLAG_SKIPINSTALL = "skipinstall"
 
     private static final Map<String,PasswordUtilityEncrypter> encrypters = getEncrypters()
 
-    private final Options options = new Options();
+    @Option(names = ['-b', '--basedir'], description = 'The basedir', paramLabel = 'PATH')
+    String baseDir
+
+    @Option(names = ['--serverdir'], description = 'The base directory for the server', paramLabel = 'PATH')
+    String serverBaseDir
+
+    @Option(names = ['-x', '--bindir'], description = 'The install directory for the tools used by users.', paramLabel = 'PATH')
+    String binDir
+
+    @Option(names = ['-s', '--sbindir'], description = 'The install directory for the tools used by administrators.', paramLabel = 'PATH')
+    String sbinDir
+
+    @Option(names = ['-c', '--configdir'], description = 'The location of the configuration.', paramLabel = 'PATH')
+    String configDir
+
+    @Option(names = ['--datadir'], description = 'The location of Rundeck\'s runtime data.', paramLabel = 'PATH')
+    String dataDir
+
+    @Option(names = ['-p', '--projectdir'], description = 'The location of Rundeck\'s project data.', paramLabel = 'PATH')
+    String projectDir
+
+    @Option(names = ['-h', '--help'], usageHelp = true, description = 'Display this message.')
+    boolean helpRequested
+
+    @Option(names = ['-d'], description = 'Show debug information')
+    boolean debug
+
+    @Option(names = ['--skipinstall'], description = 'Skip the extraction of the utilities from the launcher.')
+    boolean skipInstall
+
+    @Option(names = ['--installonly'], description = 'Perform installation only and do not start the server.')
+    boolean installOnly
+
+    @Option(names = ['--testauth'], description = 'Test Jaas authentication configuration.')
+    boolean testAuth
+
+    @Option(names = ['--encryptpwd'], description = 'Encrypt a password for use in a property file using the specified service. Available services: ${COMPLETION-CANDIDATES}', paramLabel = 'ENCRYPTION-SERVICE')
+    String encryptPwdService
+
+    @Option(names = ['-r', '--rollback'], description = 'Down migrate or rollback to previous db versions.', paramLabel = 'TAGNAME')
+    String tag
+
+    @Option(names = ['-m', '--migrate-only'], description = 'Run database migrations then exit.')
+    boolean migrate
+
+    private static final String USAGE_HEADER = "\nRun the rundeck server, installing the " +
+                                                                               "necessary components if they do not exist.\n"
+    private static final String USAGE_FOOTER = "\nhttp://rundeck.org\n"
+
+    void run() {
+        // This method is required by picocli, but not used directly.
+    }
+    IntConsumer exitHandler = System::exit
 
     CommandLineSetup() {
-        Option baseDir =    OptionBuilder.withLongOpt("basedir")
-                                         .hasArg()
-                                         .withDescription("The basedir")
-                                         .withArgName("PATH")
-                                         .create('b');
+    }
 
-        Option serverDir =  OptionBuilder.withLongOpt("serverdir")
-                                         .hasArg()
-                                         .withDescription("The base directory for the server")
-                                         .withArgName("PATH")
-                                         .create();
-
-        Option binDir =     OptionBuilder.withLongOpt("bindir")
-                                         .hasArg()
-                                         .withArgName("PATH")
-                                         .withDescription("The install directory for the tools used by users.")
-                                         .create('x');
-
-        Option sbinDir =    OptionBuilder.withLongOpt("sbindir")
-                                         .hasArg()
-                                         .withArgName("PATH")
-                                         .withDescription("The install directory for the tools used by administrators.")
-                                         .create('s');
-
-        Option configDir =  OptionBuilder.withLongOpt("configdir")
-                                         .hasArg()
-                                         .withArgName("PATH")
-                                         .withDescription("The location of the configuration.")
-                                         .create('c');
-
-        Option dataDir =    OptionBuilder.withLongOpt("datadir")
-                                         .hasArg()
-                                         .withArgName("PATH")
-                                         .withDescription("The location of Rundeck's runtime data.")
-                                         .create();
-        Option projectDir =    OptionBuilder.withLongOpt("projectdir")
-                                            .hasArg()
-                                            .withArgName("PATH")
-                                            .withDescription("The location of Rundeck's project data.")
-                                            .create('p');
-
-        Option help =       OptionBuilder.withLongOpt("help")
-                                         .withDescription("Display this message.")
-                                         .create('h');
-
-        Option debugFlag =  OptionBuilder.withDescription("Show debug information")
-                                         .create('d');
-
-        Option skipInstall = OptionBuilder.withLongOpt(FLAG_SKIPINSTALL)
-                                          .withDescription("Skip the extraction of the utilities from the launcher.")
-                                          .create();
-
-        Option installonly = OptionBuilder.withLongOpt(FLAG_INSTALLONLY)
-                                          .withDescription("Perform installation only and do not start the server.")
-                                          .create();
-
-        Option testauth = OptionBuilder.withLongOpt(FLAG_TEST_AUTH)
-                                          .withDescription("Test Jaas authentication configuration.")
-                                          .create();
-
-        Option encryptpwd = OptionBuilder.withLongOpt(FLAG_ENCRYPT_PWD)
-                                         .withArgName("ENCRYPTION-SERVICE")
-                                         .hasArg()
-                                         .withDescription("Encrypt a password for use in a property file using the specified service. Available services: " + encrypters.values().collect { it.name() }.join(", "))
-                                         .create();
-        Option rollback =   OptionBuilder.withLongOpt("rollback")
-                .hasArg()
-                .withArgName("TAGNAME")
-                .withDescription("Down migrate or rollback to previous db versions.")
-                .create('r');
-
-        Option migrate =   OptionBuilder.withLongOpt("migrate-only")
-                .withDescription("Run database migrations then exit.")
-                .create('m');
-
-        options.addOption(baseDir);
-        options.addOption(dataDir);
-        options.addOption(serverDir);
-        options.addOption(binDir);
-        options.addOption(sbinDir);
-        options.addOption(configDir);
-        options.addOption(help);
-        options.addOption(debugFlag);
-        options.addOption(skipInstall);
-        options.addOption(installonly);
-        options.addOption(testauth);
-        options.addOption(projectDir);
-        options.addOption(encryptpwd);
-        options.addOption(rollback);
-        options.addOption(migrate);
+    CommandLineSetup(final IntConsumer exitHandler) {
+        this.exitHandler = exitHandler
     }
 
     RundeckCliOptions runSetup(String[] args) {
-        RundeckCliOptions cliOptions = new RundeckCliOptions()
-        cliOptions.debug = Boolean.getBoolean(SYS_PROP_RUNDECK_LAUNCHER_DEBUG);
-        cliOptions.rewrite = Boolean.getBoolean(SYS_PROP_RUNDECK_LAUNCHER_REWRITE);
-        final CommandLineParser parser = new GnuParser();
-
-        CommandLine cl;
+        // Use picocli to parse args and populate this instance
+        CommandLine cmd = new CommandLine(this)
         try {
-            cl = parser.parse(this.options, args);
-
-            if(cl.hasOption('h')) {
-                printUsage();
-                System.exit(0);
-            }
-            if(cl.hasOption(FLAG_INSTALLONLY) && cl.hasOption(FLAG_SKIPINSTALL)) {
-                ERR("--" + FLAG_INSTALLONLY + " and --" + FLAG_SKIPINSTALL + " are mutually exclusive");
-                printUsage();
-                System.exit(1);
-            }
-            if(cl.hasOption(FLAG_ENCRYPT_PWD)) {
-                encryptPassword(cl);
-                System.exit(0);
-            }
-
-        } catch (ParseException e) {
-            // oops, something went wrong
-            System.err.println( "Parsing failed.  Reason: " + e.getMessage() );
-            System.exit(1);
+            cmd.parseArgs(args)
+        } catch (CommandLine.ParameterException ex) {
+            System.err.println(ex.getMessage())
+            cmd.usage(System.err)
+            exitHandler.accept(1)
+            return
         }
-        if(!cliOptions.debug) cliOptions.debug = cl.hasOption('d');
-
-        cliOptions.baseDir = cl.getOptionValue('b', System.getProperty(RundeckInitConfig.SYS_PROP_RUNDECK_BASE_DIR, getLaunchLocationParentDir()))
-        cliOptions.serverBaseDir = cl.getOptionValue("serverdir", System.getProperty(RundeckInitConfig.SYS_PROP_RUNDECK_SERVER_SERVER_DIR, cliOptions.baseDir+ "/server"))
-        cliOptions.logDir = cl.getOptionValue("logdir", System.getProperty(RundeckInitConfig.SYS_PROP_RUNDECK_SERVER_SERVER_DIR, cliOptions.serverBaseDir+ "/logs"))
-        cliOptions.configDir = cl.getOptionValue('c', System.getProperty(RundeckInitConfig.SYS_PROP_RUNDECK_SERVER_CONFIG_DIR, cliOptions.serverBaseDir + "/config"))
-        cliOptions.dataDir = cl.getOptionValue("datadir", System.getProperty(RundeckInitConfig.SYS_PROP_RUNDECK_SERVER_WORK_DIR, cliOptions.serverBaseDir + "/data"))
-        cliOptions.binDir = cl.getOptionValue('x', cliOptions.baseDir+"/"+TOOL_ROOT_DIR + "/bin")
-
-        cliOptions.projectDir = cl.getOptionValue('p')
-        cliOptions.skipInstall = cl.hasOption(FLAG_SKIPINSTALL)
-        cliOptions.installOnly = cl.hasOption(FLAG_INSTALLONLY)
-        cliOptions.testAuth = cl.hasOption(FLAG_TEST_AUTH)
-        cliOptions.tag = cl.getOptionValue('r', "")
-
+        if (helpRequested) {
+            cmd.usage(System.out)
+            exitHandler.accept(0)
+            return
+        }
+        if (installOnly && skipInstall) {
+            System.err.println("--installonly and --skipinstall are mutually exclusive")
+            cmd.usage(System.err)
+            exitHandler.accept(1)
+            return
+        }
+        if (encryptPwdService) {
+            if(encryptPassword(encryptPwdService)) {
+                exitHandler.accept(0)
+            }
+            return
+        }
+        RundeckCliOptions cliOptions = new RundeckCliOptions()
+        cliOptions.debug = debug || Boolean.getBoolean(SYS_PROP_RUNDECK_LAUNCHER_DEBUG)
+        cliOptions.rewrite = Boolean.getBoolean(SYS_PROP_RUNDECK_LAUNCHER_REWRITE)
+        cliOptions.baseDir = baseDir ?: System.getProperty(RundeckInitConfig.SYS_PROP_RUNDECK_BASE_DIR, getLaunchLocationParentDir())
+        cliOptions.serverBaseDir = serverBaseDir ?: System.getProperty(RundeckInitConfig.SYS_PROP_RUNDECK_SERVER_SERVER_DIR, cliOptions.baseDir+ "/server")
+        cliOptions.logDir = System.getProperty(RundeckInitConfig.SYS_PROP_RUNDECK_SERVER_SERVER_DIR, cliOptions.serverBaseDir+ "/logs")
+        cliOptions.configDir = configDir ?: System.getProperty(RundeckInitConfig.SYS_PROP_RUNDECK_SERVER_CONFIG_DIR, cliOptions.serverBaseDir + "/config")
+        cliOptions.dataDir = dataDir ?: System.getProperty(RundeckInitConfig.SYS_PROP_RUNDECK_SERVER_WORK_DIR, cliOptions.serverBaseDir + "/data")
+        cliOptions.binDir = binDir ?: cliOptions.baseDir+"/"+TOOL_ROOT_DIR + "/bin"
+        cliOptions.projectDir = projectDir
+        cliOptions.skipInstall = skipInstall
+        cliOptions.installOnly = installOnly
+        cliOptions.testAuth = testAuth
+        cliOptions.tag = tag ?: ""
         if(!System.getProperty(RundeckInitConfig.SYS_PROP_RUNDECK_BASE_DIR) && cliOptions.baseDir) {
             System.setProperty(RundeckInitConfig.SYS_PROP_RUNDECK_BASE_DIR, cliOptions.baseDir)
         }
@@ -195,17 +164,15 @@ class CommandLineSetup {
         if(!System.getProperty("logging.config")) {
             System.setProperty("logging.config",System.getProperty(RundeckInitConfig.SYS_PROP_RUNDECK_SERVER_CONFIG_DIR)+"/log4j2.properties")
         }
-        if(!StringUtil.isEmpty(cliOptions.tag))
-            cliOptions.rollback = true
-        cliOptions.migrate = cl.hasOption('m')
+        if(tag) cliOptions.rollback = true
+        cliOptions.migrate = migrate
         if(cliOptions.migrate) {
             System.setProperty(SYS_PROP_MIGRATE_ONLY, "true")
         }
         return cliOptions
-
     }
 
-//This happens when no RDECK_DIR was specified so we have to make a sane default
+    //This happens when no RDECK_DIR was specified so we have to make a sane default
     String getLaunchLocationParentDir() {
         if(Environment.current == Environment.DEVELOPMENT) {
             File baseRundeckDir = new File(System.getProperty("user.dir"),"rundeck-runtime")
@@ -226,39 +193,18 @@ class CommandLineSetup {
         }
 
     }
-
-    Class tryToLoadCorrectBaseClass() {
-        //For spring boot launched jar this will be correct
-        try {
-            return ClassLoader.getSystemClassLoader().loadClass("org.springframework.boot.loader.Launcher")
-        } catch(Exception ex) {}
-        //Otherwise use the regular class loader and use any class in it
-        return Application.class
+    static interface ConsoleI{
+        String readLine()
+        char[] readPassword()
     }
+    ConsoleI console = System.console() as ConsoleI
 
-    private void printUsage() {
-        // automatically generate the help statement
-        final HelpFormatter formatter = new HelpFormatter();
-        formatter.printHelp( "java [JAVA_OPTIONS] -jar rundeck-launcher.war ", "\nRun the rundeck server, installing the " +
-                                                                               "necessary components if they do not exist.\n", options,
-                             "\nhttp://rundeck.org\n", true );
-    }
-
-    /**
-     * Print err message
-     *
-     * @param s
-     */
-    private void ERR(final String s) {
-        System.err.println("ERROR: " + s);
-    }
-
-    static void encryptPassword(CommandLine cl) {
-        String service = cl.getOptionValue(FLAG_ENCRYPT_PWD)
+    boolean encryptPassword(String service) {
         PasswordUtilityEncrypter encrypter = encrypters[service.toUpperCase()]
         if(!encrypter) {
             System.err.println("No encryption service named: ${service}")
-            System.exit(1)
+            exitHandler.accept(1)
+            return false
         }
         Map input = [:]
         String rqMarker = "*"
@@ -266,11 +212,12 @@ class CommandLineSetup {
         encrypter.formProperties().each { prop ->
             System.out.println((prop.isRequired() ? rqMarker : "") + prop.title + " (${prop.description}):")
             String val = ""
-            if(prop.renderingOptions["displayType"] == "PASSWORD") val = new String(System.console().readPassword())
-            else val = System.console().readLine()
+            if(prop.renderingOptions["displayType"] == "PASSWORD") val = new String(console.readPassword())
+            else val = console.readLine()
             if(prop.isRequired() && val.isEmpty()) {
                 System.out.println("${prop.title} is required.")
-                System.exit(1)
+                exitHandler.accept(1)
+                return false
             }
             input[prop.name] = val
         }
@@ -278,10 +225,11 @@ class CommandLineSetup {
         encrypter.encrypt(input).each { k, v ->
             System.out.println("${k}: ${v}")
         }
+        true
     }
 
     static Map<String,PasswordUtilityEncrypter> getEncrypters() {
-        def encrypters= [:]
+        Map<String,PasswordUtilityEncrypter> encrypters= [:]
         ServiceLoader<PasswordUtilityEncrypter> encrypterServices = ServiceLoader.load(
                 PasswordUtilityEncrypter
         )
