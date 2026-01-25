be1eafc431aac82295590995b02c14048b978213
smartinellibenedetti <139791797+smartinellibenedetti@users.noreply.github.com>
2026-01-25 15:40:36 -0700

    Update JobListPage.groovy

--- functional-test/src/test/groovy/org/rundeck/util/gui/pages/jobs/JobListPage.groovy
+++ functional-test/src/test/groovy/org/rundeck/util/gui/pages/jobs/JobListPage.groovy
@@ -149,6 +149,7 @@ class JobListPage extends BasePage implements ActivityListTrait {
      */
     def expectScheduleDisabled(){
         waitForElementVisible(jobListGroupTree)
+        executeScript("window.scrollTo(0, 0);")
         waitForNumberOfElementsToBeMoreThan(bulkJobRowItemsBy, 0)
         waitForNumberOfElementsToBe(runJobButtonDisabled, 0)
         waitForNumberOfElementsToBe(executionPausedIcon, 1)
