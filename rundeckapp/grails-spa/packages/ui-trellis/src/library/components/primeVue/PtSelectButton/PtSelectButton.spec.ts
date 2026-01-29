26cfe0a297d8bc5a7a88a540bb36f90b9ecbdccb
smartinellibenedetti <139791797+smartinellibenedetti@users.noreply.github.com>
2026-01-28 21:36:00 -0700

    remove tests for now

--- rundeckapp/grails-spa/packages/ui-trellis/src/library/components/primeVue/PtSelectButton/PtSelectButton.spec.ts
+++
@@ -1,8 +0,0 @@
-import PtSelectButton from './PtSelectButton.vue';
-
-describe('PtSelectButton', () => {
-  // TODO: Replace this test with a suite of tests appropriate for the component you are building.
-  it('renders the provided content', () => {
-    return true;
-  });
-});
