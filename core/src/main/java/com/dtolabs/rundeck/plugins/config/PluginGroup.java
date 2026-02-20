a1f13b5ba325f88e0b39b78605840b4911f01058
smartinellibenedetti <139791797+smartinellibenedetti@users.noreply.github.com>
2026-02-20 15:21:43 -0700

    RUN-4102: Add groupBy annotation for plugins and update endpoints to return it (#9970)
    * move commits for group annotation
    
    * move new logic behind v57
    
    * Update PluginControllerSpec.groovy
    
    * Update PluginApiService.groovy
    
    * add headers
    
    * remove unused
    
    * bring in changes from modal branch
    
    * Update PluginControllerSpec.groovy
    
    * Update PluginApiServiceSpec.groovy
    
    * fixes
    
    * add default method to the interface
    
    * Update PluginController.groovy

--- core/src/main/java/com/dtolabs/rundeck/plugins/config/PluginGroup.java
+++ core/src/main/java/com/dtolabs/rundeck/plugins/config/PluginGroup.java
@@ -4,4 +4,13 @@ package com.dtolabs.rundeck.plugins.config;
  * A group of common Plugins, defining a set of common configuration properties.
  */
 public interface PluginGroup {
+
+    /**
+     * Get the icon URL for this plugin group.
+     * @param groupName The group name
+     * @return Icon URL for the group, or null to use first plugin's icon as fallback
+     */
+    default String getGroupIconUrl(String groupName) {
+        return null;
+    }
 }
