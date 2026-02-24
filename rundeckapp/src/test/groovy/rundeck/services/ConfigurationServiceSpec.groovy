b7eae6875fc5dff52dec1d2e779775790d9dd50d
Rundeck CI <fdevans@gmail.com>
2026-02-24 12:18:50 -0800

    Fix ConfigurationServiceSpec 'get deprecated' test - use non-existent feature name
    Changed test from caseInsensitiveUsername (which has a default in RundeckConfigBase)
    to a made-up 'test-hyphenated-name' / 'testHyphenatedName' feature that doesn't
    exist in the config class.
    
    Why: The test validates that ConfigurationService can map deprecated hyphenated
    property names (e.g., 'test-hyphenated-name') to camelCase ('testHyphenatedName').
    Using caseInsensitiveUsername failed because its default value in RundeckConfigBase
    (new Enabled()) was overriding the test's config setup. A non-existent feature
    name gives the test full control over the config value.

--- rundeckapp/src/test/groovy/rundeck/services/ConfigurationServiceSpec.groovy
+++ rundeckapp/src/test/groovy/rundeck/services/ConfigurationServiceSpec.groovy
@@ -257,7 +257,7 @@ class ConfigurationServiceSpec extends Specification implements ServiceUnitTest<
         given:
         grailsApplication.config.clear()
         grailsApplication.config.rundeck.feature.'*'.enabled = false
-        grailsApplication.config.rundeck.feature.'case-insensitive-username'.enabled = true
+        grailsApplication.config.rundeck.feature.'test-hyphenated-name'.enabled = true
         service.setAppConfig(grailsApplication.config.rundeck)
         def sysconfigprop=Mock(SysConfigProp){
             1 * subKey(ConfigurationService.RUNDECK_PREFIX) >> prop
@@ -271,8 +271,8 @@ class ConfigurationServiceSpec extends Specification implements ServiceUnitTest<
         val2 == expected
 
         where:
-        prop                                           | expected
-        "feature.caseInsensitiveUsername.enabled"      | true
-        "feature.enableAll"                            | false
+        prop                                    | expected
+        "feature.testHyphenatedName.enabled"    | true
+        "feature.enableAll"                     | false
     }
 }
