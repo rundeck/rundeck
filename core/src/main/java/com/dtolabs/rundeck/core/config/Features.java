3117022b59b5426b2ad59a94e3d30a215d2a345f
Rodrigo Navarro <rnavarro@pagerduty.com>
2026-04-14 16:41:12 -0400

    add feature flag for parallel bootstrap pro

--- core/src/main/java/com/dtolabs/rundeck/core/config/Features.java
+++ core/src/main/java/com/dtolabs/rundeck/core/config/Features.java
@@ -42,7 +42,8 @@ public enum Features implements FeaturesDefinition{
     GUI_HIDE_ROI_INSTRUCTIONS("guiHideRoiInstructions"),
     EXECUTION_CLEANUP_ENABLE("defaultExecutionCleanup"),
     MULTILINE_JOB_OPTIONS("multilineJobOptions"),
-    EARLY_ACCESS_JOB_CONDITIONAL("earlyAccessJobConditional");
+    EARLY_ACCESS_JOB_CONDITIONAL("earlyAccessJobConditional"),
+    PARALLEL_PRO_BOOTSTRAP("parallelProBootstrap");
 
     private final String propertyName;
 
