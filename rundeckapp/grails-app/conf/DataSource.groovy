f395c5df6fd34d1a35e767b24a21d1f57ed0d7d7
Jesus Osuna <jesus.m.osuna.m@gmail.com>
2026-04-15 18:01:40 -0400

    add NON_KEYWORDS=MONTH,HOUR,MINUTE,YEAR,SECONDS on data source

--- rundeckapp/grails-app/conf/DataSource.groovy
+++ rundeckapp/grails-app/conf/DataSource.groovy
@@ -32,7 +32,7 @@ environments {
 	test {
 		dataSource {
 			dbCreate = "none"
-			url = "jdbc:h2:file:./db/testDb"
+			url = "jdbc:h2:file:./db/testDb;NON_KEYWORDS=MONTH,HOUR,MINUTE,YEAR,SECONDS"
 		}
 	}
 	production {
