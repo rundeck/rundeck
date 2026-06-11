be64469b461284767011361ab2ba88d946f65520
Jaya Singh <jaya@catalytic.com>
2026-06-11 13:25:50 -0500

    fix OffsetPagination watcher oldVal guard

--- rundeckapp/grails-spa/packages/ui-trellis/src/library/components/utils/OffsetPagination.vue
+++ rundeckapp/grails-spa/packages/ui-trellis/src/library/components/utils/OffsetPagination.vue
@@ -68,8 +68,10 @@ export default defineComponent({
     },
   },
   watch: {
-    "pagination.offset"(newVal) {
-      this.currentPage = this.pageNumberForOffset(newVal);
+    "pagination.offset"(newVal, oldVal) {
+      if (newVal !== oldVal) {
+        this.currentPage = this.pageNumberForOffset(newVal);
+      }
     },
   },
   mounted() {
